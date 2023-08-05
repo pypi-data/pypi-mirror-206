827f38e7f8635c28276bc679a37f50d10ed56f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:55 +0000

    Add package LbConfiguration/0.1.0/LbConfiguration-0.1.0.tar.gz

---
+++ packages/LbConfiguration/LbConfiguration-0.1.0.tar.gz/LbConfiguration-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LbConfiguration',
+    packages=find_packages(),
+    version='0.1.0',    
+    description='LbConfiguration. Here only used for creating doxygen configuration',
+    url='https://github.com/shuds13/pyexample',
+    author='Piet Nogga',
+    author_email='piet.nogga@cern.ch',
+    license='BSD 2-clause',
+    install_requires=[                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.9',
+    ],
+)

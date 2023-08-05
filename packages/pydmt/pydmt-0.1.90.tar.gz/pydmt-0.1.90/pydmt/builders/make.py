9ffcb60ee066554203991c851717d77a8dd6c023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:44 +0000

    Add package pydmt/0.1.90/pydmt-0.1.90.tar.gz

---
+++ packages/pydmt/pydmt-0.1.90.tar.gz/pydmt-0.1.90/pydmt/builders/make.py
@@ -0,0 +1,21 @@
+"""
+This module runs make
+"""
+
+
+from pydmt.utils.filesystem import mkdir_touch
+from pydmt.utils.subprocess import check_call_ve
+
+from pydmt.api.one_source_one_target import OneSourceOneTarget
+
+SOURCE_FILE = "Makefile"
+TARGET_FOLDER = ".venv/default"
+
+
+class BuilderMake(OneSourceOneTarget):
+    def build(self) -> None:
+        args = [
+            "make",
+        ]
+        check_call_ve(args)
+        mkdir_touch(self.target)

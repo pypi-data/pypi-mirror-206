a52fc99b07ee03c65598694d114a48a69de433e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:54 +0000

    Add package kerspro/0.0.4/kerspro-0.0.4.tar.gz

---
+++ packages/kerspro/kerspro-0.0.4.tar.gz/kerspro-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'package'
+LONG_DESCRIPTION = 'A package'
+
+# Setting up
+setup(
+    name="kerspro",
+    version=VERSION,
+    author="nandan_6",
+    author_email="nandanpadia@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+)

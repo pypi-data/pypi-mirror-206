a03668c3fe1f1350a532c6adac85ce834508931e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:45 +0000

    Add package irails/1.1.1/irails-1.1.1.tar.gz

---
+++ packages/irails/irails-1.1.1.tar.gz/irails-1.1.1/irails/_loader.py
@@ -0,0 +1,49 @@
+import importlib
+import sys,os
+from .config import config,ROOT_PATH,_log
+app_cfg=config.get('app')
+app_dirs = app_cfg.get("appdir")
+app_enabled = app_cfg.get("enabled")
+
+def __list_directories(dir):
+    """
+    遍历目录，返回所有子目录的路径
+    """
+    dirs_list = [] 
+    for name in os.listdir(dir):
+       
+        path = os.path.join(dir,name)
+        if os.path.isdir(path) and name!='__pycache__': 
+            dirs_list.append(name)
+    return dirs_list
+
+def __check_if_enabled(app_name):
+    return (app_enabled == "*" or 
+            not app_enabled or 
+            (isinstance(app_enabled,list) and app_name in app_enabled))
+
+def load_module(module_name:str,module_path:str):
+    if os.path.exists(module_path):
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    return None
+
+def _load_apps(debug=False):
+    sys.path.insert(-1,ROOT_PATH)
+    unloaded = 0
+    loaded = 0
+    for app_dir in app_dirs:
+        app_list =  __list_directories(app_dir)
+        for app in app_list:
+            if __check_if_enabled(app): 
+                if debug:
+                    _log.info(f'Loading {app_dir}.{app}')
+                __import__(f'{app_dir}.{app}')
+                loaded = loaded + 1
+            else:
+                unloaded = unloaded + 1
+        
+    
+    return loaded,unloaded

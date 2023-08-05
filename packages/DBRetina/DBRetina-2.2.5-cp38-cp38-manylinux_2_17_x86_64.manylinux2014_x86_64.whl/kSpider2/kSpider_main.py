4b7f6b274c6b9ebc99fa336474c810741717f18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package DBRetina/2.2.5/DBRetina-2.2.5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/DBRetina/DBRetina-2.2.5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/kSpider2/kSpider_main.py
@@ -0,0 +1,23 @@
+import sys
+
+from kSpider2.click_context import cli
+from kSpider2.ks_pairwise import main as pairwise_main   # pylint: disable=relative-beyond-top-level
+from kSpider2.ks_sketch_dbretina import main as dbretina_sketch
+from kSpider2.ks_dataset_indexing import main as index_datasets
+from kSpider2.ks_clustering import main as clustering
+from kSpider2.ks_export import main as export
+from kSpider2.ks_query_dbretina import main as dbretina_query
+from kSpider2.ks_filter import main as dbretina_filter
+
+# cli.add_command(dbretina_sketch, name="sketch")
+cli.add_command(index_datasets, name="index")
+cli.add_command(pairwise_main, name="pairwise")
+cli.add_command(dbretina_filter, name="filter")
+cli.add_command(clustering, name="cluster")
+cli.add_command(export, name="export")
+cli.add_command(dbretina_query, name="query")
+
+
+
+if __name__ == '__main__':
+    cli()

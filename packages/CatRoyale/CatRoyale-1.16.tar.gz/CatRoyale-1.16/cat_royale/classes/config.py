2232dac79871832b986b90387c24c842664eaf3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:42 +0000

    Add package CatRoyale/1.16/CatRoyale-1.16.tar.gz

---
+++ packages/CatRoyale/CatRoyale-1.16.tar.gz/CatRoyale-1.16/cat_royale/classes/config.py
@@ -0,0 +1,48 @@
+import json
+
+
+class Config:
+    data = {}
+    images = {}
+
+    screen = None
+    map_layer = None
+    game_map = None
+    all_characters = []
+
+    items = {
+        "FOOD": [],
+        "WOOD": [],
+        "STICK": [],
+        "STONE": [],
+        "WOOD_SWORD": [],
+        "STONE_SWROD": [],
+        "PICKAXE": [],
+        "AXE": [],
+        "TORCH": [],
+
+
+    }
+
+    cursor_style = None
+
+    @staticmethod
+    def set_cursor_style(style):
+        if not style:
+            Config.cursor_style = style
+            return
+
+    @staticmethod
+    def load(path: str):
+        with open(path, 'r', encoding="UTF-8") as f:
+            Config.data = json.load(f)
+
+    @staticmethod
+    def load_image_locations(path: str):
+        with open(path, 'r', encoding="UTF-8") as f:
+            Config.images = json.load(f)
+
+    @staticmethod
+    def remove_from_items(item_type, item):
+
+        Config.items[item_type].pop(Config.items[item_type].index(item))

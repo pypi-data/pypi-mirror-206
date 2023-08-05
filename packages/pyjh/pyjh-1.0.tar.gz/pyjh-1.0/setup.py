7bf32259205c29d2babf7b0cf917e2bedc45ebd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:24 +0000

    Add package pyjh/1.0/pyjh-1.0.tar.gz

---
+++ packages/pyjh/pyjh-1.0.tar.gz/pyjh-1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pyjh",
+    version="1.0",
+    author="jianghai8",
+    author_email="jianghai8@126.com",
+    description="",
+    long_description="pyjh",
+    long_description_content_type="pyjh",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

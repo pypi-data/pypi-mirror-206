9507a0a7b462574b8e236e96ee902525e42a86a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package dinohub-seedwork-be/0.1.21/dinohub_seedwork_be-0.1.21.tar.gz

---
+++ packages/dinohub-seedwork-be/dinohub_seedwork_be-0.1.21.tar.gz/dinohub_seedwork_be-0.1.21/dino_seedwork_be/domain/IdentifiedValueObject.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from dino_seedwork_be.domain.value_object.AbstractValueObject import \
+    ValueObject
+
+from .IdentifiedDomainObject import IdentifiedDomainObject, IdentityType
+
+__all__ = ["IdentifiedValueObject"]
+
+
+@dataclass(frozen=True, kw_only=True)
+class IdentifiedValueObject(IdentifiedDomainObject[IdentityType], ValueObject):
+    id: IdentityType
+    """
+    Base class for identified objects
+    """

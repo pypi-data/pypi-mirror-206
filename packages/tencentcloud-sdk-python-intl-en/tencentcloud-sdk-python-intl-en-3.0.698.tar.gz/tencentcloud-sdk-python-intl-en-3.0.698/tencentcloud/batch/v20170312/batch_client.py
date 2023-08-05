f04b5f3df7ec1c8f5c018d31f390067d17a1be43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:43 +0000

    Add package tencentcloud-sdk-python-intl-en/3.0.698/tencentcloud-sdk-python-intl-en-3.0.698.tar.gz

---
+++ packages/tencentcloud-sdk-python-intl-en/tencentcloud-sdk-python-intl-en-3.0.698.tar.gz/tencentcloud-sdk-python-intl-en-3.0.698/tencentcloud/batch/v20170312/batch_client.py
@@ -0,0 +1,95 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2017-2021 THL A29 Limited, a Tencent company. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.common.abstract_client import AbstractClient
+from tencentcloud.batch.v20170312 import models
+
+
+class BatchClient(AbstractClient):
+    _apiVersion = '2017-03-12'
+    _endpoint = 'batch.tencentcloudapi.com'
+    _service = 'batch'
+
+
+    def DescribeAvailableCvmInstanceTypes(self, request):
+        """This API is used to view the information of available CVM model configurations.
+
+        :param request: Request instance for DescribeAvailableCvmInstanceTypes.
+        :type request: :class:`tencentcloud.batch.v20170312.models.DescribeAvailableCvmInstanceTypesRequest`
+        :rtype: :class:`tencentcloud.batch.v20170312.models.DescribeAvailableCvmInstanceTypesResponse`
+
+        """
+        try:
+            params = request._serialize()
+            headers = request.headers
+            body = self.call("DescribeAvailableCvmInstanceTypes", params, headers=headers)
+            response = json.loads(body)
+            model = models.DescribeAvailableCvmInstanceTypesResponse()
+            model._deserialize(response["Response"])
+            return model
+        except Exception as e:
+            if isinstance(e, TencentCloudSDKException):
+                raise
+            else:
+                raise TencentCloudSDKException(e.message, e.message)
+
+
+    def DescribeCvmZoneInstanceConfigInfos(self, request):
+        """This API is used to get the model configuration information of the availability zone of BatchCompute.
+
+        :param request: Request instance for DescribeCvmZoneInstanceConfigInfos.
+        :type request: :class:`tencentcloud.batch.v20170312.models.DescribeCvmZoneInstanceConfigInfosRequest`
+        :rtype: :class:`tencentcloud.batch.v20170312.models.DescribeCvmZoneInstanceConfigInfosResponse`
+
+        """
+        try:
+            params = request._serialize()
+            headers = request.headers
+            body = self.call("DescribeCvmZoneInstanceConfigInfos", params, headers=headers)
+            response = json.loads(body)
+            model = models.DescribeCvmZoneInstanceConfigInfosResponse()
+            model._deserialize(response["Response"])
+            return model
+        except Exception as e:
+            if isinstance(e, TencentCloudSDKException):
+                raise
+            else:
+                raise TencentCloudSDKException(e.message, e.message)
+
+
+    def DescribeInstanceCategories(self, request):
+        """Currently, CVM instance families are classified into different category, and each category contains several instance families. This API is used to query the instance category information.
+
+        :param request: Request instance for DescribeInstanceCategories.
+        :type request: :class:`tencentcloud.batch.v20170312.models.DescribeInstanceCategoriesRequest`
+        :rtype: :class:`tencentcloud.batch.v20170312.models.DescribeInstanceCategoriesResponse`
+
+        """
+        try:
+            params = request._serialize()
+            headers = request.headers
+            body = self.call("DescribeInstanceCategories", params, headers=headers)
+            response = json.loads(body)
+            model = models.DescribeInstanceCategoriesResponse()
+            model._deserialize(response["Response"])
+            return model
+        except Exception as e:
+            if isinstance(e, TencentCloudSDKException):
+                raise
+            else:
+                raise TencentCloudSDKException(e.message, e.message)
---
+++ packages/tencentcloud-sdk-python-intl-en/tencentcloud-sdk-python-intl-en-3.0.698.tar.gz/tencentcloud-sdk-python-intl-en-3.0.698/tencentcloud/batch/v20170312/errorcodes.py
@@ -0,0 +1,45 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2017-2021 THL A29 Limited, a Tencent company. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Internal error.
+INTERNALERROR = 'InternalError'
+
+# An error is returned for the CVM API call.
+INTERNALERROR_CALLCVM = 'InternalError.CallCvm'
+
+# The specified filter is not supported.
+INVALIDFILTER = 'InvalidFilter'
+
+# Incorrect parameter value.
+INVALIDPARAMETERVALUE = 'InvalidParameterValue'
+
+# Incorrect `Filter` parameter.
+INVALIDPARAMETERVALUE_INVALIDFILTER = 'InvalidParameterValue.InvalidFilter'
+
+# The availability zone and the region do not match.
+INVALIDPARAMETERVALUE_INVALIDZONEMISMATCHREGION = 'InvalidParameterValue.InvalidZoneMismatchRegion'
+
+# The number of filter parameter values exceeds the limit.
+INVALIDPARAMETERVALUE_LIMITEXCEEDED = 'InvalidParameterValue.LimitExceeded'
+
+# The model billing type is not supported by BatchCompute.
+INVALIDPARAMETERVALUE_UNSUPPORTEDBATCHINSTANCECHARGETYPE = 'InvalidParameterValue.UnsupportedBatchInstanceChargeType'
+
+# The specified zone does not exist.
+INVALIDZONE_MISMATCHREGION = 'InvalidZone.MismatchRegion'
+
+# Unsupported operation.
+UNSUPPORTEDOPERATION = 'UnsupportedOperation'

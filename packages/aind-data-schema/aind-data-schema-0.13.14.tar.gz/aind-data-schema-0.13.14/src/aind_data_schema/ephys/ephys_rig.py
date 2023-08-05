b4df5d5dc9f5dfd7204db08ebc40edc8ae4cc888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:30 +0000

    Add package aind-data-schema/0.13.14/aind-data-schema-0.13.14.tar.gz

---
+++ packages/aind-data-schema/aind-data-schema-0.13.14.tar.gz/aind-data-schema-0.13.14/src/aind_data_schema/ephys/ephys_rig.py
@@ -0,0 +1,277 @@
+""" ephys rig schemas """
+
+from __future__ import annotations
+
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional, Union
+
+try:
+    from typing import Literal
+except ImportError:  # pragma: no cover
+    from typing_extensions import Literal
+
+from pydantic import Field, root_validator
+
+from ..base import AindCoreModel, AindModel
+from ..device import (
+    AngleUnit,
+    Camera,
+    CameraAssembly,
+    DAQDevice,
+    DataInterface,
+    Device,
+    Disc,
+    HarpDevice,
+    Laser,
+    Manufacturer,
+    Monitor,
+    SizeUnit,
+    Treadmill,
+    Tube,
+)
+
+
+class Size2d(AindModel):
+    """2D size of an object"""
+
+    width: int = Field(..., title="Width")
+    height: int = Field(..., title="Height")
+    unit: SizeUnit = Field(SizeUnit.PX, title="Size unit")
+
+
+class Orientation3d(AindModel):
+    """3D orientation of an object"""
+
+    pitch: float = Field(..., title="Angle pitch", ge=0, le=360)
+    yaw: float = Field(..., title="Angle yaw", ge=0, le=360)
+    roll: float = Field(..., title="Angle roll", ge=0, le=360)
+    unit: AngleUnit = Field(AngleUnit.DEG, title="Angle unit")
+
+
+class ModuleOrientation2d(AindModel):
+    """2D module orientation of an object"""
+
+    arc_angle: float = Field(..., title="Arc angle")
+    module_angle: float = Field(..., title="Module angle")
+    unit: AngleUnit = Field(AngleUnit.DEG, title="Angle unit")
+
+
+class ModuleOrientation3d(AindModel):
+    """3D module orientation of an object"""
+
+    arc_angle: float = Field(..., title="Arc angle")
+    module_angle: float = Field(..., title="Module angle")
+    rotation_angle: float = Field(..., title="Rotation angle")
+    unit: AngleUnit = Field(AngleUnit.DEG, title="Angle unit")
+
+
+class Coordinates3d(AindModel):
+    """Coordinates in a 3D grid"""
+
+    x: float = Field(..., title="Position X")
+    y: float = Field(..., title="Position Y")
+    z: float = Field(..., title="Position Z")
+    unit: SizeUnit = Field(SizeUnit.UM, title="Position unit")
+
+
+class ProbePort(AindModel):
+    """Port for a probe connection"""
+
+    index: int = Field(..., title="Zero-based port index")
+    probes: List[str] = Field(..., title="Names of probes connected to this port")
+
+
+class NeuropixelsBasestation(DAQDevice):
+    """PXI-based Neuropixels DAQ"""
+
+    # required fields
+    basestation_firmware_version: str = Field(..., title="Basestation firmware version")
+    bsc_firmware_version: str = Field(..., title="Basestation connect board firmware")
+    slot: int = Field(..., title="Slot number for this basestation")
+    ports: List[ProbePort] = Field(..., title="Basestation ports")
+
+    # fixed values
+    data_interface: DataInterface = Field("PXI", const=True)
+    manufacturer: Manufacturer = Manufacturer.IMEC
+
+
+class OpenEphysAcquisitionBoard(DAQDevice):
+    """Multichannel electrophysiology DAQ"""
+
+    # required fields
+    ports: List[ProbePort] = Field(..., title="Acquisition board ports")
+
+    # fixed values
+    data_interface: DataInterface = Field("USB", const=True)
+    manufacturer: Manufacturer = Manufacturer.OEPS
+
+
+class DomeModule(AindModel):
+    """Movable module that is mounted on the ephys dome insertion system"""
+
+    # required fields
+    arc_angle: float = Field(..., title="Arc Angle", units="degrees")
+    module_angle: float = Field(..., title="Module Angle", units="degrees")
+    angle_unit: AngleUnit = Field(AngleUnit.DEG, title="Angle unit")
+
+    # optional fields
+    rotation_angle: Optional[float] = Field(0.0, title="Rotation Angle", units="degrees")
+    coordinate_transform: Optional[str] = Field(
+        None, title="Transform from local manipulator axes to rig", description="Path to coordinate transform"
+    )
+    calibration_date: Optional[datetime] = Field(None, title="Data on which coordinate transform was last calibrated")
+
+
+class Manipulator(Device):
+    """Manipulator used on a dome module"""
+
+    manufacturer: Literal[Manufacturer.NEW_SCALE_TECHNOLOGIES.value]
+
+
+class StickMicroscope(DomeModule):
+    """Stick microscope used to monitor probes during insertion"""
+
+    camera: Camera = Field(..., title="Camera for this module")
+
+
+class LaserModule(DomeModule):
+    """Module for optogenetic stimulation"""
+
+    manipulator: Manipulator = Field(..., title="Manipulator")
+    lasers: List[Laser] = Field(..., title="Lasers connected to this module")
+
+
+class ProbeModel(Enum):
+    """Probe model name"""
+
+    MI_ULED_PROBE = "Michigan uLED Probe (Version 1)"
+    MP_PHOTONIC_V1 = "MPI Photonic Probe (Version 1)"
+    NP_OPTO_DEMONSTRATOR = "Neuropixels Opto (Demonstrator)"
+    NP_UHD_FIXED = "Neuropixels UHD (Fixed)"
+    NP_UHD_SWITCHABLE = "Neuropixels UHD (Switchable)"
+    NP1 = "Neuropixels 1.0"
+    NP2_SINGLE_SHANK = "Neuropixels 2.0 (Single Shank)"
+    NP2_MULTI_SHANK = "Neuropixels 2.0 (Multi Shank)"
+    NP2_QUAD_BASE = "Neuropixels 2.0 (Quad Base)"
+
+
+class HeadstageModel(Enum):
+    """Headstage model name"""
+
+    RHD_16_CH = "Intan RHD 16-channel"
+    RHD_32_CH = "Intan RHD 32-channel"
+    RHD_64_CH = "Intan RHD 64-channel"
+
+
+class EphysProbe(Device):
+    """Named probe used in an ephys experiment"""
+
+    # required fields
+    probe_model: ProbeModel = Field(..., title="Probe model")
+
+    # optional fields
+    lasers: Optional[List[Laser]] = Field(None, title="Lasers connected to this probe")
+    headstage: Optional[HeadstageModel] = Field(None, title="Headstage for this probe")
+
+
+class EphysModule(DomeModule):
+    """Module for electrophysiological recording"""
+
+    manipulator: Manipulator = Field(..., title="Manipulator")
+    probes: List[EphysProbe] = Field(..., title="Probes that are held by this module")
+
+
+class EphysRig(AindCoreModel):
+    """Description of an ephys rig"""
+
+    describedBy: str = Field(
+        "https://github.com/AllenNeuralDynamics/data_schema/blob/main/schemas/ephys/ephys_rig.py",
+        description="The URL reference to the schema.",
+        title="Described by",
+        const=True,
+    )
+    schema_version: str = Field("0.5.8", description="schema version", title="Version", const=True)
+    rig_id: str = Field(..., description="room_stim apparatus_version", title="Rig ID")
+    ephys_modules: Optional[List[EphysModule]] = Field(None, title="Ephys probes", unique_items=True)
+    stick_microscopes: Optional[List[StickMicroscope]] = Field(None, title="Stick microscopes")
+    laser_modules: Optional[List[LaserModule]] = Field(None, title="Laser modules", unique_items=True)
+    cameras: Optional[List[CameraAssembly]] = Field(None, title="Camera assemblies", unique_items=True)
+    visual_monitors: Optional[List[Monitor]] = Field(None, title="Visual monitors", unique_items=True)
+    mouse_platform: Optional[Union[Tube, Treadmill, Disc]] = Field(None, title="Mouse platform")
+    daqs: Optional[List[Union[HarpDevice, NeuropixelsBasestation, OpenEphysAcquisitionBoard, DAQDevice]]] = Field(
+        None, title="Data acquisition devices"
+    )
+
+    @root_validator
+    def validate_device_names(cls, values):
+        """validate that all DAQ channels are connected to devices that
+        actually exist
+        """
+
+        cameras = values.get("cameras")
+        ephys_modules = values.get("ephys_modules")
+        laser_modules = values.get("laser_modules")
+        mouse_platform = values.get("mouse_platform")
+        daqs = values.get("daqs")
+
+        if daqs is None:
+            return values
+
+        device_names = [None]
+
+        if cameras is not None:
+            device_names += [c.camera.name for c in cameras]
+
+        if ephys_modules is not None:
+            device_names += [probe.name for ephys_module in ephys_modules for probe in ephys_module.probes]
+
+        if laser_modules is not None:
+            device_names += [laser.name for laser_module in laser_modules for laser in laser_module.lasers]
+
+        if mouse_platform is not None:
+            device_names += [mouse_platform.name]
+
+        for daq in daqs:
+            if daq.channels is not None:
+                for channel in daq.channels:
+                    if channel.device_name not in device_names:
+                        raise ValueError(
+                            f"Device name validation error: '{channel.device_name}' "
+                            + f"is connected to '{channel.channel_name}' on '{daq.name}', but "
+                            + "this device is not part of the rig."
+                        )
+
+        return values
+
+    @root_validator
+    def validate_probe_names(cls, values):
+        """validate that all DAQ probe ports are connected to probes that
+        actually exist
+        """
+
+        ephys_modules = values.get("ephys_modules")
+        daqs = values.get("daqs")
+
+        if daqs is None or ephys_modules is None:
+            return values
+
+        probe_names = [probe.name for ephys_module in ephys_modules for probe in ephys_module.probes]
+
+        for daq in daqs:
+            try:
+                daq.ports
+            except AttributeError:
+                pass
+            else:
+                if daq.ports is not None:
+                    for port in daq.ports:
+                        for probe in port.probes:
+                            if probe not in probe_names:
+                                raise ValueError(
+                                    f"Probe name validation error: '{probe}' "
+                                    + f"is connected to '{daq.name}' port '{port.index}', but "
+                                    + "this probe is not part of the rig."
+                                )
+
+        return values
---
+++ packages/aind-data-schema/aind-data-schema-0.13.14.tar.gz/aind-data-schema-0.13.14/src/aind_data_schema/ephys/ephys_session.py
@@ -0,0 +1,131 @@
+""" ephys session description and related objects """
+
+from __future__ import annotations
+
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import Field
+
+from ..base import AindCoreModel, AindModel
+from ..device import PowerUnit, SizeUnit
+from ..stimulus import StimulusPresentation
+from .ephys_rig import Coordinates3d
+
+
+class SessionType(Enum):
+    """Session type name"""
+
+    TEST = "Test"
+    OPTO = "Optotagging"
+    RF_MAPPING = "Receptive field mapping"
+
+
+class CcfVersion(Enum):
+    """CCF version"""
+
+    CCFv3 = "CCFv3"
+
+
+class CcfCoords(AindModel):
+    """Coordinates in CCF template space"""
+
+    ml: float = Field(..., title="ML")
+    ap: float = Field(..., title="AP")
+    dv: float = Field(..., title="DV")
+    unit: SizeUnit = Field(SizeUnit.UM, title="Coordinate unit")
+    ccf_version: CcfVersion = Field(CcfVersion.CCFv3, title="CCF version")
+
+
+class ManipulatorModule(AindModel):
+    """A module connected to a 3-axis manipulator"""
+
+    primary_targeted_structure: str = Field(..., title="Targeted structure")
+    targeted_ccf_coordinates: Optional[List[CcfCoords]] = Field(
+        None,
+        title="Targeted CCF coordinates",
+    )
+    manipulator_coordinates: Coordinates3d = Field(
+        ...,
+        title="Manipulator coordinates",
+    )
+
+
+class Laser(AindModel):
+    """Laser used in a LaserModule"""
+
+    name: str = Field(..., title="Laser name (must match rig JSON)")
+    power_level: float = Field(..., title="Power level used in this session", units="mW")
+    power_unit: PowerUnit = Field(PowerUnit.MW, title="Power unit")
+
+
+class LaserModule(ManipulatorModule):
+    """Laser Module used in a Stream"""
+
+    lasers: List[Laser] = Field(..., title="Active lasers in this module")
+
+
+class EphysProbe(ManipulatorModule):
+    """Probe recorded in a Stream"""
+
+    name: str = Field(..., title="Ephys probe name (must match rig JSON)")
+    other_targeted_structures: Optional[List[str]] = None
+
+
+class DAQDevice(AindModel):
+    """Data acquisition device recorded in a Stream"""
+
+    name: str = Field(..., title="DAQ device name (must match rig JSON)")
+
+
+class Camera(AindModel):
+    """Camera recorded in a Stream"""
+
+    name: str = Field(..., title="Camera name (must match rig JSON)")
+
+
+
+class Stream(AindModel):
+    """Stream of data with a start and stop time"""
+
+    stream_start_time: datetime = Field(..., title="Stream start time")
+    stream_end_time: datetime = Field(..., title="Stream stop time")
+    probes: Optional[List[EphysProbe]] = Field(None, title="Probes", unique_items=True)
+    laser_modules: Optional[List[LaserModule]] = Field(None, title="Laser modules", unique_items=True)
+    daqs: Optional[List[DAQDevice]] = Field(None, title="DAQ devices", unique_items=True)
+    cameras: Optional[List[Camera]] = Field(None, title="Cameras", unique_items=True)
+    stimulus_presentations: Optional[List[StimulusPresentation]] = Field(None, title="Stimulus")
+
+
+class EphysSession(AindCoreModel):
+    """Description of an ephys recording session"""
+
+    schema_version: str = Field("0.3.2", description="schema version", title="Version", const=True)
+    experimenter_full_name: str = Field(
+        ...,
+        description="First and last name of the experimenter.",
+        title="Experimenter full name",
+    )
+    session_start_time: datetime = Field(..., title="Session start time")
+    session_end_time: datetime = Field(..., title="Session end time")
+    subject_id: str = Field(..., title="Subject ID")
+    session_type: SessionType = Field(..., title="Session type")
+    session_description: Optional[str] = Field(None, title="Session description")
+    iacuc_protocol: Optional[str] = Field(None, title="IACUC protocol")
+    rig_id: str = Field(..., title="Rig ID")
+    data_streams: List[Stream] = Field(
+        ...,
+        title="Data streams",
+        description=(
+            "A data stream is a collection of devices that are recorded simultaneously. Each session can include"
+            " multiple streams (e.g., if the manipulators are moved to a new location)"
+        ),
+        unique_items=True,
+    )
+    ccf_coordinate_transform: Optional[str] = Field(
+        None,
+        description="Path to file that details the CCF-to-lab coordinate transform",
+        title="CCF coordinate transform",
+    )
+    notes: Optional[str] = None

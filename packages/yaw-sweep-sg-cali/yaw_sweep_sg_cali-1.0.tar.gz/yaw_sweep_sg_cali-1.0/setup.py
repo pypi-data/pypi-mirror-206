0d2eeda932dd3160d0fe06971ab7ce9d0c19aa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package yaw-sweep-sg-cali/1.0/yaw_sweep_sg_cali-1.0.tar.gz

---
+++ packages/yaw-sweep-sg-cali/yaw_sweep_sg_cali-1.0.tar.gz/yaw_sweep_sg_cali-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+# read the contents of your README file
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(name='yaw_sweep_sg_cali',
+version='1.0',
+description='Package to perform strain gauge of wind turbine towers calibration based on idling opeartions, so called, yaw sweeps', 
+long_description = long_description,
+long_description_content_type='text/markdown',
+author='Bruno and Zahra',
+author_email='brofa@dtu.dk',
+packages=['yaw_sweep_sg_cali'],
+# packages_data={'yaw_sweep_sg_cali':['Data/V52_1min_data/*.txt', 'Data/V52_50Hz_data/*.txt','Data/V52_inputs.txt']}
+install_requires = [
+          'matplotlib',
+          'numpy',
+          'pandas',
+          'pathlib',
+          'calendar',
+          'datetime',
+	  'dateutil',
+	  'scipy',
+	  'mysqlclient',
+	  'operator'
+          ]
+
+)

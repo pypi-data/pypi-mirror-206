d4f411681f30ebc495119100c2f9126bc22f4180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:19 +0000

    Add package miditok/2.0.4/miditok-2.0.4.tar.gz

---
+++ packages/miditok/miditok-2.0.4.tar.gz/miditok-2.0.4/miditok/__init__.py
@@ -0,0 +1,16 @@
+from .tokenizations import (
+    MIDILike,
+    REMI,
+    REMIPlus,
+    TSD,
+    Structured,
+    Octuple,
+    OctupleMono,
+    CPWord,
+    MuMIDI,
+)
+from .midi_tokenizer import MIDITokenizer
+from .classes import Event, TokSequence
+
+from .utils import utils
+from .data_augmentation import data_augmentation

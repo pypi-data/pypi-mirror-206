190c1cfff4b2572942940db338741e1f9666d9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:54 +0000

    Add package headline-detector/1.0.1/headline_detector-1.0.1.tar.gz

---
+++ packages/headline-detector/headline_detector-1.0.1.tar.gz/headline_detector-1.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name = "headline_detector",
+    version = "1.0.1",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = "Kaenova Mahendra Auditama",
+    author_email = "kaenova@gmail.com",
+    description = "An Indonesian Headline Detection Python API.",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8",
+    install_requires=[
+        "transformers",
+        "torch",
+        "lightning",
+        "pytorch-nlp",
+        "huggingface_hub",
+        "numpy",
+        "emoji",
+        "NDETCStemmer-kaenova"
+    ]
+)

b16eec5b5a4c443eb7b67029961c038d900affb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:34 +0000

    Add package pkcs1/0.9.7/pkcs1-0.9.7.tar.gz

---
+++ packages/pkcs1/pkcs1-0.9.7.tar.gz/pkcs1-0.9.7/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
+
+with open('README.rst') as fd:
+    long_description = fd.read()
+
+setup(name='pkcs1',
+      version='0.9.7',
+      license='MIT',
+      description='Python implementation of the RFC3447 or PKCS #1 version 2.0',
+      long_description=long_description,
+      long_description_type='text/x-rst`',
+      url='https://github.com/bdauvergne/python-pkcs1',
+      author='Benjamin Dauvergne',
+      author_email='bdauvergne@entrouvert.com',
+      packages=find_packages(os.path.dirname(__file__) or '.'))

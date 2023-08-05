3d3266f365fcc1c90617514df53b9413271d8b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:36 +0000

    Add package conversation-control/0.1.2/conversation_control-0.1.2.tar.gz

---
+++ packages/conversation-control/conversation_control-0.1.2.tar.gz/conversation_control-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="conversation_control",
+	packages=["conversation_control"],
+	version="0.1.2",
+	description="Este es un paquete de codigo, que busca continuar el control de conversacion, usando la herramienta de langchain",
+	author="Jesus Zerpa",
+	author_email="jesus26abraham1996@gmail.com",
+	url="https://zerpatechnology.com/conversation_control",
+	keywords=["langchain","python"]
+	) 

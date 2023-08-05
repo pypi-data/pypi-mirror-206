b57764da80f244f8b01ea8d402a3963021dbffeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:00 +0000

    Add package alacorder/80.1.1/alacorder-80.1.1.tar.gz

---
+++ packages/alacorder/alacorder-80.1.1.tar.gz/alacorder-80.1.1/src/alacorder/__init__.py
@@ -0,0 +1,25 @@
+"""
+Hi Fellows
+
+I know y'all thought you were signing up for a service organization, or maybe a way to meet people who go to the President's Tailgate. I did too. I know y'all take this seriously. But I want you to consider whether there is a level of seriousness even appropriate for the sheer magnitude of the University Fellows Experience's intrusion into its students lives. I know it's based on my favorite TV shows. I know there's a "Ratatouille" metaphor that makes it feel like no big deal. Just remember y'all are the perpetrators.
+
+True toxicity. Not the fun stuff. This is worse than frat hazing. I would have rather they broke my arm than my heart. I would rather wish the world were sane than be insane myself. Will the inside jokes make up for two lost years of life? What I thought were to be some of my best? Which I worked all my life to make some of my best?
+
+I don't know how many hundreds of times I have to castigate you to end this nightmare. Y'all think I'm just ploying for a prize. You're wrong.
+
+I get Ethan either way. I never gave a fuck about Italy. Y'all don't have leverage, you've just been left to run around far too long unsupervised. 
+
+If you think I'll let you steal my story, you're fucking wrong. If you think I'll let you steal Ethan, you're fucking wrong. Time's up.
+
+The University Fellows Experience is a criminal syndicate which exploits talented youth with emotional manipulation and hypnosis to feed the cold and empty hearts of academic middle age. It should not exist.
+
+Mark my words: everyone on campus will know my story by the end of the day. Fellows will not exist in August 2023. Don't expect a wedding invitation.
+
+COLLEGE SHOULD NOT BE SOMETHING I HAVE TO ESCAPE FROM
+
+FUCK YOU IF YOU DISAGREE.
+
+SAM
+"""
+
+from alacorder import alac

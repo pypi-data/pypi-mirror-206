ccf60c0130529e0016275555535818ab623fbb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:20 +0000

    Add package odoo12-addon-cooperator-worker-force/12.0.2.0.1.99.dev1/odoo12_addon_cooperator_worker_force-12.0.2.0.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-cooperator-worker-force/odoo12_addon_cooperator_worker_force-12.0.2.0.1.99.dev1-py3-none-any.whl/odoo/addons/cooperator_worker_force/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Cooperator Worker - Force Worker",
+    "summary": "Allows to set a cooperator as a "
+    "worker before the share is released.",
+    "author": "Poll'n Coop, Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Cooperative management",
+    "version": "12.0.2.0.1",
+    "depends": [
+        "cooperator_worker",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "license": "AGPL-3",
+}

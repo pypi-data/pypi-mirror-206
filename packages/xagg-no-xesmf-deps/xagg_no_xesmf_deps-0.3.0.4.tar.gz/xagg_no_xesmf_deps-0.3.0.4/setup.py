63e89f0498e52aa76fd9f60f61990e26691b7a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:46 +0000

    Add package xagg-no-xesmf-deps/0.3.0.4/xagg_no_xesmf_deps-0.3.0.4.tar.gz

---
+++ packages/xagg-no-xesmf-deps/xagg_no_xesmf_deps-0.3.0.4.tar.gz/xagg_no_xesmf_deps-0.3.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+	name="xagg_no_xesmf_deps",
+    version="0.3.0.4",
+    author="Kevin Schwarzwald",
+    author_email="kschwarzwald@iri.columbia.edu",
+    description="Aggregating raster data over polygons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ks905383/xagg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'xarray',
+        'pandas',
+        'netcdf4',
+        'geopandas',
+        'shapely',
+	    'tables',
+        'cf_xarray>=0.5.1',
+	 ],
+)

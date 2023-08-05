0ddbf59b68a93804d69285b2811b3f38b3855351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:44 +0000

    Add package jodAI/0.0.91/jodAI-0.0.91.tar.gz

---
+++ packages/jodAI/jodAI-0.0.91.tar.gz/jodAI-0.0.91/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jodAI',
+    version='0.0.91',
+    description='AI ke Expts hai',
+    long_description="Bhai",
+    author='Godfather',
+    packages=['jodAI'],
+    install_requires=[]
+
+)

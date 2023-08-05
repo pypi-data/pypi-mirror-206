7ae054aa1f58dc4b5269340cb2e2e29d4696c2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:50 +0000

    Add package EverLoguru/1.1.3/EverLoguru-1.1.3.tar.gz

---
+++ packages/EverLoguru/EverLoguru-1.1.3.tar.gz/EverLoguru-1.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='EverLoguru',
+    version='1.1.3',
+    packages=['ever_loguru'],
+    url='',
+    license='',
+    author='cyan',
+    author_email='lc_shizuku@outlook.com',
+    description='Loguru all over the world. | 全世界都在用 Lo~guru~~',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    requires=['loguru']
+)

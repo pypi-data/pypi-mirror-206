9e0d731cdc47cbe06d7cf6eb2e9439493d0016db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:16 +0000

    Add package MVCactus/0.0.3/MVCactus-0.0.3.tar.gz

---
+++ packages/MVCactus/MVCactus-0.0.3.tar.gz/MVCactus-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'MVCactus is a lightweight, open-source web framework for building small web applications in Python.'
+
+# Setting up
+setup(
+    name="MVCactus",
+    version=VERSION,
+    author="Dekel Cohen",
+    author_email="<dcohen52@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=['Jinja2'],
+    keywords=['python', 'web', 'framework', 'desktop', 'html', 'javascript', 'styles', 'js', 'API', 'microservices',
+              'REST'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

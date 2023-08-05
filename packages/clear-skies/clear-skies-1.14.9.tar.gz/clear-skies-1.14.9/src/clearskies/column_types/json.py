3071bd82ec3edb0cddbb681c2cfed6da0543fd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:41 +0000

    Add package clear-skies/1.14.9/clear-skies-1.14.9.tar.gz

---
+++ packages/clear-skies/clear-skies-1.14.9.tar.gz/clear-skies-1.14.9/src/clearskies/column_types/json.py
@@ -0,0 +1,17 @@
+import json
+from .column import Column
+class JSON(Column):
+    def from_backend(self, value):
+        if type(value) == list or type(value) == dict:
+            return value
+        if not value:
+            return None
+        try:
+            return json.loads(value)
+        except json.JSONDecodeError:
+            return None
+
+    def to_backend(self, data):
+        if self.name in data:
+            data[self.name] = json.dumps(data[self.name]) if data[self.name] else ''
+        return data

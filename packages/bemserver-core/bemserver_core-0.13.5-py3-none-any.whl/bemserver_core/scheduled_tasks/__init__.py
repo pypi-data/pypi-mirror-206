b2c09eac697013458cb6d79eae538e639f107c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:58 +0000

    Add package bemserver-core/0.13.5/bemserver_core-0.13.5-py3-none-any.whl

---
+++ packages/bemserver-core/bemserver_core-0.13.5-py3-none-any.whl/bemserver_core/scheduled_tasks/__init__.py
@@ -0,0 +1,28 @@
+"""Scheduled tasks"""
+from pathlib import Path
+
+from .cleanup import ST_CleanupByCampaign, ST_CleanupByTimeseries
+from .check_missing import ST_CheckMissingByCampaign
+from .check_outliers import ST_CheckOutliersByCampaign
+from .download_weather_data import ST_DownloadWeatherDataBySite
+
+
+__all__ = [
+    "ST_CleanupByCampaign",
+    "ST_CleanupByTimeseries",
+    "ST_CheckMissingByCampaign",
+    "ST_CheckOutliersByCampaign",
+    "ST_DownloadWeatherDataBySite",
+]
+
+
+AUTH_MODEL_CLASSES = [
+    ST_CleanupByCampaign,
+    ST_CleanupByTimeseries,
+    ST_CheckMissingByCampaign,
+    ST_CheckOutliersByCampaign,
+    ST_DownloadWeatherDataBySite,
+]
+
+
+AUTH_POLAR_FILE = Path(__file__).parent / "authorization.polar"

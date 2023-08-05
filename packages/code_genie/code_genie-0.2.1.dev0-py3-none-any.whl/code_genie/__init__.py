ca1acb483e5193f529faf8d8ad19112693a1f1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package code_genie/0.2.1.dev0/code_genie-0.2.1.dev0-py3-none-any.whl

---
+++ packages/code_genie/code_genie-0.2.1.dev0-py3-none-any.whl/code_genie/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.2.1-dev0"
+
+from code_genie._cache import _CacheManager
+
+
+def set_cache_dir(path: str):
+    """
+    Set global options for the package
+
+    Args:
+        path: path to the cache dir
+    """
+    _CacheManager._set_cache_dir(path)

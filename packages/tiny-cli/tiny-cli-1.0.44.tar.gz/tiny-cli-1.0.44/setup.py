e0030ceebc7710120bb88f036e0db75f3fe2c3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package tiny-cli/1.0.44/tiny-cli-1.0.44.tar.gz

---
+++ packages/tiny-cli/tiny-cli-1.0.44.tar.gz/tiny-cli-1.0.44/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+dependencies = [
+    "httpcore[http2]",
+    "httpx==0.23.1",
+    "tabulate",
+    "anytree",
+    "humanize",
+]
+
+setup(
+    name='tiny-cli',
+    version='1.0.44',
+    description='TinyBio genome analysis tool',
+    long_description_content_type='text/markdown',
+    url='https://github.com/tinybio-cloud/tiny',
+    long_description=readme(),
+    author='TinyBio LLC',
+    author_email='tiny-packages@tinybio.cloud',
+    license='MIT',
+    packages=['tiny'],
+    install_requires=dependencies,
+    python_requires=">=3.7",
+    zip_safe=False
+)

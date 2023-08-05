842fcdb9d0f816df5828e79a69ee298dd91a5eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:45 +0000

    Add package aws-prototyping-sdk/0.17.5/aws_prototyping_sdk-0.17.5-py3-none-any.whl

---
+++ packages/aws-prototyping-sdk/aws_prototyping_sdk-0.17.5-py3-none-any.whl/aws_prototyping_sdk/__init__.py
@@ -0,0 +1,67 @@
+'''
+> **DEPRECATION NOTICE:** The `aws-prototyping-sdk` is on path to deprecation. This package is simply a wrapper around the individual `@aws-prototyping-skd/*` packages and has no unique code aside from bundling the other packages. Due to lack of significant value compared to maintenance overhead, this package will be deprecated soon in favor of using the individual packages directly. Please use the individual packages starting now to prevent future migration and loss of update capabilities.
+
+The aws-prototyping-sdk provides stable CDK and Projen constructs, allowing developers to have access to higher level abstractions than provided by the CDK or Projen alone.
+
+For detailed documentation, please refer to the [documentation website](https://aws.github.io/aws-prototyping-sdk/).
+
+## Bundling
+
+This package simply bundles various packages from the `@aws-prototyping-sdk` namespace which have been marked as `stable`. As such, not all constructs or classes may be exported from this package and will need to be explicitly imported by creating a dependency on the individual packages.
+
+To illustrate, at the time of writing the following individual packages are published:
+
+```
+@aws-prototyping-sdk
+        |_ pipeline       : [stable]
+        |_ nx-monorepo    : [stable]
+        |_ static-website : [experimental]
+        |_ identity       : [experimental]
+```
+
+The aws-prototyping-sdk package will bundle all stable packages and export them as namespaces as follows:
+
+```
+aws-prototyping-sdk
+        |_ pipeline
+        |_ nx_monorepo
+```
+
+This means if you wanted to access the PDKPipeline which is a stable construct, you simply add a dependency on the `aws-prototyping-sdk` and import it as follows:
+
+```python
+import { nx_monorepo, pipeline } from "aws-prototyping-sdk";
+```
+
+To import `experimental` constructs, a dependency on the individual package is required. In the case of `static-website`, a dependency on `@aws-prototyping-sdk/static-website` is required. The constructs can then be imported as follows:
+
+```python
+import { StaticWebsite } from "@aws-prototyping-sdk/static-website";
+```
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+__all__ = [
+    "nx_monorepo",
+    "pdk_nag",
+    "pipeline",
+]
+
+publication.publish()
+
+# Loading modules to ensure their types are registered with the jsii runtime library
+from . import nx_monorepo
+from . import pdk_nag
+from . import pipeline

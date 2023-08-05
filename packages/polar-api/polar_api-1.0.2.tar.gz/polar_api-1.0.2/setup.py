4c3d1fa8f52887c78ccc5e12c42b0f212c4db0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:47 +0000

    Add package polar-api/1.0.2/polar_api-1.0.2.tar.gz

---
+++ packages/polar-api/polar_api-1.0.2.tar.gz/polar_api-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+VERSION = "1.0.2"
+DESCRIPTION = "API wrapper for POLAR TeamPro"
+
+setup(
+    name="polar_api",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.28.2",
+        "isodate>=0.6.1",
+        "pandas>=1.5.3",
+        "beautifulsoup4>=4.12.2",
+    ],
+)

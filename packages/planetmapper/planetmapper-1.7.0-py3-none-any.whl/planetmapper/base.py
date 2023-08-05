e7c938abc2cc9590542125828c2b1b7b9a6e4139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:21 +0000

    Add package planetmapper/1.7.0/planetmapper-1.7.0-py3-none-any.whl

---
+++ packages/planetmapper/planetmapper-1.7.0-py3-none-any.whl/planetmapper/base.py
@@ -0,0 +1,399 @@
+import datetime
+import glob
+import os
+from typing import TypeVar
+
+import astropy.time
+import numpy as np
+import spiceypy as spice
+
+from . import progress
+
+DEFAULT_KERNEL_PATH = '~/spice_kernels/'
+
+_KERNEL_DATA = {
+    'kernel_path': None,
+    'kernel_patterns': ('**/*.bsp', '**/*.tpc', '**/*.tls'),
+}
+
+Numeric = TypeVar('Numeric', bound=float | np.ndarray)
+
+
+class SpiceBase:
+    """
+    Class containing methods to interface with spice and manipulate coordinates.
+
+    This is the base class for all the main classes used in planetmapper.
+
+    Args:
+        show_progress: Show progress bars for long running processes. This is mainly
+            useful for complex functions in derived classes, such as backplane
+            generation in :class:`BodyXY`. These progress bars can be quite messy, but
+            can be useful to keep track of very long operations.
+        optimize_speed: Toggle speed optimizations. For typical observations, the
+            optimizations can make code significantly faster with no effect on accuracy,
+            so should generally be left enabled.
+        load_kernels: Toggle automatic kernel loading with :func:`load_spice_kernels`.
+        kernel_path: Passed to  :func:`load_spice_kernels` if `load_kernels` is True.
+        manual_kernels: Passed to  :func:`load_spice_kernels` if `load_kernels` is True.
+    """
+
+    _DEFAULT_DTM_FORMAT_STRING = '%Y-%m-%dT%H:%M:%S.%f'
+    _KERNELS_LOADED = False
+
+    def __init__(
+        self,
+        show_progress: bool = False,
+        optimize_speed: bool = True,
+        load_kernels: bool = True,
+        kernel_path: str | None = None,
+        manual_kernels: None | list[str] = None,
+    ) -> None:
+        super().__init__()
+        self._optimize_speed = optimize_speed
+
+        self._progress_hook: progress.ProgressHook | None = None
+        self._progress_call_stack: list[str] = []
+
+        if show_progress:
+            self._set_progress_hook(progress.CLIProgressHook())
+
+        if load_kernels:
+            self.load_spice_kernels(
+                kernel_path=kernel_path, manual_kernels=manual_kernels
+            )
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self._get_equality_tuple() == other._get_equality_tuple()
+
+    def _get_equality_tuple(self) -> tuple:
+        return (self._optimize_speed, repr(self))
+
+    def standardise_body_name(self, name: str | int) -> str:
+        """
+        Return a standardised version of the name of a SPICE body.
+
+        This converts the provided `name` into the SPICE ID code with `spice.bods2c`,
+        then back into a string with `spice.bodc2s`. This standardises to the version of
+        the name preferred by SPICE. For example, `'jupiter'`, `'JuPiTeR'`,
+        `' Jupiter '`, `'599'` and `599` are all standardised to `'JUPITER'`
+
+        Args:
+            name: The name of a body (e.g. a planet). This can also be the numeric ID
+                code of a body.
+
+        Returns:
+            Standardised version of the body's name preferred by SPICE.
+
+        Raises:
+            NotFoundError: If SPICE does not recognise the provided `name`
+        """
+        name = spice.bodc2s(spice.bods2c(str(name)))
+        return name
+
+    def et2dtm(self, et: float) -> datetime.datetime:
+        """
+        Convert ephemeris time to a Python datetime object.
+
+        Args:
+            et: Ephemeris time in seconds past J2000.
+
+        Returns:
+            Timezone aware (UTC) datetime corresponding to `et`.
+        """
+        s = spice.et2utc(et, 'ISOC', 6) + '+0000'
+        # manually add '+0000' to string to make it timezone aware
+        # i.e. this lets python know it is UTC
+        return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+    @staticmethod
+    def mjd2dtm(mjd: float) -> datetime.datetime:
+        """
+        Convert Modified Julian Date into a python datetime object.
+
+        Args:
+            mjd: Float representing MJD.
+
+        Returns:
+            Python datetime object corresponding to `mjd`. This datetime is timezone
+            aware and set to the UTC timezone.
+        """
+        dtm: datetime.datetime = astropy.time.Time(mjd, format='mjd').datetime
+        return dtm.replace(tzinfo=datetime.timezone.utc)
+
+    def speed_of_light(self) -> float:
+        """
+        Return the speed of light in km/s. This is a convenience function to call
+        `spice.clight()`.
+
+        Returns:
+            Speed of light in km/s.
+        """
+        return spice.clight()
+
+    def calculate_doppler_factor(self, radial_velocity: Numeric) -> Numeric:
+        """
+        Calculates the doppler factor caused by a target's radial velocity relative to
+        the observer. This doppler factor, :math:`D` can be used to calculate the
+        doppler shift caused by this velocity as :math:`\\lambda_r = \\lambda_e D`
+        where :math:`\\lambda_r` is the wavelength received by the observer and
+        :math:`\\lambda_e` is the wavelength emitted at the target.
+
+        This doppler factor is calculated as
+        :math:`D = \\sqrt{\\frac{1 + v/c}{1 - v/c}}` where :math:`v` is the input
+        `radial_velocity` and :math:`c` is the speed of light.
+
+        See also
+        https://en.wikipedia.org/wiki/Relativistic_Doppler_effect#Relativistic_longitudinal_Doppler_effect
+
+        Args:
+            radial_velocity: Radial velocity in km/s with positive values corresponding
+                to motion away from the observer. This can be a single float value or a
+                numpy array containing multiple velocity values.
+
+        Returns:
+            Doppler factor calculated from input radial velocity. If the input
+            `radial_velocity` is a single value, then a `float` is returned. If the
+            input `radial_velocity` is a numpy array, then a numpy array of doppler
+            factors is returned.
+        """
+        beta = radial_velocity / self.speed_of_light()
+        return np.sqrt((1 + beta) / (1 - beta))  #  type: ignore
+
+    @classmethod
+    def load_spice_kernels(
+        cls,
+        kernel_path: str | None = None,
+        manual_kernels: None | list[str] = None,
+        only_if_needed: bool = True,
+    ) -> None:
+        """
+        Attempt to intelligently SPICE kernels using `spice.furnsh`.
+
+        If `manual_kernels` is `None` (the default), then all kernels in the directory
+        given by `kernel_path` which match the following patterns are loaded:
+
+        - `**/*.bsp`
+        - `**/*.tpc`
+        - `**/*.tls`
+
+        Note that these patterns match an arbitrary number of nested directories (within
+        `kernel_path`). If more control is required, you can instead specify a list of
+        specific kernels to load with `manual_kernels`.
+
+        .. hint::
+            See the :ref:`SPICE kernel documentation <SPICE kernels>` for more detail
+            about downloading SPICE kernels and the automatic kernel loading behaviour.
+
+        Args:
+            kernel_path: Path to directory where kernels are stored. If this is `None`
+                (the default) then the result of :func:`get_kernel_path` is used. It is
+                usually recommended to use one of the methods described in
+                :ref:`the kernel directory documentation<kernel directory>` rather than
+                using this `kernel_path` argument.
+            manual_kernels: Optional manual list of paths to kernels to load instead of
+                using `kernel_path`.
+            only_if_needed: If this is `True`, kernels will only be loaded once per
+                session.
+        """
+        if only_if_needed and cls._KERNELS_LOADED:
+            return
+        if manual_kernels:
+            kernels = manual_kernels
+        else:
+            if kernel_path is None:
+                kernel_path = get_kernel_path()
+            kernel_path = os.path.expanduser(kernel_path)
+            kernels = [
+                os.path.join(kernel_path, pattern)
+                for pattern in _KERNEL_DATA['kernel_patterns']
+            ]
+
+        kernel_paths = load_kernels(*kernels)
+
+        if len(kernel_paths) == 0:
+            print()
+            print(f'WARNING: no SPICE kernels found in directory {kernel_path!r}')
+            print(
+                'Try running planetmapper.set_kernel_path to change where PlanetMapper looks for kernels'
+            )
+            print()
+        else:
+            cls._KERNELS_LOADED = True
+
+    @staticmethod
+    def close_loop(arr: np.ndarray) -> np.ndarray:
+        """
+        Return copy of array with first element appended to the end.
+
+        This is useful for cases like plotting the limb of a planet where the array of
+        values forms a loop with the first and last values in `arr` adjacent to each
+        other.
+
+        Args:
+            arr: Array of values of length :math:`n`.
+
+        Returns:
+            Array of values of length :math:`n + 1` where the final value is the same as
+            the first value.
+        """
+        return np.append(arr, [arr[0]], axis=0)
+
+    @staticmethod
+    def unit_vector(v: np.ndarray) -> np.ndarray:
+        """
+        Return normalised copy of a vector.
+
+        For an input vector :math:`\\vec{v}`, return the unit vector
+        :math:`\\hat{v} = \\frac{\\vec{v}}{|\\vec{v}|}`.
+
+        Args:
+            v: Input vector to normalise.
+
+        Returns:
+            Normalised vector which is parallel to `v` and has a magnitude of 1.
+        """
+        # Profiling suggests this is the quickest method
+        return v / (sum(v * v)) ** 0.5
+
+    @staticmethod
+    def vector_magnitude(v: np.ndarray) -> float:
+        """
+        Return the magnitude of a vector.
+
+        For an input vector :math:`\\vec{v}`, return magnitude
+        :math:`|\\vec{v}| = \\sqrt{\\sum{v_i^2}}`.
+
+        Args:
+            v: Input vector.
+
+        Returns:
+            Magnitude (length) of vector.
+        """
+        # Profiling suggests this is the quickest method
+        return (sum(v * v)) ** 0.5
+
+    def _encode_str(self, s: str) -> bytes | str:
+        if self._optimize_speed:
+            return s.encode('UTF-8')
+        else:
+            return s
+
+    @staticmethod
+    def _radian_pair2degrees(
+        radians0: Numeric, radians1: Numeric
+    ) -> tuple[Numeric, Numeric]:
+        return np.rad2deg(radians0), np.rad2deg(radians1)  # type: ignore
+
+    @staticmethod
+    def _degree_pair2radians(
+        degrees0: Numeric, degrees1: Numeric
+    ) -> tuple[Numeric, Numeric]:
+        return np.deg2rad(degrees0), np.deg2rad(degrees1)  # type: ignore
+
+    @staticmethod
+    def _rotation_matrix_radians(theta: float) -> np.ndarray:
+        return np.array(
+            [[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]]
+        )
+
+    @staticmethod
+    def angular_dist(ra1: float, dec1: float, ra2: float, dec2: float) -> float:
+        """
+        Calculate the angular distance between two RA/Dec coordinate pairs.
+
+        Args:
+            ra1: RA of first point.
+            dec1: Dec of first point.
+            ra2: RA of second point
+            dec2: Dec of second point.
+
+        Returns:
+            Angular distance in degrees between the two points.
+        """
+        return np.rad2deg(
+            np.arccos(
+                np.sin(np.deg2rad(dec1)) * np.sin(np.deg2rad(dec2))
+                + np.cos(np.deg2rad(dec1))
+                * np.cos(np.deg2rad(dec2))
+                * np.cos(np.deg2rad(ra1) - np.deg2rad(ra2))
+            )
+        )
+
+    def _set_progress_hook(self, progress_hook: progress.ProgressHook) -> None:
+        self._progress_hook = progress_hook
+        self._progress_call_stack = []
+
+    def _get_progress_hook(self) -> progress.ProgressHook | None:
+        return self._progress_hook
+
+    def _remove_progress_hook(self) -> None:
+        self._progress_hook = None
+        self._progress_call_stack = []
+
+    def _update_progress_hook(self, progress: float) -> None:
+        """Update progress hook with `progress` of current function between 0 & 1"""
+        if self._progress_hook is not None:
+            self._progress_hook(progress, self._progress_call_stack)
+
+
+def load_kernels(*paths, clear_before: bool = False) -> list[str]:
+    """
+    Load spice kernels defined by patterns
+
+    Args:
+        *paths: Paths to spice kernels, evaluated using `glob.glob` with
+            `recursive=True`.
+        clear_before: Clear kernel pool before loading new kernels.
+    """
+    if clear_before:
+        spice.kclear()
+    kernels = set()
+    for pattern in paths:
+        kernels.update(glob.glob(os.path.expanduser(pattern), recursive=True))
+    for kernel in sorted(kernels):
+        spice.furnsh(kernel)
+    return list(kernels)
+
+
+def set_kernel_path(path: str | None) -> None:
+    """
+    Set the path of the directory containing SPICE kernels. See
+    :ref:`the kernel directory documentation<kernel directory>` for more detail.
+
+    Args:
+        path: Directory which PlanetMapper will search for SPICE kernels. If `None`,
+            then the default value of `'~/spice_kernels/'` will be used.
+    """
+    _KERNEL_DATA['kernel_path'] = path
+
+
+def get_kernel_path() -> str:
+    """
+    Get the path of the directory of SPICE kernels used in PlanetMapper.
+
+    #. If a kernel path has been manually set using :func:`set_kernel_path`, then this
+       path is used.
+
+    #. Otherwise the value of the environment variable `PLANETMAPPER_KERNEL_PATH` is
+       used.
+
+    #. If `PLANETMAPPER_KERNEL_PATH` is not set, then the default value,
+       `'~/spice_kernels/'` is used.
+    """
+    if _KERNEL_DATA['kernel_path'] is not None:
+        return _KERNEL_DATA['kernel_path']
+
+    try:
+        path = os.environ['PLANETMAPPER_KERNEL_PATH']
+        if path:
+            return path
+    except KeyError:
+        pass
+
+    return DEFAULT_KERNEL_PATH
---
+++ packages/planetmapper/planetmapper-1.7.0-py3-none-any.whl/planetmapper/utils.py
@@ -0,0 +1,423 @@
+"""
+Various general helpful utilities.
+"""
+import os
+import pathlib
+import traceback
+import warnings
+from datetime import datetime
+from typing import Literal
+
+import matplotlib.ticker
+import numpy as np
+from matplotlib.axes import Axes
+
+
+def format_radec_axes(
+    ax: Axes,
+    dec: float,
+    dms_ticks: bool = True,
+    add_axis_labels: bool = True,
+    aspect_adjustable: Literal['box', 'datalim'] = 'datalim',
+) -> None:
+    """
+    Format an axis to display RA/Dec coordinates nicely.
+
+    Args:
+        ax: Matplotlib axis to format.
+        dec: Declination in degrees of centre of axis.
+        dms_ticks: Toggle between showing ticks as degrees, minutes and seconds
+            (e.g. 12°34′56″) or decimal degrees (e.g. 12.582).
+        add_axis_labels: Add axis labels.
+        aspect_adjustable: Set `adjustable` parameter when setting the aspect ratio.
+            Passed to :func:`matplotlib.axes.Axes.set_aspect`.
+    """
+    if add_axis_labels:
+        ax.set_xlabel('Right Ascension')
+        ax.set_ylabel('Declination')
+    ax.set_aspect(1 / np.cos(np.deg2rad(dec)), adjustable=aspect_adjustable)
+    if not ax.xaxis_inverted():
+        ax.invert_xaxis()
+    if dms_ticks:
+        ax.yaxis.set_major_locator(DMSLocator())
+        ax.yaxis.set_major_formatter(DMSFormatter())
+        ax.xaxis.set_major_locator(DMSLocator())
+        ax.xaxis.set_major_formatter(DMSFormatter())
+
+
+class DMSFormatter(matplotlib.ticker.FuncFormatter):
+    """
+    Matplotlib tick formatter to display angular values as degrees, minutes and seconds
+    e.g. `12°34′56″`. Designed to work with :class:`DMSLocator`. ::
+
+        ax = plt.cga()
+
+        ax.yaxis.set_major_locator(planetmapper.utils.DMSLocator())
+        ax.yaxis.set_major_formatter(planetmapper.utils.DMSFormatter())
+
+        ax.xaxis.set_major_locator(planetmapper.utils.DMSLocator())
+        ax.xaxis.set_major_formatter(planetmapper.utils.DMSFormatter())
+    """
+
+    def __init__(self) -> None:
+        super().__init__(self._format)
+        self.skip_parts = set()
+        self.fmt_s = '02.0f'
+
+    def _format(self, dd, pos):
+        d, m, s = decimal_degrees_to_dms(dd)
+        out = []
+        if 'd' not in self.skip_parts or m == 0 and s == 0:
+            out.append(f'{d}°')
+        if 'm' not in self.skip_parts or s == 0:
+            out.append(f'{m:02.0f}′')
+        if 's' not in self.skip_parts:
+            out.append(f'{s:{self.fmt_s}}″')
+        return ''.join(out)
+
+    def set_locs(self, locs) -> None:
+        """:meta private:"""
+        vmin, vmax = sorted(self.axis.get_view_interval())
+        dms_min = decimal_degrees_to_dms(vmin)
+        dms_max = decimal_degrees_to_dms(vmax)
+        vrange = abs(vmax - vmin)
+
+        self.skip_parts.clear()
+        ofs = ''
+        if dms_min[:2] == dms_max[:2]:
+            d, m, s = dms_min
+            self.skip_parts.add('d')
+            self.skip_parts.add('m')
+            if d != 0 or m != 0:
+                ofs = f'{d:+.0f}°{m:02.0f}′'
+        elif dms_min[0] == dms_max[0]:
+            d, m, s = dms_min
+            self.skip_parts.add('d')
+            if d != 0:
+                ofs = f'{d:+.0f}°'
+
+        if vrange > 10 / 60:
+            self.skip_parts.add('s')
+        if vrange > 10:
+            self.skip_parts.add('m')
+
+        if vrange < 10 / 3600:
+            self.skip_parts.add('m')
+        if vrange < 10 / 60:
+            self.skip_parts.add('d')
+
+        if vrange < 0.01 / 3600:
+            self.fmt_s = '.3g'
+        elif vrange < 0.1 / 3600:
+            self.fmt_s = '.3f'
+        elif vrange < 1 / 3600:
+            self.fmt_s = '.2f'
+        elif vrange < 10 / 3600:
+            self.fmt_s = '.1f'
+        else:
+            self.fmt_s = '02.0f'
+
+        if self.skip_parts == {'d', 'm', 's'}:
+            self.skip_parts = {'d', 'm', 's'}
+        self.set_offset_string(ofs)
+        return super().set_locs(locs)
+
+
+class DMSLocator(matplotlib.ticker.Locator):
+    """
+    Matplotlib tick locator to display angular values as degrees, minutes and seconds.
+    Designed to work with :class:`DMSFormatter`. ::
+
+        ax = plt.cga()
+
+        ax.yaxis.set_major_locator(planetmapper.utils.DMSLocator())
+        ax.yaxis.set_major_formatter(planetmapper.utils.DMSFormatter())
+
+        ax.xaxis.set_major_locator(planetmapper.utils.DMSLocator())
+        ax.xaxis.set_major_formatter(planetmapper.utils.DMSFormatter())
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        steps = [1, 2, 5, 10]
+        self.locator = matplotlib.ticker.MaxNLocator(steps=steps, nbins=8)
+
+    def __call__(self):
+        vmin, vmax = self.axis.get_view_interval()
+        return self.tick_values(vmin, vmax)
+
+    def tick_values(self, vmin: float, vmax: float) -> np.ndarray:
+        """:meta private:"""
+        vrange = abs(vmax - vmin)
+        if vrange < 1 / 60:
+            multiplier = 3600
+        elif vrange < 1:
+            multiplier = 60
+        else:
+            multiplier = 1
+        ticks = self.locator.tick_values(vmin * multiplier, vmax * multiplier)
+        return ticks / multiplier
+
+
+def decimal_degrees_to_dms(decimal_degrees: float) -> tuple[int, int, float]:
+    """
+    Get degrees, minutes, seconds from decimal degrees.
+
+    `decimal_degrees_to_dms(-11.111)` returns `(-11.0, 6.0, 39.6)`.
+
+    Args:
+        decimal_degrees: Decimal degrees.
+
+    Returns:
+        `(degrees, minutes, seconds)` tuple
+    """
+    dd = abs(decimal_degrees)
+    minutes, seconds = divmod(dd * 3600, 60)
+    degrees, minutes = divmod(minutes, 60)
+    if decimal_degrees < 0:
+        if degrees:
+            degrees = -degrees
+        elif minutes:
+            minutes = -minutes
+        else:
+            seconds = -seconds
+    return int(degrees), int(minutes), seconds
+
+
+def decimal_degrees_to_dms_str(decimal_degrees: float, seconds_fmt: str = '') -> str:
+    """
+    Create nicely formated DMS string from decimal degrees value (e.g. `'12°34′56″'`).
+
+    Uses :func:`decimal_degrees_to_dms` to perform the conversion.
+
+    Args:
+        decimal_degrees: Decimal degrees.
+        seconds_fmt: Optionally specify a format string for the seconds part of the
+            returned value. For example, `seconds_fmt='.3f'` will fix three decimal
+            places for the fractional part of the seconds value.
+
+    Returns:
+        String representting the degress, minutes, seconds of the angle.
+    """
+    d, m, s = decimal_degrees_to_dms(decimal_degrees)
+    return f'{d}°{m}′{s:{seconds_fmt}}″'
+
+
+class ignore_warnings(warnings.catch_warnings):
+    """
+    Context manager to ignore general warnings using warnings.filterwarnings.
+    """
+
+    def __init__(self, *warining_strings: str, **kwargs):
+        super().__init__(**kwargs)
+        self.warning_strings = warining_strings
+
+    def __enter__(self):
+        out = super().__enter__()
+        for ws in self.warning_strings:
+            warnings.filterwarnings('ignore', ws)
+        return out
+
+
+class filter_fits_comment_warning(warnings.catch_warnings):
+    """
+    Context manager to hide FITS `Card is too long, comment will be truncated` warnings.
+    """
+
+    def __enter__(self):
+        out = super().__enter__()
+        warnings.filterwarnings(
+            'ignore',
+            message='Card is too long, comment will be truncated.',
+            module='astropy.io.fits.card',
+        )
+        return out
+
+
+def cprint(*msg, fg=None, bg=None, style=None, skip_print=False, sep=' ', **kwargs):
+    """
+    Prints coloured and formatted text.
+
+    Parameters
+    ----------
+    msg
+        Message to print.
+
+    fg, bg : {'k', 'r', 'g', 'b', 'y', 'm', 'c', 'w'}
+        Foreground and background colours (see code for options).
+
+    style : {'b', 'f', 'i', 'u', 'x', 'y', 'r', 'h', 's'}
+        Formatting style to apply to text. Can be multiple values, e.g. 'bi'
+        for bold and italic style.
+
+    """
+    colcode = {
+        'k': 0,  # black
+        'r': 1,  # red
+        'g': 2,  # green
+        'y': 3,  # yellow
+        'b': 4,  # blue
+        'm': 5,  # magenta
+        'c': 6,  # cyan
+        'w': 7,  # white
+    }
+
+    fmtcode = {
+        'b': 1,  # bold
+        'f': 2,  # faint
+        'i': 3,  # italic
+        'u': 4,  # underline
+        'x': 5,  # blinking
+        'y': 6,  # fast blinking
+        'r': 7,  # reverse
+        'h': 8,  # hide
+        's': 9,  # strikethrough
+    }
+
+    # Properties
+    props = []
+    if isinstance(style, str):
+        props = [fmtcode[s] for s in style]
+    if isinstance(fg, str):
+        props.append(30 + colcode[fg])
+    if isinstance(bg, str):
+        props.append(40 + colcode[bg])
+
+    # Display
+    msg = sep.join(
+        str(x) for x in msg
+    )  # Reproduce print() behaviour for easy translation
+    props = ';'.join(str(x) for x in props)
+
+    if props:
+        msg = '\x1b[%sm%s\x1b[0m' % (props, msg)
+
+    if not skip_print:
+        print(msg, **kwargs)
+
+    return msg
+
+
+def print_progress(annotation=None, c1='g', c2='k', style='b'):
+    """
+    Print progress summary of current code.
+
+    Prints summary of code location and execution time for use in optimising and monitoring code.
+    Uses traceback to identify the call stack and prints tree-like diagram of stacks where this
+    function was called. The call stack is relative to the first time this function is called as it
+    uses properties of print_progress to communicate between calls.
+
+    Printed output contains:
+        - Seconds elapsed since last call of print_progress.
+        - Current time.
+        - Traceback for location print_progress is called from, relative to first location
+          print_progress was called. This includes file names, function names and line numbers.
+        - Optional annotation provided to explain what current line is.
+
+    Arguments
+    ---------
+    annotation : str
+        Optionally provide annotation about current line of code.
+
+    c1, c2, style : str
+        Formatting options to pass to cprint()
+    """
+    now = datetime.now()  # Ignore duration of current code
+
+    # Get timings
+    if print_progress.last_dtm is None:
+        title = ' seconds'
+    else:
+        td = now - print_progress.last_dtm
+        title = f'{td.total_seconds():8.2f}'
+    title += ' @ '
+    title += now.strftime('%H:%M:%S')
+    title += ' '
+
+    # Get stack
+    stack = traceback.extract_stack()[:-1]
+    if print_progress.first_stack is None:
+        print_progress.first_stack = stack
+    first_stack = print_progress.first_stack
+    split_idx = len([None for a, b in zip(stack[:-1], first_stack) if a == b])
+    stack = stack[split_idx:]
+    stack_parts = [(s[2], s[1], os.path.split(s[0])[1]) for s in stack]
+    stack_text = [
+        f'{a} (line {b} in <ipython>)'
+        if c.startswith('<ipython-input-')
+        else f'{a} (line {b} in {c})'
+        for a, b, c in stack_parts
+    ]
+    last_stack = print_progress.last_stack
+    if last_stack is not None:
+        last_idx = len([None for a, b in zip(stack[:-1], last_stack) if a == b])
+        stack_text = stack_text[last_idx:]
+    else:
+        last_idx = 0
+    print_progress.last_stack = stack
+
+    # Do actual printing
+    for st in stack_text:
+        msg = cprint(
+            ' ' + '│  ' * last_idx + st + ' ',
+            fg=c1,
+            bg=c2,
+            style=style,
+            skip_print=True,
+        )
+        if st == stack_text[-1]:
+            tt = title
+            if annotation:
+                msg += cprint(
+                    f'{annotation} ', fg=c1, bg=c2, style=style + 'i', skip_print=True
+                )
+        else:
+            tt = ' ' * len(title)
+        tt = cprint(tt, fg=c2, bg=c1, style=style, skip_print=True)
+        print(tt + msg, flush=True)
+        last_idx += 1
+    print_progress.last_dtm = datetime.now()  # Ignore duration of this code
+
+
+print_progress.last_dtm = None
+print_progress.last_stack = None
+print_progress.first_stack = None
+
+
+def normalise(values, top=1, bottom=0, single_value=None):
+    """
+    Normalise iterable.
+    """
+    assert top > bottom
+    values = np.array(values)
+    if single_value is not None and len(set(values)) == 1:
+        return np.full(values.shape, single_value)
+    vmin = np.nanmin(values)
+    vmax = np.nanmax(values)
+
+    # Put into 0 to 1 range
+    if vmax != vmin:
+        values = (values - vmin) / (vmax - vmin)
+    else:
+        values = values - vmin
+    return values * (top - bottom) + bottom
+
+
+def check_path(path: str):
+    """
+    Checks if file path's directory tree exists, and creates it if necessary.
+
+    Assumes path is to a file if `os.path.split(path)[1]` contains '.',
+    otherwise assumes path is to a directory.
+    """
+    path = os.path.expandvars(os.path.expanduser(path))
+    if os.path.isdir(path):
+        return
+    if '.' in os.path.split(path)[1]:
+        path = os.path.split(path)[0]
+        if os.path.isdir(path):
+            return
+    if path == '':
+        return
+    print('Creating directory path "{}"'.format(path))
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)

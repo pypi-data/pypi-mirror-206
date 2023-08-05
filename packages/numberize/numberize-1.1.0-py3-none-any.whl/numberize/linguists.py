2cdaa6918df1ea0eff44628a51c200fdbbf099b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:47 +0000

    Add package numberize/1.1.0/numberize-1.1.0-py3-none-any.whl

---
+++ packages/numberize/numberize-1.1.0-py3-none-any.whl/numberize/linguists.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import pymorphy3
+
+import numberize.dicts as dicts
+
+
+class Linguist(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_number(token: str) -> Optional[int]:
+        """
+        E.g. 'two' -> 2, 'twenty-five' -> 25, "п'ять" -> 5, "миллион" -> 1E6
+        'horse' -> None
+        :param token: simple numeral
+        :return: corresponding number, returns None if token is not a numeral
+        """
+
+
+class EnLinguist(Linguist):
+    @staticmethod
+    def get_number(token: str) -> Optional[int]:
+        token = token.lower()
+        if '-' in token:
+            parts = token.split('-')
+            if len(parts) != 2:
+                return
+            left = dicts.en.nums.get(parts[0])
+            if not left or left < 20 or left > 90:
+                return
+            right = dicts.en.nums.get(parts[1])
+            if not right or right > 9 or right < 1:
+                return
+            return left + right
+        return dicts.en.nums.get(token)
+
+
+class RuLinguist(Linguist):
+    def __init__(self, morph: 'pymorphy3.MorphAnalyzer'):
+        """
+        :param morph: MorphAnalyzer to normalize words
+        """
+        self.analyzer = morph
+
+    def get_number(self, token: str) -> Optional[int]:
+        token = token.lower()
+        if token[-1] == '.' and len(token) > 3:
+            token = token[:-1]
+        for form in self.analyzer.normal_forms(token):
+            number = dicts.ru.nums.get(form)
+            if number:
+                return number
+
+
+class UkLinguist(Linguist):
+    def __init__(self, morph: 'pymorphy3.MorphAnalyzer'):
+        """
+        :param morph: MorphAnalyzer to normalize words
+        """
+        self.analyzer = morph
+
+    def get_number(self, token: str) -> Optional[int]:
+        token = token.lower()
+        if token[-1] == '.' and len(token) > 3:  # TokTokTokenizer sometimes
+            token = token[:-1]              # doesn't tokenize points "тисяча."
+        for form in self.analyzer.normal_forms(token):
+            number = dicts.uk.nums.get(form)
+            if number:
+                return number

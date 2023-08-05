ba37c5e0659560271b3e9a94026b4fab13d52ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:04 +0000

    Add package mbta-gtfs-sqlite/0.9.2/mbta_gtfs_sqlite-0.9.2-py3-none-any.whl

---
+++ packages/mbta-gtfs-sqlite/mbta_gtfs_sqlite-0.9.2-py3-none-any.whl/mbta_gtfs_sqlite/compact.py
@@ -0,0 +1,15 @@
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from .models.stop_times import StopTime
+
+
+def vacuum(session: Session):
+    session.commit()
+    session.execute(text("vacuum"))
+    session.commit()
+
+
+def make_compact_db(session: Session):
+    session.query(StopTime).delete()
+    vacuum(session)

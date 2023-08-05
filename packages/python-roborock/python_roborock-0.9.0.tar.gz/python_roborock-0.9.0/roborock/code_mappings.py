772ca19b129f6d89c37a1b2a202569966868ef67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:37 +0000

    Add package python-roborock/0.9.0/python_roborock-0.9.0.tar.gz

---
+++ packages/python-roborock/python_roborock-0.9.0.tar.gz/python_roborock-0.9.0/roborock/code_mappings.py
@@ -0,0 +1,183 @@
+from __future__ import annotations
+
+import logging
+from enum import Enum
+from typing import Any, Type, TypeVar
+
+_StrEnumT = TypeVar("_StrEnumT", bound="RoborockEnum")
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class RoborockEnum(str, Enum):
+    def __new__(cls: Type[_StrEnumT], value: str, *args: Any, **kwargs: Any) -> _StrEnumT:
+        """Create a new StrEnum instance."""
+        if not isinstance(value, str):
+            raise TypeError(f"{value!r} is not a string")
+        return super().__new__(cls, value, *args, **kwargs)
+
+    def __str__(self):
+        return str(self.value)
+
+    @classmethod
+    def _missing_(cls: Type[_StrEnumT], code: object):
+        if cls._member_map_.get(str(code)):
+            return cls._member_map_.get(str(code))
+        else:
+            _LOGGER.warning(f"Unknown code {code} for {cls.__name__}")
+            return cls._member_map_.get(str(-9999))
+
+    @classmethod
+    def as_dict(cls: Type[_StrEnumT]):
+        return {int(i.name): i.value for i in cls if i.value != "UNKNOWN"}
+
+    @classmethod
+    def values(cls: Type[_StrEnumT]):
+        return list(cls.as_dict().values())
+
+    @classmethod
+    def keys(cls: Type[_StrEnumT]):
+        return list(cls.as_dict().keys())
+
+    @classmethod
+    def items(cls: Type[_StrEnumT]):
+        return cls.as_dict().items()
+
+    @classmethod
+    def __getitem__(cls: Type[_StrEnumT], item):
+        return cls.__getitem__(item)
+
+
+def create_code_enum(name: str, data: dict) -> RoborockEnum:
+    data[-9999] = "UNKNOWN"
+    return RoborockEnum(name, {str(key): value for key, value in data.items()})
+
+
+RoborockStateCode = create_code_enum(
+    "RoborockStateCode",
+    {
+        1: "starting",
+        2: "charger_disconnected",
+        3: "idle",
+        4: "remote_control_active",
+        5: "cleaning",
+        6: "returning_home",
+        7: "manual_mode",
+        8: "charging",
+        9: "charging_problem",
+        10: "paused",
+        11: "spot_cleaning",
+        12: "error",
+        13: "shutting_down",
+        14: "updating",
+        15: "docking",
+        16: "going_to_target",
+        17: "zoned_cleaning",
+        18: "segment_cleaning",
+        22: "emptying_the_bin",  # on s7+, see #1189
+        23: "washing_the_mop",  # on a46, #1435
+        26: "going_to_wash_the_mop",  # on a46, #1435
+        100: "charging_complete",
+        101: "device_offline",
+    },
+)
+
+RoborockErrorCode = create_code_enum(
+    "RoborockErrorCode",
+    {
+        0: "none",
+        1: "lidar_blocked",
+        2: "bumper_stuck",
+        3: "wheels_suspended",
+        4: "cliff_sensor_error",
+        5: "main_brush_jammed",
+        6: "side_brush_jammed",
+        7: "wheels_jammed",
+        8: "robot_trapped",
+        9: "no_dustbin",
+        12: "low_battery",
+        13: "charging_error",
+        14: "battery_error",
+        15: "wall_sensor_dirty",
+        16: "robot_tilted",
+        17: "side_brush_error",
+        18: "fan_error",
+        21: "vertical_bumper_pressed",
+        22: "dock_locator_error",
+        23: "return_to_dock_fail",
+        24: "nogo_zone_detected",
+        27: "vibrarise_jammed",
+        28: "robot_on_carpet",
+        29: "filter_blocked",
+        30: "invisible_wall_detected",
+        31: "cannot_cross_carpet",
+        32: "internal_error",
+    },
+)
+
+RoborockFanPowerCode = create_code_enum(
+    "RoborockFanPowerCode",
+    {
+        105: "off",
+        101: "silent",
+        102: "balanced",
+        103: "turbo",
+        104: "max",
+        108: "max_plus",
+        106: "custom",
+    },
+)
+
+RoborockMopModeCode = create_code_enum(
+    "RoborockMopModeCode",
+    {
+        300: "standard",
+        301: "deep",
+        303: "deep_plus",
+        302: "custom",
+    },
+)
+
+RoborockMopIntensityCode = create_code_enum(
+    "RoborockMopIntensityCode",
+    {
+        200: "off",
+        201: "mild",
+        202: "moderate",
+        203: "intense",
+        204: "custom",
+    },
+)
+
+RoborockDockErrorCode = create_code_enum(
+    "RoborockDockErrorCode",
+    {
+        0: "ok",
+        38: "water empty",
+        39: "waste water tank full",
+    },
+)
+
+RoborockDockTypeCode = create_code_enum(
+    "RoborockDockTypeCode",
+    {0: "no_dock", 1: "unknown", 2: "unknown", 3: "empty_wash_fill_dock", 4: "unknown", 5: "auto_empty_dock_pure"},
+)
+
+RoborockDockDustCollectionModeCode = create_code_enum(
+    "RoborockDockDustCollectionModeCode",
+    {
+        0: "smart",
+        1: "light",
+        2: "balanced",
+        4: "max",
+    },
+)
+
+RoborockDockWashTowelModeCode = create_code_enum(
+    "RoborockDockWashTowelModeCode",
+    {
+        0: "light",
+        1: "balanced",
+        2: "deep",
+    },
+)
---
+++ packages/python-roborock/python_roborock-0.9.0.tar.gz/python_roborock-0.9.0/roborock/local_api.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from asyncio import Lock, Transport
+from typing import Optional
+
+import async_timeout
+
+from .api import COMMANDS_SECURED, QUEUE_TIMEOUT, RoborockClient
+from .containers import RoborockLocalDeviceInfo
+from .exceptions import CommandVacuumError, RoborockConnectionException, RoborockException
+from .roborock_message import AP_CONFIG, RoborockMessage, RoborockParser
+from .roborock_typing import CommandInfoMap, RoborockCommand
+from .util import get_running_loop_or_create_one
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class RoborockLocalClient(RoborockClient, asyncio.Protocol):
+    def __init__(self, device_info: RoborockLocalDeviceInfo):
+        super().__init__("abc", device_info)
+        self.loop = get_running_loop_or_create_one()
+        self.ip = device_info.network_info.ip
+        self._batch_structs: list[RoborockMessage] = []
+        self._executing = False
+        self.remaining = b""
+        self.transport: Transport | None = None
+        self._mutex = Lock()
+
+    def data_received(self, message):
+        if self.remaining:
+            message = self.remaining + message
+            self.remaining = b""
+        (parser_msg, remaining) = RoborockParser.decode(message, self.device_info.device.local_key)
+        self.remaining = remaining
+        self.on_message_received(parser_msg)
+
+    def connection_lost(self, exc: Optional[Exception]):
+        self.on_connection_lost(exc)
+
+    def is_connected(self):
+        return self.transport and self.transport.is_reading()
+
+    async def async_connect(self) -> None:
+        async with self._mutex:
+            try:
+                if not self.is_connected():
+                    async with async_timeout.timeout(QUEUE_TIMEOUT):
+                        _LOGGER.info(f"Connecting to {self.ip}")
+                        self.transport, _ = await self.loop.create_connection(  # type: ignore
+                            lambda: self, self.ip, 58867
+                        )
+                        _LOGGER.info(f"Connected to {self.ip}")
+            except Exception as e:
+                raise RoborockConnectionException(f"Failed connecting to {self.ip}") from e
+
+    def sync_disconnect(self) -> None:
+        if self.transport and not self.loop.is_closed():
+            self.transport.close()
+
+    async def async_disconnect(self) -> None:
+        async with self._mutex:
+            self.sync_disconnect()
+
+    def build_roborock_message(self, method: RoborockCommand, params: Optional[list | dict] = None) -> RoborockMessage:
+        secured = True if method in COMMANDS_SECURED else False
+        request_id, timestamp, payload = self._get_payload(method, params, secured)
+        _LOGGER.debug(f"id={request_id} Requesting method {method} with {params}")
+        command_info = CommandInfoMap.get(method)
+        if not command_info:
+            raise RoborockException(f"Request {method} have unknown prefix. Can't execute in offline mode")
+        command = CommandInfoMap.get(method)
+        if command is None:
+            raise RoborockException(f"No prefix found for {method}")
+        prefix = command.prefix
+        request_protocol = 4
+        return RoborockMessage(
+            prefix=prefix,
+            timestamp=timestamp,
+            protocol=request_protocol,
+            payload=payload,
+        )
+
+    async def ping(self):
+        command_info = CommandInfoMap[RoborockCommand.NONE]
+        roborock_message = RoborockMessage(prefix=command_info.prefix, protocol=AP_CONFIG, payload=b"")
+        return (await self.send_message(roborock_message))[0]
+
+    async def send_command(self, method: RoborockCommand, params: Optional[list | dict] = None):
+        roborock_message = self.build_roborock_message(method, params)
+        return (await self.send_message(roborock_message))[0]
+
+    async def async_local_response(self, roborock_message: RoborockMessage):
+        request_id = roborock_message.get_request_id()
+        if request_id is None:
+            request_id = roborock_message.seq
+        # response_protocol = 5 if roborock_message.prefix == secured_prefix else 4
+        response_protocol = 4
+        (response, err) = await self._async_response(request_id, response_protocol)
+        if err:
+            raise CommandVacuumError("", err) from err
+        _LOGGER.debug(f"id={request_id} Response from {roborock_message.get_method()}: {response}")
+        return response
+
+    def _send_msg_raw(self, data: bytes):
+        try:
+            if not self.transport:
+                raise RoborockException("Can not send message without connection")
+            self.transport.write(data)
+        except Exception as e:
+            raise RoborockException(e) from e
+
+    async def send_message(self, roborock_messages: list[RoborockMessage] | RoborockMessage):
+        await self.validate_connection()
+        if isinstance(roborock_messages, RoborockMessage):
+            roborock_messages = [roborock_messages]
+        local_key = self.device_info.device.local_key
+        msg = RoborockParser.encode(roborock_messages, local_key)
+        # Send the command to the Roborock device
+        _LOGGER.debug(f"Requesting device with {roborock_messages}")
+        self._send_msg_raw(msg)
+
+        responses = await asyncio.gather(
+            *[self.async_local_response(roborock_message) for roborock_message in roborock_messages],
+            return_exceptions=True,
+        )
+        exception = next((response for response in responses if isinstance(response, BaseException)), None)
+        if exception:
+            await self.async_disconnect()
+            raise exception
+        return responses

94bba27915fa3db19994b8fa2e919f0f7f063d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:19 +0000

    Add package mell/2.0.5/mell-2.0.5.tar.gz

---
+++ packages/mell/mell-2.0.5.tar.gz/mell-2.0.5/mell/consts.py
@@ -0,0 +1,6 @@
+name = "mell"
+version = "2.0.5"
+author = "Diego Souza"
+author_email = "diegofpsouza+mell@gmail.com"
+url = "https://github.com/diegofps/mell"
+description = "Generate anything from template files and metadata files!"

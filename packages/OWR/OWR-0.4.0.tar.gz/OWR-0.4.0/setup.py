78b21e0f68c7c547e1da655d43a0cf76098122b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:02 +0000

    Add package OWR/0.4.0/OWR-0.4.0.tar.gz

---
+++ packages/OWR/OWR-0.4.0.tar.gz/OWR-0.4.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='OWR',
+    version='0.4.0',
+    description='Obscene word recognition package',
+    url='https://github.com/VladVslv/OWR',
+    author='Vlad Vasilev',
+    author_email='vpvasilev.work@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['owr'],
+    install_requires=['huggingsound',
+                      'librosa',
+                      'Levenshtein',
+                      'soundfile',
+                      'fonetika',
+                      'numpy',
+                      'playsound',
+                      'pylcs',
+                      'logging'],
+)

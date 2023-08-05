7daa51556615deec4333db7a1d6e49eb60f4d7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:33 +0000

    Add package PyTerm-Shell/0.1.12/PyTerm-Shell-0.1.12.tar.gz

---
+++ packages/PyTerm-Shell/PyTerm-Shell-0.1.12.tar.gz/PyTerm-Shell-0.1.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup (
+ name = 'PyTerm-Shell',
+ description = 'Making an aesthetic minimalist Linux shell in Python',
+ long_description = long_description,
+ long_description_content_type='text/markdown',
+ version = '0.1.12',
+ python_requires='>=3.7',
+ entry_points = {
+        'console_scripts': ['PyTerm=PyTerm.cli_assist:main'],
+ },
+ include_package_data=True,
+ install_requires=[
+          'textual',
+      ],
+ author="Nick Tsiones",
+ license='MIT',
+ url='https://github.com/MetalKamina/PyTerm',
+  author_email='ntsiones@gmail.com',
+  classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ]
+)

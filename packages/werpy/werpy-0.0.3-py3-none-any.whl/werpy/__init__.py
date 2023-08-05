d7f49bbae71c17d474901b0a1e88927ec1749993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package werpy/0.0.3/werpy-0.0.3-py3-none-any.whl

---
+++ packages/werpy/werpy-0.0.3-py3-none-any.whl/werpy/__init__.py
@@ -0,0 +1,11 @@
+"""
+The werpy package provides tools for calculating word error rates (WERs) and related metrics on text data.
+"""
+
+from .normalize import normalize
+from .metrics import metrics
+from .wer import wer
+from .wers import wers
+from .werp import werp
+from .werps import werps
+from .summary import summary

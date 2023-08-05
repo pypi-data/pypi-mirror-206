53c86b1c5cd481ebecd3f9c735d805970b58f7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:59 +0000

    Add package rewards-api/0.1.2/rewards_api-0.1.2.tar.gz

---
+++ packages/rewards-api/rewards_api-0.1.2.tar.gz/rewards_api-0.1.2/rewards_api/__init__.py
@@ -0,0 +1,7 @@
+from rewards_api.config import CONFIG 
+from rewards_api.streamer import RewardsStreamer
+from rewards_api import utils as utils 
+from rewards_api.cli import main 
+from rewards_api import cli 
+
+__version__ = "0.0.2"

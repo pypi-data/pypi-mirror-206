cf7376cf08101d41ae9d0b729c5a9eba8d3da730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:53 +0000

    Add package synsec/0.1/synsec-0.1.tar.gz

---
+++ packages/synsec/synsec-0.1.tar.gz/synsec-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='synsec',
+    version='0.1',
+    description='Execute code from a URL',
+    author='Synthetic',
+    author_email='synthetic@lol.com',
+    py_modules=['synsec'],
+    install_requires=['requests'],
+)

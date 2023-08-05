8a9b0ccd840ab50ada6974298d8213adbb975578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:19 +0000

    Add package canvasrobot/0.6.1/canvasrobot-0.6.1-py3-none-any.whl

---
+++ packages/canvasrobot/canvasrobot-0.6.1-py3-none-any.whl/canvasrobot/__init__.py
@@ -0,0 +1,6 @@
+from .canvas_robot import CanvasRobot, LocalDAL,ENROLLMENT_TYPES, \
+    EDUCATIONS, COMMUNITIES
+from .canvas_robot_model import STUDADMIN, SHORTNAMES
+__all__=["CanvasRobot","LocalDAL","ENROLLMENT_TYPES","EDUCATIONS","COMMUNITIES",
+         "STUDADMIN","SHORTNAMES"]
+__version__ = "0.6.1" # It MUST match the version in pyproject.toml file

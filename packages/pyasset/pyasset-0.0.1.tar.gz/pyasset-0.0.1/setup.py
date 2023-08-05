46992f45a9dee0d4625afac4a7b0e2cf9b9b594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:42 +0000

    Add package pyasset/0.0.1/pyasset-0.0.1.tar.gz

---
+++ packages/pyasset/pyasset-0.0.1.tar.gz/pyasset-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyasset", # Replace with your own username
+    version="0.0.1",
+    author="kimssammwu",
+    author_email="dlalsdn1009@naver.com",
+    description="many pattern in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dlams",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

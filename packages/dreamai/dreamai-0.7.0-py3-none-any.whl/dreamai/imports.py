238eb0f9fd5c5affe05241b0a931b9380a3d7cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:26 +0000

    Add package dreamai/0.7.0/dreamai-0.7.0-py3-none-any.whl

---
+++ packages/dreamai/dreamai-0.7.0-py3-none-any.whl/dreamai/imports.py
@@ -0,0 +1,30 @@
+# import torch
+# from torch import nn
+
+import re
+import os
+import cv2
+import json
+import copy
+import pickle
+import random
+import imutils
+import operator
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from itertools import chain
+from functools import reduce
+from functools import partial
+from matplotlib import colors
+from yaml import load, Loader
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from configparser import ConfigParser
+from imutils import resize as resize_img
+from PIL import Image, ImageDraw, ImageFont
+from typing import Iterable,Generator,Sequence,Iterator,List,Set,Dict,Union,Optional,Tuple
+
+from fastcore.foundation import is_bool, L
+from fastcore.basics import merge as merge_dicts
+from fastcore.basics import chunked, store_attr, camel2snake, snake2camel, flatten

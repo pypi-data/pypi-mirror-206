e2afeeade7d192c84974a5ff47e83747632b93ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package deepdog/0.7.0/deepdog-0.7.0-py3-none-any.whl

---
+++ packages/deepdog/deepdog-0.7.0-py3-none-any.whl/deepdog/__init__.py
@@ -0,0 +1,22 @@
+import logging
+from deepdog.meta import __version__
+from deepdog.bayes_run import BayesRun
+from deepdog.bayes_run_simulpairs import BayesRunSimulPairs
+from deepdog.real_spectrum_run import RealSpectrumRun
+from deepdog.temp_aware_real_spectrum_run import TempAwareRealSpectrumRun
+
+
+def get_version():
+	return __version__
+
+
+__all__ = [
+	"get_version",
+	"BayesRun",
+	"BayesRunSimulPairs",
+	"RealSpectrumRun",
+	"TempAwareRealSpectrumRun",
+]
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())

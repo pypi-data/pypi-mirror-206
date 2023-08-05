594584ca8a8712c6cfb7c04a43e84832edc757d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package gdsfactory/6.90.9/gdsfactory-6.90.9-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-6.90.9-py3-none-any.whl/gdsfactory/geometry/check_duplicated_cells.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Union
+
+
+def check_duplicated_cells(gdspath: Union[Path, str]):
+    """Reads cell and checks for duplicated cells.
+
+    Args:
+        gdspath: path to GDS or Component
+
+    """
+    import klayout.db as pya
+
+    from gdsfactory.component import Component
+
+    if isinstance(gdspath, Component):
+        gdspath.flatten()
+        gdspath = gdspath.write_gds()
+    layout = pya.Layout()
+    layout.read(str(gdspath))
+    return layout.top_cell()

f73ba0d7e1afd1efb1b1f6a0a5e00eb6c307a3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:32 +0000

    Add package autora-workflow/0.2.1/autora_workflow-0.2.1-py3-none-any.whl

---
+++ packages/autora-workflow/autora_workflow-0.2.1-py3-none-any.whl/autora/workflow/__main__.py
@@ -0,0 +1,68 @@
+import logging
+import os
+import pathlib
+from typing import Optional
+
+import typer
+import yaml
+
+from .controller import Controller
+
+_logger = logging.getLogger(__name__)
+
+
+def main(
+    manager: pathlib.Path = typer.Argument(..., help="Manager path"),
+    directory: pathlib.Path = typer.Argument(..., help="Directory path"),
+    step_name: Optional[str] = typer.Argument(None, help="Name of step"),
+    verbose: bool = typer.Option(False, help="Turns on info logging level."),
+    debug: bool = typer.Option(False, help="Turns on debug logging level."),
+):
+    _logger.debug("initializing")
+    _configure_logger(debug, verbose)
+    controller_ = _load_manager(manager)
+
+    _logger.debug(f"loading manager state from {directory=}")
+    controller_.load(directory)
+
+    if step_name is not None:
+        controller_ = _set_next_step_name(controller_, step_name)
+
+    _logger.info("running next step")
+    next(controller_)
+
+    _logger.debug(f"last result: {controller_.state.history[-1]}")
+
+    _logger.info("writing out results")
+    controller_.dump(directory)
+
+    return
+
+
+def _configure_logger(debug, verbose):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+        _logger.debug("using DEBUG logging level")
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+        _logger.info("using INFO logging level")
+
+
+def _load_manager(path: pathlib.Path) -> Controller:
+    _logger.debug(f"_load_manager: loading from {path=} (currently in {os.getcwd()})")
+    with open(path, "r") as f:
+        controller_ = yaml.load(f, yaml.Loader)
+    assert isinstance(
+        controller_, Controller
+    ), f"controller type {type(controller_)=} unsupported"
+    return controller_
+
+
+def _set_next_step_name(controller: Controller, step_name: str):
+    _logger.info(f"setting next {step_name=}")
+    controller.planner = lambda _: step_name
+    return controller
+
+
+if __name__ == "__main__":
+    typer.run(main)

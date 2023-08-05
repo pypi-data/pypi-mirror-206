1042410bd26bcdb7471f197fe933887f8ebc3558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:29 +0000

    Add package odoo12-addon-beesdoo-pos/12.0.3.0.0/odoo12_addon_beesdoo_pos-12.0.3.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-beesdoo-pos/odoo12_addon_beesdoo_pos-12.0.3.0.0-py3-none-any.whl/odoo/addons/beesdoo_pos/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+#   - Elouan Lebars <elouan@coopiteasy.be>
+#   - Rémy Taymans <remy@coopiteasy.be>
+#   - Vincent Van Rossem <vincent@coopiteasy.be>
+#   - Elise Dupont
+#   - Thibault François
+#   - Grégoire Leeuwerck
+#   - Houssine Bakkali <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "BEES coop Point of sale",
+    "summary": """Emptied.""",
+    "author": "BEES coop - Cellule IT, Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Point Of Sale",
+    "version": "12.0.3.0.0",
+    "depends": [
+        "pos_eater",
+        "pos_available_in_pos_unlink",
+        "pos_partner_no_create",
+        "pos_hide_partner_info",
+        "pos_auto_invoice_company",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}

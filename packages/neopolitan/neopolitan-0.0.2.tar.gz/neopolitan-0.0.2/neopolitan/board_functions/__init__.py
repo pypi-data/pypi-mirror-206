e59109aed16a61ac3a780fa575765c7674139a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:14 +0000

    Add package neopolitan/0.0.2/neopolitan-0.0.2.tar.gz

---
+++ packages/neopolitan/neopolitan-0.0.2.tar.gz/neopolitan-0.0.2/neopolitan/board_functions/__init__.py
@@ -0,0 +1,5 @@
+"""Init board_functions module"""
+# didn't solve
+# print('importing colors, display, board, board_display from board_functions')
+# from . import colors, display, board, board_display
+
---
+++ packages/neopolitan/neopolitan-0.0.2.tar.gz/neopolitan-0.0.2/neopolitan/writing/__init__.py
@@ -0,0 +1,5 @@
+"""Init writing module"""
+# didn't solve
+# print('importing letters_8, groups_8, and data_transformations from writing')
+# from . import letters_8, groups_8, data_transformation
+

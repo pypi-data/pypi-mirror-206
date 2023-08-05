3217558daa32e6d13b080a3192e68466fb6a1215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:53 +0000

    Add package afloat/0.0.8/afloat-0.0.8.tar.gz

---
+++ packages/afloat/afloat-0.0.8.tar.gz/afloat-0.0.8/afloat/__init__.py
@@ -0,0 +1,5 @@
+import afloat.time as time
+# import afloat.sfodautils.harmonic_analysis as harmonic_analysis  
+from afloat.sfodautils import harmonic_analysis as sha
+import afloat.timeseries as timeseries
+import afloat.clean as clean

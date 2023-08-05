9aa2a135e8bd8ad52d95db61cc39274f8fa2fe46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:36 +0000

    Add package exception.py/0.3.3/exception.py-0.3.3.tar.gz

---
+++ packages/exception.py/exception.py-0.3.3.tar.gz/exception.py-0.3.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exception.py',
+    version='0.3.3',
+    packages=['except_py'],
+    url='',
+    license='',
+    author='cyan',
+    author_email='',
+    description='Except.Py that Make *unexceptional* problem'
+)

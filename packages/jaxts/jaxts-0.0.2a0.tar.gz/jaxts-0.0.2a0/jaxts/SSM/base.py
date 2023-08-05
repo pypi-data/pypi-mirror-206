0bc76294e53504f13324e0e385a5421aefcefbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:03 +0000

    Add package jaxts/0.0.2a0/jaxts-0.0.2a0.tar.gz

---
+++ packages/jaxts/jaxts-0.0.2a0.tar.gz/jaxts-0.0.2a0/jaxts/SSM/base.py
@@ -0,0 +1,9 @@
+class StateSpaceModel:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def predict(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def fit(self, *args, **kwargs):
+        raise NotImplementedError()

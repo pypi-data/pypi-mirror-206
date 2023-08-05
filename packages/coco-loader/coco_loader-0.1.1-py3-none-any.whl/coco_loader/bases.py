59353333fb33ae3738689f6bd5fa85734dc7079d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:04 +0000

    Add package coco-loader/0.1.1/coco_loader-0.1.1-py3-none-any.whl

---
+++ packages/coco-loader/coco_loader-0.1.1-py3-none-any.whl/coco_loader/bases.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from dataclasses import dataclass
+from os import PathLike
+from typing import List
+
+from dataclasses_json import dataclass_json
+
+from coco_loader.common import Info, Image, License
+
+
+class Annotation(ABC):
+    pass
+
+
+class Category(ABC):
+    pass
+
+
+@dataclass_json
+@dataclass
+class Dataset(ABC):
+    info: Info
+    images: List[Image]
+    licenses: List[License]
+
+    def save(self, path: PathLike, **kwargs):
+        with open(path, 'w') as f:
+            f.write(self.to_json(**kwargs))
+
+    @classmethod
+    def load(cls, path: PathLike):
+        with open(path, 'r') as f:
+            return cls.from_json(f.read())
---
+++ packages/coco-loader/coco_loader-0.1.1-py3-none-any.whl/coco_loader/common.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+
+from dataclasses_json import dataclass_json, config
+from marshmallow import fields
+
+
+@dataclass_json
+@dataclass
+class Info:
+    year: int
+    version: str
+    description: str
+    contributor: str
+    url: str
+    date_created: datetime = field(
+        metadata=config(
+            encoder=lambda d: d.strftime("%Y/%m/%d"),
+            decoder=lambda s: datetime.strptime(s, "%Y/%m/%d"),
+            mm_field=fields.DateTime(format="%Y/%m/%d"),
+        )
+    )
+
+
+@dataclass_json
+@dataclass
+class Image:
+    id: int
+    width: int
+    height: int
+    file_name: str
+    license: int
+    coco_url: str
+    date_captured: Optional[datetime] = field(
+        metadata=config(
+            encoder=lambda d: d.strftime("%Y-%m-%d %H:%M:%S")
+            if d is not None
+            else None,
+            decoder=lambda s: datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+            if s is not None
+            else None,
+            mm_field=fields.DateTime(format="iso"),
+        )
+    )
+    flickr_url: Optional[str] = None
+
+
+@dataclass_json
+@dataclass
+class License:
+    id: int
+    name: str
+    url: str

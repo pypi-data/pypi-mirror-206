89a40f7e5306e30c39186eedd7f53ff1eb1b43cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:37 +0000

    Add package python-guerrilla-mail-brb/0.1.1/python-guerrilla_mail_brb-0.1.1.tar.gz

---
+++ packages/python-guerrilla-mail-brb/python-guerrilla_mail_brb-0.1.1.tar.gz/python-guerrilla_mail_brb-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from codecs import open
+
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+setup(
+    name='python-guerrilla_mail_brb',
+    version='0.1.1',
+    description='Client for the Guerrillamail temporary email server',
+    long_description=readme + '\n\n' + history,
+    keywords='guerrillamail email client cli',
+    author='Nathan Jones',
+    url='https://github.com/sutkyne/python-guerrillamail',
+    py_modules=['guerrillamail'],
+    install_requires=[
+        'requests',
+    ],
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.11'
+    ],
+    entry_points={
+        'console_scripts': [
+            'guerrillamail=guerrillamail:main',
+        ],
+    }
+)

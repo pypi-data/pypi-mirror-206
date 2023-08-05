d1fc623e2b30f8e53fd27ebbc426eaddb2dcc369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:27 +0000

    Add package lesscode-tool/0.0.3/lesscode_tool-0.0.3-py3-none-any.whl

---
+++ packages/lesscode-tool/lesscode_tool-0.0.3-py3-none-any.whl/pkg/lesscode_tool.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Example of program which uses [options] shortcut in pattern.
+
+Usage:
+  lesscodeTool (new -d dir [-p project]|sqlacodegen -u url [-t table][-f file])
+Options:
+  -h, --help                查看帮助
+  -v, --version            展示版本号
+  -d, --dir dir            项目目录
+  -u, --url url            数据库连接
+  -f, --file file          表结构类输出文件
+  -p, --project project    项目模板名
+  -t, --table table        表名
+"""
+from docopt import docopt
+
+from pkg.tool.new import create_lesscode_project
+from pkg.tool.sqlacodegen import sqlacodegen
+from pkg.version import __version__
+
+
+def main():
+    arguments = docopt(__doc__, version=__version__)
+    new_command_flag = arguments.get("new")
+    sqlacodegen_command_flag = arguments.get("sqlacodegen")
+    if new_command_flag:
+        project = arguments.get("--project")
+        project_dir = arguments.get("--dir")
+        if project is None:
+            project = "lesscode-py"
+        if project == "lesscode-py":
+            create_lesscode_project(project_dir)
+    if sqlacodegen_command_flag:
+        url = arguments.get("--url")
+        file = arguments.get("--file")
+        table = arguments.get("--table")
+        sqlacodegen(url, table, file)
+
+
+if __name__ == '__main__':
+    main()

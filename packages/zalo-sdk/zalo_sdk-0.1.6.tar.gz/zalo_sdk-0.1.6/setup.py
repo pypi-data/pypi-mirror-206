5dfd79232bcbf2f5c63287b92156267fac34a181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:16 +0000

    Add package zalo-sdk/0.1.6/zalo_sdk-0.1.6.tar.gz

---
+++ packages/zalo-sdk/zalo_sdk-0.1.6.tar.gz/zalo_sdk-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='zalo_sdk',
+    version='0.1.6',
+    description="Zalo SDK",
+    long_description=readme,
+    author='Khoa Tran',
+    author_email='khoa@connext.biz',
+    url='https://github.com/connext-biz/zalo_integration',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)

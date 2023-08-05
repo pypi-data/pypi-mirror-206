367da8a06e016ac6154dbec8ae8633349179b04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:02 +0000

    Add package granular-engine/0.2.4/granular_engine-0.2.4-py3-none-any.whl

---
+++ packages/granular-engine/granular_engine-0.2.4-py3-none-any.whl/engine/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for Granular phobos."""
+from engine.grain import Engine 
+
+__author__ = """Sid Gupta, Sagar Verma"""
+__email__ = 'sid@granular.ai, sagar@granular.ai'
+__version__ = 'v0.2.4'
+
+__all__ = ["Engine"]

fa58da08c63682a302c79d7941faf5548c6b5bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:28 +0000

    Add package temporalio/1.2.0/temporalio-1.2.0-cp37-abi3-manylinux2014_aarch64.manylinux_2_17_aarch64.whl

---
+++ packages/temporalio/temporalio-1.2.0-cp37-abi3-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/temporalio/api/taskqueue/v1/__init__.py
@@ -0,0 +1,21 @@
+from .message_pb2 import (
+    CompatibleVersionSet,
+    PollerInfo,
+    StickyExecutionAttributes,
+    TaskIdBlock,
+    TaskQueue,
+    TaskQueueMetadata,
+    TaskQueuePartitionMetadata,
+    TaskQueueStatus,
+)
+
+__all__ = [
+    "CompatibleVersionSet",
+    "PollerInfo",
+    "StickyExecutionAttributes",
+    "TaskIdBlock",
+    "TaskQueue",
+    "TaskQueueMetadata",
+    "TaskQueuePartitionMetadata",
+    "TaskQueueStatus",
+]

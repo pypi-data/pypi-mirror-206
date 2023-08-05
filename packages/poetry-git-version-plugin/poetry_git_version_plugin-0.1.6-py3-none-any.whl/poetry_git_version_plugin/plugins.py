d6d6fdb1a335744471f66f92a39d2f30a41a82c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:23 +0000

    Add package poetry-git-version-plugin/0.1.6/poetry_git_version_plugin-0.1.6-py3-none-any.whl

---
+++ packages/poetry-git-version-plugin/poetry_git_version_plugin-0.1.6-py3-none-any.whl/poetry_git_version_plugin/plugins.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from cleo.io.io import IO
+from cleo.io.outputs.output import Verbosity
+from poetry.plugins.application_plugin import ApplicationPlugin
+from poetry.plugins.plugin import Plugin
+from poetry.poetry import Poetry
+
+from poetry_git_version_plugin import config
+from poetry_git_version_plugin.commands import GitVersionCommand
+from poetry_git_version_plugin.exceptions import plugin_exception_wrapper
+from poetry_git_version_plugin.services import VersionService
+
+
+class PoetryGitVersionPlugin(Plugin):
+    """Плагин определения версии по гит тегу"""
+
+    @plugin_exception_wrapper
+    def activate(self, poetry: Poetry, io: IO):  # pragma: no cover
+        io.write_line(f'<b>{config.PLUGIN_NAME}</b>: Init', Verbosity.VERBOSE)
+
+        plugin_config = config.PluginConfig(poetry.pyproject)
+
+        version = VersionService(io, plugin_config).get_version()
+
+        if version is not None:
+            poetry.package.version = version
+
+        io.write_error_line(f'<b>{config.PLUGIN_NAME}</b>: Finished\n', Verbosity.VERBOSE)
+
+
+class PoetryGitVersionApplicationPlugin(ApplicationPlugin):
+    commands = [GitVersionCommand]

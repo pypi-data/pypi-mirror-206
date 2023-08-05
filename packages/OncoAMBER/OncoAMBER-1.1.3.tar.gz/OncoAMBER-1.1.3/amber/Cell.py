40eaf48954442644a7a38659148d01cc07b42b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:38 +0000

    Add package OncoAMBER/1.1.3/OncoAMBER-1.1.3.tar.gz

---
+++ packages/OncoAMBER/OncoAMBER-1.1.3.tar.gz/OncoAMBER-1.1.3/amber/Cell.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Cell (object):
+    def __init__(self, radius, cycle_hours, cycle_std, radiosensitivity, o2_to_vitality_factor, type = 'NormalCell'):
+        self.radius = radius
+        self.necrotic = False
+        self.usual_cycle_length = cycle_hours
+        self.cycle_length_std = cycle_std
+        self.doubling_time = self.random_doubling_time(self.usual_cycle_length, self.cycle_length_std) #new cells have a random doubling time
+        self.volume = 4/3 * np.pi * self.radius**3
+        self.oxygen = 1.0
+        self.radiosensitivity = radiosensitivity
+        self.o2_to_vitality_factor = o2_to_vitality_factor
+        if type != 'NormalCell' and type != 'TumorCell':
+            print(type + ' is not a valid cell type')
+            raise ValueError('Cell type must be either NormalCell or TumorCell')
+        self.type = type
+        self.time_before_death = None
+        self.time_spent_cycling = self.random_doubling_time(self.usual_cycle_length, self.cycle_length_std) #new cells have already been cycling for a random amount of time
+    def duplicate(self): #returns a new cell with the same properties
+        cell = Cell(self.radius, self.usual_cycle_length, self.cycle_length_std, self.radiosensitivity, self.o2_to_vitality_factor, self.type)
+        cell.time_spent_cycling = 0 #the new cell has not been cycling yet
+        return cell
+
+    def vitality(self):
+        factor = self.o2_to_vitality_factor
+        vitality = self.oxygen * factor
+        vitality = min(vitality, 1.0)
+        return vitality #needs to be normalized between 0 and 1
+
+    def radiosensitivity(self):
+        return self.radiosensitivity*self.oxygen
+
+    def random_doubling_time(self, mu = None, sigma = None):
+        if mu is None:
+            mu = self.usual_cycle_length
+        if sigma is None:
+            sigma = self.cycle_length_std
+        #return a sample of a gamma distribution with mean cycle_hours
+        shape = (mu / sigma) ** 2
+        scale = sigma ** 2 / mu
+        return np.random.gamma(shape = shape, scale = scale)
+

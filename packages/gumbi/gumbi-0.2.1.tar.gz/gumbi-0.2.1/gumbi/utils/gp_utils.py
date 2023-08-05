de0e59610c3bfac836cffb2ac38ca8fa038858bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:54 +0000

    Add package gumbi/0.2.1/gumbi-0.2.1.tar.gz

---
+++ packages/gumbi/gumbi-0.2.1.tar.gz/gumbi-0.2.1/gumbi/utils/gp_utils.py
@@ -0,0 +1,11 @@
+from scipy.spatial.distance import pdist
+
+
+def get_ℓ_prior(points):
+    distances = pdist(points[:, None])
+    distinct = distances != 0
+    ℓ_l = distances[distinct].min() if sum(distinct) > 0 else 0.1
+    ℓ_u = distances[distinct].max() if sum(distinct) > 0 else 1
+    ℓ_σ = max(0.1, (ℓ_u - ℓ_l) / 6)
+    ℓ_μ = ℓ_l + 3 * ℓ_σ
+    return ℓ_μ, ℓ_σ

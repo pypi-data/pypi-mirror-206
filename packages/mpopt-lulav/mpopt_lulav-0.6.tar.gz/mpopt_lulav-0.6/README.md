3ca03c957bbf14e3c383be281155a8888e7a7eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:45 +0000

    Add package mpopt-lulav/0.6/mpopt_lulav-0.6.tar.gz

---
+++ packages/mpopt-lulav/mpopt_lulav-0.6.tar.gz/mpopt_lulav-0.6/README.md
@@ -0,0 +1,12 @@
+# mpopt_lulav
+
+# TODO
+
+## publish CI/CD
+
+pushing the package to [PiPy](https://pypi.org/project/citros/)
+
+```python
+git tag -a "v0.05" -m "beta version testing"
+git push --tags
+```
---
+++ packages/mpopt-lulav/mpopt_lulav-0.6.tar.gz/mpopt_lulav-0.6/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'mpopt_lulav',   
+  packages = ['mpopt_lulav'], 
+  version = '0.6', 
+  license='MIT',   
+  description = 'Optimal control lib modified by Lulav Space', 
+  long_description_content_type="text/markdown",
+  long_description=open('README.md').read(),
+  author = 'gtep',   
+  author_email = 'gleb@lulav.space', 
+  url = 'https://github.com/lulav/mpopt_lulav',
+  download_url = 'https://github.com/lulav/mpopt_lulav/archive/refs/tags/v0.1.tar.gz', 
+  keywords = ['Optimal', 'Control', 'Lulav'],  
+  install_requires=[           
+          'numpy',
+          'casadi',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',    
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

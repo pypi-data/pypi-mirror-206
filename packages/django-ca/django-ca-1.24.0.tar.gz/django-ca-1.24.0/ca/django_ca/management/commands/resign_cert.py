f385740aff8b2bcb4eafa957b8584de5b32fc18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:26 +0000

    Add package django-ca/1.24.0/django-ca-1.24.0.tar.gz

---
+++ packages/django-ca/django-ca-1.24.0.tar.gz/django-ca-1.24.0/ca/django_ca/management/commands/resign_cert.py
@@ -0,0 +1,194 @@
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca. If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""Management command to resign an existing certificate.
+
+.. seealso:: https://docs.djangoproject.com/en/dev/howto/custom-management-commands/
+"""
+
+from datetime import timedelta
+from typing import Any, List, Optional
+
+from cryptography import x509
+from cryptography.x509.oid import ExtensionOID, NameOID
+
+from django.core.management.base import CommandError, CommandParser
+
+from django_ca import ca_settings
+from django_ca.constants import EXTENSION_KEYS
+from django_ca.management.actions import CertificateAction
+from django_ca.management.base import BaseSignCertCommand
+from django_ca.models import Certificate, CertificateAuthority, Watcher
+from django_ca.profiles import Profile, profiles
+from django_ca.typehints import AllowedHashTypes
+
+
+class Command(BaseSignCertCommand):  # pylint: disable=missing-class-docstring
+    help = f"""Sign a CSR and output signed certificate. The defaults depend on the configured
+default profile, currently {ca_settings.CA_DEFAULT_PROFILE}."""
+
+    add_extensions_help = "Override certificate extensions."
+    subject_help = "Override subject for new certificate."
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        self.add_base_args(parser, no_default_ca=True)
+        self.add_profile(parser, """Use given profile to determine certificate expiry.""")
+        parser.add_argument(
+            "cert", action=CertificateAction, allow_revoked=True, help="The certificate to resign."
+        )
+
+    def get_profile(self, profile: Optional[str], cert: Certificate) -> Profile:
+        """Get requested profile based on command line and given certificate."""
+        if profile is not None:
+            return profiles[profile]
+        if cert.profile == "":
+            return profiles[None]
+
+        try:
+            return profiles[cert.profile]
+        except KeyError:
+            # Occurs if the certificate specifies a profile which has since been removed from settings
+            raise CommandError(  # pylint: disable=raise-missing-from
+                f'Profile "{cert.profile}" for original certificate is no longer defined, please set one via the command line.'  # NOQA: E501
+            )
+
+    def handle(  # pylint: disable=too-many-arguments,too-many-locals,too-many-branches
+        self,
+        cert: Certificate,
+        ca: Optional[CertificateAuthority],
+        subject: Optional[x509.Name],
+        expires: Optional[timedelta],
+        watch: List[str],
+        password: Optional[bytes],
+        profile: Optional[str],
+        algorithm: Optional[AllowedHashTypes],
+        extended_key_usage: Optional[x509.ExtendedKeyUsage],
+        extended_key_usage_critical: bool,
+        key_usage: Optional[x509.KeyUsage],
+        key_usage_critical: bool,
+        ocsp_no_check: bool,
+        ocsp_no_check_critical: bool,
+        tls_feature: Optional[x509.TLSFeature],
+        tls_feature_critical: bool,
+        **options: Any,
+    ) -> None:
+        if ca is None:
+            ca = cert.ca
+
+        profile_obj = self.get_profile(profile, cert)
+        self.test_options(ca=ca, password=password, expires=expires, profile=profile_obj, **options)
+
+        # Get/validate signature hash algorithm
+        algorithm = self.get_hash_algorithm(ca.key_type, algorithm, ca.algorithm)
+
+        # get list of watchers
+        if watch:
+            watchers = [Watcher.from_addr(addr) for addr in watch]
+        else:
+            watchers = list(cert.watchers.all())
+
+        if subject is None:
+            subject = cert.subject
+
+        extensions: List[x509.Extension[x509.ExtensionType]] = []
+        have_san = False
+        for ext_type in self.sign_extensions:
+            if not options[EXTENSION_KEYS[ext_type.oid]]:
+                ext = cert.x509_extensions.get(ext_type.oid)
+            else:
+                ext = options[EXTENSION_KEYS[ext_type.oid]]
+
+            if ext is not None:
+                if ext_type == x509.SubjectAlternativeName:  # pragma: no branch
+                    have_san = True
+                extensions.append(ext)
+
+        if extended_key_usage is not None:
+            extensions.append(
+                x509.Extension(
+                    oid=ExtensionOID.EXTENDED_KEY_USAGE,
+                    critical=extended_key_usage_critical,
+                    value=extended_key_usage,
+                )
+            )
+        elif cert_extended_key_usage := cert.x509_extensions.get(ExtensionOID.EXTENDED_KEY_USAGE):
+            extensions.append(cert_extended_key_usage)
+
+        if key_usage is not None:
+            extensions.append(
+                x509.Extension(oid=ExtensionOID.KEY_USAGE, critical=key_usage_critical, value=key_usage)
+            )
+        elif cert_key_usage := cert.x509_extensions.get(ExtensionOID.KEY_USAGE):
+            extensions.append(cert_key_usage)
+
+        if ocsp_no_check is True:
+            extensions.append(
+                x509.Extension(
+                    oid=ExtensionOID.OCSP_NO_CHECK, critical=ocsp_no_check_critical, value=x509.OCSPNoCheck()
+                )
+            )
+        elif cert_ocsp_no_check := cert.x509_extensions.get(ExtensionOID.OCSP_NO_CHECK):
+            extensions.append(cert_ocsp_no_check)
+
+        if tls_feature is not None:
+            extensions.append(
+                x509.Extension(oid=ExtensionOID.TLS_FEATURE, critical=tls_feature_critical, value=tls_feature)
+            )
+        elif cert_tls_feature := cert.x509_extensions.get(ExtensionOID.TLS_FEATURE):
+            extensions.append(cert_tls_feature)
+
+        if not subject.get_attributes_for_oid(NameOID.COMMON_NAME) and have_san is False:
+            raise CommandError("Must give at least a CN in --subject or one or more --alt arguments.")
+
+        # Copy over extensions that are not handled above already:
+        for oid, extension in cert.x509_extensions.items():
+            if oid in (
+                ExtensionOID.KEY_USAGE,
+                ExtensionOID.OCSP_NO_CHECK,
+                ExtensionOID.EXTENDED_KEY_USAGE,
+                ExtensionOID.TLS_FEATURE,
+            ):
+                continue
+            if oid in (ext_type.oid for ext_type in self.sign_extensions):
+                continue
+            # These extensions is handled by the manager itself based on the CA:
+            if oid in (
+                ExtensionOID.AUTHORITY_INFORMATION_ACCESS,
+                ExtensionOID.AUTHORITY_KEY_IDENTIFIER,
+                ExtensionOID.CRL_DISTRIBUTION_POINTS,
+            ):
+                continue
+            extensions.append(extension)
+
+        try:
+            cert = Certificate.objects.create_cert(
+                ca=ca,
+                csr=cert.csr.loaded,
+                profile=profile_obj,
+                expires=expires,
+                subject=subject,
+                algorithm=algorithm,
+                extensions=extensions,
+                password=password,
+                cn_in_san=False,  # we already copy the SAN/CN from the original cert
+            )
+        except Exception as ex:
+            raise CommandError(ex) from ex
+
+        cert.watchers.add(*watchers)
+
+        if options["out"]:
+            with open(options["out"], "w", encoding="ascii") as stream:
+                stream.write(cert.pub.pem)
+        else:
+            self.stdout.write(cert.pub.pem)

95e90f9327946699484a088d8df99183db29d271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package croto/1.2/croto-1.2.tar.gz

---
+++ packages/croto/croto-1.2.tar.gz/croto-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='croto',
+    version='1.2',
+    license='MIT',
+    author="Defee",
+    author_email='fdiaznemeth@example.com',
+    packages=['src'],
+    url='https://github.com/Defeeeee/croto',
+    keywords='compiled language',
+
+)

61e5e782853ddc3b6058ea812a6ecb2d1f3c3185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:40 +0000

    Add package charmonium-cache/1.4.1/charmonium_cache-1.4.1-py3-none-any.whl

---
+++ packages/charmonium-cache/charmonium_cache-1.4.1-py3-none-any.whl/charmonium/cache/index.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+
+import enum
+from typing import Any, Callable, Dict, Generic, Iterable, Optional, TypeVar, cast, Union
+
+
+class IndexKeyType(enum.IntEnum):
+    MATCH = 0
+    LOOKUP = 1
+
+
+Key = TypeVar("Key")
+Val = TypeVar("Val")
+_T = TypeVar("_T")
+
+
+class Index(Generic[Key, Val]):
+    def __init__(
+        self,
+        schema: tuple[IndexKeyType, ...],
+        deleter: Optional[Callable[[tuple[tuple[Key, ...], Val]], None]] = None,
+    ) -> None:
+        self.schema = schema
+        self._data: dict[Key, Any] = {}
+        self._deleter = deleter
+
+    def __getstate__(self) -> dict[str, Any]:
+        return {
+            "schema": self.schema,
+            "_data": self._data,
+        }
+
+    def __setstate__(self, state: dict[str, Any]) -> None:
+        self.schema = state["schema"]
+        self._data = state["_data"]
+
+    @staticmethod
+    def _items(
+        data: dict[Key, Any], keys: tuple[Key, ...], max_depth: int
+    ) -> Iterable[tuple[tuple[Key, ...], Val]]:
+        # print("_items", keys, data)
+        if len(keys) == max_depth:
+            yield (keys, cast(Val, data))
+        else:
+            for key, subdict in data.items():
+                yield from Index._items(
+                    cast(Dict[Key, Any], subdict), keys + (key,), max_depth
+                )
+
+    def items(self) -> Iterable[tuple[tuple[Key, ...], Val]]:
+        yield from self._items(self._data, (), len(self.schema))
+
+    def _delete(self, data: dict[Key, Any], keys: tuple[Key, ...]) -> None:
+        if self._deleter:
+            for item in self._items(data, keys, len(self.schema)):
+                self._deleter(item)
+        data.clear()
+
+    def _get_last_level(
+        self, keys: tuple[Key, ...]
+    ) -> Optional[tuple[dict[Key, Val], Key, IndexKeyType]]:
+        if len(keys) != len(self.schema):
+            raise ValueError(
+                f"Keys {keys} should be the same len as schema ({len(self.schema)})"
+            )
+        obj = self._data
+        for key in keys[:-1]:
+            if key not in obj:
+                return None
+            obj = cast(Dict[Key, Any], obj[key])
+        return cast(Dict[Key, Val], obj), keys[-1], self.schema[-1]
+
+    def _get_or_create_last_level(
+        self,
+        keys: tuple[Key, ...],
+    ) -> tuple[dict[Key, Val], Key, IndexKeyType]:
+        if len(keys) != len(self.schema):
+            raise ValueError(
+                f"Keys {keys} should be the same len as schema ({len(self.schema)})"
+            )
+        obj = self._data
+        completed_keys: tuple[Key, ...] = ()
+        for key_type, key in zip(self.schema[:-1], keys[:-1]):
+            assert key_type != IndexKeyType.MATCH or len(obj) < 2
+            if key not in obj:
+                if key_type == IndexKeyType.MATCH:
+                    self._delete(obj, completed_keys)
+                obj[key] = cast(Dict[Key, Any], {})
+            completed_keys += (key,)
+            obj = cast(Dict[Key, Any], obj[key])
+        return cast(Dict[Key, Val], obj), keys[-1], self.schema[-1]
+
+    # TODO: refactor for less complexity
+    # Maybe combine get_or_create with get
+
+    def get_or(self, keys: tuple[Key, ...], thunk: Callable[[], Val]) -> Val:
+        last_level, last_key, last_key_type = self._get_or_create_last_level(keys)
+        if last_key not in last_level:
+            if last_key_type == IndexKeyType.MATCH:
+                self._delete(last_level, keys[:-1])
+            last_level[last_key] = thunk()
+        return last_level[last_key]
+
+    def __setitem__(self, keys: tuple[Key, ...], val: Val) -> None:
+        last_level, last_key, last_key_type = self._get_or_create_last_level(keys)
+        if last_key_type == IndexKeyType.MATCH:
+            self._delete(last_level, keys[:-1])
+        last_level[last_key] = val
+
+    def __delitem__(self, keys: tuple[Key, ...]) -> None:
+        result = self._get_last_level(keys)
+        if result:
+            last_level, last_key, _ = result
+            if last_key in last_level:
+                del last_level[last_key]
+
+    def __getitem__(self, keys: tuple[Key, ...]) -> Val:
+        result = self._get_last_level(keys)
+        if result:
+            last_level, last_key, _ = result
+            return last_level[last_key]
+        else:
+            raise KeyError(keys)
+
+    def get(self, keys: tuple[Key, ...], default: _T) -> Union[Val, _T]:
+        result = self._get_last_level(keys)
+        if result:
+            last_level, last_key, _ = result
+            return last_level.get(last_key, default)
+        else:
+            return default
+
+    def __contains__(self, keys: tuple[Key, ...]) -> bool:
+        if len(keys) != len(self.schema):
+            raise ValueError(
+                f"Keys {keys} should be the same len as schema ({len(self.schema)})"
+            )
+        result = self._get_last_level(keys)
+        if result:
+            last_level, last_key, _ = result
+            return last_key in last_level
+        else:
+            return False
+
+    def update(self, other: Index[Key, Val]) -> None:
+        if other.schema != self.schema:
+            raise ValueError(f"Schema mismatch {self.schema} != {other.schema}")
+        for key, val in other.items():
+            if key not in self:
+                self[key] = val
---
+++ packages/charmonium-cache/charmonium_cache-1.4.1-py3-none-any.whl/charmonium/cache/util.py
@@ -0,0 +1,147 @@
+from __future__ import annotations
+
+import tempfile
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Callable, Generic, Optional, TypeVar, Union, cast
+
+_T = TypeVar("_T")
+
+# Thanks Eric Traut
+# https://github.com/microsoft/pyright/discussions/1763#discussioncomment-617220
+if TYPE_CHECKING:
+    # ParamSpec is just for type checking, hence pylint disable
+    from typing_extensions import ParamSpec  # pylint: disable=no-name-in-module
+
+    FuncParams = ParamSpec("FuncParams")
+else:
+    FuncParams = TypeVar("FuncParams")
+
+FuncReturn = TypeVar("FuncReturn")
+
+
+class Constant(Generic[FuncParams, FuncReturn]):
+    def __init__(self, val: FuncReturn):
+        self.val = val
+
+    def __repr__(self) -> str:
+        return f"Constant({self.val!r})"
+
+    def __call__(
+        self, *args: FuncParams.args, **kwargs: FuncParams.kwargs
+    ) -> FuncReturn:
+        return self.val
+
+
+class Future(Generic[_T]):
+    def __init__(self, thunk: Callable[[], _T]) -> None:
+        self._thunk = thunk
+        self._value: Optional[_T] = None
+        self.computed = False
+
+    def unwrap(self) -> _T:
+        if not self.computed:
+            self.computed = True
+            self._value = self._thunk()
+        return cast(_T, self._value)
+
+    def __getattr__(self, this_attr: str) -> Any:
+        return getattr(self.unwrap(), this_attr)
+
+    @classmethod
+    def create(cls, thunk: Callable[[], _T]) -> _T:
+        return cast(_T, cls(thunk))
+
+
+class GetAttr(Generic[_T]):
+    """When you want to getattr or use a default, with static types.
+
+    Example: ``obj_hash = GetAttr[Callable[[], int]]()(obj, "__hash__", lambda: hash(obj))()``
+
+    """
+
+    error = object()
+
+    def __init__(self) -> None:
+        ...
+
+    def __call__(
+        self,
+        obj: object,
+        attr_name: str,
+        default: Union[_T, object] = error,
+        check_callable: bool = False,
+    ) -> _T:
+        if hasattr(obj, attr_name):
+            attr_val = getattr(obj, attr_name)
+            if check_callable and not hasattr(attr_val, "__call__"):
+                raise TypeError(
+                    f"GetAttr expected ({obj!r}).{attr_name} to be callable, but it is {type(attr_val)}."
+                )
+            else:
+                return cast(_T, attr_val)
+        elif default is not GetAttr.error:
+            return cast(_T, default)
+        else:
+            raise AttributeError(
+                f"{obj!r}.{attr_name} does not exist, and no default was provided"
+            )
+
+
+def identity(obj: _T) -> _T:
+    return obj
+
+
+def none_tuple(obj: Any) -> tuple[Any, ...]:
+    if obj is not None:
+        return (obj,)
+    else:
+        return ()
+
+
+# class DontPickle(Generic[_T]):
+#     def __init__(self, init: str) -> None:
+#         self.__setstate__(init)
+
+#     def __getstate__(self) -> Any:
+#         return self.init
+
+#     def __setstate__(self, init: str) -> None:
+#         self.init = init
+#         self.obj = cast(_T, eval(self.init))
+
+#     def __getattr__(self, attrib: str) -> Any:
+#         return getattr(self.obj, attrib)
+
+#     @staticmethod
+#     def create(init: str) -> _T:
+#         return cast(_T, DontPickle[_T](init))
+
+
+def with_attr(obj: _T, attr_name: str, attr_val: Any) -> _T:
+    object.__setattr__(obj, attr_name, attr_val)
+    return obj
+
+
+def temp_path(
+    suffix: Optional[str] = None,
+    prefix: Optional[str] = None,
+    directory: Optional[Union[str, Path]] = None,
+) -> Path:
+    # TODO: Remove this function.
+    temp_dir = Path(
+        tempfile.TemporaryDirectory(
+            suffix=suffix, prefix=prefix, dir=directory
+        ).name  # pylint: disable=consider-using-with
+    )
+    return temp_dir
+
+
+def ellipsize(string: str, size: int, ellipsis: str = "...") -> str:
+    if size < 5:
+        raise ValueError("Size is too small")
+    elif len(string) < size:
+        return string
+    else:
+        break_point = (size - len(ellipsis)) // 2
+        odd = int((size - len(ellipsis)) % 2)
+        return string[: (break_point + odd)] + ellipsis + string[-break_point:]

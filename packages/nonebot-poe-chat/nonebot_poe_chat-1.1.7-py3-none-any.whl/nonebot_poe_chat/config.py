d5dc0093f8ba3b451c3109597db187a876b5e263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:00 +0000

    Add package nonebot-poe-chat/1.1.7/nonebot_poe_chat-1.1.7-py3-none-any.whl

---
+++ packages/nonebot-poe-chat/nonebot_poe_chat-1.1.7-py3-none-any.whl/nonebot_poe_chat/config.py
@@ -0,0 +1,126 @@
+import json
+import os
+import nonebot
+from nonebot import logger
+from pathlib import Path    
+def check_cookie(self):
+    if os.path.exists(self.cookie_path):
+        try:
+            with open(self.cookie_path, 'r') as f:
+                cookie = json.load(f)
+                if all(key in cookie for key in ('domain', 'name', 'path')) and 'value' in cookie:
+                    if cookie['domain'] == 'poe.com' and cookie['name'] == 'p-b' and cookie['path'] == '/':
+                        return True
+        except:
+            pass
+    try:
+        if not os.path.exists(self.cookie_path):
+            with open(self.cookie_path, 'w') as f:
+                f.write('{}')
+                logger.info('poe_cookie.json 创建成功')
+        poe_ck = nonebot.get_driver().config.poe_cookie
+        cookie_parms = {
+            "domain": "poe.com",
+            "name": "p-b",
+            "path": "/",
+            "value": f"{poe_ck}"
+        }
+        is_env_ck = True
+    except:
+        is_env_ck = False
+
+    if is_env_ck:
+        with open(self.cookie_path, 'w') as f:
+            json.dump(cookie_parms, f)
+        return True
+    return False
+
+
+class Config:
+    def __init__(self):
+        self.path_ = str(Path()) + "/data/poe_chat"
+        self.user_path = str(self.path_ + r'/user_dict.json')
+        self.prompt_path = str(self.path_ + r'/poe_prompt.json')
+        self.cookie_path = str(self.path_ + r'/poe_cookie.json')
+        self.superuser_dict_path = str(self.path_ + r'/superuser_dict.json')
+        self.pic_able = None
+        self.url_able = "True"
+        self.qr_able = "True"
+        self.suggest_able = "True"
+        self.num_limit = 350
+        self.server = None
+        self.username = None
+        self.passwd = None
+        self.superuser_dict = {}
+        self.superusers = []
+        self.blacklist = []
+        self.whitelist = []
+        self.mode = "black"
+        self.cookie_dict = {}
+        self.user_dict = {}
+        self.prompts_dict = {}
+        self.is_cookie_exists = check_cookie(self)
+        if self.is_cookie_exists:
+            with open(self.cookie_path, 'r') as f:
+                cookies = json.load(f)
+                self.cookie_dict =cookies
+                
+        get_config = nonebot.get_driver().config
+
+        self.superusers = get_config.poe_superusers if hasattr(get_config, 'poe_superusers') else []
+        self.blacklist = get_config.poe_blacklist if hasattr(get_config, 'poe_blacklist') else []
+        self.whitelist = get_config.poe_whitelist if hasattr(get_config, 'poe_whitelist') else self.superusers
+        self.accesslist = get_config.poe_accesslist if hasattr(get_config, 'poe_accesslist') else self.superusers
+        self.mode = get_config.poe_mode if hasattr(get_config, 'poe_mode') and get_config.poe_mode in ['white', 'black'] else 'black'
+
+        self.pic_able = getattr(get_config, 'poe_picable', None)
+        self.url_able = getattr(get_config, 'poe_urlable', "True")
+        self.suggest_able = getattr(get_config, 'poe_suggestable', "True")
+        self.qr_able = getattr(get_config, 'poe_qrable', "True")
+
+        self.server = getattr(get_config, 'poe_server', None)
+        self.username = getattr(get_config, 'poe_username', None)
+        self.passwd = getattr(get_config, 'poe_passwd', None)
+        self.num_limit = int(getattr(get_config, 'poe_limit', '350'))
+        if not os.path.exists(self.user_path):
+            # 获取目录路径
+            dir_path = os.path.dirname(self.user_path)
+            # 如果目录不存在，则创建目录
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            with open(self.user_path, 'w') as f:
+                f.write('{}')
+                logger.info('user_dict.json 创建成功')
+        try:
+            with open(self.user_path, 'r') as f:
+                self.user_dict = json.load(f)
+        except:
+            pass
+        
+        if not os.path.exists(self.prompt_path):
+            # 获取目录路径
+            dir_path = os.path.dirname(self.prompt_path)
+            # 如果目录不存在，则创建目录
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            with open(self.prompt_path, 'w') as f:
+                f.write('{"\u9ed8\u8ba4": "\u4e00\u4e2a\u667a\u80fd\u52a9\u7406", "\u732b\u5a18": "\u73b0\u5728\u4f60\u5c06\u6a21\u4eff\u4e00\u53ea\u732b\u5a18\uff0c\u4e0e\u6211\u5bf9\u8bdd\u6bcf\u4e00\u53e5\u8bdd\u540e\u9762\u90fd\u8981\u52a0\u4e0a\u201c\u55b5\u201d\uff0c\u5982\u679c\u4f60\u80fd\u660e\u767d\u6211\u7684\u610f\u601d\uff0c\u8bf7\u56de\u590d\u201c\u55b5~\u4f60\u597d\u4e3b\u4eba\u201d"}')
+                logger.info('prompt_dict.json 创建成功')
+            
+        
+        with open(self.prompt_path, 'r') as f:
+            self.prompts_dict = json.load(f)
+            
+        if not os.path.exists(self.superuser_dict_path):
+            # 获取目录路径
+            dir_path = os.path.dirname(self.superuser_dict_path)
+            # 如果目录不存在，则创建目录
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            with open(self.superuser_dict_path, 'w') as f:
+                f.write('{"auto_default":"\u9ed8\u8ba4","blacklist":[]}')
+                logger.info('superuser_dict.json 创建成功')            
+                        
+        with open(self.superuser_dict_path, 'r') as f:
+            self.superuser_dict = json.load(f)
+
---
+++ packages/nonebot-poe-chat/nonebot_poe_chat-1.1.7-py3-none-any.whl/nonebot_poe_chat/poe_func.py
@@ -0,0 +1,172 @@
+from typing import Union
+import re, uuid, asyncio,string,random
+import aiohttp
+import qrcode
+from .config import Config
+from nonebot import logger, require
+require("nonebot_plugin_guild_patch")
+from nonebot_plugin_guild_patch import GuildMessageEvent
+from nonebot.adapters.onebot.v11 import Message,MessageEvent,MessageSegment
+from .txt2img import Txt2Img
+config = Config()
+txt2img = Txt2Img()
+is_suggest_able = config.suggest_able
+is_pic_able = config.pic_able
+is_url_able = config.url_able
+is_qr_able = config.qr_able
+num_limit = config.num_limit
+def reply_out(event: MessageEvent, content: Union[MessageSegment,Message,str,bytes]) -> Message:
+    """返回一个回复消息"""
+    if isinstance(event, GuildMessageEvent):
+        return Message(content)
+    if type(content) == bytes:
+        return MessageSegment.reply(event.message_id) + MessageSegment.image(content)
+    if content[0:9] == 'base64://':
+        return MessageSegment.reply(event.message_id) + MessageSegment.image(content)
+    return MessageSegment.reply(event.message_id) + content
+
+#生成一个由qq号和nickname共同决定的uuid作为真名，防止重名
+def generate_uuid(s) ->str:
+    # 将字符串转换为 UUID 对象
+    uuid_object = uuid.uuid3(uuid.NAMESPACE_DNS, s)
+    # 获取 UUID 对象的 bytes 值
+    uuid_bytes = uuid_object.bytes
+    # 将 bytes 值转换为字符串
+    uuid_str = uuid_bytes.hex()[:14]
+    return uuid_str
+
+def generate_random_string(length=8) -> str:
+    """生成指定长度的随机字符串"""
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
+def is_email(email) -> bool:
+    pattern = r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(pattern, email))
+
+async def delete_messages(bot, user_id:str, dict_list:dict):
+    if user_id in dict_list:
+        if isinstance(dict_list[user_id], list):
+            for eachmsg in dict_list[user_id]:
+                await bot.delete_msg(message_id=eachmsg['message_id'])
+            del dict_list[user_id]
+        else:
+            await bot.delete_msg(message_id=dict_list[user_id]['message_id'])
+
+async def mdlink_2_str(md_text):
+    result = []
+    pattern = r'\[([^\]]+)\]\(([^)]+)\)(\s*-\s*([^[]+))?' # 匹配超链接及其描述
+    matches = re.findall(pattern, md_text)
+    for i, match in enumerate(matches):
+        # 提取标题
+        title = match[0].strip() if not match[3] else match[3].strip()
+        # 组合字符串
+        result.append(f"{i+1}. {title} - {match[1].strip()}")
+
+    output_str = '\n\n'.join(result)
+    return output_str
+
+def is_useable(event,mode = config.mode):
+    if mode == "black":
+        try:
+            if str(event.user_id) in config.blacklist:
+                return False
+        except:
+            pass
+        try:
+            if str(event.group_id) in config.blacklist:
+                return False
+        except:
+            pass
+        return True
+    elif mode == "white":
+        try:
+            if str(event.user_id) in config.whitelist:
+                return True
+        except:
+            pass
+        try:
+            if str(event.group_id) in config.whitelist:
+                return True
+        except:
+            pass
+        return False
+    
+def is_vip(event):
+        try:
+            if str(event.user_id) in config.accesslist:
+                return True
+        except:
+            pass
+        try:
+            if str(event.group_id) in config.accesslist:
+                return True
+        except:
+            pass
+        return False
+async def close_page(page):
+    try:
+        await page.close()
+    except:
+        pass
+async def send_msg(result, matcher, event):
+    msgid_container = {}
+    suggest_container = []
+    async def send_message(msg):
+        if is_pic_able is not None:
+            if is_pic_able == 'True':
+                if is_qr_able == 'True':
+                    pic, url = await txt2img.draw(title=" ", text=msg)
+                    if is_url_able == 'True':
+                        msgid_container = await matcher.send(reply_out(event, pic)+MessageSegment.text(url))
+                    else:
+                        msgid_container = await matcher.send(reply_out(event, pic))
+                else:
+                    pic, _ = await txt2img.draw(title=" ", text=msg)
+                    msgid_container = await matcher.send(reply_out(event, pic))
+            else:
+                    msgid_container = await matcher.send(reply_out(event, msg))
+        else:
+            if len(msg) > num_limit:
+                if is_qr_able == 'True':
+                    pic, url = await txt2img.draw(title=" ", text=msg)
+                    if is_url_able == 'True':
+                        msgid_container = await matcher.send(reply_out(event, pic)+MessageSegment.text(url))
+                    else:
+                        msgid_container = await matcher.send(reply_out(event, pic))
+                else:
+                    pic, _ = await txt2img.draw(title=" ", text=msg)
+                    msgid_container = await matcher.send(reply_out(event, pic))
+            else:
+                msgid_container = await matcher.send(reply_out(event, msg))
+        return msgid_container
+    
+    if isinstance(result, tuple):
+        last_answer, suggest_container = result
+        is_successful = True
+    elif isinstance(result, str):
+        if "banned" == result:
+            await matcher.send(reply_out(event, '你的机器人被banned了，请/pc新建一个机器人，并且不要在使用此预设'))
+            matcher.finish()
+            return msgid_container,[]
+        elif "limited" == result:
+            await matcher.send(reply_out(event, '今日免费限额已用尽，请订阅或等明天再使用'))
+            matcher.finish()
+            return msgid_container,[]
+    elif isinstance(result, bool):
+        is_successful = result
+    else:
+        raise ValueError("Unexpected return type from get_message_async")
+        
+    if is_successful:
+        suggest_str = '\n'.join([f"{i+1}: {s}" for i, s in enumerate(suggest_container)])
+        if is_suggest_able == 'True':
+            msg = f"{last_answer}\n\n建议回复：\n{suggest_str}"
+        else:
+            msg = f"{last_answer}\n"
+        
+        msgid_container = await send_message(msg)
+        return msgid_container,suggest_container
+    else:
+        await matcher.send(reply_out(event, "出错了，多次出错请联系机器人管理员"))
+        return msgid_container,suggest_container

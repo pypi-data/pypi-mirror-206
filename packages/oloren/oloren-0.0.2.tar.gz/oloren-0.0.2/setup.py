fef8cad202de25e069bb8200a0ac1d2b30e72d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:56 +0000

    Add package oloren/0.0.2/oloren-0.0.2.tar.gz

---
+++ packages/oloren/oloren-0.0.2.tar.gz/oloren-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oloren",
+    version="0.0.2",
+    packages=find_packages(exclude=("examples", "frontend")),
+    python_requires=">=3.6",
+    install_requires=[
+        "flask",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True,
+)

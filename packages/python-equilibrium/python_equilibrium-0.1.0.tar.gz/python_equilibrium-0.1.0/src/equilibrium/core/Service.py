4e12759071ac7318aa3d1b1950a9c57d9fe3c0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:33 +0000

    Add package python-equilibrium/0.1.0/python_equilibrium-0.1.0.tar.gz

---
+++ packages/python-equilibrium/python_equilibrium-0.1.0.tar.gz/python_equilibrium-0.1.0/src/equilibrium/core/Service.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar, NewType, TypeVar
+
+from equilibrium.core.Resource import Resource
+from equilibrium.core.ResourceStore import ResourceStore
+
+
+def validate_service_id(s: str) -> None:
+    try:
+        Resource.Type.of(s)
+    except ValueError:
+        raise ValueError(f"Invalid serviceId: {s!r}")
+
+
+class Service(ABC):
+    """
+    Base class for services that can be registered to a context and retrieved by controllers. Services are pluggable
+    components that support various actions no resources.
+    """
+
+    Id = NewType("Id", str)
+    T = TypeVar("T", bound="Service")
+
+    #: A globally unique identifier of the service type. The identifier is used to retrieve the service from the
+    #: context. This must begin with an apiVersion and end with a kind, separated by a slash. The apiVersion and kind
+    #: must be valid DNS subdomains.
+    SERVICE_ID: ClassVar[Id] = Id("")
+
+    #: Assigned to the service upon registration to the context.
+    resources: ResourceStore
+    services: Service.Provider
+
+    class Provider(ABC):
+        @abstractmethod
+        def get(self, resource_type: Resource.Type, service_type: type[Service.T]) -> Service.T | None:
+            ...
+
+    def __init_subclass__(cls, serviceId: str, **kwargs: Any) -> None:
+        validate_service_id(serviceId)
+        cls.SERVICE_ID = cls.Id(serviceId)
+        return super().__init_subclass__(**kwargs)

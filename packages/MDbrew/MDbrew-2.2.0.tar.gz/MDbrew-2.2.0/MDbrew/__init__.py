0bf6980437fd0fb15fc5ed0a9f94589e483df6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:29 +0000

    Add package MDbrew/2.2.0/MDbrew-2.2.0.tar.gz

---
+++ packages/MDbrew/MDbrew-2.2.0.tar.gz/MDbrew-2.2.0/MDbrew/__init__.py
@@ -0,0 +1,13 @@
+from .analysis.msd import MSD
+from .analysis.rdf import RDF
+from .brewery import Brewery
+from .application.brewcp2k import BrewCP2K
+
+
+
+
+
+
+__version__ = "2.2.0"
+__name__ = "MDbrew"
+

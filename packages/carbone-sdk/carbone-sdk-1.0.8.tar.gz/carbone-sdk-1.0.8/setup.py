ed589839e24f1030a3da0d8b078f0158424c9245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:08 +0000

    Add package carbone-sdk/1.0.8/carbone-sdk-1.0.8.tar.gz

---
+++ packages/carbone-sdk/carbone-sdk-1.0.8.tar.gz/carbone-sdk-1.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="carbone-sdk",
+    version="1.0.8",
+    author="CarboneIO",
+    author_email="support@carbone.io",
+    description="Carbone API Python SDK to generate documents (PDF, docx, xlsx, ods, odt, ...) from a JSON and a template.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carboneio/carbone-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='Apache-2.0'
+)

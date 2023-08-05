7b5e372f5e92ee0d5a7bab2d858b53d75cd6a462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:26 +0000

    Add package CatRoyale/1.22/CatRoyale-1.22.tar.gz

---
+++ packages/CatRoyale/CatRoyale-1.22.tar.gz/CatRoyale-1.22/cat_royale/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from cat_royale.main import main_func as main
+
+if __name__ == "__main__":
+    print("Starting CatRoyale...")
+    main()
---
+++ packages/CatRoyale/CatRoyale-1.22.tar.gz/CatRoyale-1.22/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="CatRoyale",
+    version="1.22",
+    description="A cat themed battle royale game",
+    author="Tarsoly Barnabás",
+    author_email="tarsoly.barnabas2002@gmail.com",
+    url="https://github.com/Iseroo/Python-kotprog",
+    packages=["cat_royale"],
+    install_requires=["pygame", "webcolors", "pillow", "ordered_set"],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'CatRoyale = cat_royale.main:main_func'
+        ]
+    },
+
+)

492c3ea4b97c33a251a00c4d63c866891e51673c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:35 +0000

    Add package imod/0.13.0/imod-0.13.0.tar.gz

---
+++ packages/imod/imod-0.13.0.tar.gz/imod-0.13.0/imod/mf6/sto.py
@@ -0,0 +1,261 @@
+import abc
+import pathlib
+
+import numpy as np
+
+from imod.mf6.pkgbase import Package
+from imod.mf6.validation import PKG_DIMS_SCHEMA
+from imod.schemata import (
+    AllValueSchema,
+    DTypeSchema,
+    IdentityNoDataSchema,
+    IndexesSchema,
+)
+
+
+class Storage(Package):
+    _pkg_id = "sto_deprecated"
+
+    def __init__(*args, **kwargs):
+        raise NotImplementedError(
+            r"Storage package has been removed. Use SpecificStorage or StorageCoefficient instead."
+        )
+
+
+class StorageBase(Package, abc.ABC):
+    def _render_dict(self, directory, pkgname, globaltimes, binary):
+        d = {}
+        stodirectory = pathlib.Path(directory.stem) / pkgname
+        for varname in self._grid_data:
+            key = self._keyword_map.get(varname, varname)
+            layered, value = self._compose_values(
+                self[varname], stodirectory, key, binary=binary
+            )
+            if self._valid(value):  # skip False or None
+                d[f"{key}_layered"], d[key] = layered, value
+
+        periods = {}
+        if "time" in self.dataset["transient"].coords:
+            package_times = self.dataset["transient"].coords["time"].values
+            starts = np.searchsorted(globaltimes, package_times) + 1
+            for i, s in enumerate(starts):
+                periods[s] = self.dataset["transient"].isel(time=i).values[()]
+        else:
+            periods[1] = self.dataset["transient"].values[()]
+
+        d["periods"] = periods
+        return d
+
+
+class SpecificStorage(StorageBase):
+    """
+    Storage Package with specific storage.
+
+    From wikipedia (https://en.wikipedia.org/wiki/Specific_storage):
+
+    "The specific storage is the amount of water that a portion of an aquifer
+    releases from storage, per unit mass or volume of aquifer, per unit change
+    in hydraulic head, while remaining fully saturated."
+
+    If the STO Package is not included for a model, then storage changes will
+    not be calculated, and thus, the model will be steady state. Only one STO
+    Package can be specified for a GWF model.
+
+    Parameters
+    ----------
+    specific_storage: array of floats (xr.DataArray)
+        Is specific storage. Specific storage values must be greater than
+        or equal to 0. (ss)
+    specific_yield: array of floats (xr.DataArray)
+        Is specific yield. Specific yield values must be greater than or
+        equal to 0. Specific yield does not have to be specified if there are no
+        convertible cells (convertible=0 in every cell). (sy)
+    convertible: array of int (xr.DataArray)
+        Is a flag for each cell that specifies whether or not a cell is
+        convertible for the storage calculation. 0 indicates confined storage is
+        used. >0 indicates confined storage is used when head is above cell top
+        and a mixed formulation of unconfined and confined storage is used when
+        head is below cell top. (iconvert)
+    transient: ({True, False})
+        Boolean to indicate if the model is transient or steady-state.
+    """
+
+    _pkg_id = "sto"
+    _grid_data = {
+        "convertible": np.int32,
+        "specific_storage": np.float64,
+        "specific_yield": np.float64,
+    }
+    _keyword_map = {
+        "specific_storage": "ss",
+        "specific_yield": "sy",
+        "convertible": "iconvert",
+    }
+
+    _init_schemata = {
+        "convertible": [
+            DTypeSchema(np.integer),
+            IndexesSchema(),
+            PKG_DIMS_SCHEMA,
+        ],
+        "specific_storage": [
+            DTypeSchema(np.floating),
+            IndexesSchema(),
+            PKG_DIMS_SCHEMA,
+        ],
+        "specific_yield": [
+            DTypeSchema(np.floating),
+            IndexesSchema(),
+            PKG_DIMS_SCHEMA,
+        ],
+    }
+
+    _write_schemata = {
+        "convertible": (
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+            # No need to check coords: dataset ensures they align with idomain.
+        ),
+        "specific_storage": (
+            AllValueSchema(">=", 0.0),
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+        ),
+        "specific_yield": (
+            AllValueSchema(">=", 0.0),
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+        ),
+    }
+
+    _template = Package._initialize_template(_pkg_id)
+
+    def __init__(
+        self,
+        specific_storage,
+        specific_yield,
+        transient,
+        convertible,
+        validate: bool = True,
+    ):
+        super().__init__(locals())
+        self.dataset["specific_storage"] = specific_storage
+        self.dataset["specific_yield"] = specific_yield
+        self.dataset["convertible"] = convertible
+        self.dataset["transient"] = transient
+
+        self._validate_init_schemata(validate)
+
+    def render(self, directory, pkgname, globaltimes, binary):
+        d = self._render_dict(directory, pkgname, globaltimes, binary)
+        return self._template.render(d)
+
+
+class StorageCoefficient(StorageBase):
+    """
+    Storage Package with a storage coefficient.  Be careful,
+    this is not the same as the specific storage.
+
+    From wikipedia (https://en.wikipedia.org/wiki/Specific_storage):
+
+    "Storativity or the storage coefficient is the volume of water released
+    from storage per unit decline in hydraulic head in the aquifer, per
+    unit area of the aquifer.  Storativity is a dimensionless quantity, and
+    is always greater than 0.
+
+    Under confined conditions:
+
+    S = Ss * b, where S is the storage coefficient,
+    Ss the specific storage, and b the aquifer thickness.
+
+    Under unconfined conditions:
+
+    S = Sy, where Sy is the specific yield"
+
+    If the STO Package is not included for a model, then storage changes will
+    not be calculated, and thus, the model will be steady state. Only one STO
+    Package can be specified for a GWF model.
+
+    Parameters
+    ----------
+    storage_coefficient: array of floats (xr.DataArray)
+        Is storage coefficient. Storage coefficient values must be greater than
+        or equal to 0. (ss)
+    specific_yield: array of floats (xr.DataArray)
+        Is specific yield. Specific yield values must be greater than or
+        equal to 0. Specific yield does not have to be specified if there are no
+        convertible cells (convertible=0 in every cell). (sy)
+    convertible: array of int (xr.DataArray)
+        Is a flag for each cell that specifies whether or not a cell is
+        convertible for the storage calculation. 0 indicates confined storage is
+        used. >0 indicates confined storage is used when head is above cell top
+        and a mixed formulation of unconfined and confined storage is used when
+        head is below cell top. (iconvert)
+    transient: ({True, False})
+        Boolean to indicate if the model is transient or steady-state.
+    validate: {True, False}
+        Flag to indicate whether the package should be validated upon
+        initialization. This raises a ValidationError if package input is
+        provided in the wrong manner. Defaults to True.
+    """
+
+    _pkg_id = "sto"
+    _grid_data = {
+        "convertible": np.int32,
+        "storage_coefficient": np.float64,
+        "specific_yield": np.float64,
+    }
+    _keyword_map = {
+        "storage_coefficient": "ss",
+        "specific_yield": "sy",
+        "convertible": "iconvert",
+    }
+
+    _init_schemata = {
+        "convertible": [
+            DTypeSchema(np.integer),
+            PKG_DIMS_SCHEMA,
+        ],
+        "storage_coefficient": [
+            DTypeSchema(np.floating),
+            PKG_DIMS_SCHEMA,
+        ],
+        "specific_yield": [
+            DTypeSchema(np.floating),
+            PKG_DIMS_SCHEMA,
+        ],
+    }
+
+    _write_schemata = {
+        "convertible": (
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+            # No need to check coords: dataset ensures they align with idomain.
+        ),
+        "storage_coefficient": (
+            AllValueSchema(">=", 0.0),
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+        ),
+        "specific_yield": (
+            AllValueSchema(">=", 0.0),
+            IdentityNoDataSchema(other="idomain", is_other_notnull=(">", 0)),
+        ),
+    }
+
+    _template = Package._initialize_template(_pkg_id)
+
+    def __init__(
+        self,
+        storage_coefficient,
+        specific_yield,
+        transient,
+        convertible,
+        validate: bool = True,
+    ):
+        super().__init__(locals())
+        self.dataset["storage_coefficient"] = storage_coefficient
+        self.dataset["specific_yield"] = specific_yield
+        self.dataset["convertible"] = convertible
+        self.dataset["transient"] = transient
+        self._validate_init_schemata(validate)
+
+    def render(self, directory, pkgname, globaltimes, binary):
+        d = self._render_dict(directory, pkgname, globaltimes, binary)
+        d["storagecoefficient"] = True
+        return self._template.render(d)
---
+++ packages/imod/imod-0.13.0.tar.gz/imod-0.13.0/imod/tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from .fixtures.flow_basic_fixture import (
+    basic_dis,
+    get_render_dict,
+    horizontal_flow_barrier_gdf,
+    metaswap_dict,
+    three_days,
+    two_days,
+    well_df,
+)
+from .fixtures.flow_example_fixture import imodflow_model
+from .fixtures.mf6_circle_fixture import circle_model, circle_result
+from .fixtures.mf6_flow_with_transport_fixture import (
+    bulk_density_fc,
+    concentration_fc,
+    conductance_fc,
+    decay_fc,
+    decay_sorbed_fc,
+    distcoef_fc,
+    elevation_fc,
+    flow_model_with_concentration,
+    head_fc,
+    porosity_fc,
+    proportion_depth_fc,
+    proportion_rate_fc,
+    rate_fc,
+    sp2_fc,
+)
+from .fixtures.mf6_lake_package_fixture import ijsselmeer, lake_package, naardermeer
+from .fixtures.mf6_twri_disv_fixture import twri_disv_model
+from .fixtures.mf6_twri_fixture import transient_twri_model, twri_model, twri_result
+from .fixtures.mf6_welltest_fixture import (
+    well_test_data_stationary,
+    well_test_data_transient,
+)
+from .fixtures.msw_fixture import fixed_format_parser
+from .fixtures.msw_model_fixture import coupled_mf6_model, msw_model

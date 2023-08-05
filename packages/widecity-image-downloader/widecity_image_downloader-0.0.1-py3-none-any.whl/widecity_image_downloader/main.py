35aa6dce3cdd9289b0202376c68f89ba441dd1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:05 +0000

    Add package widecity-image-downloader/0.0.1/widecity_image_downloader-0.0.1-py3-none-any.whl

---
+++ packages/widecity-image-downloader/widecity_image_downloader-0.0.1-py3-none-any.whl/widecity_image_downloader/main.py
@@ -0,0 +1,8 @@
+from simple_image_download import simple_image_download
+
+response = simple_image_download.Downloader
+keywords = ['flowers']
+
+for kw in keywords:
+    response().download(kw,2)
+    

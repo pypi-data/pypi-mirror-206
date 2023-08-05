b77162eca2ec7d3d773af28e41fadc352cea53f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:26 +0000

    Add package odoo12-addon-beesdoo-base/12.0.2.0.1.99.dev1/odoo12_addon_beesdoo_base-12.0.2.0.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-beesdoo-base/odoo12_addon_beesdoo_base-12.0.2.0.1.99.dev1-py3-none-any.whl/odoo/addons/beesdoo_base/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+#   - Robin Keunen <robin@coopiteasy.be>
+#   - Houssine bakkali <houssine@coopiteasy.be>
+#   - Rémy Taymans <remy@coopiteasy.be>
+#   - Elise Dupont
+#   - Thibault François
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "BEES coop Base Module",
+    "summary": "Emptied",
+    "author": "Beescoop - Cellule IT, Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Sales",
+    "version": "12.0.2.0.1",
+    "depends": [
+        "company_supplier_context",
+        "member_card",
+        "eater",
+        "eater_member_card",
+    ],
+    "data": [],
+    "installable": True,
+    "license": "AGPL-3",
+}

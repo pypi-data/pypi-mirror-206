8db88727f77bc41dfd16f79a94019a6e0bc2f818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:04 +0000

    Add package warn-transformer/1.3.42/warn_transformer-1.3.42-py3-none-any.whl

---
+++ packages/warn-transformer/warn_transformer-1.3.42-py3-none-any.whl/warn_transformer/transformers/nm.py
@@ -0,0 +1,19 @@
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform New Mexico raw data for consolidation."""
+
+    postal_code = "NM"
+    fields = dict(
+        company="JOB SITE NAME",
+        location="CITY NAME",
+        notice_date="NOTICE DATE",
+        effective_date="LAYOFF DATE",
+        jobs="TOTAL LAYOFF NUMBER",
+    )
+    date_format = ("%d-%b-%Y", "%d-%b-%y", "%m/%d/%Y", "%m/%d/%y")
+    jobs_corrections = {
+        "Not Disclosed": None,
+        "?": None,
+    }

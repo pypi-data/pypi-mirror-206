b8654ae68e94bd352fd0ea6e6b7c5f0e64b60a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:45 +0000

    Add package PennyLane/0.30.0/PennyLane-0.30.0.tar.gz

---
+++ packages/PennyLane/PennyLane-0.30.0.tar.gz/PennyLane-0.30.0/tests/conftest.py
@@ -0,0 +1,288 @@
+# Copyright 2018-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Pytest configuration file for PennyLane test suite.
+"""
+import os
+import pathlib
+
+import numpy as np
+import pytest
+
+import pennylane as qml
+from pennylane.devices import DefaultGaussian
+
+# defaults
+TOL = 1e-3
+TF_TOL = 2e-2
+TOL_STOCHASTIC = 0.05
+
+
+class DummyDevice(DefaultGaussian):
+    """Dummy device to allow Kerr operations"""
+
+    _operation_map = DefaultGaussian._operation_map.copy()
+    _operation_map["Kerr"] = lambda *x, **y: np.identity(2)
+
+
+@pytest.fixture(scope="session")
+def tol():
+    """Numerical tolerance for equality tests."""
+    return float(os.environ.get("TOL", TOL))
+
+
+@pytest.fixture(scope="session")
+def tol_stochastic():
+    """Numerical tolerance for equality tests of stochastic values."""
+    return TOL_STOCHASTIC
+
+
+@pytest.fixture(scope="session")
+def tf_tol():
+    """Numerical tolerance for equality tests."""
+    return float(os.environ.get("TF_TOL", TF_TOL))
+
+
+@pytest.fixture(scope="session", params=[1, 2])
+def n_layers(request):
+    """Number of layers."""
+    return request.param
+
+
+@pytest.fixture(scope="session", params=[2, 3])
+def n_subsystems(request):
+    """Number of qubits or qumodes."""
+    return request.param
+
+
+@pytest.fixture(scope="session")
+def qubit_device(n_subsystems):
+    return qml.device("default.qubit", wires=n_subsystems)
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qubit_device_1_wire(request):
+    return qml.device("default.qubit", wires=1, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qubit_device_2_wires(request):
+    return qml.device("default.qubit", wires=2, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qubit_device_3_wires(request):
+    return qml.device("default.qubit", wires=3, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+"""The following 3 fixtures are for default.qutrit devices to be used for testing with various
+real and complex dtypes."""
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qutrit_device_1_wire(request):
+    return qml.device("default.qutrit", wires=1, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qutrit_device_2_wires(request):
+    return qml.device("default.qutrit", wires=2, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+@pytest.fixture(scope="function", params=[(np.float32, np.complex64), (np.float64, np.complex128)])
+def qutrit_device_3_wires(request):
+    return qml.device("default.qutrit", wires=3, r_dtype=request.param[0], c_dtype=request.param[1])
+
+
+@pytest.fixture(scope="session")
+def gaussian_device(n_subsystems):
+    """Number of qubits or modes."""
+    return DummyDevice(wires=n_subsystems)
+
+
+@pytest.fixture(scope="session")
+def gaussian_dummy():
+    """Gaussian device with dummy Kerr gate."""
+    return DummyDevice
+
+
+@pytest.fixture(scope="session")
+def gaussian_device_2_wires():
+    """A 2-mode Gaussian device."""
+    return DummyDevice(wires=2)
+
+
+@pytest.fixture(scope="session")
+def gaussian_device_4modes():
+    """A 4 mode Gaussian device."""
+    return DummyDevice(wires=4)
+
+
+############### Package Support ##########################
+
+
+@pytest.fixture(scope="session")
+def dask_support():
+    """Boolean fixture for dask support"""
+    try:
+        import dask
+
+        dask_support = True
+    except ImportError as e:
+        dask_support = False
+
+    return dask_support
+
+
+@pytest.fixture()
+def skip_if_no_dask_support(dask_support):
+    if not dask_support:
+        pytest.skip("Skipped, no dask support")
+
+
+#######################################################################
+
+
+@pytest.fixture(scope="module", params=[1, 2, 3])
+def seed(request):
+    """Different seeds."""
+    return request.param
+
+
+@pytest.fixture(scope="function")
+def mock_device(monkeypatch):
+    """A mock instance of the abstract Device class"""
+
+    with monkeypatch.context() as m:
+        dev = qml.Device
+        m.setattr(dev, "__abstractmethods__", frozenset())
+        m.setattr(dev, "short_name", "mock_device")
+        m.setattr(dev, "capabilities", lambda cls: {"model": "qubit"})
+        m.setattr(dev, "operations", {"RX", "RY", "RZ", "CNOT", "SWAP"})
+        yield qml.Device(wires=2)
+
+
+@pytest.fixture
+def tear_down_hermitian():
+    yield None
+    qml.Hermitian._eigs = {}
+
+
+@pytest.fixture
+def tear_down_thermitian():
+    yield None
+    qml.THermitian._eigs = {}
+
+
+#######################################################################
+
+try:
+    import tensorflow as tf
+except (ImportError, ModuleNotFoundError) as e:
+    tf_available = False
+else:
+    tf_available = True
+
+try:
+    import torch
+    from torch.autograd import Variable
+
+    torch_available = True
+except ImportError as e:
+    torch_available = False
+
+try:
+    import jax
+    import jax.numpy as jnp
+
+    jax_available = True
+except ImportError as e:
+    jax_available = False
+
+
+def pytest_collection_modifyitems(items, config):
+    rootdir = pathlib.Path(config.rootdir)
+    for item in items:
+        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
+        if "qchem" in rel_path.parts:
+            mark = getattr(pytest.mark, "qchem")
+            item.add_marker(mark)
+        if "legacy" in rel_path.parts:
+            mark = getattr(pytest.mark, "legacy")
+            item.add_marker(mark)
+        if "finite_diff" in rel_path.parts:
+            mark = getattr(pytest.mark, "finite-diff")
+            item.add_marker(mark)
+        if "parameter_shift" in rel_path.parts:
+            mark = getattr(pytest.mark, "param-shift")
+            item.add_marker(mark)
+
+    # Tests that do not have a specific suite marker are marked `core`
+    for item in items:
+        markers = {mark.name for mark in item.iter_markers()}
+        if (
+            not any(
+                elem
+                in [
+                    "autograd",
+                    "torch",
+                    "tf",
+                    "jax",
+                    "qchem",
+                    "qcut",
+                    "all_interfaces",
+                    "legacy",
+                    "finite-diff",
+                    "param-shift",
+                ]
+                for elem in markers
+            )
+            or not markers
+        ):
+            item.add_marker(pytest.mark.core)
+
+
+def pytest_runtest_setup(item):
+    """Automatically skip tests if interfaces are not installed"""
+    # Autograd is assumed to be installed
+    interfaces = {"tf", "torch", "jax"}
+    available_interfaces = {
+        "tf": tf_available,
+        "torch": torch_available,
+        "jax": jax_available,
+    }
+
+    allowed_interfaces = [
+        allowed_interface
+        for allowed_interface in interfaces
+        if available_interfaces[allowed_interface] is True
+    ]
+
+    # load the marker specifying what the interface is
+    all_interfaces = {"tf", "torch", "jax", "all_interfaces"}
+    marks = {mark.name for mark in item.iter_markers() if mark.name in all_interfaces}
+
+    for b in marks:
+        if b == "all_interfaces":
+            required_interfaces = {"tf", "torch", "jax"}
+            for interface in required_interfaces:
+                if interface not in allowed_interfaces:
+                    pytest.skip(
+                        f"\nTest {item.nodeid} only runs with {allowed_interfaces} interfaces(s) but {b} interface provided",
+                    )
+        else:
+            if b not in allowed_interfaces:
+                pytest.skip(
+                    f"\nTest {item.nodeid} only runs with {allowed_interfaces} interfaces(s) but {b} interface provided",
+                )

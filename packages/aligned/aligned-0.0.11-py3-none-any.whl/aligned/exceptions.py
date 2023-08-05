aadf63b3a4bab3bb19a12517f2c46eb82804d4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:03 +0000

    Add package aligned/0.0.11/aligned-0.0.11-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.11-py3-none-any.whl/aligned/exceptions.py
@@ -0,0 +1,11 @@
+class UnableToFindFileException(Exception):
+    pass
+
+
+class CombinedFeatureViewQuerying(Exception):
+    pass
+
+
+class NotSupportedYet(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(f'{message}. What about contributing and adding a PR for this?')

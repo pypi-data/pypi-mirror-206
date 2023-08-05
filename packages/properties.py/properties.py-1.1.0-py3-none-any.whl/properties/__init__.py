74cfe98e6fcbc8c8afbd580772d19d0f2a696939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:30 +0000

    Add package properties.py/1.1.0/properties.py-1.1.0-py3-none-any.whl

---
+++ packages/properties.py/properties.py-1.1.0-py3-none-any.whl/properties/__init__.py
@@ -0,0 +1,10 @@
+from .properties import (
+    loads,
+    load,
+    dump,
+    dumps,
+    loads_tree,
+    load_tree,
+    dumps_tree,
+    dump_tree
+)
---
+++ packages/properties.py/properties.py-1.1.0-py3-none-any.whl/properties/functional.py
@@ -0,0 +1,46 @@
+import re
+from typing import Any, Union, Iterable, TypeVar, List, Dict, Type
+
+T, NT = TypeVar("T"), TypeVar("NT")
+
+def is_comment_line(line: str) -> bool:
+    c = 0
+    for i in line:
+        if i != " ":
+            if (i == "#") and (c == 0): return True
+            else: c += 1
+    return False
+
+def is_pass_line(line: str) -> bool:
+    if "=" not in line:
+        return True
+    elif line.replace(" ", "") == "":
+        return True
+    return False
+
+def stendel(text: str): return re.sub(r"^\s+|\n|\r|\s+$", '', text)
+
+def conv(value: str) -> Union[Any, str]:
+    try: return eval(stendel(value))
+    except: pass
+    return stendel(value)
+
+def removes(l: Iterable[Union[T, NT]], d: Iterable[NT]) -> List[T]:
+    dl = []
+    for i in l:
+        if i not in d:
+            dl.append(i)
+    return dl
+
+def recursive_search_keys(data: Dict[str, Any]) -> List[List[str]]:
+    keys_paths = []
+    for key, value in data.items():
+        if isinstance(value, dict):
+            for key_path in recursive_search_keys(value):
+                keys_paths.append([key, *key_path])
+        else:
+            keys_paths.append([key])
+    return keys_paths
+
+def getter(key_path: List[str], data: Dict[str, Any]) -> Any:
+    return eval(f"data{''.join(['['+repr(key)+']' for key in key_path])}")

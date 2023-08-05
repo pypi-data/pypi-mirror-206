fd8b3d0015458c7ad32a20a5676fd5ce33ec6173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:54 +0000

    Add package anylabeling/0.2.16/anylabeling-0.2.16.tar.gz

---
+++ packages/anylabeling/anylabeling-0.2.16.tar.gz/anylabeling-0.2.16/setup.py
@@ -0,0 +1,80 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version():
+    """Get package version from app_info.py file"""
+    filename = "anylabeling/app_info.py"
+    with open(filename, encoding="utf-8") as f:
+        match = re.search(
+            r"""^__version__ = ['"]([^'"]*)['"]""", f.read(), re.M
+        )
+    if not match:
+        raise RuntimeError(f"{filename} doesn't contain __version__")
+    version = match.groups()[0]
+    return version
+
+
+def get_install_requires():
+    """Get python requirements based on context"""
+    install_requires = [
+        "imgviz>=0.11",
+        "natsort>=7.1.0",
+        "numpy",
+        "Pillow>=2.8",
+        "PyYAML",
+        "termcolor",
+        "opencv-python-headless",
+        'PyQt5>=5.15.7; platform_system != "Darwin"',
+        "onnx==1.13.1",
+        'onnxruntime==1.14.1; platform_system == "Darwin"',
+        'onnxruntime-gpu==1.14.1; platform_system != "Darwin"',
+        "qimage2ndarray==1.10.0",
+    ]
+
+    if os.name == "nt":  # Windows
+        install_requires.append("colorama")
+
+    return install_requires
+
+
+def get_long_description():
+    """Read long description from README"""
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name="anylabeling",
+    version=get_version(),
+    packages=find_packages(),
+    description="Effortless data labeling with AI support",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Viet-Anh Nguyen",
+    author_email="vietanh.dev@gmail.com",
+    url="https://github.com/vietanhdev/anylabeling",
+    install_requires=get_install_requires(),
+    license="GPLv3",
+    keywords="Image Annotation, Machine Learning, Deep Learning",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "anylabeling=anylabeling.app:main",
+        ],
+    },
+)

d8dfd4c651e21cf3b75bf513671ad64521dbcf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:04 +0000

    Add package CS333-Final-Project/0.0.1/CS333-Final-Project-0.0.1.tar.gz

---
+++ packages/CS333-Final-Project/CS333-Final-Project-0.0.1.tar.gz/CS333-Final-Project-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'CS333 Final Project'
+LONG_DESCRIPTION = 'CS333 Testing and DevOps Final Project @ University of Nevada, Reno.'
+
+# Setting up
+setup(
+    name="CS333-Final-Project",
+    version=VERSION,
+    author="Colin Martires",
+    author_email="cmartires@nevada.unr.edu",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix"
+    ]
+)

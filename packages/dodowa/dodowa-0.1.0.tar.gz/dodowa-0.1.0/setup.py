e6ee02fce29a0626e619c02496a03007edcaa5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:04 +0000

    Add package dodowa/0.1.0/dodowa-0.1.0.tar.gz

---
+++ packages/dodowa/dodowa-0.1.0.tar.gz/dodowa-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="dodowa",
+    version="0.1.0",
+    author="dodojalil",
+    author_email="dodogmaicom@gmail.com",
+    description="A Python library for test",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    platforms=["Windows", "Linux", "Android"],
+    python_requires=">=3.2",
+)

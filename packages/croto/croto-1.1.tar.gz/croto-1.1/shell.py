b9dfec275e24491c1b9647d4622df6cbcab924e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package croto/1.1/croto-1.1.tar.gz

---
+++ packages/croto/croto-1.1.tar.gz/croto-1.1/shell.py
@@ -0,0 +1,9 @@
+from src import croto
+
+while True:
+    text = input('croto > ')
+    result, error = croto.run('<stdin>', text)
+    if error:
+        print(error.as_string())
+    else:
+        print(result)

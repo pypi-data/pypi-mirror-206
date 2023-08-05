c54d889c4fc680d6ede69332f417daa4ac606bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:58 +0000

    Add package pathao-courier-api/1.0.0/pathao-courier-api-1.0.0.tar.gz

---
+++ packages/pathao-courier-api/pathao-courier-api-1.0.0.tar.gz/pathao-courier-api-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Api client for Pathao Courier API'
+LONG_DESCRIPTION = 'A package that allows to perform api operations on Pathao Courier'
+
+# Setting up
+setup(
+    name="pathao-courier-api",
+    version=VERSION,
+    author="Omar Faruk Riyad",
+    author_email="<omfriyad@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'pathao', 'courier', 'api'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ]
+)

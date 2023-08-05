ff5cb03227aa56345c7271f8c6bb59d9da074c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:37 +0000

    Add package dynamicfluency/0.1.2/dynamicfluency-0.1.2.tar.gz

---
+++ packages/dynamicfluency/dynamicfluency-0.1.2.tar.gz/dynamicfluency-0.1.2/src/dynamicfluency/model_data.py
@@ -0,0 +1,107 @@
+import sys
+from typing import Set
+import nltk
+import spacy
+
+VALID_LANGUAGES = {
+    "zh": "Chinese",
+    "hr": "Croatian",
+    "nl": "Dutch",
+    "en": "English",
+    "fi": "Finnish",
+    "de": "German",
+    "it": "Italian",
+    "ko": "Korean",
+    "lt": "Lithuanian",
+    "pl": "Polish",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sv": "Swedish",
+}
+
+NLTK_TAGGERS = {
+    "en": "eng",
+    "ru": "rus",
+}
+
+SPACY_MODELS = {
+    "zh": "zh_core_web_sm",
+    "hr": "hr_core_news_sm",
+    "nl": "nl_core_news_sm",
+    "en": "en_core_web_sm",
+    "fi": "fi_core_news_sm",
+    "de": "de_core_news_sm",
+    "it": "it_core_news_sm",
+    "ko": "ko_core_news_sm",
+    "lt": "lt_core_news_sm",
+    "pl": "pl_core_news_sm",
+    "ro": "ro_core_news_sm",
+    "sv": "sv_core_news_sm",
+}
+
+NLTK_POS_TAGS = {
+    "CC",
+    "CD",
+    "DT",
+    "EX",
+    "FW",
+    "IN",
+    "JJ",
+    "JJR",
+    "JJS",
+    "LS",
+    "MD",
+    "NN",
+    "NNS",
+    "NNP",
+    "NNPS",
+    "PDT",
+    "WRB",
+    "WP$",
+    "WP",
+    "WDT",
+    "VBZ",
+    "VBP",
+    "VBN",
+    "VBG",
+    "VBD",
+    "VB",
+    "UH",
+    "TO",
+    "RP",
+    "RBS",
+    "RB",
+    "RBR",
+    "PRP",
+    "PRP$",
+}
+
+
+def assert_valid_language(lang: str) -> None:
+    "A simple helper function to raise when non-valid language string is passed"
+
+    if lang not in VALID_LANGUAGES.keys():
+        raise ValueError(f"lang must be one of: {VALID_LANGUAGES.keys()}")
+
+
+def load_spacy_model(model: str):
+    try:
+        return spacy.load(model)
+    except OSError:
+        spacy.cli.download(model)
+        return spacy.load(model)
+
+
+def load_nltk_model(model: str):
+    if model == "rus":
+        nltk.download("averaged_perceptron_tagger_ru", quiet=True, halt_on_error=True)
+    else:
+        nltk.download("averaged_perceptron_tagger", quiet=True, halt_on_error=True)
+
+
+def get_valid_tags(lang: str) -> Set[str]:
+    assert_valid_language(lang)
+    if lang in NLTK_TAGGERS.keys():
+        return NLTK_POS_TAGS
+    else:
+        return {*spacy.glossary.GLOSSARY.keys()}

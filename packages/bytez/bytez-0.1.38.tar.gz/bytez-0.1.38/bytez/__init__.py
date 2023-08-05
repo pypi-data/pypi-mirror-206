941768dab568fbb33cf3e782fc5a0dbc14febf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:31 +0000

    Add package bytez/0.1.38/bytez-0.1.38.tar.gz

---
+++ packages/bytez/bytez-0.1.38.tar.gz/bytez-0.1.38/bytez/__init__.py
@@ -0,0 +1,2 @@
+from dataclasses import dataclass
+from bytez.exports import model, task
---
+++ packages/bytez/bytez-0.1.38.tar.gz/bytez-0.1.38/bytez/exports.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from bytez.tasks.super_resolution import SuperResolutionModels
+from bytez.tasks.style_transfer import StyleTransferModels
+from bytez.task_list import task_list
+import re
+
+
+def concat_non_alphanumeric(arr):
+    updated_arr = []
+    for item in arr:
+        updated_item = re.sub(r'\W+', '_', item)
+        updated_arr.append(updated_item)
+    return updated_arr
+
+# TODO make this dynamically import from the tasks modules
+
+
+# Define base class for dynamically generated attributes
+@dataclass
+class Task:
+    pass
+
+
+for task_name in concat_non_alphanumeric(task_list):
+    setattr(Task, task_name, None)
+
+
+@dataclass
+class model:
+    holmes_alan_dsrvae = SuperResolutionModels.holmes_alan_dsrvae
+    cmd_style_transfer = StyleTransferModels.cmd_style_transfer
+    fast_style_transfer = StyleTransferModels.fast_style_transfer
+    tensorflow_fast_style = StyleTransferModels.tensorflow_fast_style
+
+
+@dataclass
+class task:
+    super_resolution = SuperResolutionModels
+    style_transfer = StyleTransferModels

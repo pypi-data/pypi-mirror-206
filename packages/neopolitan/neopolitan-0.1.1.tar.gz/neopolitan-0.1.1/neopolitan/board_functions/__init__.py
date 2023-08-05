4953a8bcff3529844118fa5b422b7dda90e9b9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:01 +0000

    Add package neopolitan/0.1.1/neopolitan-0.1.1.tar.gz

---
+++ packages/neopolitan/neopolitan-0.1.1.tar.gz/neopolitan-0.1.1/neopolitan/board_functions/__init__.py
@@ -0,0 +1,4 @@
+"""Init board_functions module"""
+# didn't solve
+# print('importing colors, display, board, board_display from board_functions')
+# from . import colors, display, board, board_display
---
+++ packages/neopolitan/neopolitan-0.1.1.tar.gz/neopolitan-0.1.1/neopolitan/display/abstract_display.py
@@ -0,0 +1,24 @@
+"""
+Handles initialization of the framework
+that will allow LEDs to be displayed
+
+This means initializing the pygame or neopixel libraries
+"""
+
+from abc import ABC, abstractmethod
+
+class Display(ABC):
+    """Wrapper code for initializing a display framework"""
+    def __init__(self):
+        self.should_exit = False # todo??
+
+    def __del__(self):
+        pass
+
+    @abstractmethod
+    def draw(self):
+        """Initiate the drawing of the board data"""
+
+    @abstractmethod
+    def loop(self):
+        """Execution loop; used for drawing"""

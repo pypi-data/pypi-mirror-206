b7e55aa63e5e9de96159502495785821e7a5f0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:20 +0000

    Add package hexo-inpainting/1.0.2/hexo_inpainting-1.0.2.tar.gz

---
+++ packages/hexo-inpainting/hexo_inpainting-1.0.2.tar.gz/hexo_inpainting-1.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open('hexo_inpainting/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+ name='hexo_inpainting',
+ version='1.0.2',
+ author="Hexo.ai",
+ author_email="saurabh@hexo.ai",
+ description="This is an image editing tool",
+ packages=setuptools.find_packages(),
+ install_requires=required,
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)

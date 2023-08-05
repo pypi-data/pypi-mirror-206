bbc08e0e0f47c5e92dad64c64a9f3694f6e9dcae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package pynamer/0.5.1/pynamer-0.5.1.tar.gz

---
+++ packages/pynamer/pynamer-0.5.1.tar.gz/pynamer-0.5.1/tests/test_pynamer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tests for package cookietest.py
+To use tests either:
+    1 - Use pip to install package as "editable"
+            pip install -e .
+    2 - Import pathmagic.py to enable tests to find the package
+"""
+# Third party modules
+
+# First party modules
+from pynamer import pynamer

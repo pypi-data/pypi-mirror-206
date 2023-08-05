c0166f56ec72ccaa8694d82102532a5f92116bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:22 +0000

    Add package robotControl/1.6/robotControl-1.6.tar.gz

---
+++ packages/robotControl/robotControl-1.6.tar.gz/robotControl-1.6/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='robotControl',
+      version='1.6',
+      description='A simple calculator module',
+      author='Carl',
+      packages=['robotControl'])

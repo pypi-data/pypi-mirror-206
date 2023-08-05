4f62f6801e64bc81ec9698bffa22bbfedc6355f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:43 +0000

    Add package pysui-sdk/1.0.7/pysui-sdk-1.0.7.tar.gz

---
+++ packages/pysui-sdk/pysui-sdk-1.0.7.tar.gz/pysui-sdk-1.0.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    author="shawngmy",
+    author_email="shawngmy@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    include_package_data=True,
+    install_requires=["httpx", "pynacl"],
+    name="pysui-sdk",
+    packages=["pysui_sdk"],
+    url="https://github.com/satisfywithmylife/sui_python_sdk",
+    python_requires=">=3.7",
+    version="1.0.7",
+)

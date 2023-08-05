0b1290affcbdf5305f18a150e5b2b3575159bfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:24 +0000

    Add package poetry-git-version-plugin/0.1.4.dev2/poetry_git_version_plugin-0.1.4.dev2-py3-none-any.whl

---
+++ packages/poetry-git-version-plugin/poetry_git_version_plugin-0.1.4.dev2-py3-none-any.whl/poetry_git_version_plugin/exceptions.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+from poetry_git_version_plugin import config
+
+
+class PluginException(RuntimeError):
+    def __str__(self) -> str:
+        if isinstance(self.args[0], BaseException):
+            ex = self.args[0]
+            message = ex.__class__.__name__
+            if ex.args:
+                message = f'{message}: {ex.args}'
+
+        else:
+            message = '; '.join(self.args)
+
+        return f'<b>{config.PLUGIN_NAME}</b>: {message}'
+
+
+def plugin_exception_wrapper(func: Callable):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except PluginException as ex:
+            raise ex
+
+        except BaseException as ex:
+            raise PluginException(ex) from ex
+
+    return wrapper

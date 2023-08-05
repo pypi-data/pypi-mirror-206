318439db6264090d922de8d98411663b75e20d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:57 +0000

    Add package utils-easy/0.0.2/utils_easy-0.0.2.tar.gz

---
+++ packages/utils-easy/utils_easy-0.0.2.tar.gz/utils_easy-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="utils_easy",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)

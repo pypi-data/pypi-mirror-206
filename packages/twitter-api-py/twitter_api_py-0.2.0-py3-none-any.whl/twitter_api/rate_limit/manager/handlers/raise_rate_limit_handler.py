30d78fd66b1577b679467eaff658749d81b77ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:50 +0000

    Add package twitter-api-py/0.2.0/twitter_api_py-0.2.0-py3-none-any.whl

---
+++ packages/twitter-api-py/twitter_api_py-0.2.0-py3-none-any.whl/twitter_api/rate_limit/manager/handlers/raise_rate_limit_handler.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager, contextmanager
+from typing import AsyncGenerator, Generator
+
+from twitter_api.error import RateLimitOverError
+from twitter_api.rate_limit.manager.rate_limit_manager import RateLimitManager
+from twitter_api.rate_limit.types.rate_limit_info import RateLimitInfo
+
+
+class RaiseRateLimitHandler(RateLimitManager):
+    """
+    レートリミットオーバーが発生した場合、例外を投げるもっとも単純な handler。
+    """
+
+    @contextmanager
+    def handle_rate_limit_sync(
+        self, rate_limit_info: RateLimitInfo
+    ) -> Generator[None, None, None]:
+        """
+        同期的な TwitterApiClient を用いている場合のレートリミットの処理方法。
+        """
+
+        if self.check_limit_over(rate_limit_info) is not None:
+            raise RateLimitOverError(rate_limit_info)
+
+        yield
+
+    @asynccontextmanager
+    async def handle_rate_limit_async(
+        self, rate_limit_info: RateLimitInfo
+    ) -> AsyncGenerator[None, None]:
+        """
+        非同期的な TwitterApiAsyncClient を用いている場合のレートリミットの処理方法。
+        """
+
+        if self.check_limit_over(rate_limit_info) is not None:
+            raise RateLimitOverError(rate_limit_info)
+
+        yield
---
+++ packages/twitter-api-py/twitter_api_py-0.2.0-py3-none-any.whl/twitter_api/rate_limit/manager/handlers/sleep_rate_limit_handler.py
@@ -0,0 +1,73 @@
+import asyncio
+import random
+import time
+from abc import ABCMeta
+from contextlib import asynccontextmanager, contextmanager
+from logging import getLogger
+
+from twitter_api.error import TwitterApiErrorCode, TwitterApiResponseFailed
+from twitter_api.rate_limit.manager.rate_limit_manager import RateLimitManager
+from twitter_api.rate_limit.types.rate_limit_info import RateLimitInfo
+from twitter_api.warning import RateLimitOverWarning, UnmanagedRateLimitOverWarning
+
+logger = getLogger(__file__)
+
+
+class SleepRateLimitHandler(RateLimitManager, metaclass=ABCMeta):
+    """
+    レートリミットに遭遇した場合、スリープする handler。
+    """
+
+    def random_sleep_seconds(self) -> float:
+        """
+        予期しないレートリミットに遭遇した場合にランダムに休む時間[秒]。
+        """
+
+        # デフォルトは 5 ~ 15 分。
+        return random.randint(5 * 60, 15 * 60)
+
+    @contextmanager
+    def handle_rate_limit_sync(self, rate_limit_info: RateLimitInfo):
+        while True:
+            # レートリミットを超えてしまっていたら、必要な待ち時間分だけ待つ。
+            wait_time_seconds = self.check_limit_over(rate_limit_info)
+            if wait_time_seconds is not None:
+                logger.warning(RateLimitOverWarning(rate_limit_info))
+                time.sleep(wait_time_seconds)
+                continue
+
+            try:
+                yield
+            except TwitterApiResponseFailed as error:
+                # レートリミット以外のエラーなら上流に投げる。
+                if error.status_code != TwitterApiErrorCode.TooManyRequests.value:
+                    raise error
+
+                # 予期しないレートリミットに遭遇した場合、投機的な待機を行う
+                logger.warning(UnmanagedRateLimitOverWarning())
+                time.sleep(self.random_sleep_seconds())
+            else:
+                return
+
+    @asynccontextmanager
+    async def handle_rate_limit_async(self, rate_limit_info: RateLimitInfo):
+        while True:
+            # レートリミットを超えてしまっていたら、必要な待ち時間分だけ待つ。
+            wait_time_seconds = self.check_limit_over(rate_limit_info)
+            if wait_time_seconds is not None:
+                logger.warning(RateLimitOverWarning(rate_limit_info))
+                await asyncio.sleep(wait_time_seconds)
+                continue
+
+            try:
+                yield
+            except TwitterApiResponseFailed as error:
+                # レートリミットのエラーでないなら上流に投げる。
+                if error.status_code != TwitterApiErrorCode.TooManyRequests.value:
+                    raise error
+
+                # 予期しないレートリミットに遭遇した場合、投機的な待機を行う
+                logger.warning(UnmanagedRateLimitOverWarning())
+                await asyncio.sleep(self.random_sleep_seconds())
+            else:
+                return

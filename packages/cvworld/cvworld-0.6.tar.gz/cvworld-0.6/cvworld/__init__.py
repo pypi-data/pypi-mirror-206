f38ab16b1cfa2d17d0629e8fbd9c2cecb4713322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package cvworld/0.6/cvworld-0.6.tar.gz

---
+++ packages/cvworld/cvworld-0.6.tar.gz/cvworld-0.6/cvworld/__init__.py
@@ -0,0 +1,4 @@
+from cvworld.Utils import cornerRect, edge, putTextRect, \
+    canny, blur, rotateImage, line, circle
+from cvworld.HandTrachkingModule import HandDetector
+from cvworld.FaceTrackingModule import FaceDetector

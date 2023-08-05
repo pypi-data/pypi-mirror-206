b3ad14642d5046654ade13ba667de8651bf3f791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:21 +0000

    Add package odoo12-addon-cooperator-worker/12.0.2.0.1/odoo12_addon_cooperator_worker-12.0.2.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-cooperator-worker/odoo12_addon_cooperator_worker-12.0.2.0.1-py3-none-any.whl/odoo/addons/cooperator_worker/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Cooperator Worker Configuration",
+    "summary": """Working and shopping configuration based on Share product""",
+    "author": "BEES coop, Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Cooperative management",
+    "version": "12.0.2.0.1",
+    "depends": [
+        "shift",
+        "cooperator",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "demo": ["demo/product_share.xml"],
+    "license": "AGPL-3",
+}

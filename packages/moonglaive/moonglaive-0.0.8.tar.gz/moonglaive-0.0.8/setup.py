6334facca98a9855f45f1c21adb80610e81544fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:02 +0000

    Add package moonglaive/0.0.8/moonglaive-0.0.8.tar.gz

---
+++ packages/moonglaive/moonglaive-0.0.8.tar.gz/moonglaive-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='moonglaive',
+    version='0.0.8',
+    description='Three-bladed weapon of the night elf Sentinels',
+    url='https://github.com/MalfurionWhitehat/moonglaive',
+    author='MalfurionWhitehat',
+    author_email='MalfurionWhitehat@proton.me',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'typing',
+        'datetime',
+        'python-dateutil',
+        'python-Levenshtein',
+        'fuzzywuzzy',
+        'requests',
+        'argparse',
+        'tabulate'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)

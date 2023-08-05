60c51034ca1d0d0a14b109754417e91dbaf07f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:16 +0000

    Add package swiftshadow/0.2.2/swiftshadow-0.2.2.tar.gz

---
+++ packages/swiftshadow/swiftshadow-0.2.2.tar.gz/swiftshadow-0.2.2/swiftshadow/helpers.py
@@ -0,0 +1,37 @@
+from swiftshadow.constants import CountryCodes
+from requests import get
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - [swiftshadow] - %(levelname)s - %(message)s", level=20
+)
+
+
+def getCountryCode(countryName):
+    try:
+        return CountryCodes[countryName]
+    except KeyError:
+        for name in list(CountryCodes.keys()):
+            if countryName in name:
+                return CountryCodes[name]
+
+
+def checkProxy(proxy, countries):
+    if countries != []:
+        if proxy[-1].upper() not in countries:
+            return False
+    proxyDict = {proxy[1]: proxy[0]}
+    try:
+        resp = get(f"{proxy[1]}://ipinfo.io/ip", proxies=proxyDict, timeout=2).text
+        if resp.count(".") == 3:
+            return True
+        return False
+    except Exception as e:
+        # log('error',str(e))
+        return False
+
+
+def log(level, message):
+    level = level.upper()
+    levels = {"INFO": 20, "WARNING": 30, "ERROR": 40}
+    logging.log(levels[level], message)

8ea50f4ebe04a9ea15ffd4f764440db847ce6732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package bow-cli/1.0.0/bow_cli-1.0.0-py3-none-any.whl

---
+++ packages/bow-cli/bow_cli-1.0.0-py3-none-any.whl/src/bow/cli/commands/list.py
@@ -0,0 +1,24 @@
+import click
+from rich.console import Console
+from rich.table import Table
+
+from bow.cli.config import templates
+
+
+@click.command()
+def list():
+    """List of all available templates."""
+    console = Console()
+    table = Table(title="All available templates")
+
+    table.add_column("Name")
+    table.add_column("Requires")
+    table.add_column("URL")
+
+    for ent in templates:
+        temp = templates[ent]
+        requires = ", ".join(temp["requires"])
+        table.add_row(ent, requires, temp["url"])
+
+    console = Console()
+    console.print(table)

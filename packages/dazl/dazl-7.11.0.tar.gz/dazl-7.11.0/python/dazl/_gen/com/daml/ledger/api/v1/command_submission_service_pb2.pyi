9f633b3c7839fc7971c48159cf18a94345551ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package dazl/7.11.0/dazl-7.11.0.tar.gz

---
+++ packages/dazl/dazl-7.11.0.tar.gz/dazl-7.11.0/python/dazl/_gen/com/daml/ledger/api/v1/command_submission_service_pb2.pyi
@@ -0,0 +1,28 @@
+# Copyright (c) 2017-2023 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# fmt: off
+# isort: skip_file
+
+import builtins as _builtins, sys, typing as _typing
+
+from google.protobuf.message import Message as _Message
+
+from .commands_pb2 import Commands
+
+if sys.version_info >= (3, 8):
+    from typing import Literal as _L
+else:
+    from typing_extensions import Literal as _L
+
+__all__ = [
+    "SubmitRequest",
+]
+
+
+class SubmitRequest(_Message):
+    @property
+    def commands(self) -> Commands: ...
+    def __init__(self, *, commands: _typing.Optional[Commands] = ...): ...
+    def HasField(self, field_name: _L["commands"]) -> _builtins.bool: ...
+    def ClearField(self, field_name: _L["commands"]) -> None: ...
+    def WhichOneof(self, oneof_group: _typing.NoReturn) -> _typing.NoReturn: ...

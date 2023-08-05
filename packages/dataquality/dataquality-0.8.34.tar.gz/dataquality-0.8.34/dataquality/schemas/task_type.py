23d48231fdc3eed243019749b56a6fbf82379874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:29 +0000

    Add package dataquality/0.8.34/dataquality-0.8.34.tar.gz

---
+++ packages/dataquality/dataquality-0.8.34.tar.gz/dataquality-0.8.34/dataquality/schemas/task_type.py
@@ -0,0 +1,30 @@
+from enum import Enum, unique
+from typing import List
+
+
+@unique
+class TaskType(str, Enum):
+    """Valid task types supported for logging by Galileo"""
+
+    text_classification = "text_classification"
+    text_multi_label = "text_multi_label"
+    text_ner = "text_ner"
+    image_classification = "image_classification"
+    structured_classification = "structured_classification"
+    object_detection = "object_detection"
+
+    @staticmethod
+    def get_valid_tasks() -> List[str]:
+        return list(map(lambda x: x.value, TaskType))
+
+    @staticmethod
+    def get_mapping(task_int: int) -> "TaskType":
+        """Converts the servers task type enum to client names"""
+        return {
+            0: TaskType.text_classification,
+            1: TaskType.text_multi_label,
+            2: TaskType.text_ner,
+            3: TaskType.image_classification,
+            4: TaskType.structured_classification,
+            5: TaskType.object_detection,
+        }[task_int]

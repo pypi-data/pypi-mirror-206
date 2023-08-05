620d83ab55756c35697db53f39440bbdf824651e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:39 +0000

    Add package TrackerGG/2.4.0/TrackerGG-2.4.0-py3-none-any.whl

---
+++ packages/TrackerGG/TrackerGG-2.4.0-py3-none-any.whl/TrackerGG/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+TrackerGG API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+An API Wrapper for TrackerGG API
+
+Copyright (c) 2023 DevRuby
+
+"""
+
+"""
+Copyright (c) 2023 DevRuby
+
+MIT License
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+from .Models import CSGOProfile
+from .Models import CSGOMapSegment
+from .Models import CSGOWeaponSegment
+from .Models import CSGOWeapon
+from .client import CSGOClient
+from . import utils
+from .Models import Platform

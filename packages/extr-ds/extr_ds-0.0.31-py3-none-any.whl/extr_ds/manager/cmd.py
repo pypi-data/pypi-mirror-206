c43f738040d06ca0356fe21ed570bbf4d600d4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:12 +0000

    Add package extr-ds/0.0.31/extr_ds-0.0.31-py3-none-any.whl

---
+++ packages/extr-ds/extr_ds-0.0.31-py3-none-any.whl/extr_ds/manager/cmd.py
@@ -0,0 +1,36 @@
+import sys
+
+from .process import workspace
+from .process import split, annotate, save
+
+
+def main() -> int:
+    if len(sys.argv) == 1:
+        return -1
+
+    args = sys.argv[1:]
+    method = args[0]
+
+    if method == '--init':
+        workspace.init()
+
+    elif method == '--reset':
+        workspace.clean()
+
+    elif method == '--split':
+        split()
+        annotate()
+
+    elif method == '--annotate':
+        annotate()
+
+    elif method == '--save':
+        save()
+
+    elif method == '--reset':
+        workspace.clean()
+
+    else:
+        print(f'"{method}" is not a valid command.')
+
+    return 0

73cdec176e2cc32ca746baa8aadbcf24dfe8b2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:47 +0000

    Add package landingzone-organization/0.2.2/landingzone_organization-0.2.2.tar.gz

---
+++ packages/landingzone-organization/landingzone_organization-0.2.2.tar.gz/landingzone_organization-0.2.2/landingzone_organization/__init__.py
@@ -0,0 +1,8 @@
+from landingzone_organization.adapters.aws_organization import AWSOrganization
+from landingzone_organization.organization import Organization
+from landingzone_organization.organization_unit import OrganizationUnit
+from landingzone_organization.account import Account
+from landingzone_organization.groups import Groups
+from landingzone_organization.group import Group
+
+__version__ = '0.2.2'

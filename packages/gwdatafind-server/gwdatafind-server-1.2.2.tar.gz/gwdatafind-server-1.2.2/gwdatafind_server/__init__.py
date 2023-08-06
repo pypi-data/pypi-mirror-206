04e7948792c7cb3d5d101faa54854acc3a09cb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:58 +0000

    Add package gwdatafind-server/1.2.2/gwdatafind-server-1.2.2.tar.gz

---
+++ packages/gwdatafind-server/gwdatafind-server-1.2.2.tar.gz/gwdatafind-server-1.2.2/gwdatafind_server/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (2019) Cardiff University
+# Licensed under GPLv3+ - see LICENSE
+
+"""The GWDataFind Service provides a metadata-based
+data discover mechanism for data files.
+
+Users provide a few key details as part of an HTTP(S)
+query and receive URLs that indicate the locations
+of the relevant data.
+"""
+
+__version__ = '1.2.2'
+
+from .app import GWDataFindApp
+from .config import get_config_path
+
+
+def create_app():
+    """Create an instance of the application
+    """
+    return GWDataFindApp(__name__, get_config_path())

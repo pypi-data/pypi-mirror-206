ee9cd9e74002873234b66f2e4db684858b6e7f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:26 +0000

    Add package budgetguard/0.1.0/budgetguard-0.1.0.tar.gz

---
+++ packages/budgetguard/budgetguard-0.1.0.tar.gz/budgetguard-0.1.0/budgetguard/tests/conftest.py
@@ -0,0 +1,16 @@
+import unittest
+from pyspark.sql import SparkSession
+
+
+class SparkETLTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.spark = (
+            SparkSession.builder.master("local[*]")
+            .appName("Unit-tests")
+            .getOrCreate()
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()

2a725c0577cd20335e195cc2b29dffb36d1bfe64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package component-registry-bindings/1.3.4/component_registry_bindings-1.3.4-py3-none-any.whl

---
+++ packages/component-registry-bindings/component_registry_bindings-1.3.4-py3-none-any.whl/component_registry_bindings/constants.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+
+COMPONENT_REGISTRY_API_VERSION: str = "v1"
+COMPONENT_REGISTRY_BINDINGS_USERAGENT: str = "component-registry-bindings-1.3.4"
+COMPONENT_REGISTRY_BINDINGS_API_PATH: str = (
+    f".bindings.python_client.api.{COMPONENT_REGISTRY_API_VERSION}"
+)
+
+# all available session operations
+ALL_SESSION_OPERATIONS: List[str] = (
+    "retrieve",
+    "update",
+    "list",
+    "create",
+    "destroy",
+    "search",
+)
+
+# mapping for resources which model names are different from the endpoint name
+RESOURCE_TO_MODEL_MAPPING: Dict[str, str] = {
+    "builds": "SoftwareBuild",
+}

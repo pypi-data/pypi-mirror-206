1b8f19e983553befe2b4fe0a784a0bf55485e883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:58 +0000

    Add package ft-package-42-jekim/0.0.5/ft_package_42_jekim-0.0.5.tar.gz

---
+++ packages/ft-package-42-jekim/ft_package_42_jekim-0.0.5.tar.gz/ft_package_42_jekim-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = "0.0.5"
+SUMMARY = "A small example package"
+
+setuptools.setup(
+    name="ft_package_42_jekim",
+    version=VERSION,
+    author="Two-Jay",
+    author_email="djeeee1272@gmail.com",
+    license="MIT",
+    description=SUMMARY,
+    long_description=long_description,
+    url="https://github.com/Two-Jay/42_Python_for_DataScience",
+    packages=setuptools.find_packages('ft_package_42_jekim'),
+)
+
+# how to install:
+
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade setuptools
+# python3 -m pip install --upgrade wheel
+# python3 -m pip install --upgrade twine
+
+# python3 setup.py sdist bdist_wheel
+
+# python -m twine upload dist/*
+
+# then you can install it with 'pip install ft_package_42_jekim'

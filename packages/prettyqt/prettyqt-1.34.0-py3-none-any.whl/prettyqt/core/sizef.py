bb947c77f019c14b88049d583312da4997f4808d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:01 +0000

    Add package prettyqt/1.34.0/prettyqt-1.34.0-py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-1.34.0-py3-none-any.whl/prettyqt/core/sizef.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from prettyqt.qt import QtCore
+from prettyqt.utils import datatypes, get_repr
+
+
+class SizeF(QtCore.QSizeF):
+    def __repr__(self):
+        return get_repr(self, self.width(), self.height())
+
+    def __getitem__(self, index) -> float:
+        return (self.width(), self.height())[index]
+
+    def __reduce__(self):
+        return type(self), (self.width(), self.height())
+
+    def expanded_to(self, size: datatypes.SizeFType) -> SizeF:
+        if isinstance(size, tuple):
+            size = QtCore.QSizeF(*size)
+        return SizeF(self.expandedTo(size))
+
+    def shrunk_by(self, margins: datatypes.MarginsFType) -> SizeF:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMarginsF(*margins)
+        return SizeF(self.marginsAdded(margins))
+
+    def grown_by(self, margins: datatypes.MarginsFType) -> SizeF:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMarginsF(*margins)
+        return SizeF(self.marginsRemoved(margins))
+
+
+if __name__ == "__main__":
+    size = SizeF(10, 20)
+    print(tuple(size))
+    size = size.expanded_to(QtCore.QSizeF(100, 100))
+    print(type(size))

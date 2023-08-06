80cd54bd8651722226f7419fba49a620954c3a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:33 +0000

    Add package quorum-data-py/1.2.7/quorum_data_py-1.2.7-py3-none-any.whl

---
+++ packages/quorum-data-py/quorum_data_py-1.2.7-py3-none-any.whl/quorum_data_py/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+from quorum_data_py import converter, feed, util
+from quorum_data_py.trx_type import get_trx_type
+
+__version__ = "1.2.7"
+__author__ = "liujuanjuan1984"
+
+# Set default logging handler to avoid "No handler found" warnings.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+logger.info("Version %s", __version__)

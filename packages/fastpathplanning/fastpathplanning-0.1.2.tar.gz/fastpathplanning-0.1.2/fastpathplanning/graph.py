e3e2b269fe27102d03c0f45696f353da22189210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:27 +0000

    Add package fastpathplanning/0.1.2/fastpathplanning-0.1.2.tar.gz

---
+++ packages/fastpathplanning/fastpathplanning-0.1.2.tar.gz/fastpathplanning-0.1.2/fastpathplanning/graph.py
@@ -0,0 +1,176 @@
+import numpy as np
+import scipy as sp
+import cvxpy as cp
+import networkx as nx
+from time import time
+from itertools import product
+
+class LineGraph(nx.Graph):
+
+    def __init__(self, B, verbose=True, *args, **kwargs):
+
+        if verbose:
+            print('Computing line graph')
+
+        # Initialize and store boxes.
+        super().__init__(*args, **kwargs)
+        self.B = B
+
+        # Compute line graph using networkx.
+        inters_graph = nx.Graph()
+        inters_graph.add_nodes_from(B.inters.keys())
+        for k, k_inters in B.inters.items():
+            k_inters_unique = [l for l in k_inters if l > k]
+            inters_graph.add_edges_from(product([k], k_inters_unique))
+        line_graph = nx.line_graph(inters_graph)
+        self.add_nodes_from(line_graph.nodes)
+        self.add_edges_from(line_graph.edges)
+        self.v2i = {v: i for i, v in enumerate(self.nodes)}
+        self.i2v = {i: v for i, v in enumerate(self.nodes)}
+
+        if verbose:
+            print(f'...line graph has {self.number_of_nodes()} vertices ' \
+                f'and {self.number_of_edges()} edges.')
+
+        if verbose:
+            print('Optimizing representative points')
+
+        # Pair each vertex with the corresponding box intersection.
+        for v in self.nodes:
+            boxk = B.boxes[v[0]]
+            boxl = B.boxes[v[1]]
+            self.nodes[v]['box'] = boxk.intersect(boxl)
+
+        # Place representative point in each box intersection.
+        self.optimize_points()
+
+        # Assign fixed length to each edge of the line graph.
+        for e in self.edges:
+            pu = self.nodes[e[0]]['point']
+            pv = self.nodes[e[1]]['point']
+            self.edges[e]['weight'] = np.linalg.norm(pv - pu)
+
+        # Store adjacency matrix for scipy's shortest-path algorithms.
+        self.adj_mat = nx.to_scipy_sparse_array(self)
+
+        if verbose:
+            print('...representative points optimized.')
+        
+    def optimize_points(self):
+
+        x = cp.Variable((self.number_of_nodes(), self.B.d))
+        x.value = np.array([self.nodes[v]['box'].c for v in self.nodes])
+
+        l = np.vstack([self.nodes[v]['box'].l for v in self.nodes])
+        u = np.vstack([self.nodes[v]['box'].u for v in self.nodes])
+        constraints = [x >= l, x <= u]
+
+        A = nx.incidence_matrix(self, oriented=True)
+        y = A.T.dot(x)
+        cost = cp.sum(cp.norm(y, 2, axis=1))
+
+        prob = cp.Problem(cp.Minimize(cost), constraints)
+        prob.solve(solver='CLARABEL')
+
+        for i, v in enumerate(self.nodes):
+            self.nodes[v]['point'] = x[i].value
+
+    @staticmethod
+    def node(k, l):
+
+        return (k, l) if k < l else (l, k)
+
+    def shortest_path(self, goal):
+
+        tic = time()
+
+        rows = []
+        data = []
+        for k in self.B.contain(goal):
+            for l in self.B.inters[k]:
+                v = self.node(k, l)
+                i = self.v2i[v]
+                rows.append(i)
+                pv = self.nodes[v]['point']
+                data.append(np.linalg.norm(goal - pv))
+        cols = [0] * len(rows)
+        shape = (len(self.nodes), 1)
+        adj_col = sp.sparse.csr_matrix((data, (rows, cols)), shape)
+        adj_mat = sp.sparse.bmat([[self.adj_mat, adj_col], [adj_col.T, None]])
+
+        dist, succ = sp.sparse.csgraph.dijkstra(
+            csgraph=adj_mat,
+            directed=False,
+            return_predecessors=True,
+            indices=-1
+        )
+
+        planner = lambda start: self._planner_all_to_one(start, dist, succ)
+
+        return planner, time() - tic
+
+    def _planner_all_to_one(self, start, dist, succ):
+
+        tic = time()
+
+        length = np.inf
+        for k in self.B.contain(start):
+            for l in self.B.inters[k]:
+                v = self.node(k, l)
+                i = self.v2i[v]
+                dist_vg = dist[i]
+                if np.isinf(dist_vg):
+                    return None, None, time() - tic
+                dist_sv = np.linalg.norm(self.nodes[v]['point'] - start)
+                dist_sg = dist_sv + dist_vg
+                if dist_sg < length:
+                    length = dist_sg
+                    first_box = k
+                    first_vertex = i
+
+        box_sequence = self._succ_to_box_sequence(succ, first_box, first_vertex)
+
+        return box_sequence, length, time() - tic
+
+    def _succ_to_box_sequence(self, succ, first_box, first_vertex):
+
+        box_sequence = [first_box]
+        i = first_vertex
+        while succ[i] >= 0:
+            v = self.i2v[i]
+            j = succ[i]
+            if succ[j] >= 0:
+                w = self.i2v[j]
+                while box_sequence[-1] in w:
+                    i = j
+                    v = w
+                    j = succ[i]
+                    w = self.i2v[j]
+            if v[0] == box_sequence[-1]:
+                box_sequence.append(v[1])
+            else:
+                box_sequence.append(v[0])
+            i = succ[i]
+
+        return box_sequence
+
+    def plot(self, **kwargs):
+
+        plot_graph(self, **kwargs)
+
+def plot_graph(G, **kwargs):
+
+    import matplotlib.pyplot as plt
+
+    options = {'marker':'o', 'markerfacecolor':'w', 'markeredgecolor':'k', 'c':'k'}
+    options.update(kwargs)
+
+    if 'label' in options:
+        plt.plot([np.nan], [np.nan], **options)
+        options.pop('label')
+
+    for e in G.edges:
+        start = G.nodes[e[0]]['point']
+        stop = G.nodes[e[1]]['point']
+        P = np.array([start, stop])
+        plt.plot(*P.T, **options)
---
+++ packages/fastpathplanning/fastpathplanning-0.1.2.tar.gz/fastpathplanning-0.1.2/fastpathplanning/smooth.py
@@ -0,0 +1,386 @@
+import numpy as np
+import cvxpy as cp
+from time import time
+from itertools import accumulate
+from bisect import bisect
+from scipy.special import binom
+
+class BezierCurve:
+
+    def __init__(self, points, a=0, b=1):
+
+        assert b > a
+
+        self.points = points
+        self.h = points.shape[0] - 1
+        self.d = points.shape[1]
+        self.a = a
+        self.b = b
+        self.duration = b - a
+
+    def __call__(self, t):
+
+        c = np.array([self.berstein(t, n) for n in range(self.h + 1)])
+        return c.T.dot(self.points)
+
+    def berstein(self, t, n):
+
+        c1 = binom(self.h, n)
+        c2 = (t - self.a) / self.duration 
+        c3 = (self.b - t) / self.duration
+        value = c1 * c2 ** n * c3 ** (self.h - n) 
+
+        return value
+
+    def start_point(self):
+
+        return self.points[0]
+
+    def end_point(self):
+
+        return self.points[-1]
+        
+    def derivative(self):
+
+        points = (self.points[1:] - self.points[:-1]) * (self.h / self.duration)
+
+        return BezierCurve(points, self.a, self.b)
+
+    def l2_squared(self):
+
+        A = np.zeros((self.h + 1, self.h + 1))
+        for m in range(self.h + 1):
+            for n in range(self.h + 1):
+                A[m, n] = binom(self.h, m) * binom(self.h, n) / binom(2 * self.h, m + n)
+        A *= self.duration / (2 * self.h + 1)
+        A = np.kron(A, np.eye(self.d))
+
+        p = self.points.flatten()
+
+        return p.dot(A.dot(p))
+
+    def plot2d(self, samples=51, **kwargs):
+
+        import matplotlib.pyplot as plt
+
+        options = {'c':'b'}
+        options.update(kwargs)
+        t = np.linspace(self.a, self.b, samples)
+        plt.plot(*self(t).T, **options)
+
+    def scatter2d(self, **kwargs):
+
+        import matplotlib.pyplot as plt
+
+        options = {'fc':'orange', 'ec':'k', 'zorder':3}
+        options.update(kwargs)
+        plt.scatter(*self.points.T, **options)
+
+    def plot_2dpolygon(self, **kwargs):
+
+        import matplotlib.pyplot as plt
+        from matplotlib.patches import Polygon
+        from scipy.spatial import ConvexHull
+
+        options = {'fc':'lightcoral'}
+        options.update(kwargs)
+        hull = ConvexHull(self.points)
+        ordered_points = hull.points[hull.vertices]
+        poly = Polygon(ordered_points, **options)
+        plt.gca().add_patch(poly)
+
+
+class CompositeBezierCurve:
+
+    def __init__(self, beziers):
+
+        for bez1, bez2 in zip(beziers[:-1], beziers[1:]):
+            assert bez1.b == bez2.a
+            assert bez1.d == bez2.d
+
+        self.beziers = beziers
+        self.N = len(self.beziers)
+        self.d = beziers[0].d
+        self.a = beziers[0].a
+        self.b = beziers[-1].b
+        self.duration = self.b - self.a
+        self.transition_times = [self.a] + [bez.b for bez in beziers]
+
+    def find_segment(self, t):
+
+        return min(bisect(self.transition_times, t) - 1, self.N - 1)
+
+    def __call__(self, t):
+
+        i = self.find_segment(t)
+
+        return self.beziers[i](t)
+
+    def start_point(self):
+
+        return self.beziers[0].start_point()
+
+    def end_point(self):
+
+        return self.beziers[-1].end_point()
+
+    def derivative(self):
+
+        return CompositeBezierCurve([b.derivative() for b in self.beziers])
+
+    def bound_on_integral(self, f):
+
+        return sum(bez.bound_on_integral(f) for bez in self.beziers)
+
+    def plot2d(self, **kwargs):
+
+        for bez in self.beziers:
+            bez.plot2d(**kwargs)
+
+    def scatter2d(self, **kwargs):
+
+        for bez in self.beziers:
+            bez.scatter2d(**kwargs)
+
+    def plot_2dpolygon(self, **kwargs):
+
+        for bez in self.beziers:
+            bez.plot_2dpolygon(**kwargs)
+
+def optimize_bezier(L, U, durations, alpha, initial, final,
+    n_points=None, **kwargs):
+
+    # Problem size.
+    n_boxes, d = L.shape
+    D = max(alpha)
+    assert max(initial) <= D
+    assert max(final) <= D
+    if n_points is None:
+        n_points = (D + 1) * 2
+
+    # Control points of the curves and their derivatives.
+    points = {}
+    for k in range(n_boxes):
+        points[k] = {}
+        for i in range(D + 1):
+            size = (n_points - i, d)
+            points[k][i] = cp.Variable(size)
+
+    # Boundary conditions.
+    constraints = []
+    for i, value in initial.items():
+        constraints.append(points[0][i][0] == value)
+    for i, value in final.items():
+        constraints.append(points[n_boxes - 1][i][-1] == value)
+
+    # Loop through boxes.
+    cost = 0
+    continuity = {}
+    for k in range(n_boxes):
+        continuity[k] = {}
+
+        # Box containment.
+        Lk = np.array([L[k]] * n_points)
+        Uk = np.array([U[k]] * n_points)
+        constraints.append(points[k][0] >= Lk)
+        constraints.append(points[k][0] <= Uk)
+
+        # Bezier dynamics.
+        for i in range(D):
+            h = n_points - i - 1
+            ci = durations[k] / h
+            constraints.append(points[k][i][1:] - points[k][i][:-1] == ci * points[k][i + 1])
+
+        # Continuity and differentiability.
+        if k < n_boxes - 1:
+            for i in range(D + 1):
+                constraints.append(points[k][i][-1] == points[k + 1][i][0])
+                if i > 0:
+                    continuity[k][i] = constraints[-1]
+
+        # Cost function.
+        for i, ai in alpha.items():
+            h = n_points - 1 - i
+            A = np.zeros((h + 1, h + 1))
+            for m in range(h + 1):
+                for n in range(h + 1):
+                    A[m, n] = binom(h, m) * binom(h, n) / binom(2 * h, m + n)
+            A *= durations[k] / (2 * h + 1)
+            A = np.kron(A, np.eye(d))
+            p = cp.vec(points[k][i], order='C')
+            cost += ai * cp.quad_form(p, A)
+
+    # Solve problem.
+    prob = cp.Problem(cp.Minimize(cost), constraints)
+    prob.solve(solver='CLARABEL')
+
+    # Reconstruct trajectory.
+    beziers = []
+    a = 0
+    for k in range(n_boxes):
+        b = a + durations[k]
+        beziers.append(BezierCurve(points[k][0].value, a, b))
+        a = b
+    path = CompositeBezierCurve(beziers)
+
+    retiming_weights = {}
+    for k in range(n_boxes - 1):
+        retiming_weights[k] = {}
+        for i in range(1, D + 1):
+            primal = points[k][i][-1].value
+            dual = continuity[k][i].dual_value
+            retiming_weights[k][i] = primal.dot(dual)
+
+    # Reconstruct costs.
+    cost_breakdown = {}
+    for k in range(n_boxes):
+        cost_breakdown[k] = {}
+        bez = beziers[k]
+        for i in range(1, D + 1):
+            bez = bez.derivative()
+            if i in alpha:
+                cost_breakdown[k][i] = alpha[i] * bez.l2_squared()
+
+    # Solution statistics.
+    sol_stats = {}
+    sol_stats['cost'] = prob.value
+    sol_stats['runtime'] = prob.solver_stats.solve_time
+    sol_stats['cost_breakdown'] = cost_breakdown
+    sol_stats['retiming_weights'] = retiming_weights
+
+    return path, sol_stats
+
+
+def retiming(kappa, costs, durations, retiming_weights, **kwargs):
+
+    # Decision variables.
+    n_boxes = max(costs) + 1
+    eta = cp.Variable(n_boxes)
+    eta.value = np.ones(n_boxes)
+    constr = [durations @ eta == sum(durations)]
+
+    # Scale costs from previous trajectory.
+    cost = 0
+    for i, ci in costs.items():
+        for j, cij in ci.items():
+            cost += cij * cp.power(eta[i], 1 - 2 * j)
+
+    # Retiming weights.
+    for k in range(n_boxes - 1):
+        for i, w in retiming_weights[k].items():
+            cost += i * retiming_weights[k][i] * (eta[k + 1] - eta[k])
+
+    # Trust region.
+    if not np.isinf(kappa):
+        constr.append(eta[1:] - eta[:-1] <= kappa)
+        constr.append(eta[:-1] - eta[1:] <= kappa)
+        
+    # Solve SOCP and get new durarations.
+    prob = cp.Problem(cp.Minimize(cost), constr)
+    prob.solve(solver='CLARABEL')
+    new_durations = np.multiply(eta.value, durations)
+
+    # New candidate for kappa.
+    kappa_max = max(np.abs(eta.value[1:] - eta.value[:-1]))
+
+    return new_durations, prob.solver_stats.solve_time, kappa_max
+
+def log(s1, size=10):
+    s1 = str(s1)
+    s0 = list('|' + ' ' * size + '|')
+    s0[2:2 + len(s1)] = s1
+    return ''.join(s0)
+
+def init_log():
+    print(log('Iter.') + log('Cost') + log('Decr.') + \
+          log('Kappa') + log('Accept'))
+    print('-' * 60)
+
+def term_log():
+    print('-' * 60)
+
+def update_log(i, cost, cost_decrease, kappa, accept):
+    print(log(i) + \
+              log('{:.2e}'.format(cost)) + \
+              log('{:.1e}'.format(cost_decrease)) + \
+              log('{:.1e}'.format(kappa)) + \
+              log(accept))
+
+def optimize_bezier_with_retiming(L, U, durations, alpha, initial, final,
+    omega=3, kappa_min=1e-2, verbose=False, **kwargs):
+
+    # Solve initial Bezier problem.
+    path, sol_stats = optimize_bezier(L, U, durations, alpha, initial, final, **kwargs)
+    cost = sol_stats['cost']
+    cost_breakdown = sol_stats['cost_breakdown']
+    retiming_weights = sol_stats['retiming_weights']
+
+    if verbose:
+        init_log()
+        update_log(0, cost, np.nan, np.inf, True)
+
+    # Lists to populate.
+    costs = [cost]
+    paths = [path]
+    durations_iter = [durations]
+    bez_runtimes = [sol_stats['runtime']]
+    retiming_runtimes = []
+
+    # Iterate retiming and Bezier.
+    kappa = 1
+    n_iters = 0
+    i = 1
+    while True:
+        n_iters += 1
+
+        # Retime.
+        new_durations, runtime, kappa_max = retiming(kappa, cost_breakdown,
+            durations, retiming_weights, **kwargs)
+        durations_iter.append(new_durations)
+        retiming_runtimes.append(runtime)
+
+        # Improve Bezier curves.
+        path_new, sol_stats = optimize_bezier(L, U, new_durations,
+            alpha, initial, final, **kwargs)
+        cost_new = sol_stats['cost']
+        costs.append(cost_new)
+        paths.append(path_new)
+        bez_runtimes.append(sol_stats['runtime'])
+
+        decr = cost_new - cost
+        accept = decr < 0
+        if verbose:
+            update_log(i, cost_new, decr, kappa, accept)
+
+        # If retiming improved the trajectory.
+        if accept:
+            durations = new_durations
+            path = path_new
+            cost = cost_new
+            cost_breakdown = sol_stats['cost_breakdown']
+            retiming_weights = sol_stats['retiming_weights']
+
+        if kappa < kappa_min:
+            break
+        kappa = kappa_max / omega
+        i += 1
+
+    runtime = sum(bez_runtimes) + sum(retiming_runtimes)
+    if verbose:
+        term_log()
+        print(f'Smooth phase terminated in {i} iterations')
+        print(f'Final cost is ' + '{:.3e}'.format(cost))
+        print(f'Solver time was {np.round(runtime, 5)}')
+
+    # Solution statistics.
+    sol_stats = {}
+    sol_stats['cost'] = cost
+    sol_stats['n_iters'] = n_iters
+    sol_stats['costs'] = costs
+    sol_stats['paths'] = paths
+    sol_stats['durations_iter'] = durations_iter
+    sol_stats['bez_runtimes'] = bez_runtimes
+    sol_stats['retiming_runtimes'] = retiming_runtimes
+    sol_stats['runtime'] = runtime
+    
+    return path, sol_stats

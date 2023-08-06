742c84cc5117be71ac0d5eed85d1100125580de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:29 +0000

    Add package nvm/1.0.4/nvm-1.0.4.tar.gz

---
+++ packages/nvm/nvm-1.0.4.tar.gz/nvm-1.0.4/docs/source/nvm.rst
@@ -0,0 +1,32 @@
+nvm package
+===========
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   nvm.aux_log
+   nvm.aux_pandas
+   nvm.aux_str
+   nvm.aux_sys
+
+Submodules
+----------
+
+nvm.nvm module
+--------------
+
+.. automodule:: nvm.nvm
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: nvm
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ packages/nvm/nvm-1.0.4.tar.gz/nvm-1.0.4/nvm/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Top-level package for NVM."""
+
+# fmt: off
+from . import _version
+__version__ = _version.get_versions()['version']
+__version_dict__ = _version.get_versions()
+# fmt: on
+
+__author__ = """cogsys.io"""
+__email__ = "cogsys@cogsys.io"
+
+
+from .aux_log import Log0
+from .aux_sys import chdir
+from .aux_str import clean_str
+from .aux_pandas import disp_df
+from .aux_pandas import disp_df as ddf
+
+
+def get_module_version():
+    return __version__
+
+
+# end

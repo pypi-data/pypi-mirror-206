d57a96accf4572b2df86c12f9ba1ddf1ba46c936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:46 +0000

    Add package NDETCStemmer-kaenova/1.4.0/NDETCStemmer_kaenova-1.4.0-py3-none-any.whl

---
+++ packages/NDETCStemmer-kaenova/NDETCStemmer_kaenova-1.4.0-py3-none-any.whl/NDETCStemmer/__init__.py
@@ -0,0 +1,7 @@
+from NDETCStemmer.NDETCStemmer import NDETCStemmer
+from NDETCStemmer.ModelDownloader import (
+    ModelDownloaderAbstract,
+    HuggingfaceModelDownloader,
+    GoogleDriveModelDownloader,
+    CustomLinkModelDownloader
+)

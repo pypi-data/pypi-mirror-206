2cfe1d2c3137b550ac322c9a13877cc6143e7dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:10 +0000

    Add package sansarlib/0.1/sansarlib-0.1.tar.gz

---
+++ packages/sansarlib/sansarlib-0.1.tar.gz/sansarlib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sansarlib',
+    version='0.1',
+    author='sanskar khanna',
+    author_email='sanskarkhanna1@gamil.com',
+    description='This library improve chatbot technology',
+    packages=[],
+    install_requires=[],
+)

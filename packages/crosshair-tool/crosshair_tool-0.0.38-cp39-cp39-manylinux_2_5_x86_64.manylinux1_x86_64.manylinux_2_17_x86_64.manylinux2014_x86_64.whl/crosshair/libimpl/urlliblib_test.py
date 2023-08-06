b25a94c45efb9ded34a2ecaebc846c7a31f8f7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:51 +0000

    Add package crosshair-tool/0.0.38/crosshair_tool-0.0.38-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/crosshair-tool/crosshair_tool-0.0.38-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/crosshair/libimpl/urlliblib_test.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse
+
+from crosshair.options import AnalysisOptionSet
+from crosshair.statespace import POST_FAIL, MessageType
+from crosshair.test_util import check_states
+
+_SLOW_TEST = AnalysisOptionSet(per_condition_timeout=10)
+
+
+def test_urllib_parse():
+    def f(urlstring: str):
+        """
+        pre: len(urlstring) == 3
+        pre: urlstring[0:2] == "//"
+        pre: urlstring[2] != "["
+        pre: urlstring[2] != "]"
+        post: __return__.netloc != "h"
+        """
+        return urlparse(urlstring)
+
+    check_states(f, POST_FAIL, _SLOW_TEST)

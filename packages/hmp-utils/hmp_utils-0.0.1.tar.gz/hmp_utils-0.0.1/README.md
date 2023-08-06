27bad72d8bd96060eceeb5c598600477982f7ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:12 +0000

    Add package hmp-utils/0.0.1/hmp_utils-0.0.1.tar.gz

---
+++ packages/hmp-utils/hmp_utils-0.0.1.tar.gz/hmp_utils-0.0.1/README.md
@@ -0,0 +1,9 @@
+# hmp_utils
+
+Install in an apptainer container with:
+```
+pip install . --user
+```
+```
+pip install hmp_utils --user
+```
---
+++ packages/hmp-utils/hmp_utils-0.0.1.tar.gz/hmp_utils-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmp_utils',
+    version='0.0.1',
+    author="C-Square Team",
+    author_email='csquare@mailfence.com',
+    packages=find_packages(),
+    # package_dir={'': 'src'},
+    # url='https://github.com/.../...',
+    # keywords='example project',
+    install_requires=[
+        # //examples:
+        # 'numpy',
+        # 'torch',
+    ],
+
+)

5787966421f4e220a1fef983ffc90238b2972196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:29 +0000

    Add package clk/0.33.0/clk-0.33.0-py2.py3-none-any.whl

---
+++ packages/clk/clk-0.33.0-py2.py3-none-any.whl/clk/netrc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import netrc
+import os
+
+
+class Netrc():
+
+    def set_password(self, servicename, username, password):
+        raise NotImplementedError
+
+    def get_password(self, servicename, username):
+        try:
+            authenticator = netrc.netrc(os.path.expanduser('~/.netrc')).authenticators(username)
+            return authenticator[2]
+        except:  # NOQA: E722
+            return None
+
+    def delete_password(self, servicename, username, password):
+        raise NotImplementedError

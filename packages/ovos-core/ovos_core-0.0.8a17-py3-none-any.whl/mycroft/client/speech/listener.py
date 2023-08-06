658c2d8f72b7a97e73c99018b7d934997f041553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:39 +0000

    Add package ovos-core/0.0.8a17/ovos_core-0.0.8a17-py3-none-any.whl

---
+++ packages/ovos-core/ovos_core-0.0.8a17-py3-none-any.whl/mycroft/client/speech/listener.py
@@ -0,0 +1,9 @@
+"""
+This module contains back compat imports only
+Speech client moved into mycroft.listener module
+"""
+from ovos_listener.stt import STTFactory
+from ovos_listener.hotword_factory import HotWordFactory
+from ovos_listener.listener import AudioConsumer, AudioProducer, AudioStreamHandler, \
+    AUDIO_DATA, STREAM_DATA, STREAM_STOP, STREAM_START, MAX_MIC_RESTARTS, \
+    RecognizerLoop, RecognizerLoopState, recognizer_conf_hash

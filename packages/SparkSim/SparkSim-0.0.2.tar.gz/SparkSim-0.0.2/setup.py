4b213771ffc0876216841909bd1b1e9d90601390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package SparkSim/0.0.2/SparkSim-0.0.2.tar.gz

---
+++ packages/SparkSim/SparkSim-0.0.2.tar.gz/SparkSim-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+VERSION = '0.0.2'
+DESCRIPTION = 'Simple Simulator for Computer Vision'
+LONG_DESCRIPTION = 'A package that allows to test how computer vision algorithms for advanced driving systems performs '
+
+# Setting up
+setup(
+    name="SparkSim",
+    version=VERSION,
+    author="TucuAI (Tucudean Adrian-Ionut)",
+    author_email="<Tucudean.Adrian.Ionut@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    package_data={
+        'images': ['images/LineFollower1.png', 'images/LineFollower2.png','images/player.png'],
+    },
+    install_requires=['opencv-python', 'pygame'],
+    keywords=['python', 'simulator', 'computer vision', 'advanced driving', 'camera stream', 'camera handler'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

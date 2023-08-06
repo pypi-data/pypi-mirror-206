fd69cdbe7de78351c1e824df49887376eef425ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package maxeqx-dc-webhook.py/1.0.6/maxeqx-dc-webhook.py-1.0.6.tar.gz

---
+++ packages/maxeqx-dc-webhook.py/maxeqx-dc-webhook.py-1.0.6.tar.gz/maxeqx-dc-webhook.py-1.0.6/maxwebhook/__init__.py
@@ -0,0 +1,23 @@
+import requests
+
+class Webhook():
+    def __init__(self):
+        self.url : str = None
+        self.username : str = None
+        self.avatar_url : str = None
+        self.message : str = None
+        self.embedtitle : str = None
+        self.embeddescription : str = None
+    def send(self):     
+        url = self.url
+        data = {
+            "content" : self.message,
+            "username" : self.username,
+            "avatar_url" : self.avatar_url
+            }
+        data["embed"] = [{
+            "description" : self.embeddescription,
+            "title" : self.embedtitle
+        }]
+        result = requests.post(url, json = data)
+        result.raise_for_status()

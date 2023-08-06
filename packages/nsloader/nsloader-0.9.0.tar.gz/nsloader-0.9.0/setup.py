56f28d3f4b77ed5cfc459e65bbfd092cd5c1b76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:46 +0000

    Add package nsloader/0.9.0/nsloader-0.9.0.tar.gz

---
+++ packages/nsloader/nsloader-0.9.0.tar.gz/nsloader-0.9.0/setup.py
@@ -0,0 +1,19 @@
+''' setup.py
+'''
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nsloader',
+    version='0.9.0',
+    author='new-village',
+    url='https://github.com/new-village/nsloader',
+    description='This script collects articles from Wall Street Journal and returns it in dict format.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['beautifulsoup4', 'selenium', 'webdriver_manager', 'chromedriver_binary'],
+    packages=find_packages(),
+    package_data={'': ['config/*.json']},
+)

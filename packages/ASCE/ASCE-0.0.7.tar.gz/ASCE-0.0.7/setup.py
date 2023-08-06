81cf0acad3fa059ab8639b110f2ef0e5164eef8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:45 +0000

    Add package ASCE/0.0.7/ASCE-0.0.7.tar.gz

---
+++ packages/ASCE/ASCE-0.0.7.tar.gz/ASCE-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ASCE',
+  version='0.0.7',
+  description='''ASCE is a simple program that operates on matrices''',
+  url='',  
+  author='CZB ASCE GROUP',
+  author_email='sandrine.sila@net.usj.edu.lb',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='library', 
+  packages=find_packages()
+)

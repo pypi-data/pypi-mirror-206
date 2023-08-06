a837e7822ef74f0daffffcb958c9bfc7a32944af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:51 +0000

    Add package xuyangcao/1.0/xuyangcao-1.0.tar.gz

---
+++ packages/xuyangcao/xuyangcao-1.0.tar.gz/xuyangcao-1.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name = 'xuyangcao',
+        version = '1.0',
+        description = 'xuyangcao\'s model',
+        author = 'xuyangcao',
+        py_modules = ['msg.sendMsg', 'msg.revMsg'])

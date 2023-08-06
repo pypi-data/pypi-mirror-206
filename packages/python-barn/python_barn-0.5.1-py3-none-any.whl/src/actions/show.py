2594738c3a2ea7041bdc9754e87219ac1037b18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:58 +0000

    Add package python-barn/0.5.1/python_barn-0.5.1-py3-none-any.whl

---
+++ packages/python-barn/python_barn-0.5.1-py3-none-any.whl/src/actions/show.py
@@ -0,0 +1,7 @@
+from src.core import barn_action, Context
+
+@barn_action
+def show(package, verbose=False, files=False, context: Context=None):
+    verbose_string = "--verbose" if verbose else ""
+    files_string = "--files" if files else ""
+    return context.run_command_in_context(f"pip show {verbose_string} {files_string} {package}")

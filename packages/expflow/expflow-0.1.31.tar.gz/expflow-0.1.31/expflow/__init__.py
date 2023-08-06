40b68bffbeb945d01ac9f7cf793591d2c645df49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:05 +0000

    Add package expflow/0.1.31/expflow-0.1.31.tar.gz

---
+++ packages/expflow/expflow-0.1.31.tar.gz/expflow-0.1.31/expflow/__init__.py
@@ -0,0 +1,18 @@
+from expflow.expflow import (
+    get_expflow_dir,
+    set_expflow_dir,
+    expflow_dir,
+    USER_DIR,
+    Participant,
+    using_compression,
+    ParticipantExistsError,
+    Experiment,
+    ExperimentExistsError,
+    Trial,
+    WrongClassError,
+    ParticipantDoesNotExistError,
+    is_valid_id,
+    get_participant_ids,
+    get_experiment_ids,
+    get_participated_in,
+)

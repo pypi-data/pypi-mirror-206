47597f6c150c63fac10ac090c42d808b5bed5119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package structsvm/0.1.1/structsvm-0.1.1-py3-none-any.whl

---
+++ packages/structsvm/structsvm-0.1.1-py3-none-any.whl/structsvm/__init__.py
@@ -0,0 +1,6 @@
+from .bundle_method import BundleMethod
+from .soft_margin_loss import SoftMarginLoss
+from .hamming_costs import HammingCosts
+
+__version__ = '0.1.1'
+__all__ = ['BundleMethod', 'SoftMarginLoss', 'HammingCosts']

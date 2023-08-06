2eae8cad1272a38f53660bd5f88bd7ca49f371e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:19 +0000

    Add package uniprot-redis/1.3.0/uniprot_redis-1.3.0.tar.gz

---
+++ packages/uniprot-redis/uniprot_redis-1.3.0.tar.gz/uniprot_redis-1.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['uniprot_redis', 'uniprot_redis.store']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['docopt>=0.6.2,<0.7.0',
+ 'fastapi>=0.79.0,<0.80.0',
+ 'pyproteinsext>=3.0.3,<4.0.0',
+ 'pyrediscore>=1.0.0,<2.0.0',
+ 'uvicorn>=0.18.2,<0.19.0']
+
+setup_kwargs = {
+    'name': 'uniprot-redis',
+    'version': '1.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Cecile Hilpert',
+    'author_email': 'cecile.hilpert@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

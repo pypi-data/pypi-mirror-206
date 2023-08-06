0eb68f816e6861a989d3ae7f8ac25a00ec495ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:47 +0000

    Add package ifd-python/7.6.15/ifd_python-7.6.15.tar.gz

---
+++ packages/ifd-python/ifd_python-7.6.15.tar.gz/ifd_python-7.6.15/ifd/repository/SqlTicketRepository.py
@@ -0,0 +1,27 @@
+from .abstract import AbsSqlRepository
+import pandas as pd 
+from ..entities import Ticket
+
+class SqlTicketRepository(AbsSqlRepository):
+    
+    def getAll(self):
+        df = pd.read_sql(
+            """
+            SELECT [ITA_ID]
+                ,[IMG_ID]
+                ,[ITA_TICKET_REQUEST]
+                ,[ITA_VALIDATION_STATUT]
+                ,[IVA_ID]
+                ,[ITA_DATE_ACTION]
+                ,[ITA_DATE_CREATION]
+                ,[ITA_USER_ACTION]
+                ,[ITA_MOTIF_REJET]
+            FROM [Infradeep].[dbo].[T_D_IRRIS_TICKET_AUTOMATIC_ITA]     
+            """, 
+            self.connection
+        )
+        
+        return self._dataframe_to_list_of_ticket(df)
+    
+    def _dataframe_to_list_of_ticket(self, df):
+        return [Ticket(**kwargs) for kwargs in df.to_dict(orient='records')]

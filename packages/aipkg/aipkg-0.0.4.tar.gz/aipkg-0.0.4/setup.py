c677910eefb81f59b1c7e7fc07eb1830de0c2c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:36 +0000

    Add package aipkg/0.0.4/aipkg-0.0.4.tar.gz

---
+++ packages/aipkg/aipkg-0.0.4.tar.gz/aipkg-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Streaming video data via networks'
+
+
+# Setting up
+setup(
+    name="aipkg",
+    version=VERSION,
+    author="Deez",
+    author_email="<hello@yahoo.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'ai', 'pkg'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

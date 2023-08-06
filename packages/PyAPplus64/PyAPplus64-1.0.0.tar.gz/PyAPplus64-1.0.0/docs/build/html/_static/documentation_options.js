b5341bc90de06cf8bca84ae5139f7aa4e4ccb517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:38 +0000

    Add package PyAPplus64/1.0.0/PyAPplus64-1.0.0.tar.gz

---
+++ packages/PyAPplus64/PyAPplus64-1.0.0.tar.gz/PyAPplus64-1.0.0/docs/build/html/_static/documentation_options.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 Thomas Tuerk (kontakt@thomas-tuerk.de)
+ *
+ * This file is part of PyAPplus64 (see https://www.thomas-tuerk.de/de/pyapplus64).
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT. */
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '',
+    LANGUAGE: 'de',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};

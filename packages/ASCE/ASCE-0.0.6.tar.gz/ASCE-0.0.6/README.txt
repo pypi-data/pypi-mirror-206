5e73f9470781bb95b1efafb5336e92c46e3fe3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:33 +0000

    Add package ASCE/0.0.6/ASCE-0.0.6.tar.gz

---
+++ packages/ASCE/ASCE-0.0.6.tar.gz/ASCE-0.0.6/README.txt
@@ -0,0 +1,13 @@
+"ASCE"
+
+
+
+'ASCE' is a simple program that operates on matrices.
+Some of the operations are:add, substract, multiply,triangle_inf, triangle_sup,
+determinant,trace,inverse...
+
+
+The purpose of this project is to find an algorithm that can multiply matrices with a better time 
+complexity than the normal code(Strassen)
+P.S: We implemented these functions in a class called "matrix" so before calling any function we need to write  matrix.function(),
+except for the multiplication, addition and substraction, we just use the maths operations "+, -, *"
---
+++ packages/ASCE/ASCE-0.0.6.tar.gz/ASCE-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ASCE',
+  version='0.0.6',
+#   description='A fast library/similar to numpy',
+#   long_description=open('README.txt').read()
+  url='',  
+  author='CZB ASCE GROUP',
+  author_email='sandrine.sila@net.usj.edu.lb',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='library', 
+  packages=find_packages()
+)

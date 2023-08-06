a8eca63a9e1a7c079e0bb4cd6455aea4364d7ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:29 +0000

    Add package haplopy/0.1.1/haplopy-0.1.1.tar.gz

---
+++ packages/haplopy/haplopy-0.1.1.tar.gz/haplopy-0.1.1/haplopy/__init__.py
@@ -0,0 +1,12 @@
+"""HaploPy package
+
+"""
+
+import importlib.metadata
+
+from haplopy.datautils import *
+from haplopy.multinomial import *
+from haplopy.plot import *
+
+
+__version__ = importlib.metadata.version("haplopy")

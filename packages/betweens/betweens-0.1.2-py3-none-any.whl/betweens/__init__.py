ce3326537791b13ff1f740fd393161a331c0c5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package betweens/0.1.2/betweens-0.1.2-py3-none-any.whl

---
+++ packages/betweens/betweens-0.1.2-py3-none-any.whl/betweens/__init__.py
@@ -0,0 +1,10 @@
+print('''
+author:siyuan_zhou
+email:yuanbaoge@outlook.com
+version:run command:betweens.version.ver
+''')
+print('''
+between1: two intergers
+between2: two lists or tuples (l1[a],l2[b])
+between3: two length or area unit ('mm','dm') , ('cm2','hm2')
+between3 must from small to large!!! (nm,um,mm,cm,dm,m,km;nm2,um2,mm2,cm2,dm2,m2,hm2,km2)''')

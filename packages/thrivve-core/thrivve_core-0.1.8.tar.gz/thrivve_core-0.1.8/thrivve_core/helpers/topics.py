b9f451f3461b9077a835bcfbd1f11a88514490ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package thrivve-core/0.1.8/thrivve_core-0.1.8.tar.gz

---
+++ packages/thrivve-core/thrivve_core-0.1.8.tar.gz/thrivve_core-0.1.8/thrivve_core/helpers/topics.py
@@ -0,0 +1,8 @@
+class Topics(object):
+    INTERNAL_NOTIFICATION_MESSAGE = 'internal_notification_message'
+    SEND_MAIL = 'send_mail'
+    SEND_SMS = 'send_sms'
+    LOG_MODEL_CHANGES = 'log_model_changes'
+
+    THRIVVE_SEND_SMS = 'thrivve_send_sms'
+    THRIVVE_SEND_PUSH_NOTIFICATION = 'thrivve_send_push_notification'

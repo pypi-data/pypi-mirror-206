e82a6b7b0442bee5cd74f3b6a1d63aa622325d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:44 +0000

    Add package enterprise-platform-compute/0.0.3/enterprise_platform_compute-0.0.3-py3-none-any.whl

---
+++ packages/enterprise-platform-compute/enterprise_platform_compute-0.0.3-py3-none-any.whl/compute/__init__.py
@@ -0,0 +1,11 @@
+from .graph import Scalar, Vector
+from .storage import Storage, File
+from .types import Runners
+
+__all__ = [
+    "Scalar",
+    "Vector",
+    "Runners",
+    "Storage",
+    "File",
+]
---
+++ packages/enterprise-platform-compute/enterprise_platform_compute-0.0.3-py3-none-any.whl/compute/storage.py
@@ -0,0 +1,8 @@
+class Storage:
+    def __init__(self, api_key: str):
+        ...
+
+
+class File:
+    def __init__(self, name: str):
+        ...

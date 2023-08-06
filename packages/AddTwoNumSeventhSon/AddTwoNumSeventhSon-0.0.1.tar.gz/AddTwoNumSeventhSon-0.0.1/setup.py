8eb77e0aa74dc15ca3e9846604c10470acbf44af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:51 +0000

    Add package AddTwoNumSeventhSon/0.0.1/AddTwoNumSeventhSon-0.0.1.tar.gz

---
+++ packages/AddTwoNumSeventhSon/AddTwoNumSeventhSon-0.0.1.tar.gz/AddTwoNumSeventhSon-0.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+"""
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+"""
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Add two numbers'
+LONG_DESCRIPTION = 'Add Two integer number'
+
+# Setting up
+setup(
+    name="AddTwoNumSeventhSon",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'add two num'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

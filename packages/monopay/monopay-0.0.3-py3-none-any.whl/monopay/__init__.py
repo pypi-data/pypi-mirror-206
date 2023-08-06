0279c11a6ef6a756d9b5a87767caeafcaa9a23fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:31 +0000

    Add package monopay/0.0.3/monopay-0.0.3-py3-none-any.whl

---
+++ packages/monopay/monopay-0.0.3-py3-none-any.whl/monopay/__init__.py
@@ -0,0 +1,16 @@
+# from .__main__ import *
+from . import exceptions
+from . import utils
+from . import instances
+from . import responses
+from . import methods
+
+from .exceptions import *
+from .utils import *
+from .instances import *
+from .responses import *
+from .methods import *
+
+__author__ = "yeghorkikhai"
+__version__ = "0.1.4"
+__email__ = "yeghorkikhai@gmail.com"

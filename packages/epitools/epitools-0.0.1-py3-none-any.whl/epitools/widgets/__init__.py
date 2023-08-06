1e057d72bfea96243380ba7e5ae1a7cc1af4dffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:18 +0000

    Add package epitools/0.0.1/epitools-0.0.1-py3-none-any.whl

---
+++ packages/epitools/epitools-0.0.1-py3-none-any.whl/epitools/widgets/__init__.py
@@ -0,0 +1,3 @@
+from epitools.widgets.cell_statistics import create_cell_statistics_widget
+from epitools.widgets.projection import create_projection_widget
+from epitools.widgets.segmentation import create_segmentation_widget

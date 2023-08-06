f29ac8e7356b3c7b811dc8f74ac9eaf9c3d2f377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:50 +0000

    Add package splight-cli/2.3.27/splight_cli-2.3.27-py3-none-any.whl

---
+++ packages/splight-cli/splight_cli-2.3.27-py3-none-any.whl/cli/component/exceptions.py
@@ -0,0 +1,34 @@
+from typing import Set
+
+
+class InvalidCSVColumns(Exception):
+    def __init__(self, columns: Set[str]):
+        self._msg = (
+            "CSV file does not have the necessary columns. "
+            f"The required columns are {columns}."
+        )
+
+    def __str__(self) -> str:
+        return self._msg
+
+
+class InvalidSplightCLIVersion(Exception):
+    def __init__(self, component_cli_version: str, cli_version: str):
+        self._msg = (
+            f"Component uses splight cli version {component_cli_version} but "
+            f"should be {cli_version}"
+        )
+
+    def __str__(self) -> str:
+        return self._msg
+
+
+class ReadmeExists(Exception):
+    def __init__(self, readme_path: str):
+        self._msg = (
+            f"\nReadme already exists at {readme_path}"
+            "\nRemove it or use --force to overwrite it"
+        )
+
+    def __str__(self) -> str:
+        return self._msg

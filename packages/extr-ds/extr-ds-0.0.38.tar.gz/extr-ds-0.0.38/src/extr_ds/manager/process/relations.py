ba1babbf2a4b7079246d8f8be4a0fa9d63746dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:01 +0000

    Add package extr-ds/0.0.38/extr-ds-0.0.38.tar.gz

---
+++ packages/extr-ds/extr-ds-0.0.38.tar.gz/extr-ds-0.0.38/src/extr_ds/manager/process/relations.py
@@ -0,0 +1,85 @@
+from typing import Dict, List
+import re
+import os
+import json
+
+from extr.entities import create_entity_extractor
+from extr.relations import RelationExtractor, HtmlRelationAnnotator
+from extr.utils import flatten
+from extr_ds.models import RelationLabel
+from extr_ds.labelers import RelationClassification
+
+from .workspace import WORKSPACE
+from ..utils.filesystem import load_data
+from ..utils import imports
+
+utils = imports.load_file(
+    'utils',
+    os.path.join(WORKSPACE, 'utils.py')
+)
+
+def get_labeler() -> RelationClassification:
+    labels = imports.load_file(
+        'labels',
+        os.path.join(WORKSPACE, 'labels.py')
+    )
+
+    entity_extractor = create_entity_extractor(labels.entity_patterns, labels.kb)
+    relation_extractor = RelationExtractor(labels.relation_patterns)
+
+    return RelationClassification(
+        utils.sentence_tokenizer,
+        entity_extractor,
+        relation_extractor,
+        labels.relation_defaults,
+    )
+
+def relate() -> None:
+    labeler = get_labeler()
+
+    relation_groups: Dict[str, List[RelationLabel]] = {}
+    for row in load_data(os.path.join(WORKSPACE, '2', 'dev.txt')):
+        text = utils.transform_text(row)
+        for relation_label in labeler.label(text):
+            if not relation_label.definition in relation_groups:
+                relation_groups[relation_label.definition] = []
+
+            relation_groups[relation_label.definition].append(relation_label)
+
+    blobs = list(map(lambda rl: rl.todict(), flatten(relation_groups.values())))
+    with open(os.path.join(WORKSPACE, '3', 'dev-rels.json'), 'w', encoding='utf-8') as relation_outputs:
+        relation_outputs.write(json.dumps(blobs, indent=2))
+
+    rows = []
+    html_annotator = HtmlRelationAnnotator()
+    for key, items in relation_groups.items():
+        rows.append(f'<tr><td class="header" colspan=2>{key}</td></tr>')
+        for relation_label in items:
+            text = html_annotator.annotate(
+                re.sub(r'</?e\d+>', '', relation_label.sentence),
+                relation_label.relation
+            )
+
+            rows.append(f'<tr><td class="label">{relation_label.relation.label}</td><td>{text}</td></tr>')
+
+    styles = """
+span.entity { border: 1px solid black; border-radius: 5px; padding: 5px; margin: 3px; cursor: pointer; }
+span.label { font-weight: bold; padding: 3px; color: black; }
+span.e1 { background-color: aqua; }
+span.e2 { background-color: coral; }
+td { line-height: 30px; border: 1px solid black; padding: 5px; }
+td.header { font-weight: bold; }
+td.label { font-weight: bold; text-align: center; }
+"""
+
+    html = """
+<html>
+    <head>
+        <style>""" + styles + """</style>
+    </head>
+    <body><table>""" + '\n'.join(rows) + """</table></body>
+</html>
+"""
+    html_path = os.path.join(WORKSPACE, '3', 'dev-rels.html')
+    with open(html_path, 'w', encoding='utf-8') as html_file:
+        html_file.write(html)

cef5b3acd40c2d35f4a2eb9c381a80686a494e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:52 +0000

    Add package research-client/0.4.2/research_client-0.4.2-py3-none-any.whl

---
+++ packages/research-client/research_client-0.4.2-py3-none-any.whl/research_client/datavalidator/__init__.py
@@ -0,0 +1,5 @@
+"""Simple data validation tools.
+
+This package contains a set of classes providing a simple interface for data validation
+and validation-based user feedback via Validator objects.
+"""

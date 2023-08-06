f994ee1c6d15e155b72c963ad7890f624de3bcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:05 +0000

    Add package austrakka/0.28.1/austrakka-0.28.1-py3-none-any.whl

---
+++ packages/austrakka/austrakka-0.28.1-py3-none-any.whl/austrakka/components/project/__init__.py
@@ -0,0 +1,31 @@
+# pylint: disable=expression-not-assigned
+from austrakka.utils.output import table_format_option
+from austrakka.utils.cmd_filter import hide_admin_cmds, show_admin_cmds
+from austrakka.components.project.funcs import list_projects, add_project
+
+from ...utils.options import *
+
+
+@click.group()
+@click.pass_context
+def project(ctx):
+    '''Commands related to projects'''
+    ctx.context = ctx.parent.context
+
+
+@project.command('add', hidden=hide_admin_cmds())
+@opt_abbrev()
+@opt_name(help="Project name")
+@opt_description(required=False)
+def project_add(abbrev, name, description):
+    '''
+    Add a new project to AusTrakka.
+    '''
+    add_project(abbrev, name, description)
+
+
+@project.command('list')
+@table_format_option()
+def projects_list(out_format: str):
+    '''List projects in AusTrakka'''
+    list_projects(out_format)

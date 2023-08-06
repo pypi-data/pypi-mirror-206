bf0fa5432f8feb41558887c8f5841d041f286a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:15 +0000

    Add package mango-cli/0.0.9a1/mango-cli-0.0.9a1.tar.gz

---
+++ packages/mango-cli/mango-cli-0.0.9a1.tar.gz/mango-cli-0.0.9a1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name='mango-cli',
+    version='0.0.9-alpha-1',
+    packages=find_packages(),
+    author='Mangosoft',
+    author_email="wilson.mendoza@mango-soft.com",
+    description="Mangosoft CLI",
+    py_modules=["app"],
+    include_dirs=["app"],
+    install_requires=[requirements],
+    entry_points='''
+        [console_scripts]
+        mango=app.application:cli
+    '''
+)
+    

195baee91b524bacb995ba7a2fda1e826f472736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:53 +0000

    Add package vocode/0.1.107/vocode-0.1.107-py3-none-any.whl

---
+++ packages/vocode/vocode-0.1.107-py3-none-any.whl/vocode/streaming/agent/restful_user_implemented_agent.py
@@ -0,0 +1,57 @@
+from .base_agent import BaseAgent
+from ..models.agent import (
+    RESTfulUserImplementedAgentConfig,
+    RESTfulAgentInput,
+    RESTfulAgentOutput,
+    RESTfulAgentOutputType,
+    RESTfulAgentText,
+)
+from typing import Generator, Optional, Tuple, cast
+import requests
+import logging
+import aiohttp
+
+
+class RESTfulUserImplementedAgent(BaseAgent):
+    def __init__(
+        self,
+        agent_config: RESTfulUserImplementedAgentConfig,
+        logger=None,
+    ):
+        super().__init__(agent_config)
+        if self.agent_config.generate_responses:
+            raise NotImplementedError(
+                "Use the WebSocket user implemented agent to stream responses"
+            )
+        self.agent_config = agent_config
+        self.logger = logger or logging.getLogger(__name__)
+
+    async def respond(
+        self,
+        human_input,
+        is_interrupt: bool = False,
+        conversation_id: Optional[str] = None,
+    ) -> Tuple[Optional[str], bool]:
+        config = self.agent_config.respond
+        try:
+            async with aiohttp.ClientSession() as session:
+                payload = RESTfulAgentInput(
+                    human_input=human_input, conversation_id=conversation_id
+                ).dict()
+                async with session.request(config.method, config.url, json=payload, timeout=aiohttp.ClientTimeout(total=15)) as response:
+                    assert response.status == 200
+                    output: RESTfulAgentOutput = RESTfulAgentOutput.parse_obj(await response.json())
+                    output_response = None
+                    should_stop = False
+                    if output.type == RESTfulAgentOutputType.TEXT:
+                        output_response = cast(RESTfulAgentText, output).response
+                    elif output.type == RESTfulAgentOutputType.END:
+                        should_stop = True
+                    return output_response, should_stop
+        except Exception as e:
+            self.logger.error(f"Error in response from RESTful agent: {e}")
+            return None, True
+
+    def generate_response(self, human_input, is_interrupt: bool = False) -> Generator:
+        """Returns a generator that yields a sentence at a time."""
+        raise NotImplementedError

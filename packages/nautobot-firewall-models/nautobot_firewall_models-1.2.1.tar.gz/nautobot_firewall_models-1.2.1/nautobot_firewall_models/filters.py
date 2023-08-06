9d9aedd41829862d6320b9643df7a1329edcef43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:41 +0000

    Add package nautobot-firewall-models/1.2.1/nautobot_firewall_models-1.2.1.tar.gz

---
+++ packages/nautobot-firewall-models/nautobot_firewall_models-1.2.1.tar.gz/nautobot_firewall_models-1.2.1/nautobot_firewall_models/filters.py
@@ -0,0 +1,250 @@
+"""Filtering for Firewall Model Plugin."""
+
+import django_filters
+from django.db.models import Q
+from django_filters import CharFilter
+from django_filters.filterset import FilterSet
+from nautobot.dcim.models import Device
+from nautobot.extras.filters import NautobotFilterSet
+from nautobot.extras.models import Status
+from nautobot.utilities.filters import TagFilter
+
+from nautobot_firewall_models import models
+
+
+class BaseFilterSet(FilterSet):
+    """A base class for adding the search method to models which only expose the `name` and `description` fields."""
+
+    q = CharFilter(
+        method="search",
+        label="Search",
+    )
+    status = django_filters.ModelMultipleChoiceFilter(
+        field_name="status__slug",
+        to_field_name="slug",
+        queryset=Status.objects.all(),
+    )
+
+    def search(self, queryset, name, value):  # pylint: disable=unused-argument, no-self-use
+        """Construct Q filter for filterset."""
+        if not value.strip():
+            return queryset
+        return queryset.filter(Q(name__icontains=value) | Q(description__icontains=value))
+
+
+class IPRangeFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for IPRange."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.IPRange
+
+        # fields = ["id", "start_address", "end_address", "vrf", "size", "description"]
+        fields = ["id", "vrf", "size", "description"]
+
+
+class FQDNFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for FQDN."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.FQDN
+
+        fields = ["id", "name", "description"]
+
+
+class AddressObjectFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for AddressObject."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.AddressObject
+
+        fields = ["id", "name", "ip_address", "prefix", "ip_range", "fqdn", "description"]
+
+
+class AddressObjectGroupFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for AddressObjectGroup."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.AddressObjectGroup
+
+        fields = ["id", "name", "address_objects", "description"]
+
+
+class ApplicationObjectFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for ApplicationObject."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.ApplicationObject
+
+        fields = ["id", "name", "description", "category", "subcategory", "risk"]
+
+
+class ApplicationObjectGroupFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for ApplicationObjectGroup."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.ApplicationObjectGroup
+
+        fields = ["id", "name", "application_objects", "description"]
+
+
+class ServiceObjectFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for ServiceObject."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.ServiceObject
+
+        fields = ["id", "name", "ip_protocol", "port", "description"]
+
+
+class ServiceObjectGroupFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for ServiceObjectGroup."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.ServiceObjectGroup
+
+        fields = ["id", "name", "service_objects", "description"]
+
+
+class UserObjectFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for UserObject."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.UserObject
+        fields = ["id", "name", "username"]
+
+
+class UserObjectGroupFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for UserObjectGroup."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.UserObjectGroup
+        fields = ["id", "name", "user_objects", "description"]
+
+
+class ZoneFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for Zone."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.Zone
+
+        fields = ["id", "name", "vrfs", "interfaces", "description"]
+
+
+# TODO: Refactor to allow for better filtering, currently very limited.
+class PolicyRuleFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for PolicyRule."""
+
+    tag = TagFilter()
+
+    q = CharFilter(
+        method="search",
+        label="Search",
+    )
+
+    def search(self, queryset, name, value):  # pylint: disable=unused-argument, no-self-use
+        """Construct Q filter for filterset."""
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=value) | Q(description__icontains=value) | Q(request_id__icontains=value)
+        )
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.PolicyRule
+        fields = ["id", "action", "log", "request_id", "description", "name"]
+
+
+# TODO: Refactor to allow for better filtering, currently very limited.
+class NATPolicyRuleFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for NATPolicyRule."""
+
+    tag = TagFilter()
+
+    q = CharFilter(
+        method="search",
+        label="Search",
+    )
+
+    def search(self, queryset, name, value):  # pylint: disable=unused-argument, no-self-use
+        """Construct Q filter for filterset."""
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=value) | Q(description__icontains=value) | Q(request_id__icontains=value)
+        )
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.NATPolicyRule
+        fields = ["id", "remark", "log", "request_id", "name"]
+
+
+class PolicyFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for Policy."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.Policy
+        fields = ["id", "name", "description", "policy_rules", "assigned_devices", "assigned_dynamic_groups"]
+
+    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
+        """Overload init to allow for deep=True on detail API call."""
+        super().__init__(data, queryset, request=request, prefix=prefix)
+        self.data.pop("deep", None)
+
+
+class NATPolicyFilterSet(BaseFilterSet, NautobotFilterSet):
+    """Filter for NATPolicy."""
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.NATPolicy
+        fields = ["id", "name", "description", "nat_policy_rules", "assigned_devices", "assigned_dynamic_groups"]
+
+
+class CapircaPolicyFilterSet(NautobotFilterSet):
+    """Filter for CapircaPolicy."""
+
+    device = django_filters.ModelMultipleChoiceFilter(
+        field_name="device__name",
+        queryset=Device.objects.all(),
+        to_field_name="name",
+        label="Device Name",
+    )
+    device_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=Device.objects.all(),
+        label="Device ID",
+    )
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.CapircaPolicy
+        fields = ["id"]
---
+++ packages/nautobot-firewall-models/nautobot_firewall_models-1.2.1.tar.gz/nautobot_firewall_models-1.2.1/nautobot_firewall_models/tests/test_api.py
@@ -0,0 +1,600 @@
+"""Unit tests for API views."""
+# flake8: noqa: F403,405
+from unittest import skip
+from nautobot.utilities.testing import APIViewTestCases
+from nautobot.ipam.models import Prefix
+
+from nautobot_firewall_models import models
+from .fixtures import create_env, create_ip_range, create_fqdn
+
+
+class IPRangeAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the IPRange viewsets."""
+
+    model = models.IPRange
+    brief_fields = ["display", "end_address", "id", "start_address", "url"]
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+
+        cls.create_data = [
+            {"start_address": "10.0.0.1", "end_address": "10.0.0.3"},
+            {"start_address": "10.0.0.4", "end_address": "10.0.0.10"},
+        ]
+        create_ip_range()
+
+    @skip("Not implemented")
+    def test_notes_url_on_object(self):
+        pass
+
+
+class FQDNAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the Protocol viewsets."""
+
+    model = models.FQDN
+    brief_fields = ["display", "id", "name", "url"]
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+
+        cls.create_data = [
+            {"name": "test.local"},
+            {"name": "sub.test.local"},
+        ]
+        create_fqdn()
+
+    @skip("Not implemented")
+    def test_notes_url_on_object(self):
+        pass
+
+
+class ApplicationObjectAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the ApplicationObject viewsets."""
+
+    model = models.ApplicationObject
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+
+        cls.create_data = [
+            {"name": "obj2", "risk": 1},
+            {"name": "obj1", "risk": 1},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class ApplicationObjectGroupAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the ApplicationObjectGroup viewsets."""
+
+    model = models.ApplicationObjectGroup
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        app_obj = models.ApplicationObject.objects.first()
+
+        cls.create_data = [
+            {"name": "test1", "application_objects": [app_obj.id]},
+            {"name": "test2", "application_objects": [app_obj.id]},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+
+class AddressObjectAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the AddressObject viewsets."""
+
+    model = models.AddressObject
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        ip_range = models.IPRange.objects.first()
+        prefix = Prefix.objects.first()
+
+        cls.create_data = [
+            {"name": "obj2", "prefix": prefix.id},
+            {"name": "obj1", "ip_range": ip_range.id},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class AddressObjectGroupAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the AddressObjectGroup viewsets."""
+
+    model = models.AddressObjectGroup
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        addr_obj = models.AddressObject.objects.first()
+
+        cls.create_data = [
+            {"name": "test1", "address_objects": [addr_obj.id]},
+            {"name": "test2", "address_objects": [addr_obj.id]},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+    def test_create_object(self):
+        self.validation_excluded_fields = ["address_objects"]
+        return super().test_create_object()
+
+    def test_bulk_create_objects(self):
+        self.validation_excluded_fields = ["address_objects"]
+        return super().test_bulk_create_objects()
+
+
+class ServiceObjectAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the ServiceObject viewsets."""
+
+    model = models.ServiceObject
+    bulk_update_data = {"description": "test update description"}
+    choices_fields = ["ip_protocol"]
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+
+        cls.create_data = [
+            {"name": "HTTP", "port": "8088", "ip_protocol": "TCP"},
+            {"name": "HTTP", "port": "8080-8088", "ip_protocol": "TCP"},
+        ]
+        create_env()
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class ServiceGroupAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the ServiceGroup viewsets."""
+
+    model = models.ServiceObjectGroup
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        svc_obj = models.ServiceObject.objects.first()
+
+        cls.create_data = [
+            {"name": "test1", "service_objects": [svc_obj.id]},
+            {"name": "test2", "service_objects": [svc_obj.id]},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+    def test_create_object(self):
+        self.validation_excluded_fields = ["service_objects"]
+        return super().test_create_object()
+
+    def test_bulk_create_objects(self):
+        self.validation_excluded_fields = ["service_objects"]
+        return super().test_bulk_create_objects()
+
+
+class UserObjectAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the User viewsets."""
+
+    model = models.UserObject
+    bulk_update_data = {"name": "User Name 123"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+
+        cls.create_data = [
+            {"username": "test1", "name": "Foo"},
+            {"username": "test2", "name": "Bar"},
+        ]
+        create_env()
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class UserObjectGroupAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the UserGroup viewsets."""
+
+    model = models.UserObjectGroup
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        user = models.UserObject.objects.first()
+
+        cls.create_data = [
+            {"name": "test1", "user_objects": [user.id]},
+            {"name": "test2", "user_objects": [user.id]},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+    def test_create_object(self):
+        self.validation_excluded_fields = ["user_objects"]
+        return super().test_create_object()
+
+    def test_bulk_create_objects(self):
+        self.validation_excluded_fields = ["user_objects"]
+        return super().test_bulk_create_objects()
+
+
+class ZoneAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the Zone viewsets."""
+
+    model = models.Zone
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+
+        cls.create_data = [
+            {"name": "trust"},
+            {"name": "untrust"},
+        ]
+        create_env()
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class PolicyRuleAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the PolicyRule viewsets."""
+
+    model = models.PolicyRule
+    bulk_update_data = {"log": False}
+    choices_fields = ["action"]
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        src_usr = models.UserObject.objects.first()
+        src_addr = models.AddressObject.objects.first()
+        dest_addr = models.AddressObject.objects.last()
+
+        svc = models.ServiceObject.objects.first()
+        cls.create_data = [
+            {
+                # pylint: disable=R0801
+                "source_users": [src_usr.id],
+                "source_addresses": [src_addr.id],
+                "destination_addresses": [dest_addr.id],
+                "action": "deny",
+                "log": True,
+                "destination_services": [svc.id],
+                "name": "test rule",
+            },
+            {
+                "source_users": [src_usr.id],
+                "source_addresses": [src_addr.id],
+                "destination_addresses": [dest_addr.id],
+                "action": "deny",
+                "log": False,
+                "destination_services": [svc.id],
+                "name": "test rule",
+            },
+        ]
+
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+
+class PolicyAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the Policy viewsets."""
+
+    model = models.Policy
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        pol_rule = models.PolicyRule.objects.first()
+
+        cls.create_data = [
+            {"name": "test 1", "policy_rules": [pol_rule.id]},
+            {"name": "test 2", "policy_rules": [pol_rule.id], "description": "Test desc"},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    def test_create_object(self):
+        self.validation_excluded_fields = ["policy_rules"]
+        return super().test_create_object()
+
+    def test_update_object(self):
+        self.validation_excluded_fields = ["policy_rules"]
+        return super().test_update_object()
+
+    def test_bulk_create_objects(self):
+        self.validation_excluded_fields = ["policy_rules"]
+        return super().test_bulk_create_objects()
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+
+class NATPolicyRuleAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the PolicyRule viewsets."""
+
+    model = models.NATPolicyRule
+    bulk_update_data = {"log": False}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        src_addr = models.AddressObject.objects.first()
+        dest_addr = models.AddressObject.objects.last()
+
+        svc = models.ServiceObject.objects.first()
+        cls.create_data = [
+            {
+                "original_source_addresses": [src_addr.id],
+                "original_destination_addresses": [dest_addr.id],
+                "translated_destination_addresses": [src_addr.id],
+                "log": True,
+                "original_destination_services": [svc.id],
+                "name": "test rule",
+            },
+            {
+                "original_source_addresses": [src_addr.id],
+                "original_destination_addresses": [dest_addr.id],
+                "translated_destination_addresses": [src_addr.id],
+                "log": False,
+                "original_destination_services": [svc.id],
+                "name": "test rule",
+            },
+        ]
+
+    @skip("not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass
+
+    @skip("Incompatible with nested serializer starting in 1.5.18")
+    def test_update_object(self):
+        pass
+
+
+class NATPolicyAPIViewTest(APIViewTestCases.APIViewTestCase):
+    """Test the Policy viewsets."""
+
+    model = models.NATPolicy
+    bulk_update_data = {"description": "test update description"}
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data for API calls."""
+        create_env()
+        nat_pol_rule = models.NATPolicyRule.objects.first()
+
+        cls.create_data = [
+            {"name": "test 1", "nat_policy_rules": [nat_pol_rule.id]},
+            {"name": "test 2", "nat_policy_rules": [nat_pol_rule.id], "description": "Test desc"},
+        ]
+
+    @skip("Not implemented")
+    def test_list_objects_brief(self):
+        pass
+
+    def test_create_object(self):
+        self.validation_excluded_fields = ["nat_policy_rules"]
+        return super().test_create_object()
+
+    def test_update_object(self):
+        self.validation_excluded_fields = ["nat_policy_rules"]
+        return super().test_update_object()
+
+    def test_bulk_create_objects(self):
+        self.validation_excluded_fields = ["nat_policy_rules"]
+        return super().test_bulk_create_objects()
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_bulk_delete_objects(self):
+        pass
+
+    @skip("on_delete set to PROTECT")
+    def test_delete_object_without_permission(self):
+        pass

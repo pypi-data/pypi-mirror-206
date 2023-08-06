23b035db81fa926f5c410b03d37799581e2c731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:59 +0000

    Add package duckcloud/0.1.0/duckcloud-0.1.0.tar.gz

---
+++ packages/duckcloud/duckcloud-0.1.0.tar.gz/duckcloud-0.1.0/duckcloud/VM.py
@@ -0,0 +1,55 @@
+import requests, urllib, socketio
+
+class VMstream:
+	def __init__(self, VM, connectcallback, msgcallback):
+		self.vm = VM
+		self.sio = socketio.Client()
+		self.connectcallback = connectcallback
+		self.msgcallback = msgcallback
+		@self.sio.event
+		def connect():
+			self.sio.emit("vmselect", (self.vm.index,))
+			self.connectcallback(self)
+		@self.sio.event
+		def datad(data):
+			self.msgcallback(self,data)
+	def send(self, data):
+		self.sio.emit("datad", data)
+	def open(self):
+		self.sio.connect("https://duckcloud.pcprojects.tk", headers={"cookie": "token="+self.vm.token})
+		self.sio.wait()
+	def close(self):
+		self.sio.disconnect()
+		del self
+	
+
+class VM:
+	def __init__(self, index, data, token):
+		self.token = token
+		self.index = index
+		self.name = data["vmname"]
+		self.name_encoded = data["vmname_encoded"]
+		self.status = data["status"]
+	def delete(self):
+		r = requests.get("https://duckcloud.pcprojects.tk/burn/"+str(self.index), headers={"cookie": "token="+self.token}, allow_redirects=False)
+		print(r.status)
+		return True # i will handle this stuff later
+	def rename(self, newname):
+		r = requests.post("https://duckcloud.pcprojects.tk/ren/"+str(self.index), json={"vmname": newname}, headers={"cookie": "token="+self.token}, allow_redirects=False)
+		if r.headers["Location"] == "/settings/"+str(self.index):
+			return True
+		else:
+			return False
+	def toggle(self):
+		r = requests.get("https://duckcloud.pcprojects.tk/shutoff/"+str(self.index), headers={"cookie": "token="+self.token}, allow_redirects=False)
+		if "Location" in r.headers:
+			if r.headers["Location"] == "/settings/"+str(self.index):
+				if self.status == "online": self.status = "offline"
+				if self.status == "offline": self.status = "online"
+				return True
+			else:
+				return False
+		else:
+			return False
+	def getStream(self, connect, msg):
+		return VMstream(self, connect, msg)

5cc9bf3a441a145e2f8ad4b55c756a2b8e04c5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:38 +0000

    Add package sanskarinfo/0.1/sanskarinfo-0.1.tar.gz

---
+++ packages/sanskarinfo/sanskarinfo-0.1.tar.gz/sanskarinfo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sanskarinfo',
+    version='0.1',
+    author='sanskar khanna',
+    author_email='sanskarkhanna1@gamil.com',
+    description='information of sanskar khanna',
+    packages=[],
+    install_requires=[],
+)

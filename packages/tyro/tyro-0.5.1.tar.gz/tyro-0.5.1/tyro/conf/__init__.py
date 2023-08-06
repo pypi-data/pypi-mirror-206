983ad7f9454d97917172e7c46e6917d690ac3218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:03 +0000

    Add package tyro/0.5.1/tyro-0.5.1.tar.gz

---
+++ packages/tyro/tyro-0.5.1.tar.gz/tyro-0.5.1/tyro/conf/__init__.py
@@ -0,0 +1,38 @@
+"""The :mod:`tyro.conf` submodule contains helpers for attaching parsing-specific
+configuration metadata to types via [PEP 593](https://peps.python.org/pep-0593/) runtime
+annotations.
+
+Configuration flags are applied recursively, and should generally be subscripted:
+`Fixed[T]`, `Suppress[T]`, etc.
+
+Features here are supported, but generally unnecessary and should be used sparingly.
+"""
+
+from ._confstruct import arg, subcommand
+from ._markers import (
+    AvoidSubcommands,
+    ConsolidateSubcommandArgs,
+    Fixed,
+    FlagConversionOff,
+    OmitSubcommandPrefixes,
+    Positional,
+    Suppress,
+    SuppressFixed,
+    UseAppendAction,
+    configure,
+)
+
+__all__ = [
+    "arg",
+    "subcommand",
+    "AvoidSubcommands",
+    "ConsolidateSubcommandArgs",
+    "Fixed",
+    "FlagConversionOff",
+    "OmitSubcommandPrefixes",
+    "Positional",
+    "Suppress",
+    "SuppressFixed",
+    "UseAppendAction",
+    "configure",
+]

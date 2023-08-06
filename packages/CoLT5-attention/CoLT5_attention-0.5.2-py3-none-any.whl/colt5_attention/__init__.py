ccb5df822709f83c1f4edfa315d22cdac7d6a1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:23 +0000

    Add package CoLT5-attention/0.5.2/CoLT5_attention-0.5.2-py3-none-any.whl

---
+++ packages/CoLT5-attention/CoLT5_attention-0.5.2-py3-none-any.whl/colt5_attention/__init__.py
@@ -0,0 +1,12 @@
+from colt5_attention.transformer_block import (
+    ConditionalRoutedFeedForward,
+    ConditionalRoutedAttention,
+    ConditionalRoutedAutoregressiveAttention,
+    ConditionalRoutedCrossAttention,
+    ConditionalRoutedTransformerBlock,
+    DifferentiableTopKRouter,
+    SinkhornRouter,
+    CoordinateDescentRouter
+)
+
+from colt5_attention.coor_descent import coor_descent

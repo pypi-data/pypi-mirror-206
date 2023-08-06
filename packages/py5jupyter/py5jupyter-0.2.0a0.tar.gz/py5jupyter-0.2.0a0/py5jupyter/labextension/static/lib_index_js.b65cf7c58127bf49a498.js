e63e0886ea07e42c84df342cfe3b9a879fb05310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:13 +0000

    Add package py5jupyter/0.2.0a0/py5jupyter-0.2.0a0.tar.gz

---
+++ packages/py5jupyter/py5jupyter-0.2.0a0.tar.gz/py5jupyter-0.2.0a0/py5jupyter/labextension/static/lib_index_js.b65cf7c58127bf49a498.js
@@ -0,0 +1,48 @@
+"use strict";
+(self["webpackChunkjupyter_py5"] = self["webpackChunkjupyter_py5"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+// *****************************************************************************
+//
+//   Part of the py5jupyter (& py5) library
+//   Copyright (C) 2022-2023 Jim Schmitz
+//
+//   This library is free software: you can redistribute it and/or modify it
+//   under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation, either version 2.1 of the License, or (at
+//   your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but
+//   WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with this library. If not, see <https://www.gnu.org/licenses/>.
+//
+// *****************************************************************************
+__exportStar(__webpack_require__(/*! ./version */ "./lib/version.js"), exports);
+__exportStar(__webpack_require__(/*! ./widget */ "./lib/widget.js"), exports);
+//# sourceMappingURL=index.js.map
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.b65cf7c58127bf49a498.js.map

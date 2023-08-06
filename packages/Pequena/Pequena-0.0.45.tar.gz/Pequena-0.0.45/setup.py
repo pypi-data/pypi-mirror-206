01ae05edb5cdbe27e8fcc7ec007e5c5870354a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:29 +0000

    Add package Pequena/0.0.45/Pequena-0.0.45.tar.gz

---
+++ packages/Pequena/Pequena-0.0.45.tar.gz/Pequena-0.0.45/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.45"
+DESCRIPTION = 'Lightweight desktop app framework'
+with open("Readme.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="Pequena",
+    version=VERSION,
+    author="borecjeborec1",
+    author_email="<atzuki@protonmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'pythonnet ; sys_platform == "win32"',
+        'pyobjc-core ; sys_platform == "darwin"',
+        'pyobjc-framework-Cocoa ; sys_platform == "darwin"',
+        'pyobjc-framework-WebKit ; sys_platform == "darwin"',
+        'QtPy ; sys_platform == "openbsd6"',
+        'importlib_resources; python_version < "3.7"',
+        'proxy_tools',
+        'bottle'
+    ],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

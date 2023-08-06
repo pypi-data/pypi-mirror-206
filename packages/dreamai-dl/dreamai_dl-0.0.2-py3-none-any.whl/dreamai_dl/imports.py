600128beba5952cc906fe931dc93d4f7ed5223f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package dreamai-dl/0.0.2/dreamai_dl-0.0.2-py3-none-any.whl

---
+++ packages/dreamai-dl/dreamai_dl-0.0.2-py3-none-any.whl/dreamai_dl/imports.py
@@ -0,0 +1,19 @@
+import timm
+import torch
+import torch.nn as nn
+import lightning as L
+import torchmetrics as tm
+import torch.nn.functional as F
+from torchvision.utils import make_grid
+from torchvision import transforms, models
+from torch.utils.data import Dataset, DataLoader
+
+from dreamai.core import *
+from dreamai.vision import *
+from dreamai.imports import *
+
+from fastai.torch_core import Module
+from fastai.vision.core import imagenet_stats
+from fastai.vision.core import image2tensor as img_to_tensor
+from fastai.vision.learner import has_pool_type, create_head, cut_model
+from fastai.layers import LinBnDrop, Flatten, AdaptiveConcatPool2d, SigmoidRange

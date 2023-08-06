d10207ad8af97caf579996d3811813ac5d790c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:37 +0000

    Add package season/2.2.6/season-2.2.6-py3-none-any.whl

---
+++ packages/season/season-2.2.6-py3-none-any.whl/season/core/__init__.py
@@ -0,0 +1,6 @@
+from season.core import builder
+from season.core import exception
+from season.core.config import Config as config
+from season.core.http import HTTP as http
+from season.core.socket import Socket as socket
+from season.core.wiz import Wiz as wiz

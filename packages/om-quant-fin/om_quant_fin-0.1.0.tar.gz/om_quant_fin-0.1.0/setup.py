c0bb95a8602168c943d594bd500cd2d3aaa36728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:42 +0000

    Add package om-quant-fin/0.1.0/om_quant_fin-0.1.0.tar.gz

---
+++ packages/om-quant-fin/om_quant_fin-0.1.0.tar.gz/om_quant_fin-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from setuptools import setup
+from os import path
+
+# Read the contents of the README.md file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name = "om_quant_fin",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = [
+        "yfinance",
+        "pandas",
+        "scikit-learn",
+        "numpy"
+    ],
+    author = "Outspoken Market",
+    author_email = "info@outspokenmarket.com",
+    description = "A simple quantitative trading library for the OMNP Class",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
+

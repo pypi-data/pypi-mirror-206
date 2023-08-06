562a05e46255a2d41f502e10c16d12a602af2b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:47 +0000

    Add package datoso-seed-sfc-speedhacks/0.2.3/datoso_seed_sfc_speedhacks-0.2.3.tar.gz

---
+++ packages/datoso-seed-sfc-speedhacks/datoso_seed_sfc_speedhacks-0.2.3.tar.gz/datoso_seed_sfc_speedhacks-0.2.3/src/datoso_seed_sfc_speedhacks/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__.py
+"""
+__all__ = ["__version__", "__author__", "__description__"]
+__version__ = "0.2.3"
+__author__ = "Lacides Miranda"
+__description__ = "Super Famicom Speed Hacks"
+__preffix__ = "sfc_speedhacks"

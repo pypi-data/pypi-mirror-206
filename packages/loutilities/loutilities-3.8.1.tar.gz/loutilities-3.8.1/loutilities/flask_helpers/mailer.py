5368dafefa20567924865376ef9e6b65e2a2ffc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:17 +0000

    Add package loutilities/3.8.1/loutilities-3.8.1.tar.gz

---
+++ packages/loutilities/loutilities-3.8.1.tar.gz/loutilities-3.8.1/loutilities/flask_helpers/mailer.py
@@ -0,0 +1,47 @@
+'''
+mailer - send email
+================================================
+'''
+# standard
+
+# pypi
+from flask import current_app
+from flask_mail import Message
+
+debug = False
+
+#----------------------------------------------------------------------
+def sendmail(subject, fromaddr, toaddr, html, text='', ccaddr=None, replytoaddr=None):
+#----------------------------------------------------------------------
+    '''
+    send mail
+
+    :param subject: subject of email
+    :param fromaddr: from address to use
+    :param toaddr: to address to use, may be list of addresses or comma separated
+    :param html: html to send
+    :param text: optional text alternative to send
+    :param ccaddr: optional cc address to use, may be list of addresses or comma separated
+    :param replytoaddr: optional reply_to address to use, may be list of addresses or comma separated
+    :returns: response from flask_mail.send
+    '''
+    stubbed = current_app.config.get('MAIL_SUPPRESS_SEND', current_app.config.get('TESTING', False))
+    stubbed_txt = '[STUBBED] ' if stubbed else ''
+    current_app.logger.info(f'{stubbed_txt}sendmail(): from={fromaddr}, to={toaddr}, cc={ccaddr}, subject="{subject}"')
+
+    # get current app's mailer
+    mail = current_app.extensions.get('mail')
+
+    message = Message(
+        sender=fromaddr,
+        recipients=toaddr if isinstance(toaddr, list) else [a.strip() for a in toaddr.split(',')],
+        cc=ccaddr if not ccaddr or isinstance(ccaddr, list) else [a.strip() for a in ccaddr.split(',')],
+        reply_to=replytoaddr if not replytoaddr or isinstance(replytoaddr, list) else [a.strip() for a in replytoaddr.split(',')],
+        subject=subject,
+        html=html,
+        body=text,
+    )
+
+    mail.send(message)
+
+    if debug: current_app.logger.debug('sendmail(): message.html={}'.format(message.html))

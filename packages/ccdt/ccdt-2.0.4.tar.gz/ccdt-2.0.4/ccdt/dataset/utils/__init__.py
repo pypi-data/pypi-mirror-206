2195a133d04f1f0d07c160ff6bbf4656d9c8458f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:29 +0000

    Add package ccdt/2.0.4/ccdt-2.0.4.tar.gz

---
+++ packages/ccdt/ccdt-2.0.4.tar.gz/ccdt-2.0.4/ccdt/dataset/utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/3/31 13:47
+# @Author : Zhan Yong
+# @System : jk
+from .encoder import Encoder
+from .labelme_load import LabelmeLoad
+
+__all__ = ['Encoder', 'LabelmeLoad']

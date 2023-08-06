ee787d880a48d961c99cdf8ce4cf18d1e79fcdaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package gotji/1.0.6/gotji-1.0.6.tar.gz

---
+++ packages/gotji/gotji-1.0.6.tar.gz/gotji-1.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gotji",
+    version="1.0.6",
+    description="Dev by Meoaw",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exaDev99/gotji",
+    author="exaDev99",
+    author_email="meoawdev.me@gmail.com",
+    license="MIT",
+    packages=["gotji"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "pystyle",
+        "platform",
+        "os",
+        "replit",
+        "flask",
+        "flask_restful"
+    ],
+)

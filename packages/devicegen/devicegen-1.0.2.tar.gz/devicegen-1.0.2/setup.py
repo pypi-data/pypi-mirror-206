93c7f064f46d59151ec911c0db830535137b6626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:00 +0000

    Add package devicegen/1.0.2/devicegen-1.0.2.tar.gz

---
+++ packages/devicegen/devicegen-1.0.2.tar.gz/devicegen-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devicegen",
+    packages=find_packages(),
+    version="1.0.2",
+    license='GPLv3+',
+    author="Pericles Philippopoulos, Felix Beaudoin",
+    author_email="pericles@nanoacademic.com",
+    url="https://github.com/nanoacademic/devicegen",
+    description="Device generator for gated quantum devices",
+    long_description="Devicegen is a gated heterostructure device generator. It facilitates the creation of Gmsh mesh files corresponding to a gate geometry defined in a GDS layout text file.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Scientific/Engineering",
+    ],
+)

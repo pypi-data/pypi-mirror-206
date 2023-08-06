7f5a20b1b674f168a3e1e2d3b4ca1771c7786a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:32 +0000

    Add package jaco2/2.8/jaco2-2.8.tar.gz

---
+++ packages/jaco2/jaco2-2.8.tar.gz/jaco2-2.8/robotControl/robotControl.py
@@ -0,0 +1,27 @@
+import jaco2
+
+class robotControl:
+    def init_robot():
+        jaco2.init_robot()
+    
+    def close_robot():
+        jaco2.close_robot
+
+    def init_fingers():
+        jaco2.init_fingers()
+
+    def send_basic_trajectory():
+        jaco2.send_basic_trajectory()
+    
+    def move_home():
+        jaco2.move_home()
+    
+    def set_type_velocity():
+        jaco2.set_type_velocity()
+    
+    def set_type_position():
+        jaco2.set_type_position()
+    
+    def get_general_informations():
+        return(jaco2.get_general_informations())
+

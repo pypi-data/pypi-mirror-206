7c7156d759a37248cd68a05245c2a264ea3b90be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:44 +0000

    Add package pyasd/0.0.4/pyasd-0.0.4-py3-none-any.whl

---
+++ packages/pyasd/pyasd-0.0.4-py3-none-any.whl/asd/__init__.py
@@ -0,0 +1,7 @@
+__built_time__        =  "built at Wed May  3 19:06:49 2023 CST"
+__name__              =  "pyasd"
+__version__           =  "0.0.4"
+__author__            =  "Shunhong Zhang"
+__author_email__      =  "zhangshunhong.pku@gmail.com"
+__url__               =  "https://to_be_posted"
+__license__           =  "LICENSE"

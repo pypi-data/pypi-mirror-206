f0655f546120165c9ce6bef1ea2b7ddf2697105c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:06 +0000

    Add package UtilsRL/0.5.8/UtilsRL-0.5.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/UtilsRL/UtilsRL-0.5.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/UtilsRL/__init__.py
@@ -0,0 +1,15 @@
+
+__version__ = "0.5.8"
+
+from UtilsRL import data_structure, env, exp, logger, math, misc, plot, rl, monitor
+__all__ = [
+    "data_structure", 
+    "env", 
+    "exp", 
+    "logger", 
+    "math", 
+    "misc", 
+    "plot", 
+    "rl", 
+    "monitor"
+]

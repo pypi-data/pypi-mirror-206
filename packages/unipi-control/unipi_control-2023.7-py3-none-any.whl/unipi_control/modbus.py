821975a15e4b51d73c18ec470d385963ebd6f6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:54 +0000

    Add package unipi-control/2023.7/unipi_control-2023.7-py3-none-any.whl

---
+++ packages/unipi-control/unipi_control-2023.7-py3-none-any.whl/unipi_control/modbus.py
@@ -0,0 +1,130 @@
+import asyncio
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import NamedTuple
+from typing import Optional
+
+from pymodbus.client import AsyncModbusSerialClient
+from pymodbus.client import AsyncModbusTcpClient
+from pymodbus.exceptions import ModbusException
+from pymodbus.pdu import ModbusResponse
+
+from unipi_control.config import HardwareData
+from unipi_control.config import HardwareDefinition
+from unipi_control.config import LogPrefix
+from unipi_control.config import logger
+
+
+async def check_modbus_call(callback: Callable, data: dict) -> Optional[ModbusResponse]:
+    """Check modbus read/write call has errors and log the errors.
+
+    Parameters
+    ----------
+    callback: Callable
+        modbus callback function e.g. read_input_registers()
+    data: dict
+        Arguments pass to the callback function
+
+    Returns
+    -------
+    ModbusResponse: optional
+        Return modbus response if no errors found else None.
+    """
+    response: Optional[ModbusResponse] = None
+
+    try:
+        response = await callback(**data)
+
+        if response and response.isError():
+            response = None
+    except ModbusException as error:
+        logger.error("%s %s", LogPrefix.MODBUS, error)
+    except asyncio.exceptions.TimeoutError:
+        logger.error("%s Timeout on: %s", LogPrefix.MODBUS, data)
+
+    return response
+
+
+class ModbusClient(NamedTuple):
+    tcp: AsyncModbusTcpClient
+    serial: AsyncModbusSerialClient
+
+
+class ModbusCacheData:
+    """Class that scan modbus register blocks and cache the response.
+
+    Attributes
+    ----------
+    modbus_client: ModbusClient
+        A modbus client.
+    hardware: HardwareData
+        The Unipi Neuron hardware definitions.
+    """
+
+    def __init__(self, modbus_client: ModbusClient, hardware: HardwareData) -> None:
+        self.modbus_client: ModbusClient = modbus_client
+        self.hardware: HardwareData = hardware
+
+        self.data: Dict[int, Dict[int, int]] = {}
+
+    async def _save_response(self, scan_type: str, modbus_register_block: dict, definition: HardwareDefinition) -> None:
+        data: dict = {
+            "address": modbus_register_block["start_reg"],
+            "count": modbus_register_block["count"],
+            "slave": modbus_register_block.get("slave", definition.unit),
+        }
+
+        response: Optional[ModbusResponse] = None
+
+        if scan_type == "tcp":
+            response = await check_modbus_call(self.modbus_client.tcp.read_input_registers, data)
+        elif scan_type == "serial":
+            response = await check_modbus_call(self.modbus_client.serial.read_input_registers, data)
+
+        if registers := getattr(response, "registers", None):
+            for index in range(data["count"]):
+                self.data[definition.unit][
+                    data["address"] + index
+                ] = registers[  # pylint: disable=unsubscriptable-object
+                    index
+                ]
+
+    async def scan(self, scan_type: str, hardware_types: List[str]) -> None:
+        """Read modbus register blocks and cache the response."""
+        for definition in self.hardware.get_definition_by_hardware_types(hardware_types):
+            if not self.data.get(definition.unit):
+                self.data[definition.unit] = {}
+
+            if scan_type == "serial":
+                await asyncio.sleep(1)
+
+            for modbus_register_block in definition.modbus_register_blocks:
+                await self._save_response(scan_type, modbus_register_block, definition)
+
+    def get_register(self, address: int, index: int, unit: int) -> list:
+        """Get the responses from the cached modbus register blocks.
+
+        Parameters
+        ----------
+        address: int
+            The starting address to read from.
+        index: int
+            The number of registers to read.
+        unit: int
+            The unit this request is targeting.
+
+        Returns
+        -------
+        list
+            A list of cached modbus register blocks.
+        """
+        ret: list = []
+
+        for _address in range(address, address + index):
+            if _address not in self.data[unit] or self.data[unit][_address] is None:
+                logger.error("%s Error on address %s (unit: %s)", LogPrefix.MODBUS, address, unit)
+            else:
+                ret += [self.data[unit][_address]]
+
+        return ret
---
+++ packages/unipi-control/unipi_control-2023.7-py3-none-any.whl/unipi_control/unipi_control.py
@@ -0,0 +1,188 @@
+import argparse
+import asyncio
+import uuid
+from asyncio import Task
+from contextlib import AsyncExitStack
+from pathlib import Path
+from typing import Optional
+from typing import Set
+
+import sys
+from asyncio_mqtt import Client
+from pymodbus.client import AsyncModbusSerialClient
+from pymodbus.client import AsyncModbusTcpClient
+
+from superbox_utils.argparse import init_argparse
+from superbox_utils.config.exception import ConfigException
+from superbox_utils.core.exception import UnexpectedException
+from superbox_utils.mqtt.connect import mqtt_connect
+from superbox_utils.text.text import slugify
+from unipi_control.config import Config
+from unipi_control.config import DEFAULT_CONFIG_PATH
+from unipi_control.config import LogPrefix
+from unipi_control.config import logger
+from unipi_control.integrations.covers import CoverMap
+from unipi_control.modbus import ModbusClient
+from unipi_control.mqtt.discovery.binary_sensors import HassBinarySensorsMqttPlugin
+from unipi_control.mqtt.discovery.covers import HassCoversMqttPlugin
+from unipi_control.mqtt.discovery.sensors import HassSensorsMqttPlugin
+from unipi_control.mqtt.discovery.switches import HassSwitchesMqttPlugin
+from unipi_control.mqtt.features import MeterFeaturesMqttPlugin
+from unipi_control.mqtt.features import NeuronFeaturesMqttPlugin
+from unipi_control.mqtt.integrations.covers import CoversMqttPlugin
+from unipi_control.neuron import Neuron
+from unipi_control.version import __version__
+
+
+class UnipiControl:
+    """Control Unipi I/O directly with MQTT commands.
+
+    Unipi Control use Modbus for fast access to the I/O and provide MQTT
+    topics for reading and writing the circuits. Optionally you can enable
+    the Home Assistant MQTT discovery for binary sensors, sensors, switches and covers.
+    """
+
+    def __init__(self, config: Config, modbus_client: ModbusClient) -> None:
+        self.config: Config = config
+        self.modbus_client: ModbusClient = modbus_client
+        self.neuron: Neuron = Neuron(config=config, modbus_client=modbus_client)
+
+    async def _init_tasks(self, stack: AsyncExitStack, mqtt_client: Client) -> None:
+        tasks: Set[Task] = set()
+        stack.push_async_callback(self._cancel_tasks, tasks)
+
+        await NeuronFeaturesMqttPlugin(self.neuron, mqtt_client).init_tasks(stack, tasks)
+        await MeterFeaturesMqttPlugin(self.neuron, mqtt_client).init_tasks(tasks)
+
+        covers = CoverMap(self.config, self.neuron.features)
+        covers_plugin = CoversMqttPlugin(mqtt_client, covers)
+        await covers_plugin.init_tasks(stack, tasks)
+
+        if self.config.homeassistant.enabled:
+            await HassCoversMqttPlugin(self.neuron, mqtt_client, covers).init_tasks(tasks)
+            await HassBinarySensorsMqttPlugin(self.neuron, mqtt_client).init_tasks(tasks)
+            await HassSensorsMqttPlugin(self.neuron, mqtt_client).init_tasks(tasks)
+            await HassSwitchesMqttPlugin(self.neuron, mqtt_client).init_tasks(tasks)
+
+        await asyncio.gather(*tasks)
+
+    @staticmethod
+    async def _cancel_tasks(tasks) -> None:
+        for task in tasks:
+            if task.done():
+                continue
+
+            try:
+                task.cancel()
+                await task
+            except asyncio.CancelledError:
+                pass
+
+    async def _modbus_connect(self) -> None:
+        await self.modbus_client.tcp.connect()
+
+        if self.modbus_client.tcp.connected:
+            logger.info(
+                "%s TCP client connected to %s:%s",
+                LogPrefix.MODBUS,
+                self.modbus_client.tcp.params.host,
+                self.modbus_client.tcp.params.port,
+            )
+        else:
+            raise UnexpectedException(
+                f"TCP client can't connect to {self.modbus_client.tcp.params.host}:{self.modbus_client.tcp.params.port}"
+            )
+
+        await self.modbus_client.serial.connect()
+
+        if self.modbus_client.serial.connected:
+            logger.info(
+                "%s Serial client connected to %s",
+                LogPrefix.MODBUS,
+                self.modbus_client.serial.params.port,
+            )
+        else:
+            raise UnexpectedException(f"Serial client can't connect to {self.modbus_client.serial.params.port}")
+
+    async def run(self) -> None:
+        """Connect to Modbus and initialize Unipi Neuron hardware."""
+        await self._modbus_connect()
+        await self.neuron.init()
+
+        await mqtt_connect(
+            mqtt_config=self.config.mqtt,
+            logger=logger,
+            mqtt_client_id=f"{slugify(self.config.device_info.name)}-{uuid.uuid4()}",
+            callback=self._init_tasks,
+        )
+
+
+def parse_args(args: list) -> argparse.Namespace:
+    """Initialize argument parser options.
+
+    Parameters
+    ----------
+    args: list
+        Arguments as list.
+
+    Returns
+    -------
+    Argparse namespace
+    """
+    parser: argparse.ArgumentParser = init_argparse(description="Control Unipi I/O with MQTT commands")
+    parser.add_argument(
+        "-c",
+        "--config",
+        action="store",
+        default=DEFAULT_CONFIG_PATH,
+        help=f"path to the configuration (default: {DEFAULT_CONFIG_PATH})",
+    )
+    parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
+
+    return parser.parse_args(args)
+
+
+def main() -> None:
+    """Entrypoint for Unipi Control."""
+    unipi_control: Optional[UnipiControl] = None
+
+    try:
+        args: argparse.Namespace = parse_args(sys.argv[1:])
+
+        config: Config = Config(config_base_path=Path(args.config))
+        config.logging.init(log=args.log, verbose=args.verbose)
+
+        unipi_control = UnipiControl(
+            config=config,
+            modbus_client=ModbusClient(
+                tcp=AsyncModbusTcpClient(
+                    host="localhost",
+                    timeout=0.5,
+                    retries=3,
+                    retry_on_empty=True,
+                ),
+                serial=AsyncModbusSerialClient(
+                    port="/dev/extcomm/0/0",
+                    baudrate=config.modbus.baud_rate,
+                    parity=config.modbus.parity,
+                    timeout=1,
+                    retries=3,
+                    retry_on_empty=True,
+                ),
+            ),
+        )
+
+        asyncio.run(unipi_control.run())
+    except ConfigException as error:
+        logger.critical("%s %s", LogPrefix.CONFIG, error)
+        sys.exit(1)
+    except UnexpectedException as error:
+        logger.critical(error)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        pass
+    except asyncio.CancelledError:
+        pass
+    finally:
+        if unipi_control:
+            logger.info("Successfully shutdown the Unipi Control service.")

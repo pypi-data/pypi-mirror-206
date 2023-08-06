816b3a749a634f5af781139b5f369bcf160e6a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:40 +0000

    Add package serialization-tool/0.2/serialization_tool-0.2.tar.gz

---
+++ packages/serialization-tool/serialization_tool-0.2.tar.gz/serialization_tool-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="serialization_tool",
+    version="0.2",
+    license="GNU GENERAL PUBLIC LICENSE",
+    author="Alexander Skvortsov",
+    author_email="alexskvr03@gmail.com",
+    packages=find_packages("serialization_tool"),
+    package_dir={'':'serialization_tool'},
+    url="https://github.com/ALFecki/python-labs/tree/lab-3/lab-3/serialization_tool",
+    keywords="serialization xml json"
+)

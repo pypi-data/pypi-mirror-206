380b08000087dc9e2a0e4d6cb02ffbe3c8ce3486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:32 +0000

    Add package sry/0.0.0/sry-0.0.0.tar.gz

---
+++ packages/sry/sry-0.0.0.tar.gz/sry-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="sry",
+      versin="0.0.1.0",
+      description="sry模块",
+      athor="sunruiyang",
+      auhor_email="dogsmallsmile@163.com",
+      packages=[""])

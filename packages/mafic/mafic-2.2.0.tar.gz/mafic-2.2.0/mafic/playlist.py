cd20135bd8ee84ab1cf59989ec115e14d15c7eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:18 +0000

    Add package mafic/2.2.0/mafic-2.2.0.tar.gz

---
+++ packages/mafic/mafic-2.2.0.tar.gz/mafic-2.2.0/mafic/playlist.py
@@ -0,0 +1,36 @@
+"""The module containing :class:`Playlist`."""
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .track import Track
+
+if TYPE_CHECKING:
+    from .typings import PlaylistInfo, TrackWithInfo
+
+__all__ = ("Playlist",)
+
+
+class Playlist:
+    r"""Represents a playlist.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        The name of the playlist.
+    selected_track: :class:`int`
+        The index of the selected track, if any.
+    tracks: :class:`list`\[:class:`Track`]
+        A list of tracks in the playlist.
+    """
+
+    __slots__ = ("name", "selected_track", "tracks")
+
+    def __init__(self, *, info: PlaylistInfo, tracks: list[TrackWithInfo]) -> None:
+        self.name: str = info["name"]
+        self.selected_track: int = info["selectedTrack"]
+        self.tracks: list[Track] = [
+            Track.from_data_with_info(track) for track in tracks
+        ]

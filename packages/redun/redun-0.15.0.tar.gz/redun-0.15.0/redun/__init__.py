b324bbd28f1b50d411c41ba1e651b6a8de96a593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:50 +0000

    Add package redun/0.15.0/redun-0.15.0.tar.gz

---
+++ packages/redun/redun-0.15.0.tar.gz/redun-0.15.0/redun/__init__.py
@@ -0,0 +1,28 @@
+from redun.executors.alias import AliasExecutor
+from redun.executors.aws_batch import AWSBatchExecutor
+from redun.executors.aws_glue import AWSGlueExecutor
+from redun.executors.docker import DockerExecutor
+
+try:
+    from redun.executors.k8s import K8SExecutor
+except ModuleNotFoundError:
+    # Skip k8s executor if kubernetes is not installed.
+    pass
+from redun.executors.local import LocalExecutor
+from redun.file import Dir, File, ShardedS3Dataset
+from redun.handle import Handle
+from redun.namespace import get_current_namespace, namespace
+from redun.scheduler import (
+    Scheduler,
+    apply_tags,
+    catch,
+    cond,
+    get_current_scheduler,
+    merge_handles,
+    set_current_scheduler,
+    throw,
+)
+from redun.scripting import script
+from redun.task import PartialTask, Task, get_task_registry, task
+
+__version__ = "0.15.0"

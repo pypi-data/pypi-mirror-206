f11f207b8b0d3cb29acd6aa365bf1aedd08c4720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:27 +0000

    Add package scs-re/1.0.3/scs_re-1.0.3.tar.gz

---
+++ packages/scs-re/scs_re-1.0.3.tar.gz/scs_re-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+
+module = Extension(
+    'scs_sre',
+    sources=['_sre/sre.c'],
+    include_dirs=['/usr/local/include/python3.11', '/usr/local/include/python3.11/internal'],
+    extra_compile_args=["-D", "Py_BUILD_CORE"],
+)
+
+setup(
+    name='scs_re',
+    version='1.0.3',
+    url='https://github.com/IsaacRe/Syntactically-Constrained-Sampling',
+    description='Modification of cpython `re` module in which patterns match any string that is a prefix of a matching string under its regex',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    ext_modules=[module],
+    packages=["scs_re"],
+)

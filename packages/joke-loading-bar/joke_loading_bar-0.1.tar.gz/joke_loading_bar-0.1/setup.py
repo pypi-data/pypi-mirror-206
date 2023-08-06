a630fe2a18b94cc3742f11f3321d8d53b9e76cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:31 +0000

    Add package joke-loading-bar/0.1/joke_loading_bar-0.1.tar.gz

---
+++ packages/joke-loading-bar/joke_loading_bar-0.1.tar.gz/joke_loading_bar-0.1/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='joke_loading_bar',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'rich>=10.4.0',
+    ],
+    include_package_data=True
+)

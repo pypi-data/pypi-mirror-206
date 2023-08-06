00f8dbc9715bf34e6f285bd7446b5f85a89b65a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:16 +0000

    Add package NDETCStemmer-kaenova/1.4.3/NDETCStemmer_kaenova-1.4.3.tar.gz

---
+++ packages/NDETCStemmer-kaenova/NDETCStemmer_kaenova-1.4.3.tar.gz/NDETCStemmer_kaenova-1.4.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NDETCStemmer_kaenova",
+    version="1.4.3",
+    py_modules=["NDETCStemmer"],
+    description="Library untuk stemming kata dalam Bahasa Indonesia menggunakan metode Nondeterministic Context",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaenova/NDETCStemmer",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    keywords="linguistic stemming indonesian bahasa",
+    install_requires=["nltk", "gensim", "gdown", "checksumdir", "huggingface_hub"],
+)

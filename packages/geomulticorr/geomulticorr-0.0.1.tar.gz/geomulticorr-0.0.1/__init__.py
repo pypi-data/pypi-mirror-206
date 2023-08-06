18304b57916e623540b71051b7457a95403cce54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:06 +0000

    Add package geomulticorr/0.0.1/geomulticorr-0.0.1.tar.gz

---
+++ packages/geomulticorr/geomulticorr-0.0.1.tar.gz/geomulticorr-0.0.1/__init__.py
@@ -0,0 +1,5 @@
+from geomulticorr.src.session import open
+version = "0.0.1"
+print(f"""---------
+GeoMultiCorr {version}
+---------""")
---
+++ packages/geomulticorr/geomulticorr-0.0.1.tar.gz/geomulticorr-0.0.1/tests/test_session.py
@@ -0,0 +1,79 @@
+import unittest
+import geomulticorr.src.session
+
+path_to_test_session = '/media/duvanelt/TD002/sandbox_gmc/vanoise'
+pzone_name = 'iseran'
+
+class TestSession(unittest.TestCase):
+
+    def setUp(self):
+        self.test_session = geomulticorr.src.session.open(path_to_test_session)
+
+    def test_is_session(self):
+        self.assertIsInstance(self.test_session, geomulticorr.src.session.Session)
+
+    def test_get_thumbs(self):
+        self.assertIsInstance(self.test_session.get_thumbs(), list)
+        self.assertIsInstance(self.test_session.get_thumbs(pzone_name), list)
+
+    def test_get_pairs(self):
+        self.assertIsInstance(self.test_session.get_pairs(), list)
+        self.assertIsInstance(self.test_session.get_pairs(pzone_name), list)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+"""
+# %%

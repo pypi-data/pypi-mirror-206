0d62e05efa00b2abd4428eefd409d49d3f99f00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:02 +0000

    Add package toolkit-lennart/0.0.8/toolkit_lennart-0.0.8.tar.gz

---
+++ packages/toolkit-lennart/toolkit_lennart-0.0.8.tar.gz/toolkit_lennart-0.0.8/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.0.8',
+      description='Toolkit Lennart',
+      author='Lennart Rooden',
+      author_email='lennart.rs@gmail.com',
+      url='https://pypi.org/project/toolkit-lennart/0.0.8/',
+      packages=['toolkit_lennart'],
+     )

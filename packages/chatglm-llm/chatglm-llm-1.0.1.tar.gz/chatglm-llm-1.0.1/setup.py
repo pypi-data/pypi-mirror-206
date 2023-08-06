f59941860a5d59d5324031e8ef047373a50ca75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:24 +0000

    Add package chatglm-llm/1.0.1/chatglm-llm-1.0.1.tar.gz

---
+++ packages/chatglm-llm/chatglm-llm-1.0.1.tar.gz/chatglm-llm-1.0.1/setup.py
@@ -0,0 +1,39 @@
+
+from setuptools import setup, find_packages
+
+
+setup(name='chatglm-llm',
+    version='1.0.1',
+    description='chatglm llm',
+    url='https://github.com/xxx',
+    author='auth',
+    author_email='xxx@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=['sentence_transformers',
+        'tensorboard',
+        'requests',
+        "protobuf",
+        "cpm_kernels",
+        "mdtex2html",
+        "sentencepiece",
+        "accelerate",
+        'torch',
+        'termcolor',
+        'tqdm',
+        'websockets',
+        'websocket',
+        'websocket-client',
+        'transformers',
+        'aiowebsocket',
+        
+        ],
+    entry_points={
+        'console_scripts': [
+            'chatglm-web=chatglm_src.cmd:main',
+        ]
+    },
+
+)

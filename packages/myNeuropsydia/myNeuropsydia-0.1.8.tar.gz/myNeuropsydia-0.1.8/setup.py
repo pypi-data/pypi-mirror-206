2aa7f52bd6d3a0aa4f29990dfb04d355d0e68d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:26 +0000

    Add package myNeuropsydia/0.1.8/myNeuropsydia-0.1.8.tar.gz

---
+++ packages/myNeuropsydia/myNeuropsydia-0.1.8.tar.gz/myNeuropsydia-0.1.8/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='myNeuropsydia',
+    packages=find_packages(),
+    version='0.1.8',
+    description='Partially fixed version of Neuropsydia library, a Python module for creating experiments, tasks and questionnaires',
+    author='Laborde',
+    license='ENS_Paris_Saclay',
+    package_data = {
+                	"myNeuropsydia.files.font":["*.ttf", "*.otf"],
+                	"myNeuropsydia.files.binary":["*.png"],
+                	"myNeuropsydia.files.logo":["*.png"]},
+    dependency_links=[
+	"https://github.com/neuropsychology/NeuroKit.py/zipball/master"],
+    install_requires=['Pillow',
+                      'cryptography',
+                      'neurokit',
+                      'numpy',
+                      'pandas',
+                      'pygame',
+                      'python-docx',
+                      'pyxid',
+                      'statsmodels'],
+)
+
+

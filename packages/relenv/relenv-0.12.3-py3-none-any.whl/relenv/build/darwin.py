ab932c90c948f2e25b295b70277c5feee499ec83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:15 +0000

    Add package relenv/0.12.3/relenv-0.12.3-py3-none-any.whl

---
+++ packages/relenv/relenv-0.12.3-py3-none-any.whl/relenv/build/darwin.py
@@ -0,0 +1,134 @@
+# Copyright 2022-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2
+"""
+The darwin build process.
+"""
+import io
+
+from ..common import arches, DARWIN
+from .common import runcmd, finalize, build_openssl, build_sqlite, builds
+
+ARCHES = arches[DARWIN]
+
+
+def populate_env(env, dirs):
+    """
+    Make sure we have the correct environment variables set.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    """
+    env["CC"] = "clang"
+    ldflags = [
+        "-Wl,-rpath,{prefix}/lib",
+        "-L{prefix}/lib",
+    ]
+    env["LDFLAGS"] = " ".join(ldflags).format(prefix=dirs.prefix)
+    env["MACOSX_DEPLOYMENT_TARGET"] = "10.15"
+    cflags = [
+        "-L{prefix}/lib",
+        "-I{prefix}/include",
+        "-I{prefix}/include/readline",
+    ]
+    env["CFLAGS"] = " ".join(cflags).format(prefix=dirs.prefix)
+
+
+def build_python(env, dirs, logfp):
+    """
+    Run the commands to build Python.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    env["LDFLAGS"] = "-Wl,-rpath,{prefix}/lib {ldflags}".format(
+        prefix=dirs.prefix, ldflags=env["LDFLAGS"]
+    )
+    runcmd(
+        [
+            "./configure",
+            "-v",
+            "--prefix={}".format(dirs.prefix),
+            "--with-openssl={}".format(dirs.prefix),
+            "--enable-optimizations",
+            "--disable-test-modules",
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    with io.open("Modules/Setup", "a+") as fp:
+        fp.seek(0, io.SEEK_END)
+        fp.write("*disabled*\n" "_tkinter\n" "nsl\n" "ncurses\n" "nis\n")
+    runcmd(
+        ["sed", "s/#zlib/zlib/g", "Modules/Setup"], env=env, stderr=logfp, stdout=logfp
+    )
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+
+build = builds.add("darwin", populate_env=populate_env, version="3.10.11")
+
+build.add(
+    "OpenSSL",
+    build_func=build_openssl,
+    download={
+        "url": "https://www.openssl.org/source/openssl-{version}.tar.gz",
+        "version": "1.1.1t",
+        "md5sum": "1cfee919e0eac6be62c88c5ae8bcd91e",
+    },
+)
+
+build.add(
+    "XZ",
+    download={
+        "url": "http://tukaani.org/xz/xz-{version}.tar.gz",
+        "fallback_url": "https://woz.io/relenv/dependencies/xz-{version}.tar.gz",
+        "version": "5.4.2",
+        "md5sum": "4ac4e5da95aa8604a81e32079cb00d42",
+    },
+)
+
+build.add(
+    name="SQLite",
+    build_func=build_sqlite,
+    download={
+        "url": "https://sqlite.org/2023/sqlite-autoconf-{version}.tar.gz",
+        "fallback_url": "https://woz.io/relenv/dependencies/sqlite-autoconf-{version}.tar.gz",
+        "version": "3410200",
+        "md5sum": "862075fd1c38324878ef809eda39edfe",
+    },
+)
+
+build.add(
+    "python",
+    build_func=build_python,
+    wait_on=[
+        "OpenSSL",
+        "XZ",
+        "SQLite",
+    ],
+    download={
+        "url": "https://www.python.org/ftp/python/{version}/Python-{version}.tar.xz",
+        "fallback_url": "https://woz.io/relenv/dependencies/Python-{version}.tar.gz",
+        "md5sum": "1bf8481a683e0881e14d52e0f23633a6",
+        "version": build.version,
+    },
+)
+
+
+build.add(
+    "relenv-finalize",
+    build_func=finalize,
+    wait_on=[
+        "python",
+    ],
+)
+
+build = build.copy(version="3.11.3", md5sum="a957cffb58a89303b62124896881950b")
+builds.add("darwin", builder=build)

dd8d568765be20b7b48de0c476fadb24d15f7be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:58 +0000

    Add package apppium-flutter-report-generator/0.1.2/apppium_flutter_report_generator-0.1.2.tar.gz

---
+++ packages/apppium-flutter-report-generator/apppium_flutter_report_generator-0.1.2.tar.gz/apppium_flutter_report_generator-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='apppium_flutter_report_generator',
+   version='0.1.2',
+   description='A package for generating Appium Flutter Report.',
+   author='Aaradhya Gopal Nepal',
+   author_email='aradhya.1441@gmail.com',
+   packages=['apppium_flutter_report_generator'],
+   install_requires=['wheel'],
+)
---
+++ packages/apppium-flutter-report-generator/apppium_flutter_report_generator-0.1.2.tar.gz/apppium_flutter_report_generator-0.1.2/test/login/test_login.py
@@ -0,0 +1,32 @@
+from src.group_and_test import group, test
+from src.logger import Logger
+
+
+def test_login():
+    group(
+        "Testing Login",
+        group_login,
+    )
+
+
+def group_login():
+    test(
+        "Wrong Login",
+        wrong_login,
+    )
+    test(
+        "Correct Login",
+        correct_login,
+    )
+
+
+def correct_login(logger: Logger):
+    logger.add_step("Username: 9800915400 Password: Correct Password")
+    logger.add_step("Login Button Pressed")
+    assert False
+
+
+def wrong_login(logger: Logger):
+    logger.add_step("Username: 9800915400 Password: Wrong Password")
+    logger.add_step("Login Button Pressed")
+    assert True

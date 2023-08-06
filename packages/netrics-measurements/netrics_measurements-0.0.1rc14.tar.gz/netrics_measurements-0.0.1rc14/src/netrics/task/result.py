5e66b4f539da2d320e3b733dcc07bbfb08d4cd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:52 +0000

    Add package netrics-measurements/0.0.1rc14/netrics_measurements-0.0.1rc14.tar.gz

---
+++ packages/netrics-measurements/netrics_measurements-0.0.1rc14.tar.gz/netrics_measurements-0.0.1rc14/src/netrics/task/result.py
@@ -0,0 +1,28 @@
+"""Task result recording compatible with the Fate scheduler."""
+import time
+
+import fate.task.result
+
+
+def write(results, /, label=None, annotate=True, **kwargs):
+    """Write task results.
+
+    Wraps results in metadata, by default, according to `annotate=True`;
+    and, places results under the key `label`, if provided.
+
+    See `fate.task.result.write` for further details.
+
+    """
+    if label:
+        results = {label: results}
+
+    if annotate:
+        results = {
+            'Measurements': results,
+            'Meta': {
+                'Time': time.time(),
+                'Id': None,
+            },
+        }
+
+    return fate.task.result.write(results, **kwargs)

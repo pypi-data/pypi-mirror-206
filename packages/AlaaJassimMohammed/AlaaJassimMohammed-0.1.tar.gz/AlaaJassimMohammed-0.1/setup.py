029cad83ce0ffb54025e39fec35743b69c0b3aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:47 +0000

    Add package AlaaJassimMohammed/0.1/AlaaJassimMohammed-0.1.tar.gz

---
+++ packages/AlaaJassimMohammed/AlaaJassimMohammed-0.1.tar.gz/AlaaJassimMohammed-0.1/setup.py
@@ -0,0 +1,17 @@
+
+import setuptools
+
+setuptools.setup(
+    name='AlaaJassimMohammed',
+    version = "0.1",
+    author = "Alaa Jassim Mohammed",
+    description = "Small and simple image processing library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    	"Programming Language :: Python :: 3",
+    	"Operating System :: OS Independent",
+    	"License :: OSI Approved :: MIT License"
+
+    	]
+  
+)

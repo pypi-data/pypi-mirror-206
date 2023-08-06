ddd589b9ca2dc07483f0983b19ace4595cd9a836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:30 +0000

    Add package psremoter/2.0.3/psremoter-2.0.3.tar.gz

---
+++ packages/psremoter/psremoter-2.0.3.tar.gz/psremoter-2.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name = "psremoter",
+    version = "2.0.3",
+    author = "Daniel Mandelblat",
+    author_email = "danielmande@gmail.com",
+    description = "Powershell remote client tool for Python",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/danielMandelblat/psremoter",
+    project_urls = {
+        "Bug Tracker": "https://github.com/danielMandelblat/psremoter",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.9"
+)

644dc36c51671f36a8e73deb363a363631af1831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:23 +0000

    Add package dora-rs/0.2.3rc6/dora_rs-0.2.3rc6-cp37-abi3-win_amd64.whl

---
+++ packages/dora-rs/dora_rs-0.2.3rc6-cp37-abi3-win_amd64.whl/dora/__init__.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+from .dora import *
+
+__author__ = "Dora-rs Authors"
+__version__ = "0.2.3-rc6"
+
+
+class DoraStatus(Enum):
+    """Dora status to indicate if operator `on_input` loop
+     should be stopped.
+
+    Args:
+        Enum (u8): Status signaling to dora operator to
+        stop or continue the operator.
+    """
+
+    CONTINUE = 0
+    STOP = 1
+    STOP_ALL = 2

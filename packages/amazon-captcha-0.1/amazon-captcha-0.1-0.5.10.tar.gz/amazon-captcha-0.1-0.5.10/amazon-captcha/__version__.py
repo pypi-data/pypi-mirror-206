80e787fea95965ac8ec6aa9ebff963d36d37a748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:11 +0000

    Add package amazon-captcha-0.1/0.5.10/amazon-captcha-0.1-0.5.10.tar.gz

---
+++ packages/amazon-captcha-0.1/amazon-captcha-0.1-0.5.10.tar.gz/amazon-captcha-0.1-0.5.10/amazon-captcha/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'amazon-captcha-0.1'
+__description__ = "Pure Python, lightweight, Pillow-based solver for the Amazon text captcha."
+__url__ = 'https://github.com/MaxAsif/amazoncaptcha'
+__version__ = '0.5.10'
+__author__ = 'Asif Iqbal'
+__author_email__ = 'maxasif@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Asif Iqbal'

ed8e1905effbcd1d412a248416aeeeb04d03be0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package python-voi/0.0.1/python-voi-0.0.1.tar.gz

---
+++ packages/python-voi/python-voi-0.0.1.tar.gz/python-voi-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="python-voi",
+    version="0.0.1",
+    author="Core Francisco Park",
+    author_email="cfpark00@gmail.com",
+    description="Python implementation of Variation of Information",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    install_requires=["numpy","torch"],
+)

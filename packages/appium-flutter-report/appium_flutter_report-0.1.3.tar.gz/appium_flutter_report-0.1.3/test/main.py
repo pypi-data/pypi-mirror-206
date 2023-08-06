17fe9c27426d87dba265dccbffe6aa902434984e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:42 +0000

    Add package appium-flutter-report/0.1.3/appium_flutter_report-0.1.3.tar.gz

---
+++ packages/appium-flutter-report/appium_flutter_report-0.1.3.tar.gz/appium_flutter_report-0.1.3/test/main.py
@@ -0,0 +1,29 @@
+from crud.test_crud import test_crud
+from login.test_login import test_login
+
+
+def main():
+    capabilities = dict(
+        platformName='Android',
+        automationName='flutter',
+        deviceName='emulator-5554',
+        appPackage='com.example.animation',
+        appActivity='.MainActivity',
+        language='en',
+        locale='US',
+    )
+    driver = webdriver.Remote("http://localhost:4723", capabilities)
+    FlutterReportGenerator.setup(
+        driver=driver,
+        app_name="Animation Test",
+        capabilities=capabilities,
+        report_path="../Output/",  # Todo:Set Report path as per your project, for my case Output is in .gitignore
+    )
+    test_login()
+    test_crud()
+    FlutterReportGenerator.generate_report()
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()

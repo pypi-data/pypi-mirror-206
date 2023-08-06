31444c6bedaeb8eea1aa43c391d9282be9b0b30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:30 +0000

    Add package mlcvzoo-tf-classification/6.0.1/mlcvzoo_tf_classification-6.0.1-py3-none-any.whl

---
+++ packages/mlcvzoo-tf-classification/mlcvzoo_tf_classification-6.0.1-py3-none-any.whl/mlcvzoo_tf_classification/xception/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2021 Open Logistics Foundation
+#
+# Licensed under the Open Logistics License 1.0.
+# For details on the licensing terms, see the LICENSE file.
+
+"""
+Package that handles the wrapper implementation of the tensorflow Xception model.
+A model for image classification.
+"""

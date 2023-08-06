af6291dd62307c9facd4e942de7acce89c26f3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:05 +0000

    Add package stogui/0.1.10/stogui-0.1.10.tar.gz

---
+++ packages/stogui/stogui-0.1.10.tar.gz/stogui-0.1.10/stogui/dpp_table/frontend/build/precache-manifest.2a8a439e213dc74611d082dd410f86bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ced4f183062606ddf4eba770341e6ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b418dfbb1357b9abb3ed",
+    "url": "./static/js/2.41898aa7.chunk.js"
+  },
+  {
+    "revision": "1d6e26d636a9cf12c7d2307ef2439fb0",
+    "url": "./static/js/2.41898aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7c746b5aa187f96917",
+    "url": "./static/js/main.bf55452b.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "99c6de9522e46c9c69649216e988ae5e",
+    "url": "./static/media/IBMPlexSans-Bold.99c6de95.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "./static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "./static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "1ca9107e7544d3424419585c7c84cb67",
+    "url": "./static/media/IBMPlexSans-SemiBold.1ca9107e.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
---
+++ packages/stogui/stogui-0.1.10.tar.gz/stogui-0.1.10/stogui/test_results_table/frontend/build/precache-manifest.5d02d48087dbf73d69c390198230a4ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6df1bc9f43da63a3b4ee4e8cab2602",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5179d7ef4a43cd9c1850",
+    "url": "./static/js/2.6d15c91c.chunk.js"
+  },
+  {
+    "revision": "1d6e26d636a9cf12c7d2307ef2439fb0",
+    "url": "./static/js/2.6d15c91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0673245e8d4df82f34d8",
+    "url": "./static/js/main.ad6bea04.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "99c6de9522e46c9c69649216e988ae5e",
+    "url": "./static/media/IBMPlexSans-Bold.99c6de95.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "./static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "./static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "1ca9107e7544d3424419585c7c84cb67",
+    "url": "./static/media/IBMPlexSans-SemiBold.1ca9107e.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);

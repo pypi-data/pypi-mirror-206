be3db414414a9918b1e487f6a8b547ea73e4460d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:09 +0000

    Add package pythonCSVutils/0.1.1/pythonCSVutils-0.1.1-py3-none-any.whl

---
+++ packages/pythonCSVutils/pythonCSVutils-0.1.1-py3-none-any.whl/pythonCSVutils/test.py
@@ -0,0 +1,6 @@
+from pythonCSVutils import read_csv, write_csv, filter_rows, sort_rows, update_rows, select_columns, read_csv_header, append_rows, group_by, aggregate, merge, drop_columns
+
+#Reading and Writing CSV Files
+
+data = read_csv('C:\\Users\\92304\\Downloads\\hunter-domains-list-33150.csv')
+print(data)

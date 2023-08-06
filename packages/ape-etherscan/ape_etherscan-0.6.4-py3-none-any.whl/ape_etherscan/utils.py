e65adb00dbd0c9f340377f77b1bdaef25d03ab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:33 +0000

    Add package ape-etherscan/0.6.4/ape_etherscan-0.6.4-py3-none-any.whl

---
+++ packages/ape-etherscan/ape_etherscan-0.6.4-py3-none-any.whl/ape_etherscan/utils.py
@@ -0,0 +1,40 @@
+API_KEY_ENV_KEY_MAP = {
+    "arbitrum": "ARBISCAN_API_KEY",
+    "ethereum": "ETHERSCAN_API_KEY",
+    "fantom": "FTMSCAN_API_KEY",
+    "optimism": "OPTIMISTIC_ETHERSCAN_API_KEY",
+    "polygon": "POLYGONSCAN_API_KEY",
+    "avalanche": "SNOWTRACE_API_KEY",
+    "bsc": "BSCSCAN_API_KEY",
+}
+NETWORKS = {
+    "ethereum": [
+        "mainnet",
+        "goerli",
+        "sepolia",
+    ],
+    "arbitrum": [
+        "mainnet",
+        "goerli",
+    ],
+    "fantom": [
+        "opera",
+        "testnet",
+    ],
+    "optimism": [
+        "mainnet",
+        "goerli",
+    ],
+    "polygon": [
+        "mainnet",
+        "mumbai",
+    ],
+    "avalanche": [
+        "mainnet",
+        "fuji",
+    ],
+    "bsc": [
+        "mainnet",
+        "testnet",
+    ],
+}

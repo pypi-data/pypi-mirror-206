512652054afb9e1d43f53518870820933cfd797b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:58 +0000

    Add package tensorflow-dataset/0.4.14/tensorflow-dataset-0.4.14.tar.gz

---
+++ packages/tensorflow-dataset/tensorflow-dataset-0.4.14.tar.gz/tensorflow-dataset-0.4.14/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="tensorflow-dataset",
+    version='0.4.14',
+    description="",
+    long_description="",
+    author="",
+    author_email="",
+    url="",
+    license="GPLv3+",
+    install_requires=[],
+    packages=["tensorflow_dataset"]
+)

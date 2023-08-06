bd36f1c2542ffc45ae06fc9cb925a5c6838c4397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:35 +0000

    Add package pyGrained/1.0.0/pyGrained-1.0.0-py3-none-any.whl

---
+++ packages/pyGrained/pyGrained-1.0.0-py3-none-any.whl/pyGrained/utils/trajectory.py
@@ -0,0 +1,41 @@
+def applyCoarseGrainedOverTrajectory(cgMap,trajPDB,trajDCD):
+
+    mdls = [bead[0] for bead in cgMap]
+    m0   = min(mdls)
+    m0CgMap = {bead:cgMap[bead] for bead in cgMap.keys() if bead[0] == m0}
+
+    universe = mda.Universe(trajPDB,trajDCD)
+
+    chains = [ch.segid for ch in universe.segments]
+
+    cg2sel = {}
+    for bead in m0CgMap.keys():
+        ch_cg  = bead[1]
+        res_cg = bead[2]
+        atm_cg = bead[3]
+        if ch_cg in chains:
+
+            sel = "segid "+ ch_cg +" and ("
+            for atm in m0CgMap[bead]:
+                resid = atm[2]
+                name  = atm[3]
+                index = atm[4]
+
+                sel += "("
+                sel += "resid " + str(resid) + " and name " + name
+                sel += ") or "
+            sel = sel[:-3]
+            sel += ")"
+
+            cg2sel[(ch_cg,res_cg,atm_cg,index)] = universe.select_atoms(sel)
+
+    cgTraj = {}
+    for ts in tqdm(universe.trajectory):
+        cgTraj[ts.frame] = {}
+        for s in cg2sel.keys():
+            cgPos = cg2sel[s].center_of_mass()
+
+            cgTraj[ts.frame][s] = cgPos
+
+    return cgTraj
+

bcb6208bd6f6d8c2d037560dd8c30a0f08dadd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:07 +0000

    Add package caikit/0.3.1/caikit-0.3.1.tar.gz

---
+++ packages/caikit/caikit-0.3.1.tar.gz/caikit-0.3.1/tests/fixtures/sample_lib/data_model/sample.py
@@ -0,0 +1,35 @@
+"""
+Dummy data model object for testing
+"""
+
+# Local
+from caikit.core.data_model import DataObjectBase, dataobject
+
+
+@dataobject(package="caikit_data_model.sample_lib")
+class SampleInputType(DataObjectBase):
+    """A sample `domain primitive` input type for this library.
+    The analog to a `Raw Document` for the `Natural Language Processing` domain."""
+
+    name: str
+
+
+@dataobject(package="caikit_data_model.sample_lib")
+class SampleOutputType(DataObjectBase):
+    """A simple return type for the `sample_task` task"""
+
+    greeting: str
+
+
+@dataobject(package="caikit_data_model.sample_lib")
+class OtherOutputType(DataObjectBase):
+    """A simple return type for the `other_task` task"""
+
+    farewell: str
+
+
+@dataobject(package="caikit_data_model.sample_lib")
+class SampleTrainingType(DataObjectBase):
+    """A sample `training data` type for the `sample_task` task."""
+
+    number: int

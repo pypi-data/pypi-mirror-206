473084812a5e75e1dc16e361e0e0c8855e4df695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:46 +0000

    Add package graphblas-algorithms/2023.5.0/graphblas_algorithms-2023.5.0-py3-none-any.whl

---
+++ packages/graphblas-algorithms/graphblas_algorithms-2023.5.0-py3-none-any.whl/graphblas_algorithms/__init__.py
@@ -0,0 +1,17 @@
+import importlib.metadata
+
+from .classes import *
+
+from .algorithms import *  # isort:skip
+from .generators import *  # isort:skip
+
+try:
+    __version__ = importlib.metadata.version("graphblas-algorithms")
+except Exception as exc:  # pragma: no cover (safety)
+    raise AttributeError(
+        "`graphblas_algorithms.__version__` not available. This may mean "
+        "graphblas-algorithms was incorrectly installed or not installed at all. "
+        "For local development, you may want to do an editable install via "
+        "`python -m pip install -e path/to/graphblas-algorithms`"
+    ) from exc
+del importlib

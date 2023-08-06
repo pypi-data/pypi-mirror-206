7642682fa859c8a6e8e9fc306025878ec6b6abf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:40 +0000

    Add package babylog/0.0.3/babylog-0.0.3.tar.gz

---
+++ packages/babylog/babylog-0.0.3.tar.gz/babylog-0.0.3/examples/sample_usage.py
@@ -0,0 +1,44 @@
+import time
+
+import cv2
+from tqdm import tqdm
+import numpy as np
+
+from babylog import Babylog, VisionModelType, InferenceDevice
+
+
+bl = Babylog("../resources/babylog.config.yaml", save_cloud=False, stream=False)
+img = cv2.imread("../resources/panda.jpg")
+
+
+for i in tqdm(range(100)):
+    bl.log(
+        image=img,
+        prediction=img,
+        model_type=VisionModelType.DETECTION,
+        model_name="resnet50_finetuned",
+        model_version="1.0.0",
+        latency=10,
+        inference_device=InferenceDevice.CPU,
+        detection=(
+            [
+                {
+                    "x": 1,
+                    "y": 1,
+                    "width": 100,
+                    "height": 220,
+                    "confidence": 0.97,
+                    "classification": {"dog": 0.8, "cat": 0.2},
+                },
+                {
+                    "x": 1,
+                    "y": 1,
+                    "width": 50,
+                    "height": 150,
+                    "confidence": 0.97,
+                    "classification": {"dog": 0.5, "cat": 0.5},
+                },
+            ]
+        ),
+    )
+    time.sleep(1)

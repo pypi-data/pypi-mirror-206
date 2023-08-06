eeedcd7695d6d8d7b26a0a1f04b3529902c26c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:44 +0000

    Add package looki2000/0.1/looki2000-0.1.tar.gz

---
+++ packages/looki2000/looki2000-0.1.tar.gz/looki2000-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='looki2000',
+    version='0.1',
+    description='My Python library',
+    author='lubek',
+    author_email='boryskostka369@gmail.com',
+    packages=['looki2000'],
+    install_requires=[
+        # List your library's dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)

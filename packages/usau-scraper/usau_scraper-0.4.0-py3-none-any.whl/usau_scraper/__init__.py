20321570dd200b0d2d870d9b7d29186abbeac65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:31 +0000

    Add package usau-scraper/0.4.0/usau_scraper-0.4.0-py3-none-any.whl

---
+++ packages/usau-scraper/usau_scraper-0.4.0-py3-none-any.whl/usau_scraper/__init__.py
@@ -0,0 +1,26 @@
+from .teamScraper import getTeamInfo, getTeamSchedule, getTeamRoster, queryTeam, setArgs, fillInBasicInfo
+from .tournamentScraper import (
+    queryTournament,
+    getTournamentWinner,
+    getTournamentPoolPlayResults,
+    getTournamentBracketResults,
+)
+from .rankingScraper import getClubRankings, getCollegeRankings, queryRankings
+
+__all__ = [
+    'getTeamInfo',
+    'getTeamSchedule',
+    'getTeamRoster',
+    'queryTeam',
+    'setArgs',
+    'fillInBasicInfo',
+    'queryTournament',
+    'getTournamentWinner',
+    'getTournamentPoolPlayResults',
+    'getTournamentBracketResults',
+    'getCollegeRankings',
+    'getClubRankings',
+    'queryRankings',
+    '__version__',
+]
+__version__ = "0.4.0"

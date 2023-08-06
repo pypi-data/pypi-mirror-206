3a0718f1411a325a04df2f8abc4471657c105e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package junsjdeuszjunfaresd/0.2/junsjdeuszjunfaresd-0.2.tar.gz

---
+++ packages/junsjdeuszjunfaresd/junsjdeuszjunfaresd-0.2.tar.gz/junsjdeuszjunfaresd-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="junsjdeuszjunfaresd",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        # Liste des dépendances, par exemple :
+        # 'numpy>=1.18.0',
+    ],
+    author="Fares Nasser",
+    author_email="tempsinfoprocontact@gmail.com",
+    description="Un module cool.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/anyiskiidd/junsjdeuszjunfaresd",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

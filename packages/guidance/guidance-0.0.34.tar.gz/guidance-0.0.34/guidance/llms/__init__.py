b1ccccc45c837b4478fd6e353eba81262a59ef5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:37 +0000

    Add package guidance/0.0.34/guidance-0.0.34.tar.gz

---
+++ packages/guidance/guidance-0.0.34.tar.gz/guidance-0.0.34/guidance/llms/__init__.py
@@ -0,0 +1,6 @@
+from ._openai import OpenAI
+from ._transformers import Transformers
+from ._mock import Mock
+from ._llm import LLM
+from ._azure_openai import AzureOpenAI
+from ._deep_speed import DeepSpeed

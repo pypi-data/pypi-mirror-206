4cb44aa67d4dd086f867e9c092cc6ef817dab4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:23 +0000

    Add package PyBerries/0.2.6/PyBerries-0.2.6.tar.gz

---
+++ packages/PyBerries/PyBerries-0.2.6.tar.gz/PyBerries-0.2.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyBerries",
+    version="0.2.6",
+    author="Daniel Thedie",
+    author_email="daniel.thedie@ed.ac.uk",
+    description="Processing of Bacmman measurement tables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/MEKlab/pyberries",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'tifffile', 'h5py','seaborn', 'pybacmman', 'IPython']
+)

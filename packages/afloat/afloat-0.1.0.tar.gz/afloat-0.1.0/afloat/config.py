b13ba506004f02bf7bbce17f2e8bcc4260998edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:10 +0000

    Add package afloat/0.1.0/afloat-0.1.0.tar.gz

---
+++ packages/afloat/afloat-0.1.0.tar.gz/afloat-0.1.0/afloat/config.py
@@ -0,0 +1,45 @@
+import os
+
+# Windows
+HOME = os.environ['USERPROFILE']
+
+def parse_config_file(home_dir=None):
+    """
+    Parses the .afloatconfig file into a dictionary.
+    """
+    if home_dir is None:
+        home_dir = HOME
+        
+    afloatconfig_file = os.path.join(HOME, '.afloatconfig')
+    
+    if not os.path.exists(afloatconfig_file):
+        raise(Exception("{} does not exist".format(afloatconfig_file)))
+    
+    with open(afloatconfig_file) as f:
+        lines = f.readlines()
+
+    config_dict = {}
+    for line in lines:
+        line = line.split(': ')
+        config_dict[line[0].strip()] = line[1].strip()
+
+    return config_dict
+
+def get(key, home_dir=None):
+    """
+    Returns a key from the .afloatconfig file.
+
+    Example:
+
+        import afloat.config as config
+
+        extras_folder = config.get('afloat-extras', home_dir=None)
+
+    """
+    
+    config_dict = parse_config_file(home_dir=home_dir)
+    
+    if key in config_dict: 
+        return config_dict[key]
+    else: 
+        return None

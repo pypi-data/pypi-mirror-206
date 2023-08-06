f906a9742d8afd3f5240d1853ab98b6f62c172be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:48 +0000

    Add package pandasflow/0.4.6/pandasflow-0.4.6.tar.gz

---
+++ packages/pandasflow/pandasflow-0.4.6.tar.gz/pandasflow-0.4.6/pandasflow/metrics/precision_recall.py
@@ -0,0 +1,34 @@
+import pandasflow as pdf
+import pandas as pd
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from matplotlib import pyplot
+
+
+def precision_recall(target, score, plot=False, round_=2):
+	
+	if type(score) == pd.Series or type(score) == list and len(score) == 1:
+		score_ = score if type(score) == pd.Series else score[0]
+		score_name = score_.name
+		
+		precision, recall, _ = precision_recall_curve(target, score_)
+		pyplot.plot(recall, precision, marker='.', label=score_name)
+		
+		average = average_precision_score(target, score_)
+		average = round(average, round_)
+		print(f"{score_name} average precision: ", average)
+	
+	
+	if type(score) == list and len(score) > 1:
+		for sc in score:
+			precision, recall, _ = precision_recall_curve(target, sc)
+			pyplot.plot(recall, precision, marker='.', label=sc.name)
+			
+			average = average_precision_score(target, sc)
+			average = round(average, round_)
+			print(f"{sc.name} average precision: ", average)
+	
+	if plot:
+		pyplot.xlabel('Recall')
+		pyplot.ylabel('Precision')
+		pyplot.legend()
+		pyplot.show()

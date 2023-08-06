0dbc45bbeb3e4d46341bdef80f58e669bb005eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:59 +0000

    Add package gefyra/1.0.7/gefyra-1.0.7.tar.gz

---
+++ packages/gefyra/gefyra-1.0.7.tar.gz/gefyra-1.0.7/gefyra/local/check.py
@@ -0,0 +1,32 @@
+import logging
+
+from gefyra.configuration import default_configuration, ClientConfiguration
+
+logger = logging.getLogger("gefyra")
+
+
+def probe_docker(config: ClientConfiguration = default_configuration):
+    logger.debug("Probing: Docker")
+    try:
+        config.DOCKER.containers.list()
+        config.DOCKER.images.pull("quay.io/gefyra/cargo:latest")
+    except Exception:
+        logger.error("Docker does not seem to be not working for Gefyra")
+        return False
+    else:
+        logger.info("Docker: Ok")
+        return True
+
+
+def probe_kubernetes(config: ClientConfiguration = default_configuration):
+    logger.debug("Probing: Kubernetes")
+    try:
+        config.K8S_CORE_API.list_namespace()
+    except Exception:
+        logger.error(
+            "Kubernetes is not connected to a cluster or does not seem to be working for Gefyra"
+        )
+        return False
+    else:
+        logger.info("Kubernetes: Ok")
+        return True

3c4319c42b4982d278281b037e80a946dbe911a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:50 +0000

    Add package graph-envs/0.0.55/graph_envs-0.0.55-py3-none-any.whl

---
+++ packages/graph-envs/graph_envs-0.0.55-py3-none-any.whl/graph_envs/steiner_tree.py
@@ -0,0 +1,158 @@
+import gymnasium as gym
+# import torch 
+# import torch_geometric as pyg 
+import numpy as np 
+
+from typing import TYPE_CHECKING, Any, Generic, SupportsFloat, TypeVar
+
+import networkx as nx 
+import random
+
+from typing import Tuple
+
+from graph_envs.utils import vectorize_graph
+import graph_envs.feature_extraction as fe
+
+class SteinerTreeEnv(gym.Env):
+    '''
+    Environment for shortest path problem.
+    input:
+        - n_nodes: number of nodes in the graph
+        - n_edges: number of edges in the graph
+        - weighted: whether the graph is weighted or not
+        - n_dests: number of destinations to be reached
+    '''
+    
+    def __init__(self, n_nodes, n_edges, n_dests=3, weighted=True, parenting=-1, is_eval_env=False) -> None:
+        super(SteinerTreeEnv, self).__init__()
+        
+        assert parenting == -1, "Parenting not available for this environment"
+        
+        # Node status codes
+        self.NODE_HAS_MSG = 0
+        self.NODE_IS_TARGET = 1
+        
+        # Edge status codes
+        self.EDGE_IS_TAKEN = 1
+        self.EDGE_WEIGHT = 0
+        
+        self.n_nodes = n_nodes
+        self.n_edges = n_edges
+        self.n_dests = n_dests
+        self.weighted = weighted
+        self.action_space = gym.spaces.Discrete(n_edges)
+       
+        self.observation_space = gym.spaces.Box(low=0, high=1000, shape=((2 + fe.get_num_features())*n_nodes+2*n_edges*2+2*n_edges*2,))
+       
+        self.is_eval_env = is_eval_env
+        
+
+    def reset(self, seed=None, options={}) -> np.array:
+       
+        if seed != None:
+            super().reset(seed=seed)
+            random.seed(seed)
+            np.random.seed(seed)
+        
+        while True:
+            G = nx.gnm_random_graph(self.n_nodes, self.n_edges)
+            if nx.is_connected(G):
+                break
+        
+        if self.weighted:
+            delay = np.random.randint(3, 10, size=(self.n_nodes, self.n_nodes))/10.0
+        else:
+            delay = np.random.randint(1, 2, size=(self.n_nodes, self.n_nodes))/1.0
+        
+        for u, v, d in G.edges(data=True):
+            d['delay'] = delay[u, v]
+        
+        
+        x = np.zeros((self.n_nodes, 2+ fe.get_num_features()), dtype=np.float32)      
+        
+        self.dests = np.random.choice(self.n_nodes, self.n_dests+1, replace=False)
+        
+        self.approx_solution = 0
+        
+        if self.is_eval_env:
+            if self.n_dests == 1:
+                self.approx_solution = nx.algorithms.shortest_paths.generic.shortest_path_length(G, self.dests[0], self.dests[1], weight='delay')
+            elif self.n_dests == self.n_nodes - 1:
+                self.approx_solution = sum([features['delay'] for _, _, features in nx.minimum_spanning_edges(G, weight='delay')])
+                approx_solution_graph = nx.algorithms.approximation.steinertree.steiner_tree(G, self.dests, weight='delay', method='kou')  
+            else:
+                approx_solution_graph = nx.algorithms.approximation.steinertree.steiner_tree(G, self.dests, weight='delay', method='kou')  
+                self.approx_solution = sum([G[u][v]['delay'] for u, v in approx_solution_graph.edges()])
+            
+        G = G.to_directed()
+        
+        self.src, self.dests = self.dests[0], self.dests[1:]
+        
+        
+        x[self.src, self.NODE_HAS_MSG] = 1
+        x[self.dests, self.NODE_IS_TARGET] = 1
+        
+        # Adding structural features
+        sf = fe.generate_features(G)
+        x[:, -fe.get_num_features():] = sf
+        
+        self.adj = nx.adjacency_matrix(G).todense()
+        
+        edge_index = np.array(list(G.edges))
+        edge_f = np.array([G[u][v]['delay'] for u, v in G.edges], dtype=np.float32).reshape(-1, 1)
+       
+        edge_f = np.concatenate((edge_f, np.zeros((2*self.n_edges, 1), dtype=np.float32)), axis=1)
+        
+        
+        self.graph = gym.spaces.GraphInstance(nodes=x, edges=edge_f, edge_links=edge_index)
+        self.generated_solution = []
+
+        self.solution_cost = 0
+        info = {'mask': self._get_mask()}
+        
+        return vectorize_graph(self.graph), info
+    
+    
+    def _get_mask(self) -> np.array:
+        mask = np.zeros((2 * self.n_edges,), dtype=bool) < 1.0
+        mask[self.graph.nodes[self.graph.edge_links[:, 0], self.NODE_HAS_MSG] < 0.5] = False
+        mask[self.graph.nodes[self.graph.edge_links[:, 1], self.NODE_HAS_MSG] > 0.5] = False
+        return mask
+    
+    
+    def step(self, action: Tuple[int, int]) -> Tuple[gym.spaces.GraphInstance, SupportsFloat, bool, bool, dict]:
+        
+        
+        u, v = self.graph.edge_links[action, 0], self.graph.edge_links[action, 1]
+        
+        # print(self.graph.nodes.T)
+        assert (u < self.n_nodes), f"Node {u} is out of bounds!"
+        assert (v < self.n_nodes), f"Node {v} is out of bounds!"
+        assert self._get_mask()[action] == True, f"Mask of {action} is False!"
+        assert np.isclose(self.graph.edges[action, self.EDGE_IS_TAKEN], 1) == False, f"BUG IN THE LIBRARY! Edge {action} is already a part of the path!"
+        assert np.isclose(self.graph.nodes[u, self.NODE_HAS_MSG], 1), f"BUG IN THE LIBRARY! Node {u} is not a part of the path!"
+        assert np.isclose(self.graph.nodes[v, self.NODE_HAS_MSG], 0), f"BUG IN THE LIBRARY! Node {v} is already a part of the path!"
+        
+        done = False
+        reward = -self.graph.edges[action, self.EDGE_WEIGHT]
+        self.solution_cost -= reward
+        self.graph.nodes[v, self.NODE_HAS_MSG] = 1
+        self.generated_solution.append((u, v))
+        
+        if np.logical_and(self.graph.nodes[:, self.NODE_HAS_MSG] == 0, self.graph.nodes[:, self.NODE_IS_TARGET] == 1).sum() == 0:
+            done = True
+   
+            
+        info = {'mask': self._get_mask()}
+        
+        if done:
+            info['heuristic_solution'] = self.approx_solution
+            info['solved'] = True
+            info['solution_cost'] = self.solution_cost
+            pass
+        else:
+            assert info['mask'].sum() > 0, "No more actions possible! Shouldn't happen!"
+        
+        
+        return vectorize_graph(self.graph), reward, done, False, info
+        
---
+++ packages/graph-envs/graph_envs-0.0.55-py3-none-any.whl/graph_envs/tsp.py
@@ -0,0 +1,259 @@
+import gymnasium as gym
+# import torch 
+# import torch_geometric as pyg 
+import numpy as np 
+
+from typing import TYPE_CHECKING, Any, Generic, SupportsFloat, TypeVar, Tuple
+
+import networkx as nx 
+import random
+from graph_envs.utils import vectorize_graph
+import graph_envs.feature_extraction as fe
+
+class TSPEnv(gym.Env):
+    '''
+    Environment for shortest path problem.
+    input:
+        - n_nodes: number of nodes in the graph
+        - n_edges: number of edges in the graph
+        - weighted: whether the graph is weighted or not
+    '''
+    
+    def __init__(self, n_nodes, n_edges, weighted=True, return_graph_obs=False, parenting=-1, spatial=False, is_eval_env=False) -> None:
+        super(TSPEnv, self).__init__()
+        
+        assert parenting in [1,2], "Parenting must be either 1 or 2"
+        if spatial:
+            assert weighted == True, "Spatial TSP must be weighted"
+        
+        self.NODE_TAKEN = 0
+        self.NODE_IS_SOURCE = 1
+        self.NODE_X = 2
+        self.NODE_Y = 3
+        
+        self.EDGE_WEIGHT = 0
+        
+        self.n_nodes = n_nodes
+        self.n_edges = n_edges
+        self.weighted = weighted
+        self.spatial = spatial
+        
+        self.action_space = gym.spaces.Discrete(n_nodes)
+        self.observation_space = gym.spaces.Box(low=-10, high=1000, shape=((4 + fe.get_num_features()) *n_nodes+2*n_edges+2*n_edges*2,))
+        
+        self.return_graph_obs = return_graph_obs
+        self.is_eval_env = is_eval_env
+        self.parenting = parenting
+        
+        
+        
+    def reset(self, seed=None, options={}) -> np.array:
+        
+        if seed != None:
+            super().reset(seed=seed)
+            random.seed(seed)
+            np.random.seed(seed)
+        
+        self.start = 0
+        
+        
+        while True:
+            G = nx.gnm_random_graph(self.n_nodes, self.n_edges)
+            if not nx.is_connected(G):
+                continue
+            
+            if any([G.degree(node) == 1 for node in G.nodes]):
+                continue
+            
+            cp = G.copy()
+            cp.remove_node(self.start)
+            if nx.is_connected(cp):
+                break
+        
+        
+        # if self.weighted:
+        #     delay = np.random.randint(3, 10, size=(self.n_nodes, self.n_nodes))/10.0
+        # else:
+        #     delay = np.random.randint(1, 2, size=(self.n_nodes, self.n_nodes))/1.0
+        
+        if self.spatial:
+            # Generate spatial coordinates
+            for v in G.nodes():
+                G.nodes[v]['x'] = np.random.rand() * 10
+                G.nodes[v]['y'] = np.random.rand() * 10
+            # Calculate the euclidean distance between nodes and set it as delay
+            for u, v, d in G.edges(data=True):
+                d['weight'] = np.sqrt((G.nodes[u]['x'] - G.nodes[v]['x'])**2 + (G.nodes[u]['y'] - G.nodes[v]['y'])**2)
+        else:
+            if self.weighted:
+                for u, v, d in G.edges(data=True):
+                    d['weight'] = np.random.randint(3, 10)/10.0
+            else:
+                for u, v, d in G.edges(data=True):
+                    d['weight'] = np.random.randint(1, 2)/1.0
+                
+        
+        # from matplotlib import pyplot as plt
+        # fig = plt.figure(figsize=(10,10))
+        # # calculate the layout pbased on node coordinates
+        # pos = {}
+        # for v in G.nodes():
+        #     pos[v] = (G.nodes[v]['x'], G.nodes[v]['y'])
+        # # draw the graph
+        # nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+        # nx.draw_networkx_labels(G, pos)
+        # # round edge labels to 2 digits:
+        # edge_labels = nx.get_edge_attributes(G, 'weight')
+        # edge_labels = {k: round(v, 2) for k, v in edge_labels.items()}
+        # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        # plt.savefig('graph.png')
+        
+        
+        self.optimal_solution = 0
+        
+        if self.is_eval_env == True:
+            self.optimal_cycle = nx.approximation.traveling_salesman_problem(G, weight='weight', cycle=True)
+            for i in range(len(self.optimal_cycle)-1):
+                self.optimal_solution += G[self.optimal_cycle[i]][self.optimal_cycle[i+1]]['weight']
+            
+       
+        if self.parenting >= 2:
+            self.alt_G = G.copy()
+            self.alt_G.remove_node(self.start)
+            
+            
+        G = G.to_directed()
+        self.G = G
+        
+        x = np.zeros((self.n_nodes, 4+fe.get_num_features()), dtype=np.float32)
+        
+        if self.spatial:
+            x[:, self.NODE_X] = np.array([G.nodes[v]['x'] for v in G.nodes])
+            x[:, self.NODE_Y] = np.array([G.nodes[v]['y'] for v in G.nodes])
+            
+            
+        x[self.start, self.NODE_IS_SOURCE] = 1
+        self.head = self.start
+            
+        # Adding structural features
+        sf = fe.generate_features(G)
+        x[:, -fe.get_num_features():] = sf
+        
+        self.adj = nx.adjacency_matrix(G, weight='weight').todense()
+        
+        edge_index = np.array(list(G.edges))
+        edge_f = np.array([G[u][v]['weight'] for u, v in G.edges], dtype=np.float32).reshape(-1, 1)
+        self.graph = gym.spaces.GraphInstance(nodes=x, edges=edge_f, edge_links=edge_index)
+         
+        self.total_solution_cost = 0
+        self.steps_taken = 0
+        
+        info = {'mask': self._get_mask()}
+        
+        # assert info['mask'].sum() > 0, "No valid actions!"
+        if info['mask'].sum() == 0:
+            info['mask'][self.start] = 1
+            
+        if self.return_graph_obs:
+            info['graph_obs'] = self.graph
+        
+        # print('x: ', x[:, 0:4])
+        # print('edge features: ', edge_f)
+        # print('edges: ', edge_index)
+        
+        return vectorize_graph(self.graph), info
+    
+    # def _vectorize_graph(self, graph):
+    #     return np.concatenate((graph.nodes.flatten(), graph.edges.flatten(), graph.edge_links.flatten()), dtype=np.float32)
+    
+    
+    def _get_neighbors(self, node):
+        neigbors = self.graph.edge_links[self.graph.edge_links[:, 0] == node, 1]
+        return neigbors
+        
+    def _get_mask(self) -> np.array:
+        mask = np.zeros((self.n_nodes,), dtype=bool)
+        mask[self._get_neighbors(self.head)] = 1
+        mask[self.graph.nodes[:, self.NODE_TAKEN] == 1] = 0
+        if self.graph.nodes[:, self.NODE_TAKEN].sum() < self.n_nodes - 1:
+            mask[self.start] = 0
+            
+        if self.parenting >= 2:
+            valid_nodes = (mask == 1).nonzero()[0]
+            
+            # disconnecting_nodes = []
+            
+            for v in valid_nodes:
+                if v == self.start:
+                    continue
+                G_copy = self.alt_G.copy()
+                G_copy.remove_node(v)
+                if G_copy.number_of_nodes() == 0:
+                    break
+                if not nx.is_connected(G_copy):
+                    mask[v] = 0
+                    
+        
+        
+
+        return mask
+    
+    def step(self, action: int) -> Tuple[gym.spaces.GraphInstance, SupportsFloat, bool, bool, dict]:
+        
+        if (action == self.start) and (self.head == self.start):
+            done = True
+            reward = - self.n_nodes
+            info = {}
+            info['solved'] = False
+            info['heuristic_solution'] = self.optimal_solution
+            info['solution_cost'] = -1
+            info['mask'] = self._get_mask()
+            return vectorize_graph(self.graph), reward, done, False, info
+            
+        assert (action < self.n_nodes), f"Node {action} is out of bounds!"
+        assert self._get_mask()[action] == True, f"Mask of {action} is False!"
+        assert action in self._get_neighbors(self.head), f"Node {action} is not a neighbor of the current path head {self.head}!"
+        assert self.graph.nodes[action, self.NODE_TAKEN] == 0, f"Node {action} is already taken!"
+        
+        self.steps_taken += 1
+        
+        info = {}
+        done = False
+      
+        
+        # Calculate the reward:
+        reward = 0
+        reward = reward - self.adj[self.head, action]
+        # reward = reward + 1 - self.graph.nodes[action, self.NODE_TAKEN]
+        
+        # Add the cost of the edge to the total solution cost:
+        self.total_solution_cost += self.adj[self.head, action]
+        
+        # Mark the node as taken:
+        self.graph.nodes[action, self.NODE_TAKEN] = 1
+        
+        if self.parenting >= 2:
+            if action != self.start:
+                self.alt_G.remove_node(action)
+            
+        # Update the head:
+        self.head = action
+        
+        # If all nodes are taken and the head is back to the start node, the episode is done:
+        if np.isclose(self.graph.nodes[:, self.NODE_TAKEN], 0).any() == False:   
+            if action == self.start: 
+                done = True
+                info['solved'] = True
+        
+        info['mask'] = self._get_mask()
+        if (not done) and (info['mask'].sum() == 0):
+            done = True
+            reward -= self.n_nodes * 2
+            info['solved'] = False
+            
+        if done:
+            info['heuristic_solution'] = self.optimal_solution
+            info['solution_cost'] = self.total_solution_cost
+           
+        return vectorize_graph(self.graph), reward, done, False, info
+        

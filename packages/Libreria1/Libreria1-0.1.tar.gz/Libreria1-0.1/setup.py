df6ad56db9d523a702fa3898016b93382d7027e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:46 +0000

    Add package Libreria1/0.1/Libreria1-0.1.tar.gz

---
+++ packages/Libreria1/Libreria1-0.1.tar.gz/Libreria1-0.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import find_packages, setup
+VERSION = '0.1'
+PACKAGE_NAME = 'Libreria1'
+AUTHOR = 'Itzel Franco'
+AUTHOR_EMAIL = 'itzel_fd1@tesch.edu.mx'
+URL = 'https://github.com/itzelfd98'
+
+LICENSE = 'GNU GPL'
+DESCRIPTION = 'libreria de nombres de mujer y hombre'
+
+#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
+INSTALL_REQUIRES = [
+    'pandas'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)

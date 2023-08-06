d57b7ea6516df2a8bbeba1ea369672248254d8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:49 +0000

    Add package emailnator/0.1.1/emailnator-0.1.1.tar.gz

---
+++ packages/emailnator/emailnator-0.1.1.tar.gz/emailnator-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from os import path
+
+# Read the contents of README.md
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="emailnator",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "python-dotenv",
+    ],
+    author="repollo",
+    author_email="repollo.marrero@gmail.com",
+    description="A Python wrapper for the Emailnator temporary email service.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Add this line
+    keywords="emailnator wrapper temporary email",
+    url="https://github.com/repollo/emailnator",
+)

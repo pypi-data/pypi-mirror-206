a816f141def2f0a6269ab63fe25823e9814c7df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:31 +0000

    Add package nti.property/1.2.0/nti.property-1.2.0.tar.gz

---
+++ packages/nti.property/nti.property-1.2.0.tar.gz/nti.property-1.2.0/CHANGES.rst
@@ -0,0 +1,25 @@
+=========
+ Changes
+=========
+
+
+1.2.0 (2023-05-05)
+==================
+
+- Add support for Python 3.8, 3.9, 3.10 and 3.11.
+
+
+1.1.0 (2018-09-13)
+==================
+
+- Add support for Python 3.7.
+
+- Add support for zope.schema 4.7.
+
+
+1.0.0 (2017-04-26)
+==================
+
+- First PyPI release.
+- Add support for Python 3.6.
+- Remove backward compatibility exports.
---
+++ packages/nti.property/nti.property-1.2.0.tar.gz/nti.property-1.2.0/setup.py
@@ -0,0 +1,72 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.testing',
+    'pyhamcrest',
+    'zope.testrunner',
+]
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='nti.property',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Property",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    url="https://github.com/OpenNTI/nti.property",
+    license='Apache',
+    keywords='Property',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'zope.annotation',
+        'zope.cachedescriptors >= 4.2',
+        'zope.contenttype',
+        'zope.file >= 1.0',
+        'zope.schema >= 4.7.0',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'furo; python_version >= "3.6"',
+            'sphinx_rtd_theme; python_version < "3.6"',
+
+        ],
+    },
+    entry_points=entry_points,
+    test_suite="nti.property.tests",
+)

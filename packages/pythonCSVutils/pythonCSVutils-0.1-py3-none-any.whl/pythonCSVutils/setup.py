9b25463a77c544166b9500a30f4de3269a51882c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:38 +0000

    Add package pythonCSVutils/0.1/pythonCSVutils-0.1-py3-none-any.whl

---
+++ packages/pythonCSVutils/pythonCSVutils-0.1-py3-none-any.whl/pythonCSVutils/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pythonCSVutils',
+    version='0.1',
+    description='A Python module for processing CSV files efficiently',
+    packages=['pythonCSVutils'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)

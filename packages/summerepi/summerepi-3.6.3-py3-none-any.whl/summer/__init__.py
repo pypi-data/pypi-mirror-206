85106e4b3ee17f5709bab580a58b738359b04c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:48 +0000

    Add package summerepi/3.6.3/summerepi-3.6.3-py3-none-any.whl

---
+++ packages/summerepi/summerepi-3.6.3-py3-none-any.whl/summer/__init__.py
@@ -0,0 +1,4 @@
+from .adjust import Multiply, Overwrite
+from .compartment import Compartment
+from .model import CompartmentalModel
+from .stratification import AgeStratification, StrainStratification, Stratification

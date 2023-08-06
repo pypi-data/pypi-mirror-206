6ed6eadf85a970a4af2947e31dffc2d4e0d50508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:57 +0000

    Add package pystools/0.0.11/pystools-0.0.11.tar.gz

---
+++ packages/pystools/pystools-0.0.11.tar.gz/pystools-0.0.11/src/pystools/MD5util.py
@@ -0,0 +1,12 @@
+import hashlib
+
+
+def md5_encode(*args, to_upper=False):
+    m = hashlib.md5()
+    # 将args中的所有值拼接起来
+    for v in args:
+        m.update(f"{v}".encode("utf-8"))
+    if to_upper:
+        return m.hexdigest().upper()
+    return m.hexdigest()
+

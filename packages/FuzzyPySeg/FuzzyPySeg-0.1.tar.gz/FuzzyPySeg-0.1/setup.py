bccaee49fca362931519793e62585b6f1bd44c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:43 +0000

    Add package FuzzyPySeg/0.1/FuzzyPySeg-0.1.tar.gz

---
+++ packages/FuzzyPySeg/FuzzyPySeg-0.1.tar.gz/FuzzyPySeg-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'FuzzyPySeg',         # How you named your package folder (MyLib)
+  packages = ['FuzzyPySeg'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'FuzzyPySeg is a package for segmenting images using Fuzzy C Means clustering with either a Euclidean or Mahalanobis distance. You may also specify a centroid initialization using the firefly algorithm, genetic algorithm, or the Biogeography-based optimization algorithm.',   # Give a short description about your library
+  author = 'Daniel Krasnov',                   # Type in your name
+  author_email = 'dkrasnov@student.ubc.ca',      # Type in your E-Mail
+  url = 'https://github.com/Danyulll/FuzzyPySeg',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Danyulll/FuzzyPySeg/archive/refs/tags/v0.0.1.tar.gz',    # I explain this later on
+  keywords = ['image segmentation', 'clustering', 'fuzzy c-means', 'firefly algorithm', 'genetic algorithm', 'biogeography-based optimization algorithm'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'Image',
+          'numba',
+          'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

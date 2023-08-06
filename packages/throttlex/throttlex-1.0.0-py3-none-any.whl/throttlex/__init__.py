e4e4205e9df935bd9906eecf6446728470029a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package throttlex/1.0.0/throttlex-1.0.0-py3-none-any.whl

---
+++ packages/throttlex/throttlex-1.0.0-py3-none-any.whl/throttlex/__init__.py
@@ -0,0 +1,26 @@
+# Author: ASU --<andrei.suiu@gmail.com>
+# Purpose: utility library for >=Python3.8
+
+import time
+from collections import deque
+from typing import Any, Callable
+
+
+class Throttler:
+    def __init__(self, max_req: int, period: float, time_func: Callable[[], float] = time.time) -> None:
+        self._max_req = max_req
+        self._period = period
+        self._request_timestamps = deque(maxlen=max_req)
+        self._req_cnt = 0
+        self._time_func = time_func
+
+    def throttle(self, val: Any) -> Any:
+        t1 = self._time_func()
+        self._req_cnt += 1
+        self._request_timestamps.append(t1)
+        if len(self._request_timestamps) >= self._max_req:
+            t0 = self._request_timestamps[0]
+            dt = t1 - t0
+            if dt < self._period:
+                time.sleep(self._period - dt)
+        return val

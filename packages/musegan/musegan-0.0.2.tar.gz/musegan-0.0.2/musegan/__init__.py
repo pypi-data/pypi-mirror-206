5d7c2fd04c74fe77377fe2441e632e09547ad624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:29 +0000

    Add package musegan/0.0.2/musegan-0.0.2.tar.gz

---
+++ packages/musegan/musegan-0.0.2.tar.gz/musegan-0.0.2/musegan/__init__.py
@@ -0,0 +1,15 @@
+"""Dataset."""
+
+from .archs import bar_generator, critic, generator, temp_network, utils
+from .dataset import data_utils
+from .trainner import criterion, trainer
+
+__all__ = ["bar_generator",
+           "critic",
+           "generator",
+           "temp_network",
+           "utils",
+           "data_utils",
+           "criterion",
+           "trainer",
+           ]

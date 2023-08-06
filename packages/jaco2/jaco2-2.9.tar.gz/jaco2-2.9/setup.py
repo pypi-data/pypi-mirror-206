11f282c4aab89dcbafa2ba0fec12a766a7adf8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package jaco2/2.9/jaco2-2.9.tar.gz

---
+++ packages/jaco2/jaco2-2.9.tar.gz/jaco2-2.9/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+import glob
+
+jaco_module = Pybind11Extension(
+    'jaco2',
+    [str(fname) for fname in Path('src').glob('*.cpp')],
+    include_dirs=['.'],
+    extra_compile_args=['/Ox'],
+    extra_link_args=['/LIBPATH:src/CommandLayerEthernet.dll', '/LIBPATH:src/CommandLayerWindows.dll', '/LIBPATH:src/CommunicationLayerEthernet.dll', '/LIBPATH:src/CommunicationLayerWindows.dll'],
+)
+
+setup(
+    name='jaco2',
+    version=2.9,
+    author='Daniel Dharampal',
+    description='A binding of simple jaco2 SDK commands for python',
+    ext_modules=[jaco_module],
+    cmdclass={"build_ext": build_ext},
+)

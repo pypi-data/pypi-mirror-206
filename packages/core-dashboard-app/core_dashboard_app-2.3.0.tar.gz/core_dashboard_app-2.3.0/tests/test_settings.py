4f021be0a3e70a1b0ff1b5d9560686b4759e0124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package core-dashboard-app/2.3.0/core_dashboard_app-2.3.0.tar.gz

---
+++ packages/core-dashboard-app/core_dashboard_app-2.3.0.tar.gz/core_dashboard_app-2.3.0/tests/test_settings.py
@@ -0,0 +1,62 @@
+""" Tests Settings
+"""
+
+SECRET_KEY = "fake-key"
+
+INSTALLED_APPS = [
+    # Django apps
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sites",
+    # Local apps
+    "core_main_app",
+    "tests",
+]
+
+# IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+MIDDLEWARE = (
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "tz_detect.middleware.TimezoneMiddleware",
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "core_main_app.utils.custom_context_processors.domain_context_processor",
+                "django.template.context_processors.i18n",
+            ],
+        },
+    },
+]
+
+CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CUSTOM_NAME = "Curator"
+
+MONGODB_INDEXING = False
+MONGODB_ASYNC_SAVE = False
+
+ENABLE_SAML2_SSO_AUTH = False

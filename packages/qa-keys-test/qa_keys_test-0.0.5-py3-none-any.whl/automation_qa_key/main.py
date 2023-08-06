d212140b194976e43ad47545b52614c29cc81a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:50 +0000

    Add package qa-keys-test/0.0.5/qa_keys_test-0.0.5-py3-none-any.whl

---
+++ packages/qa-keys-test/qa_keys_test-0.0.5-py3-none-any.whl/automation_qa_key/main.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+config_file_path = os.path.join('..\config', 'qa_key.json')
+
+def get_qa_key():
+    with open(config_file_path, 'r', encoding='utf-8') as f:
+        config_data = json.load(f)
+        return config_data

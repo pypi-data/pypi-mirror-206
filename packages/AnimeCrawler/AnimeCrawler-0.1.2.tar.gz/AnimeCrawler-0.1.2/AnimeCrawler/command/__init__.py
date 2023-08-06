6164a26e4383ce1aa65d2666f6c8d838bf09f444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package AnimeCrawler/0.1.2/AnimeCrawler-0.1.2.tar.gz

---
+++ packages/AnimeCrawler/AnimeCrawler-0.1.2.tar.gz/AnimeCrawler-0.1.2/AnimeCrawler/command/__init__.py
@@ -0,0 +1,3 @@
+from AnimeCrawler.mhyyy.spider import AnimeSpider
+
+from .run import main
---
+++ packages/AnimeCrawler/AnimeCrawler-0.1.2.tar.gz/AnimeCrawler-0.1.2/AnimeCrawler/utils/__init__.py
@@ -0,0 +1,2 @@
+from .decode import base64_decode, unescape, is_url
+from .file import folder_path, get_video_path, merge_ts2mp4, write

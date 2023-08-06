18c769ad121f7ec4604c8abe50fd494468134282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:20 +0000

    Add package myNeuropsydia/0.1.13/myNeuropsydia-0.1.13.tar.gz

---
+++ packages/myNeuropsydia/myNeuropsydia-0.1.13.tar.gz/myNeuropsydia-0.1.13/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='myNeuropsydia',
+    packages=find_packages(),
+    version='0.1.13',
+    description='Partially fixed version of Neuropsydia library, a Python module for creating experiments, tasks and questionnaires',
+    author='Laborde',
+    license='ENS_Paris_Saclay',
+    package_data = {
+                	"myNeuropsydia.files.font":["*.ttf", "*.otf"],
+                	"myNeuropsydia.files.binary":["*.png"],
+                	"myNeuropsydia.files.logo":["*.png"]},
+    install_requires=['Pillow',
+                      'cryptography',
+                      'neurokit',
+                      'numpy',
+                      'pandas',
+                      'pygame==2.3.0',
+                      'python-docx',
+                      'pyxid',
+                      'statsmodels'],
+)
+
+

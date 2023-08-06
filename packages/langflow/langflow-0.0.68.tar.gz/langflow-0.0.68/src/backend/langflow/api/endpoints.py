73872ba8e8cba459418ede0431f4492845423d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:06 +0000

    Add package langflow/0.0.68/langflow-0.0.68.tar.gz

---
+++ packages/langflow/langflow-0.0.68.tar.gz/langflow-0.0.68/src/backend/langflow/api/endpoints.py
@@ -0,0 +1,26 @@
+import logging
+from typing import Any, Dict
+
+from fastapi import APIRouter, HTTPException
+
+from langflow.interface.run import process_graph_cached
+from langflow.interface.types import build_langchain_types_dict
+
+# build router
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+@router.get("/all")
+def get_all():
+    return build_langchain_types_dict()
+
+
+@router.post("/predict")
+def get_load(data: Dict[str, Any]):
+    try:
+        return process_graph_cached(data)
+    except Exception as e:
+        # Log stack trace
+        logger.exception(e)
+        raise HTTPException(status_code=500, detail=str(e)) from e
---
+++ packages/langflow/langflow-0.0.68.tar.gz/langflow-0.0.68/src/backend/langflow/interface/utils.py
@@ -0,0 +1,56 @@
+import base64
+from io import BytesIO
+import json
+import os
+from PIL.Image import Image
+from langchain.callbacks.manager import AsyncCallbackManager
+from langchain.chat_models import AzureChatOpenAI, ChatOpenAI
+from langchain.llms import AzureOpenAI, OpenAI
+from langflow.api.callback import StreamingLLMCallbackHandler
+
+import yaml
+
+
+def load_file_into_dict(file_path: str) -> dict:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    file_extension = os.path.splitext(file_path)[1].lower()
+
+    if file_extension == ".json":
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+    elif file_extension in [".yaml", ".yml"]:
+        with open(file_path, "r") as yaml_file:
+            data = yaml.safe_load(yaml_file)
+    else:
+        raise ValueError("Unsupported file type. Please provide a JSON or YAML file.")
+
+    return data
+
+
+def pil_to_base64(image: Image) -> str:
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str.decode("utf-8")
+
+
+def try_setting_streaming_options(langchain_object, websocket):
+    # If the LLM type is OpenAI or ChatOpenAI,
+    # set streaming to True
+    # First we need to find the LLM
+    llm = None
+    if hasattr(langchain_object, "llm"):
+        llm = langchain_object.llm
+    elif hasattr(langchain_object, "llm_chain") and hasattr(
+        langchain_object.llm_chain, "llm"
+    ):
+        llm = langchain_object.llm_chain.llm
+    if isinstance(llm, (OpenAI, ChatOpenAI, AzureOpenAI, AzureChatOpenAI)):
+        llm.streaming = bool(hasattr(llm, "streaming"))
+        stream_handler = StreamingLLMCallbackHandler(websocket)
+        stream_manager = AsyncCallbackManager([stream_handler])
+        llm.callback_manager = stream_manager
+
+    return langchain_object

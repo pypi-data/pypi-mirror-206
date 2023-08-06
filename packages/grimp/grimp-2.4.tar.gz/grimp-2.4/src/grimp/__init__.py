887b8e9ad1b50be6dca148114012cbab01ee1af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:39 +0000

    Add package grimp/2.4/grimp-2.4.tar.gz

---
+++ packages/grimp/grimp-2.4.tar.gz/grimp-2.4/src/grimp/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "2.4"
+
+from .domain.valueobjects import Module, DirectImport
+from .application.ports.graph import DetailedImport
+from .main import build_graph
+
+__all__ = ["Module", "DetailedImport", "DirectImport", "build_graph"]

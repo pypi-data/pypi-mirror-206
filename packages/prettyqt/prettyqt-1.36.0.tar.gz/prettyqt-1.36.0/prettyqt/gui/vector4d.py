096f8913c0c15205079923ea3789127190bc7f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:02 +0000

    Add package prettyqt/1.36.0/prettyqt-1.36.0.tar.gz

---
+++ packages/prettyqt/prettyqt-1.36.0.tar.gz/prettyqt-1.36.0/prettyqt/gui/vector4d.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from prettyqt import core
+from prettyqt.qt import QtGui
+
+
+class Vector4D(QtGui.QVector4D):
+    def __bool__(self):
+        return not self.isNull()
+
+    def __abs__(self) -> float:
+        return self.length()
+
+    def __reduce__(self):
+        return type(self), (self.x(), self.y(), self.z(), self.w())
+
+    def to_point(self) -> core.Point:
+        return core.Point(self.toPoint())
+
+    def to_pointf(self) -> core.PointF:
+        return core.PointF(self.toPointF())
+
+
+if __name__ == "__main__":
+    vector = Vector4D(0, 0, 0, 1)
+    print(abs(vector))

067aaa47a59e12659092c026a480bb3524b83c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:45 +0000

    Add package krx-rigger/0.0.3/krx_rigger-0.0.3.tar.gz

---
+++ packages/krx-rigger/krx_rigger-0.0.3.tar.gz/krx_rigger-0.0.3/test/test_list.py
@@ -0,0 +1,16 @@
+import logging
+from krx import KrxKindWeb
+
+def test_list():
+    web = KrxKindWeb()
+
+    items = web.fetch_list("2023-01-03", time_sleep=0.5)
+
+    print(items)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s [%(levelname)s] %(message)s')
+    logger = logging.getLogger("krx_api")
+    logger.setLevel(logging.DEBUG)
+    test_list()

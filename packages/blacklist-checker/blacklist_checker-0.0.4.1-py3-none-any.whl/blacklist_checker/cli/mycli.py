380fda733d78517745daeaaf12f0f59d7ee38bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:55 +0000

    Add package blacklist-checker/0.0.4.1/blacklist_checker-0.0.4.1-py3-none-any.whl

---
+++ packages/blacklist-checker/blacklist_checker-0.0.4.1-py3-none-any.whl/blacklist_checker/cli/mycli.py
@@ -0,0 +1,39 @@
+import typer
+import sys
+from blacklist_checker.requester.Black_list_checker import BlackListListChecker
+from blacklist_checker.messaging.Mailer import MyMailer
+app = typer.Typer()
+
+@app.command()
+def checkdns(domain:str = typer.Option("", help=("domain to test")),config:str=typer.Option("",help=("Configuration file")),reporting:bool=typer.Option(False,help=("If set a reporting is sent ")),provider:str=typer.Option("",help=("provider file"))):
+    try:
+        if config=="":
+            config_file="config.ini"
+        else:
+            config_file=config
+        mailer=MyMailer(config_file,domain,provider)
+        checker= BlackListListChecker(config_file)
+        result=checker.check(domain)
+        if not result:
+            if reporting:
+                mailer.send_regular_report()
+            print(f"Domain {domain} is not blacklisted")
+        else:
+            if reporting:
+                print("reporting enabled for alert ")
+                mailer.send_alert_report(result)
+            print(f"Domain {domain} is  blacklisted")
+            exit(2)
+    except Exception as e:
+        print(e)
+
+
+@app.command()
+def check_web_site(domain:str = typer.Option("", help=("domain to test"))):
+    pass
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    sys.exit(main())

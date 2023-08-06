116c86b50d7f96ac5bf8a1ac53e520e31c04e348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:14 +0000

    Add package Geode-Numerics/3.0.2/Geode_Numerics-3.0.2-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/Geode-Numerics/Geode_Numerics-3.0.2-cp38-cp38-manylinux2014_x86_64.whl/geode_numerics/surface.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .lib64.geode_numerics_py_surface import *
+NumericsSurfaceLibrary.initialize()

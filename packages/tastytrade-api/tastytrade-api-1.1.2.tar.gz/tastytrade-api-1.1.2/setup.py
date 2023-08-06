d84739bdf948d2736b9daed642d80c36eb95f94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:14 +0000

    Add package tastytrade-api/1.1.2/tastytrade-api-1.1.2.tar.gz

---
+++ packages/tastytrade-api/tastytrade-api-1.1.2.tar.gz/tastytrade-api-1.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tastytrade-api",
+    version="1.1.2",
+    author="Peter Oroszvari",
+    author_email="peter@oroszvari.hu",
+    description="A Python client and SDK for the Tastytrade API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peter-oroszvari/tastytrade-api",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "websocket-client",
+        "websockets"
+    ],
+)

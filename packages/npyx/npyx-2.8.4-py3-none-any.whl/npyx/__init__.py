83819ae06f09b338f41ffa7e388962ba43677f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package npyx/2.8.4/npyx-2.8.4-py3-none-any.whl

---
+++ packages/npyx/npyx-2.8.4-py3-none-any.whl/npyx/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from . import utils, inout, gl, spk_t, spk_wvf, corr, stats, plot,\
+              behav, merger, circuitProphyler, feat, metrics,\
+              info, model, h5, testing, ml, datasets
+
+from .utils import *
+from .inout import *
+from .gl import *
+from .spk_t import *
+from .spk_wvf import *
+from .corr import *
+from .stats import *
+from .plot import *
+from .behav import *
+from .merger import *
+from .circuitProphyler import *
+from .feat import *
+from .metrics import *
+from .info import *
+from .model import *
+from .h5 import *
+from .testing import *
+
+__doc__ = """
+
+npyx submodules:
+ .utils
+ .io
+ .gl
+ .spk_t
+ .spk_wvf
+ .corr
+ .stats
+ .plot
+ .behav
+ .merger
+ .circuitProphyler
+ .feat
+ .h5
+"""
+
+__version__ = "2.8.4"
+
+print(f"npyx version {__version__} imported.")

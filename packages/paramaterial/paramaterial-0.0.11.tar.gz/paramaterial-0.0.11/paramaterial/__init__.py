a59543f47c5f826c3160acd5539c5c2bd7c48513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package paramaterial/0.0.11/paramaterial-0.0.11.tar.gz

---
+++ packages/paramaterial/paramaterial-0.0.11.tar.gz/paramaterial-0.0.11/paramaterial/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.11'
+from paramaterial import example, models
+from paramaterial.plug import DataItem, DataSet
+from paramaterial.preparing import *
+from paramaterial.processing import *
+from paramaterial.modelling import *
+from paramaterial.plotting import *
+from paramaterial.screening import *
+from paramaterial.receipts import TestReceipts
+from paramaterial.zener_holloman import *
+

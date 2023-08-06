7a8d164015a4ffbc820730a5799eb52647a4895c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package AegosPy/0.5/AegosPy-0.5.tar.gz

---
+++ packages/AegosPy/AegosPy-0.5.tar.gz/AegosPy-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'AegosPy',
+    version='0.5',
+    author= 'Aegos',
+    description='Aegos Programming',
+    packages= ['AegosPy'],
+    author_email='syakkml@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)

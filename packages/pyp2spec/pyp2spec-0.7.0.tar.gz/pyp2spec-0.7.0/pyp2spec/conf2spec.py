572d6e1973aa8d01640200af19d8256753bea1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:25 +0000

    Add package pyp2spec/0.7.0/pyp2spec-0.7.0.tar.gz

---
+++ packages/pyp2spec/pyp2spec-0.7.0.tar.gz/pyp2spec-0.7.0/pyp2spec/conf2spec.py
@@ -0,0 +1,204 @@
+from importlib.resources import read_text
+from textwrap import fill
+
+import click
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+from jinja2 import Template
+
+
+TEMPLATE_FILENAME = "template.spec"
+
+
+class ConfigFile:
+    """Load configuration file and return its validated values."""
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.contents = self.load_configuration
+
+    @property
+    def load_configuration(self):
+        """Return loaded TOML configuration file contents."""
+
+        with open(self.filename, "rb") as configuration_file:
+            loaded_contents = tomllib.load(configuration_file)
+
+        return loaded_contents
+
+    def get_string(self, key):
+        """Return a value for given key. Validate the value is a string.
+        Raise TypeError otherwise."""
+
+        return self._get_value(key, str)
+
+    def get_list(self, key):
+        """Return a value for given key. Validate the value is a list.
+        Raise TypeError otherwise."""
+
+        return self._get_value(key, list)
+
+    def get_bool(self, key):
+        """Return a value for given key. Validate the value is a boolean.
+        Raise TypeError otherwise."""
+
+        return self._get_value(key, bool)
+
+    def _get_value(self, key, val_type):
+        val = self.contents.get(key, val_type())
+        if not isinstance(val, val_type):
+            raise TypeError(f"{val} must be a {val_type}")
+        return val
+
+
+def generate_extra_build_requires(config):
+    """If defined in config file, return extra build requires.
+    If none were defined, return an empty string."""
+
+    # TODO: custom tox env (-e)
+    options = {
+        "tox": "-t",
+        "extra": "-x",
+    }
+    extra_brs = config.get_list("extra_build_requires")
+
+    # No extra BuildRequires were defined - return an empty string
+    if not extra_brs:
+        return ""
+
+    generated_brs = []
+    add = generated_brs.append
+    for extra_br in extra_brs:
+        if extra_br == "extra":
+            add(options.get(extra_br))
+            add(",".join(config.get_list("extra_test_env")))
+        else:
+            add(options.get(extra_br))
+
+    return " ".join(generated_brs)
+
+
+def generate_check(config):
+    """Generate valid check section.
+    If defined in config file, use applicable test macro.
+    If no test method was defined, return an empty string."""
+
+    test_method = config.get_string("test_method")
+    if test_method == "pytest":
+        return generate_pytest(config)
+    elif test_method == "tox":
+        return generate_tox(config)
+    else:
+        return ""
+
+
+def generate_pytest(config):
+    """Return valid pytest macro with unwanted tests (if defined)."""
+
+    if unwanted_tests := generate_unwanted_tests(config):
+        return f"%pytest {unwanted_tests}"
+    return "%pytest"
+
+
+def generate_tox(config):
+    """Return valid tox macro. If additional unwanted are defined,
+    raise NotImplementedError."""
+
+    if unwanted_tests := generate_unwanted_tests(config):
+        raise NotImplementedError
+    return "%tox"
+
+
+def generate_unwanted_tests(config):
+    """If defined in config file, get the unwanted tests.
+    Given the unwanted tests are [a, b], return `-k "not a and\\\nnot b"`."""
+
+    unwanted_tests = config.get_list("unwanted_tests")
+    if not unwanted_tests:
+        return ""
+    else:
+        prep_unwanteds = [f"not {test}" for test in unwanted_tests]
+        unwanteds_as_str = " and \\\n".join(prep_unwanteds)
+        formatted_unwanteds = f"-k '{unwanteds_as_str}'"
+        return formatted_unwanteds
+
+
+def wrap_description(config):
+    """Wrap description line to 79 characters at most.
+
+    If the description line length exceeds 79 lines, tools like rpmlint start
+    emit warnings. Use newlines to prevent exceeding the max. length.
+    Return the modified description string.
+    """
+    return fill(
+        config.get_string("description"),
+        width=79,
+        break_long_words=False
+    )
+
+def fill_in_template(config):
+    """Return template rendered with data from config file."""
+
+    spec_template = Template(read_text("pyp2spec", TEMPLATE_FILENAME))
+
+    result = spec_template.render(
+        archful=config.get_bool("archful"),
+        archive_name=config.get_string("archive_name"),
+        binary_files=config.get_list("binary_files"),
+        description=wrap_description(config),
+        doc_files=" ".join(config.get_list("doc_files")),
+        extra_build_requires=generate_extra_build_requires(config),
+        license_files=" ".join(config.get_list("license_files")),
+        license=config.get_string("license"),
+        manual_build_requires=config.get_list("manual_build_requires"),
+        name=config.get_string("pypi_name"),
+        python_name=config.get_string("python_name"),
+        pypi_version=config.get_string("pypi_version"),
+        source=config.get_string("source"),
+        summary=config.get_string("summary"),
+        test_method=generate_check(config),
+        test_top_level=config.get_bool("test_top_level"),
+        url=config.get_string("url"),
+        version=config.get_string("version"),
+    )
+
+    return result
+
+
+def save_spec_file(config, output):
+    """Save the spec file in the current directory if custom output is not set.
+    Return the saved file name."""
+
+    result = fill_in_template(config)
+    if output is None:
+        output = config.get_string("python_name") + ".spec"
+    with open(output, "w") as spec_file:
+        click.secho(f"Saving spec file to '{output}'", fg="yellow")
+        spec_file.write(result)
+    click.secho("Spec file was saved successfully", fg="green")
+    return output
+
+
+def create_spec_file(config_file, spec_output=None):
+    """Create and save the generate spec file."""
+    config = ConfigFile(config_file)
+    return save_spec_file(config, spec_output)
+
+
+@click.command()
+@click.argument("config")
+@click.option(
+    "--spec-output",
+    "-o",
+    help="Provide custom output for spec file",
+)
+def main(config, spec_output):
+    create_spec_file(config, spec_output)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pyp2spec/pyp2spec-0.7.0.tar.gz/pyp2spec-0.7.0/tests/test_pyp2conf.py
@@ -0,0 +1,255 @@
+"""Test the functionality of pyp2conf part of the tool.
+The data is downloaded from the PyPI and stored in betamax casettes
+to prevent loading from the internet on each request.
+"""
+import pytest
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+from pyp2spec.pyp2conf import PypiPackage, create_config_contents
+from pyp2spec.pyp2conf import convert_version_to_rpm_scheme
+
+
+def test_non_existent_package(betamax_session):
+    with pytest.raises(SystemExit):
+        PypiPackage("definitely-nonexisting-package-name", session=betamax_session)
+
+
+@pytest.mark.parametrize("package, version",
+    [
+        ("aionotion", "2.0.3"),
+        ("click", "7.1.2"),
+    ]
+)
+def test_automatically_generated_config_is_valid(betamax_parametrized_session, package, version):
+    """Run the config rendering in fully automated mode and compare the results"""
+    config = create_config_contents(
+        package=package,
+        version=version,
+        session=betamax_parametrized_session,
+    )
+
+    with open(f"tests/test_configs/default_python-{package}.conf", "rb") as config_file:
+        loaded_contents = tomllib.load(config_file)
+
+    assert config == loaded_contents
+
+
+def test_config_with_customization_is_valid(betamax_session):
+    """Get the upstream metadata and modify some fields to get the custom config file.
+    This also tests the compliance with Fedora Legal data by making
+    a request to the remote resource.
+    """
+    package = "aionotion"
+    config = create_config_contents(
+        package=package,
+        description="A asyncio-friendly library for Notion Home Monitoring devices.\n",
+        version="2.0.3",
+        summary="Python library for Notion Home Monitoring",
+        top_level=True,
+        compliant=True,
+        session=betamax_session,
+    )
+
+    with open(f"tests/test_configs/customized_{package}.conf", "rb") as config_file:
+        loaded_contents = tomllib.load(config_file)
+
+    assert config == loaded_contents
+
+
+def test_archful_package(betamax_session):
+    """Generate config for tornado which is archful"""
+    package = "tornado"
+    config = create_config_contents(
+        package=package,
+        top_level=True,
+        license="Apache 2.0",
+        archful=True,
+        session=betamax_session,
+    )
+
+    with open(f"tests/test_configs/customized_{package}.conf", "rb") as config_file:
+        loaded_contents = tomllib.load(config_file)
+
+    assert config["archful"] == loaded_contents["archful"]
+
+
+def test_license_classifier_read_correctly():
+    fake_pkg_data = {"info":{"classifiers": [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT No Attribution License (MIT-0)",
+        "Development Status :: 3 - Alpha",
+    ]}}
+
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.filter_license_classifiers() == [
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT No Attribution License (MIT-0)"
+    ]
+
+
+def test_no_license_classifiers_and_no_license_keyword():
+    pkg = PypiPackage("_", package_metadata={"info":{"classifiers": []}})
+    assert pkg.filter_license_classifiers() == []
+    with pytest.raises(SystemExit):
+        pkg.license()
+
+
+def test_compliant_license_is_returned(fake_fedora_licenses):
+    fake_pkg_data = {"info": {"classifiers" : [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT No Attribution License (MIT-0)",
+        "Development Status :: 3 - Alpha",
+    ]}}
+
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.license(check_compliance=True, licenses_dict=fake_fedora_licenses) == "MIT AND MIT-0"
+
+
+def test_bad_license_fails_compliance_check(fake_fedora_licenses):
+    fake_pkg_data = {"info": {"classifiers" : [
+        "License :: OSI Approved :: European Union Public Licence 1.0 (EUPL 1.0)"
+    ]}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+
+    with pytest.raises(SystemExit):
+        pkg.license(check_compliance=True, licenses_dict=fake_fedora_licenses)
+
+
+def test_bad_license_without_compliance_check_is_returned():
+    fake_pkg_data = {"info": {"classifiers" : [
+        "License :: OSI Approved :: European Union Public Licence 1.0 (EUPL 1.0)"
+    ]}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+
+    assert pkg.license(check_compliance=False) == "EUPL-1.0"
+
+
+def test_mix_good_bad_licenses_fail_compliance_check(fake_fedora_licenses):
+    fake_pkg_data = {"info": {"classifiers" : [
+        "License :: OSI Approved :: European Union Public Licence 1.0 (EUPL 1.0)",
+        "License :: OSI Approved :: MIT License",
+    ]}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    with pytest.raises(SystemExit):
+        pkg.license(check_compliance=True, licenses_dict=fake_fedora_licenses)
+
+
+def test_mix_good_bad_licenses_without_compliance_check_are_returned():
+    fake_pkg_data = {"info": {"classifiers" : [
+        "License :: OSI Approved :: European Union Public Licence 1.0 (EUPL 1.0)",
+        "License :: OSI Approved :: MIT License",
+    ]}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.license(check_compliance=False) == "EUPL-1.0 AND MIT"
+
+
+def test_license_keyword_without_compliance_check():
+    fake_pkg_data = {"info": {"license": "BSD-2-Clause", "classifiers": []}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.license(check_compliance=False) == "BSD-2-Clause"
+
+
+def test_license_keyword_with_compliance_check(fake_fedora_licenses):
+    fake_pkg_data = {"info": {"license": "RSCPL", "classifiers": []}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    with pytest.raises(SystemExit):
+        pkg.license(check_compliance=True, licenses_dict=fake_fedora_licenses)
+
+
+@pytest.mark.parametrize("compliant", (True, False))
+def test_multiline_license_keyword_fails(compliant, fake_fedora_licenses):
+    fake_pkg_data = {"info": {"license": "This is absolutely\nhorrible\nidea\nto\ndo..."}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    with pytest.raises(SystemExit):
+        pkg.license(check_compliance=compliant, licenses_dict=fake_fedora_licenses)
+
+
+@pytest.mark.parametrize("compliant", (True, False))
+def test_empty_license_keyword_fails(compliant, fake_fedora_licenses):
+    fake_pkg_data = {"info": {"license": "", "classifiers": []}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    with pytest.raises(SystemExit):
+        pkg.license(check_compliance=compliant, licenses_dict=fake_fedora_licenses)
+
+
+def test_nonexisting_classifiers():
+    fake_pkg_data = {"info": {"classifiers": ["License :: OSI Approved :: XXXXXX"]}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    with pytest.raises(SystemExit):
+        pkg.license()
+
+
+def test_zip_sdist_is_added_to_source_macro():
+
+    fake_pkg_data = {
+        "releases": {
+            "1.2.3": [{
+                "filename": "Awesome_TestPkg-1.2.3.zip",
+                "packagetype": "sdist"}]
+                },
+        "info": {"version": "1.2.3"}
+    }
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    version = pkg.version()
+    assert pkg.source(version) == "%{pypi_source Awesome_TestPkg %{version} zip}"
+
+
+def test_no_homepage_in_metadata():
+    fake_pkg_data = {"info": {"package_url": "https://foo"}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.project_url() == "https://foo"
+
+
+def test_summary_is_generated_if_not_in_upstream():
+    fake_pkg_data = {"info": {"summary": ""}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.summary() == "..."
+
+
+def test_summary_is_generated_if_upstream_data_is_multiline():
+    fake_pkg_data = {"info": {"summary": "I\nforgot\nthat summary\nmust\nbe short"}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.summary() == "..."
+
+
+def test_capitalized_underscored_pypi_name_is_normalized():
+    fake_pkg_data = {"info": {"name": "Awesome_TestPkg"}}
+    pkg = PypiPackage("Awesome_TestPkg", package_metadata=fake_pkg_data)
+    assert pkg.pypi_name == "awesome-testpkg"
+    assert pkg.python_name() == "python-awesome-testpkg"
+
+
+@pytest.mark.parametrize(
+    ("pypi_version", "rpm_version"), [
+        ("1.1.0", "1.1.0"),
+        ("1!0.2.13", "1:0.2.13"),
+        ("0.0.2-beta1", "0.0.2~b1"),
+        ("0.5.40-0", "0.5.40^post0"),
+    ]
+)
+def test_pypi_version_is_converted_to_rpm(pypi_version, rpm_version):
+    assert convert_version_to_rpm_scheme(pypi_version) == rpm_version
+
+
+@pytest.mark.parametrize(
+    ("pypi_version", "pypi_version_macro"), [
+        ("1.1.0", "%{version}"),  # conversion to RPM doesn't change the string
+        ("1!0.2.13", "1!0.2.13"),  # Version string is normalized, see previous test
+        ("0.0.2-beta1", "0.0.2-beta1"),
+        ("0.5.40-0", "0.5.40-0"),
+    ]
+)
+def test_pypi_version_or_macro(pypi_version, pypi_version_macro):
+    fake_pkg_data = {"info": {"version": pypi_version}}
+    pkg = PypiPackage("_", package_metadata=fake_pkg_data)
+    assert pkg.pypi_version_or_macro(pypi_version) == pypi_version_macro

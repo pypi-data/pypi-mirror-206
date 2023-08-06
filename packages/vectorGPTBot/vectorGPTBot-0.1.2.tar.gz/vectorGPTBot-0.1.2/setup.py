bab2e69940c7f18736c157fb3cb6ecfbabeb45b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:34 +0000

    Add package vectorGPTBot/0.1.2/vectorGPTBot-0.1.2.tar.gz

---
+++ packages/vectorGPTBot/vectorGPTBot-0.1.2.tar.gz/vectorGPTBot-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vectorGPTBot',
+    version='0.1.2',
+    author='wstart',
+    description='A sample vectorGPTBot',
+    long_description='A sample vectorGPTBot',
+    long_description_content_type="text/markdown",
+    packages=find_packages('vectorGPTBot'),
+    package_dir={'': 'vectorGPTBot'},
+    install_requires=[
+        'chromadb',
+        'modelscope',
+        'transformers'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    keywords='vectorGPTBot',
+    license='Apache License 2.0',
+)

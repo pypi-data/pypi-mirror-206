f86e6807e85d65d5aa9a916b858f4598e8179615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package pythonCSVutils/0.1/pythonCSVutils-0.1.tar.gz

---
+++ packages/pythonCSVutils/pythonCSVutils-0.1.tar.gz/pythonCSVutils-0.1/pythonCSVutils/__init__.py
@@ -0,0 +1,14 @@
+from .csv_processor import (
+    read_csv,
+    write_csv,
+    filter_rows,
+    sort_rows,
+    update_rows,
+    select_columns,
+    read_csv_header,
+    append_rows,
+    group_by,
+    aggregate,
+    merge,
+    drop_columns,
+)

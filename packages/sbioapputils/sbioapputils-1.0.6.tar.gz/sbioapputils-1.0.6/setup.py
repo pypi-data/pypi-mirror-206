d88b5822d90b8f89e8fc322f9c9e658aac6072e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:37 +0000

    Add package sbioapputils/1.0.6/sbioapputils-1.0.6.tar.gz

---
+++ packages/sbioapputils/sbioapputils-1.0.6.tar.gz/sbioapputils-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="sbioapputils",
+      description="Superbio app runner utils",
+      author="Superbio AI",
+      author_email="smorgan@superbio.ai",
+      url='https://github.com/Superbio-ai/app-sbioutils',
+      install_requires=['requests==2.22.0', 'boto3==1.21.27','pandas==1.2.5', 'anndata==0.8.0', 'scanpy==1.9.1', 'numpy==1.21'],
+      packages=['sbioapputils']
+)

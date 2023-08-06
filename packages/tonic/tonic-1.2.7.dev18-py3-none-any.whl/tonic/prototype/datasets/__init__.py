7e8b6c886eee6fbd579210067d4794171663e9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package tonic/1.2.7.dev18/tonic-1.2.7.dev18-py3-none-any.whl

---
+++ packages/tonic/tonic-1.2.7.dev18-py3-none-any.whl/tonic/prototype/datasets/__init__.py
@@ -0,0 +1,16 @@
+from .ncars import NCARS
+from .nmnist import NMNIST
+from .prophesee import (
+    Gen1AutomotiveDetection,
+    Gen4Automotive,
+    Gen4AutomotiveDetectionMini,
+)
+from .stmnist import STMNIST
+
+__all__ = [
+    "NMNIST",
+    "STMNIST",
+    "NCARS",
+    "Gen4AutomotiveDetectionMini",
+    "Gen1AutomotiveDetection",
+]

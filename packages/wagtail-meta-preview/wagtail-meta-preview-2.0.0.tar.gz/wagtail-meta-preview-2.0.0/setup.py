66da5a420f0131f74cdc6bc8aa01e99449ed9802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package wagtail-meta-preview/2.0.0/wagtail-meta-preview-2.0.0.tar.gz

---
+++ packages/wagtail-meta-preview/wagtail-meta-preview-2.0.0.tar.gz/wagtail-meta-preview-2.0.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+with open("wagtail_meta_preview/version.py", "r") as f:
+    version = None
+    exec(f.read())
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+testing_extras = ["black==19.10b0", "coverage==5.2.1"]
+
+setup(
+    name="wagtail-meta-preview",
+    version=version,
+    description="Add preview panels for meta data to wagtail",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Andreas Bernacca",
+    author_email="ante.bernacca@gmail.com",
+    url="https://github.com/rinti/wagtail-meta-preview",
+    install_requires=["wagtail>=3.0.0"],
+    extras_require={
+        "testing": testing_extras,
+    },
+    setup_requires=["wheel"],
+    zip_safe=False,
+    license="MIT License",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    package_data={},
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Django",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 3",
+        "Framework :: Wagtail :: 4",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

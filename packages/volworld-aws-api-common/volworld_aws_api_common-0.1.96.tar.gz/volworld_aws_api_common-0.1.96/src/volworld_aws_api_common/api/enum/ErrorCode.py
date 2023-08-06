c76052d62281e027d3e85c54a9b1b8b67df6bada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:54 +0000

    Add package volworld-aws-api-common/0.1.96/volworld_aws_api_common-0.1.96.tar.gz

---
+++ packages/volworld-aws-api-common/volworld_aws_api_common-0.1.96.tar.gz/volworld_aws_api_common-0.1.96/src/volworld_aws_api_common/api/enum/ErrorCode.py
@@ -0,0 +1,11 @@
+from aenum import IntEnum
+from volworld_common.api.enum.common_error_code import CommonErrorCode
+
+
+class ErrorCode(IntEnum):
+
+    NoError = CommonErrorCode.NoError.value
+
+    UserNameExisting = 1001
+    WrongUserNameFormat = 1002
+

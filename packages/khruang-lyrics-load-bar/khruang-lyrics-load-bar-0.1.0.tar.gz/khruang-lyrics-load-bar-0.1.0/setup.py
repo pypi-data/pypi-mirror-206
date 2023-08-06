0e84e131bc2cb256e1797c990233bb027e315e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:57 +0000

    Add package khruang-lyrics-load-bar/0.1.0/khruang-lyrics-load-bar-0.1.0.tar.gz

---
+++ packages/khruang-lyrics-load-bar/khruang-lyrics-load-bar-0.1.0.tar.gz/khruang-lyrics-load-bar-0.1.0/setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='khruang-lyrics-load-bar',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'khruang-lyrics-load-bar = khruang_lyrics_load_bar.main:main'
+        ]
+    }
+)

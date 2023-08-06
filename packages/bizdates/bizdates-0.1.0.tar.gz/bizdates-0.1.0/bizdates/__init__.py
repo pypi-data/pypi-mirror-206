093406aabeae4f4cddf7ec930c412080ee29aa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:02 +0000

    Add package bizdates/0.1.0/bizdates-0.1.0.tar.gz

---
+++ packages/bizdates/bizdates-0.1.0.tar.gz/bizdates-0.1.0/bizdates/__init__.py
@@ -0,0 +1,14 @@
+"""
+bizdates.
+
+For working with multiple business dates
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Paul Gray'
+__credits__ = ''
+
+from .bizdates import GetMonthDate
+from .bizdates import SetFinancialMonth
+from .bizdates import SetFinancialYear
+from .bizdates import GetMonthNameByFMShort
---
+++ packages/bizdates/bizdates-0.1.0.tar.gz/bizdates-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='bizdates',
+    version='0.1.0',    
+    description='A Python package for working with business dates',
+    url='https://github.com/shuds13/pyexample',
+    author='Paul Gray',
+    author_email='prgray86@gmail.com',
+    license='BSD 2-clause',
+    packages=['bizdates'],
+    install_requires=['datetime'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)

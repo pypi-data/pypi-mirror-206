886a03eff49cf126e81069d0379f1cbe2d441858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:54 +0000

    Add package hrin-msb/0.2.25/hrin_msb-0.2.25.tar.gz

---
+++ packages/hrin-msb/hrin_msb-0.2.25.tar.gz/hrin_msb-0.2.25/msb_auth/users.py
@@ -0,0 +1,21 @@
+from .token import AuthToken
+from django.utils.functional import cached_property
+
+
+class TokenUser(AuthToken):
+
+	@property
+	def is_authenticated(self):
+		return self.session.get('is_authenticated', False) and self.is_valid
+
+	@cached_property
+	def is_admin(self):
+		return self.session.get("is_admin") == True
+
+	@cached_property
+	def auth_type(self):
+		return self.session.get('auth_type')
+
+	@property
+	def email(self):
+		return self.session.get(self._user_email_field)

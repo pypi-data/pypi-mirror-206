a34af3b6427f9880e12d58d8b6196aae0e7860fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package crunch-cli/0.17.0/crunch_cli-0.17.0-py3-none-any.whl

---
+++ packages/crunch-cli/crunch_cli-0.17.0-py3-none-any.whl/crunch/inline.py
@@ -0,0 +1,55 @@
+import typing
+import pandas
+import os
+import sys
+
+from . import utils, ensure, constants
+from . import command, tester
+
+
+class _Inline:
+
+    def __init__(self, module: typing.Any, model_directory: str):
+        self.module = module
+        self.model_directory = model_directory
+
+        self.session = utils.CustomSession(
+            os.environ["WEB_BASE_URL"],
+            os.environ["API_BASE_URL"],
+            bool(os.environ.get("DEBUG", "False")),
+        )
+
+        print(f"loaded inline runner with module: {module}")
+    
+    def load_data(self) -> typing.Tuple[pandas.DataFrame, pandas.DataFrame, pandas.DataFrame]:
+        (
+            _,
+            _,
+            x_train_path,
+            y_train_path,
+            x_test_path
+        ) = command.download(self.session)
+
+        x_train = utils.read(x_train_path)
+        y_train = utils.read(y_train_path)
+        x_test = utils.read(x_test_path)
+
+        return x_train, y_train, x_test
+
+    def test(self, force_first_train=True, train_frequency=1):
+        return tester.run(
+            self.module,
+            self.session,
+            self.model_directory,
+            force_first_train,
+            train_frequency,
+        )
+
+
+def load(module_or_module_name: typing.Any, model_directory=constants.DEFAULT_MODEL_DIRECTORY):
+    if isinstance(module_or_module_name, str):
+        module = sys.modules[module_or_module_name]
+    else:
+        module = module_or_module_name
+    
+    return _Inline(module, model_directory)

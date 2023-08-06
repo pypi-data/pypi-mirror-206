b022ef9f9a07842a251231d0fac948ceb1e9fa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:45 +0000

    Add package hubspot-api-client/8.0.0b3/hubspot_api_client-8.0.0b3-py3-none-any.whl

---
+++ packages/hubspot-api-client/hubspot_api_client-8.0.0b3-py3-none-any.whl/hubspot/discovery/crm/associations/discovery.py
@@ -0,0 +1,17 @@
+import hubspot.crm.associations as api_client
+from ...discovery_base import DiscoveryBase
+
+
+class Discovery(DiscoveryBase):
+    @property
+    def v4(self):
+        from .v4.discovery import Discovery as v4Discovery
+        return v4Discovery(self.config)
+
+    @property
+    def batch_api(self) -> api_client.BatchApi:
+        return self._configure_api_client(api_client, "BatchApi")
+
+    @property
+    def types_api(self) -> api_client.TypesApi:
+        return self._configure_api_client(api_client, "TypesApi")

f5596f45668f258358891595aaad6376ab906301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:17 +0000

    Add package metabeaver/0.1.0/metabeaver-0.1.0.tar.gz

---
+++ packages/metabeaver/metabeaver-0.1.0.tar.gz/metabeaver-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metabeaver',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'google-cloud-core',
+        'google-cloud-bigquery',
+    ],
+    description='Beaverish about data.',
+    author='Luke Anthony Pollen',
+    author_email='luke@pollenanalytics.com',
+    url='https://github.com/rainbowpusheenthe3rd/dataBeaver',
+)
+
+
+##### ERRATA #####
+
+#QHFG25#DQLPMeEy
+
+#python setup.py sdist bdist_wheel
+
+#pip show twine
+#dir /s twine.exe
+#twine upload dist/*
+
+#twine upload *
+
+##### ERRATA #####

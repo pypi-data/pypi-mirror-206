b9f22b4ad0f190efdf768dd876db444b785132ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package cc1101/3.0.0/cc1101-3.0.0.tar.gz

---
+++ packages/cc1101/cc1101-3.0.0.tar.gz/cc1101-3.0.0/tests/test_gpio.py
@@ -0,0 +1,128 @@
+# python-cc1101 - Python Library to Transmit RF Signals via CC1101 Transceivers
+#
+# Copyright (C) 2021 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import ctypes
+import ctypes.util
+import datetime
+import errno
+import re
+import unittest.mock
+
+import pytest
+
+import cc1101._gpio
+
+# pylint: disable=protected-access
+
+
+def test__load_libgpiod():
+    with unittest.mock.patch(
+        "ctypes.util.find_library", return_value=ctypes.util.find_library("c")
+    ) as find_library_mock:
+        assert isinstance(cc1101._gpio._load_libgpiod(), ctypes.CDLL)
+    find_library_mock.assert_called_once_with("gpiod")
+
+
+@pytest.mark.parametrize("name", ("GPIO24", "GPIO25"))
+def test_line_find_permission_denied(libgpiod_mock, name):
+    libgpiod_mock.gpiod_line_find.return_value = 0
+    ctypes.set_errno(errno.EACCES)
+    with pytest.raises(
+        PermissionError, match=f"^Failed to access GPIO line {re.escape(name)!r}\\.\\n"
+    ):
+        cc1101._gpio.GPIOLine.find(name.encode())
+
+
+@pytest.mark.parametrize("name", ("GPIO24", "GPIO25"))
+def test_line_find_non_existing(libgpiod_mock, name):
+    libgpiod_mock.gpiod_line_find.return_value = 0
+    ctypes.set_errno(errno.ENOENT)
+    with pytest.raises(
+        FileNotFoundError, match=f"^GPIO line {re.escape(name)!r} does not exist\\.\\n"
+    ):
+        cc1101._gpio.GPIOLine.find(name.encode())
+
+
+@pytest.mark.parametrize("name", ("GPIO24", "GPIO25"))
+def test_line_find_unknown_error(libgpiod_mock, name):
+    libgpiod_mock.gpiod_line_find.return_value = 0
+    ctypes.set_errno(errno.ENOANO)
+    with pytest.raises(
+        OSError, match=f"^Failed to open GPIO line {re.escape(name)!r}: ENOANO$"
+    ):
+        cc1101._gpio.GPIOLine.find(name.encode())
+
+
+def test_line_find(libgpiod_mock):
+    libgpiod_mock.gpiod_line_find.return_value = 21
+    line = cc1101._gpio.GPIOLine.find(b"GPIO24")
+    libgpiod_mock.gpiod_line_find.assert_called_once_with(b"GPIO24")
+    assert isinstance(line, cc1101._gpio.GPIOLine)
+    assert line._pointer.value == 21
+
+
+def test_line_release(libgpiod_mock):
+    line = cc1101._gpio.GPIOLine(42)
+    del line
+    libgpiod_mock.gpiod_line_close_chip.assert_called_once_with(42)
+
+
+@pytest.mark.parametrize("consumer", (b"CC1101 GDO0", b"test"))
+@pytest.mark.parametrize("timeout_seconds", (21.42, 0.1234))
+@pytest.mark.parametrize("reached_timeout", (False, True))
+def test_line_wait_for_rising_edge(
+    libgpiod_mock, consumer: bytes, timeout_seconds: float, reached_timeout: bool
+):
+    pointer = ctypes.c_void_p(1234)
+    line = cc1101._gpio.GPIOLine(pointer=pointer)
+    libgpiod_mock.gpiod_line_request_rising_edge_events.return_value = 0
+    libgpiod_mock.gpiod_line_event_wait.return_value = 0 if reached_timeout else 1
+    event_occured = line.wait_for_rising_edge(
+        consumer=consumer, timeout=datetime.timedelta(seconds=timeout_seconds)
+    )
+    assert event_occured is not reached_timeout
+    libgpiod_mock.gpiod_line_request_rising_edge_events.assert_called_once_with(
+        pointer, consumer
+    )
+    libgpiod_mock.gpiod_line_event_wait.assert_called_once()
+    wait_args, wait_kwargs = libgpiod_mock.gpiod_line_event_wait.call_args
+    assert wait_args[0] == pointer
+    assert (
+        wait_args[1].contents.tv_sec + wait_args[1].contents.tv_nsec / 10**9
+    ) == pytest.approx(timeout_seconds, abs=1e-10)
+    assert not wait_args[2:]
+    assert not wait_kwargs
+    libgpiod_mock.gpiod_line_release.assert_called_once_with(pointer)
+
+
+@pytest.mark.parametrize("consumer", (b"CC1101 GDO0",))
+@pytest.mark.parametrize("timeout_seconds", (21.42,))
+def test_line_wait_for_rising_edge_busy(
+    libgpiod_mock, consumer: bytes, timeout_seconds: float
+):
+    pointer = ctypes.c_void_p(1234)
+    line = cc1101._gpio.GPIOLine(pointer=pointer)
+    libgpiod_mock.gpiod_line_request_rising_edge_events.return_value = -1
+    ctypes.set_errno(errno.EBUSY)
+    with pytest.raises(
+        OSError,
+        match=r"^Request for rising edge event notifications failed \(EBUSY\)."
+        r"\nBlocked by another process\?$",
+    ):
+        line.wait_for_rising_edge(
+            consumer=consumer, timeout=datetime.timedelta(seconds=timeout_seconds)
+        )
---
+++ packages/cc1101/cc1101-3.0.0.tar.gz/cc1101-3.0.0/tests/test_receive.py
@@ -0,0 +1,82 @@
+# python-cc1101 - Python Library to Transmit RF Signals via CC1101 Transceivers
+#
+# Copyright (C) 2020 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import datetime
+import unittest.mock
+
+import pytest
+
+# pylint: disable=protected-access
+
+
+def test__enable_receive_mode(transceiver):
+    transceiver._spi.xfer.return_value = [15]
+    transceiver._enable_receive_mode()
+    transceiver._spi.xfer.assert_called_once_with([0x34 | 0x00])
+
+
+@pytest.mark.parametrize("payload", [b"\0", b"\x12\x45\x56"])
+def test__get_received_packet(transceiver, payload):
+    fifo_buffer = list(payload) + [128, (1 << 7) | 42]
+    with unittest.mock.patch.object(
+        transceiver, "_read_status_register", return_value=len(fifo_buffer)
+    ) as read_status_register, unittest.mock.patch.object(
+        transceiver, "_read_burst", return_value=fifo_buffer
+    ) as read_burst_mock:
+        received_packet = transceiver._get_received_packet()
+    read_status_register.assert_called_once_with(0x3B)
+    read_burst_mock.assert_called_once_with(
+        start_register=0x3F, length=len(fifo_buffer)
+    )
+    assert received_packet.payload == payload
+    assert received_packet._rssi_index == 128
+    assert received_packet.checksum_valid
+    assert received_packet.link_quality_indicator == 42
+
+
+@pytest.mark.parametrize("gdo0_gpio_line_name", (b"GPIO24", b"GPIO25"))
+@pytest.mark.parametrize("reached_timeout", (True, False))
+@pytest.mark.parametrize("timeout", (datetime.timedelta(seconds=4),))
+def test__wait_for_packet(transceiver, gdo0_gpio_line_name, timeout, reached_timeout):
+    line_mock = unittest.mock.MagicMock()
+    line_mock.wait_for_rising_edge.return_value = not reached_timeout
+    with unittest.mock.patch(
+        "cc1101._gpio.GPIOLine.find", return_value=line_mock
+    ) as find_line_mock, unittest.mock.patch.object(
+        transceiver, "_get_received_packet", return_value="packet-dummy"
+    ) as get_received_packet_mock, unittest.mock.patch.object(
+        transceiver, "_enable_receive_mode"
+    ) as enable_receive_mode_mock, unittest.mock.patch.object(
+        transceiver, "_command_strobe"
+    ) as command_strobe_mock:
+        packet = transceiver._wait_for_packet(
+            timeout=timeout,
+            gdo0_gpio_line_name=gdo0_gpio_line_name,
+        )
+    find_line_mock.assert_called_once_with(name=gdo0_gpio_line_name)
+    enable_receive_mode_mock.assert_called_once_with()
+    line_mock.wait_for_rising_edge.assert_called_once_with(
+        consumer=b"CC1101:GDO0", timeout=timeout
+    )
+    if reached_timeout:
+        assert packet is None
+        command_strobe_mock.assert_called_once_with(0x36)  # SIDLE
+        get_received_packet_mock.assert_not_called()
+    else:
+        command_strobe_mock.assert_not_called()
+        get_received_packet_mock.assert_called_once_with()
+        assert packet == "packet-dummy"

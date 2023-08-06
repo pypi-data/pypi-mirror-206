e34e5efd9c5d5f16bbb427a603ffe92ff8770d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:21 +0000

    Add package imagefx/1.0.0/imagefx-1.0.0.tar.gz

---
+++ packages/imagefx/imagefx-1.0.0.tar.gz/imagefx-1.0.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='imagefx',
+    version='1.0.0',
+    description='Python library for performing image processing operations',
+    author='Photon',
+    author_email='electrify0608@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+        'Pillow>=8.2.0',
+        'numpy>=1.20.3',
+        'opencv-python-headless>=4.5.2.52'
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'flake8'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'imagefx=imagefx.cli:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',  
+        'Programming Language :: Python :: 3.7',  
+        'Programming Language :: Python :: 3.8', 
+        'Programming Language :: Python :: 3.9',
+    ],
+)

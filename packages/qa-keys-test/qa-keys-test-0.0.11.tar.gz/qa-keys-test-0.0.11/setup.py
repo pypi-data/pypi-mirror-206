7b6ec50fdb20d4874624aee5e9b1d126d4afbcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:50 +0000

    Add package qa-keys-test/0.0.11/qa-keys-test-0.0.11.tar.gz

---
+++ packages/qa-keys-test/qa-keys-test-0.0.11.tar.gz/qa-keys-test-0.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qa-keys-test',
+    version='0.0.11',
+    description='A repo is for xhis single source',
+    url="https://github.com/OwenLuo123/qa-tag-test.git",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)

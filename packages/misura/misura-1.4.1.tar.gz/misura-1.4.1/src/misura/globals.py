8d4d38635d8b4fc51859550c3dc9804d2384f723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:51 +0000

    Add package misura/1.4.1/misura-1.4.1.tar.gz

---
+++ packages/misura/misura-1.4.1.tar.gz/misura-1.4.1/src/misura/globals.py
@@ -0,0 +1,19 @@
+# Global options.
+
+
+class style:
+    """
+    Styling global options for misura.
+    """
+
+    quantityHighlighting = True
+
+
+class defined:
+    """
+    User defined units.
+    """
+
+    BASE_TABLE = {}
+    DERIVED_TABLE = {}
+    DERIVED_UNPACKING_TABLE = {}

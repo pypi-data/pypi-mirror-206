47521f865bce129bfde4a788d41d0d92127e1480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:13 +0000

    Add package pybgpasn/1.0.4/pybgpasn-1.0.4-py3-none-any.whl

---
+++ packages/pybgpasn/pybgpasn-1.0.4-py3-none-any.whl/pybgpasn/pybgpasn.py
@@ -0,0 +1,10 @@
+import json
+
+class BGPASN:
+    def __init__(self):
+        with open('bgp_asn.json', 'r') as f:
+            self.asn_dict = json.load(f)
+
+    def get_asn(self, key):
+        return self.asn_dict.get(str(key), "Invalid key")
+

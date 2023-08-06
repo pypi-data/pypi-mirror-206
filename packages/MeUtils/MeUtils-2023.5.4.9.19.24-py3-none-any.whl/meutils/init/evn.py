aeee59ab2c921af5293968ffef040adb4341fe58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:14 +0000

    Add package MeUtils/2023.5.4.9.19.24/MeUtils-2023.5.4.9.19.24-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.5.4.9.19.24-py3-none-any.whl/meutils/init/evn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : evn
+# @Time         : 2023/4/27 16:57
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+import os
+
+# 环境变量配置
+os.environ['JINA_HIDE_SURVEY'] = '0'
+
+# LLM
+os.environ['LLM_ROLE'] = '你扮演的角色是ChatLLM大语言模型，是由Betterme开发'
+

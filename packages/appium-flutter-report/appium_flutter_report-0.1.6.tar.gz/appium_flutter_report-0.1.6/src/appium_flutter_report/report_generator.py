6ce7428c8decfe32997d32df34e594bd054946f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:30 +0000

    Add package appium-flutter-report/0.1.6/appium_flutter_report-0.1.6.tar.gz

---
+++ packages/appium-flutter-report/appium_flutter_report-0.1.6.tar.gz/appium_flutter_report-0.1.6/src/appium_flutter_report/report_generator.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from .test_case import TestCaseData
+import json
+from appium import webdriver
+
+
+class FlutterReportGenerator:
+    driver: webdriver.Remote
+    app_name: str
+    report_path: str
+    capabilities: dict
+    time: datetime
+    testCaseData: list
+    current_pointer: list
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(FlutterReportGenerator, cls).__new__(cls)
+        return cls.instance
+
+    @staticmethod
+    def setup(driver, app_name, report_path, capabilities):
+        FlutterReportGenerator.driver = driver
+        FlutterReportGenerator.app_name = app_name
+        FlutterReportGenerator.report_path = report_path
+        FlutterReportGenerator.capabilities = capabilities
+        FlutterReportGenerator.time = datetime.now()
+        FlutterReportGenerator.testCaseData = []
+        FlutterReportGenerator.current_pointer = []
+
+    @staticmethod
+    def generate_report():
+        report_generator_start = datetime.now()
+        response = {
+            "time": FlutterReportGenerator.time,
+            "appName": FlutterReportGenerator.app_name,
+            "capabilities": FlutterReportGenerator.capabilities,
+            "result": FlutterReportGenerator.__get_result()
+        }
+        report_generation_time = datetime.now() - report_generator_start
+        duration = datetime.now() - FlutterReportGenerator.time
+        response["duration"] = str(duration.total_seconds() * 1000) + " ms"
+        response["generatingReportTime"] = str(report_generation_time.total_seconds() * 1000) + " ms"
+
+        # For now no screenshots
+        file_name = FlutterReportGenerator.app_name + "_" + FlutterReportGenerator.time.strftime(
+            "%y_%m_%d_%H_%M_%S") + ".json"
+        file_name = file_name.replace(" ", "_")
+        file = open(FlutterReportGenerator.report_path + file_name, "a")
+        file.write(json.dumps(response, default=str), )
+        file.close()
+
+    @staticmethod
+    def __get_result():
+        result = []
+        print(len(FlutterReportGenerator.testCaseData))
+        for item in FlutterReportGenerator.testCaseData:
+            item: TestCaseData = item
+            result.append(item.to_json())
+        return result
---
+++ packages/appium-flutter-report/appium_flutter_report-0.1.6.tar.gz/appium_flutter_report-0.1.6/test/crud/test_crud.py
@@ -0,0 +1,100 @@
+import time
+
+from src import group, test
+from src import Logger
+
+
+def test_crud():
+    group(
+        "Testing CRUD",
+        group_crud
+    )
+
+
+def group_crud():
+    group(
+        "Create",
+        group_create,
+    )
+    test(
+        "Testing Read",
+        read,
+    )
+    group(
+        "Update",
+        group_update
+    )
+    test(
+        "Testing Delete",
+        delete,
+    )
+
+
+def read(logger: Logger):
+    group(
+        "Delete: Must Throw test must not be inside test",
+        delete2,
+    )
+    logger.add_step("Read Button Was Pressed")
+    logger.add_step("Yes I want to read it any how was pressed")
+    assert False
+
+
+def delete(logger: Logger):
+    logger.add_step("Delete Button Was Pressed")
+    logger.add_step("Confirm To Delete Was Pressed")
+    assert True
+
+def delete2():
+    assert True
+
+
+def group_create():
+    test(
+        "Create Correct",
+        create_correct,
+    )
+    test(
+        "Create Wrong",
+        create_wrong,
+    )
+
+
+def create_correct(logger:Logger):
+    logger.add_step("Correct Info Was Added")
+    logger.add_step("Create Button Is Pressed")
+    assert False
+
+
+def create_wrong(logger:Logger):
+    logger.add_step("Wrong Info Was Added")
+    logger.add_step("Create Button Is Pressed")
+    assert True
+
+
+def group_update():
+    test(
+        "Update Correct",
+        update_correct,
+    )
+    test(
+        "Update Wrong",
+        update_wrong,
+    )
+
+
+def update_correct(logger:Logger):
+    time.sleep(0.5)
+    logger.add_step("Correct Info Was Added")
+    logger.add_step("Update Button Is Pressed")
+    assert True
+
+
+def update_wrong(logger:Logger):
+    # group("hello",testABC)
+    logger.add_step("Wrong Info Was Added")
+    logger.add_step("Update Button Is Pressed")
+    assert False
+
+def testABC():
+    print("test")

92e946745a74df371ae44c7cf284789112836f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:12 +0000

    Add package volworld-aws-api-common/0.1.79/volworld_aws_api_common-0.1.79.tar.gz

---
+++ packages/volworld-aws-api-common/volworld_aws_api_common-0.1.79.tar.gz/volworld_aws_api_common-0.1.79/src/volworld_aws_api_common/api/Url.py
@@ -0,0 +1,10 @@
+from typing import Final
+
+
+class UrlRef:
+    AWS: Final[str] = "https://958e36p8n5.execute-api.ap-northeast-1.amazonaws.com/prod/"
+    NodeJsS: Final[str] = "http://localhost:33000/"
+
+
+class Url:
+    Root = UrlRef.AWS
---
+++ packages/volworld-aws-api-common/volworld_aws_api_common-0.1.79.tar.gz/volworld_aws_api_common-0.1.79/src/volworld_aws_api_common/test/aws/url.py
@@ -0,0 +1,21 @@
+
+from volworld_aws_api_common.api.AA import AA
+from volworld_aws_api_common.api.Url import Url
+
+# URL_ROOT = "https://958e36p8n5.execute-api.ap-northeast-1.amazonaws.com/prod/"
+
+
+def build_api_root_url(*elms) -> str:
+    es = list()
+    for e in elms:
+        es.append(e)
+    return f"{Url.Root}/{AA.Api}/{'/'.join(es)}"
+    # return URL_ROOT + '/' + '/'.join(es)
+
+
+def build_url(root, *elms) -> str:
+    es = list()
+    for e in elms:
+        es.append(e)
+    return f"{root}/{'/'.join(es)}"
+    # return root + '/' + '/'.join(es)

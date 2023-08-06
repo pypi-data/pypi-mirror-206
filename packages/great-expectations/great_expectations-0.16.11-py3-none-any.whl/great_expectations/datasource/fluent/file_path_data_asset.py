d6ad05d2e10e1be33c447955bba590109ada87ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:04 +0000

    Add package great-expectations/0.16.11/great_expectations-0.16.11-py3-none-any.whl

---
+++ packages/great-expectations/great_expectations-0.16.11-py3-none-any.whl/great_expectations/datasource/fluent/file_path_data_asset.py
@@ -0,0 +1,307 @@
+from __future__ import annotations
+
+import copy
+import logging
+from pprint import pformat as pf
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    ClassVar,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Pattern,
+    Set,
+)
+
+import pydantic
+
+import great_expectations.exceptions as gx_exceptions
+from great_expectations.core._docs_decorators import public_api
+from great_expectations.datasource.fluent.batch_request import (
+    BatchRequest,
+    BatchRequestOptions,
+)
+from great_expectations.datasource.fluent.constants import MATCH_ALL_PATTERN
+from great_expectations.datasource.fluent.data_asset.data_connector import (
+    FILE_PATH_BATCH_SPEC_KEY,
+)
+from great_expectations.datasource.fluent.data_asset.data_connector.regex_parser import (
+    RegExParser,
+)
+from great_expectations.datasource.fluent.interfaces import (
+    Batch,
+    DataAsset,
+    TestConnectionError,
+)
+
+if TYPE_CHECKING:
+    from great_expectations.core.batch import BatchDefinition, BatchMarkers
+    from great_expectations.core.id_dict import BatchSpec
+    from great_expectations.datasource.fluent.data_asset.data_connector import (
+        DataConnector,
+    )
+    from great_expectations.datasource.fluent.interfaces import (
+        BatchMetadata,
+        BatchSlice,
+    )
+    from great_expectations.execution_engine import (
+        PandasExecutionEngine,
+        SparkDFExecutionEngine,
+    )
+
+logger = logging.getLogger(__name__)
+
+
+class _FilePathDataAsset(DataAsset):
+    _EXCLUDE_FROM_READER_OPTIONS: ClassVar[Set[str]] = {
+        "type",
+        "name",
+        "order_by",
+        "batch_metadata",
+        "batching_regex",  # file_path argument
+        "kwargs",  # kwargs need to be unpacked and passed separately
+        "batch_metadata",
+        "connect_options",
+    }
+
+    # General file-path DataAsset pertaining attributes.
+    batching_regex: Pattern = (  # must use typing.Pattern for pydantic < v1.10
+        MATCH_ALL_PATTERN
+    )
+    connect_options: Mapping = pydantic.Field(
+        default_factory=dict,
+        description="Optional filesystem specific advanced parameters for connecting to data assets",
+    )
+
+    _unnamed_regex_param_prefix: str = pydantic.PrivateAttr(
+        default="batch_request_param_"
+    )
+    _regex_parser: RegExParser = pydantic.PrivateAttr()
+
+    _all_group_name_to_group_index_mapping: Dict[str, int] = pydantic.PrivateAttr()
+    _all_group_index_to_group_name_mapping: Dict[int, str] = pydantic.PrivateAttr()
+    _all_group_names: List[str] = pydantic.PrivateAttr()
+
+    # `_data_connector`` should be set inside `_build_data_connector()`
+    _data_connector: DataConnector = pydantic.PrivateAttr()
+    # more specific `_test_connection_error_message` can be set inside `_build_data_connector()`
+    _test_connection_error_message: str = pydantic.PrivateAttr(
+        "Could not connect to your asset"
+    )
+
+    class Config:
+        """
+        Need to allow extra fields for the base type because pydantic will first create
+        an instance of `_FilePathDataAsset` before we select and create the more specific
+        asset subtype.
+        Each specific subtype should `forbid` extra fields.
+        """
+
+        extra = pydantic.Extra.allow
+
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        self._regex_parser = RegExParser(
+            regex_pattern=self.batching_regex,
+            unnamed_regex_group_prefix=self._unnamed_regex_param_prefix,
+        )
+
+        self._all_group_name_to_group_index_mapping = (
+            self._regex_parser.get_all_group_name_to_group_index_mapping()
+        )
+        self._all_group_index_to_group_name_mapping = (
+            self._regex_parser.get_all_group_index_to_group_name_mapping()
+        )
+        self._all_group_names = self._regex_parser.get_all_group_names()
+
+    @property
+    def batch_request_options(
+        self,
+    ) -> tuple[str, ...]:
+        """The potential keys for BatchRequestOptions.
+
+        Example:
+        ```python
+        >>> print(asset.batch_request_options)
+        ("day", "month", "year", "path")
+        >>> options = {"year": "2023"}
+        >>> batch_request = asset.build_batch_request(options=options)
+        ```
+
+        Returns:
+            A tuple of keys that can be used in a BatchRequestOptions dictionary.
+        """
+        return tuple(self._all_group_names) + (FILE_PATH_BATCH_SPEC_KEY,)
+
+    @public_api
+    def build_batch_request(
+        self,
+        options: Optional[BatchRequestOptions] = None,
+        batch_slice: Optional[BatchSlice] = None,
+    ) -> BatchRequest:
+        """A batch request that can be used to obtain batches for this DataAsset.
+
+        Args:
+            options: A dict that can be used to filter the batch groups returned from the asset.
+                The dict structure depends on the asset type. The available keys for dict can be obtained by
+                calling batch_request_options.
+            batch_slice: A python slice that can be used to limit the sorted batches by index.
+                e.g. `batch_slice = "[-5:]"` will request only the last 5 batches after the options filter is applied.
+
+        Returns:
+            A BatchRequest object that can be used to obtain a batch list from a Datasource by calling the
+            get_batch_list_from_batch_request method.
+        """
+        if options:
+            for option, value in options.items():
+                if (
+                    option in self._all_group_name_to_group_index_mapping
+                    and value
+                    and not isinstance(value, str)
+                ):
+                    raise gx_exceptions.InvalidBatchRequestError(
+                        f"All batching_regex matching options must be strings. The value of '{option}' is "
+                        f"not a string: {value}"
+                    )
+
+        if options is not None and not self._valid_batch_request_options(options):
+            allowed_keys = set(self.batch_request_options)
+            actual_keys = set(options.keys())
+            raise gx_exceptions.InvalidBatchRequestError(
+                "Batch request options should only contain keys from the following set:\n"
+                f"{allowed_keys}\nbut your specified keys contain\n"
+                f"{actual_keys.difference(allowed_keys)}\nwhich is not valid.\n"
+            )
+
+        return BatchRequest(
+            datasource_name=self.datasource.name,
+            data_asset_name=self.name,
+            options=options or {},
+            batch_slice=batch_slice,
+        )
+
+    def _validate_batch_request(self, batch_request: BatchRequest) -> None:
+        """Validates the batch_request has the correct form.
+
+        Args:
+            batch_request: A batch request object to be validated.
+        """
+        if not (
+            batch_request.datasource_name == self.datasource.name
+            and batch_request.data_asset_name == self.name
+            and self._valid_batch_request_options(batch_request.options)
+        ):
+            options = {option: None for option in self.batch_request_options}
+            expect_batch_request_form = BatchRequest(
+                datasource_name=self.datasource.name,
+                data_asset_name=self.name,
+                options=options,
+                batch_slice=batch_request._batch_slice_input,  # type: ignore[attr-defined]
+            )
+            raise gx_exceptions.InvalidBatchRequestError(
+                "BatchRequest should have form:\n"
+                f"{pf(expect_batch_request_form.dict())}\n"
+                f"but actually has form:\n{pf(batch_request.dict())}\n"
+            )
+
+    def get_batch_list_from_batch_request(
+        self, batch_request: BatchRequest
+    ) -> List[Batch]:
+        self._validate_batch_request(batch_request)
+
+        execution_engine: PandasExecutionEngine | SparkDFExecutionEngine = (
+            self.datasource.get_execution_engine()
+        )
+
+        batch_definition_list: List[
+            BatchDefinition
+        ] = self._data_connector.get_batch_definition_list(batch_request=batch_request)
+
+        batch_list: List[Batch] = []
+
+        batch_spec: BatchSpec
+        batch_spec_options: dict
+        batch_data: Any
+        batch_markers: BatchMarkers
+        batch_metadata: BatchMetadata
+        batch: Batch
+        for batch_definition in batch_definition_list:
+            batch_spec = self._data_connector.build_batch_spec(
+                batch_definition=batch_definition
+            )
+            batch_spec_options = {
+                "reader_method": self._get_reader_method(),
+                "reader_options": self.dict(
+                    include=self._get_reader_options_include(),
+                    exclude=self._EXCLUDE_FROM_READER_OPTIONS,
+                    exclude_unset=True,
+                    by_alias=True,
+                    config_provider=self._datasource._config_provider,
+                ),
+            }
+            batch_spec.update(batch_spec_options)
+
+            batch_data, batch_markers = execution_engine.get_batch_data_and_markers(
+                batch_spec=batch_spec
+            )
+
+            fully_specified_batch_request = copy.deepcopy(batch_request)
+            fully_specified_batch_request.options.update(
+                batch_definition.batch_identifiers
+            )
+            batch_metadata = self._get_batch_metadata_from_batch_request(
+                batch_request=fully_specified_batch_request
+            )
+
+            # Some pydantic annotations are postponed due to circular imports.
+            # Batch.update_forward_refs() will set the annotations before we
+            # instantiate the Batch class since we can import them in this scope.
+            Batch.update_forward_refs()
+
+            batch = Batch(
+                datasource=self.datasource,
+                data_asset=self,
+                batch_request=fully_specified_batch_request,
+                data=batch_data,
+                metadata=batch_metadata,
+                legacy_batch_markers=batch_markers,
+                legacy_batch_spec=batch_spec,
+                legacy_batch_definition=batch_definition,
+            )
+            batch_list.append(batch)
+
+        # TODO: <Alex>ALEX_INCLUDE_SORTERS_FUNCTIONALITY_UNDER_PYDANTIC-MAKE_SURE_SORTER_CONFIGURATIONS_ARE_VALIDATED</Alex>
+        # TODO: <Alex>ALEX-MOVE_SORTING_INTO_FILE_PATH_DATA_CONNECTOR_ON_BATCH_DEFINITION_OBJECTS</Alex>
+        self.sort_batches(batch_list)
+
+        return batch_list
+
+    def test_connection(self) -> None:
+        """Test the connection for the DataAsset.
+
+        Raises:
+            TestConnectionError: If the connection test fails.
+        """
+        try:
+            if self._data_connector.test_connection():
+                return None
+        except Exception as e:
+            raise TestConnectionError(
+                f"Could not connect to asset using {type(self._data_connector).__name__}: Got {type(e).__name__}"
+            ) from e
+        raise TestConnectionError(self._test_connection_error_message)
+
+    def _get_reader_method(self) -> str:
+        raise NotImplementedError(
+            """One needs to explicitly provide "reader_method" for File-Path style DataAsset extensions as temporary \
+work-around, until "type" naming convention and method for obtaining 'reader_method' from it are established."""
+        )
+
+    def _get_reader_options_include(self) -> Set[str] | None:
+        raise NotImplementedError(
+            """One needs to explicitly provide set(str)-valued reader options for "pydantic.BaseModel.dict()" method \
+to use as its "include" directive for File-Path style DataAsset processing."""
+        )

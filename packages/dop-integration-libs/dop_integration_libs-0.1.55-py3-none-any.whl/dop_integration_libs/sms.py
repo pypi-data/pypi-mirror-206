b7a8753b3486537497bd7b50badeb10dd77ae89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package dop-integration-libs/0.1.55/dop_integration_libs-0.1.55-py3-none-any.whl

---
+++ packages/dop-integration-libs/dop_integration_libs-0.1.55-py3-none-any.whl/dop_integration_libs/sms.py
@@ -0,0 +1,27 @@
+
+import requests
+from requests.auth import HTTPBasicAuth
+from .environment import Environment
+
+
+class Sms(object):
+    def __init__(self, env: Environment):
+        self.env = env
+
+    def send(self, to: str, message: str):
+        url = f"{self.env.SMS_URL}/Submit"
+        print(f"SMS URL: {url}")
+        basic = HTTPBasicAuth(self.env.SMS_USERNAME, self.env.SMS_PASSWORD)
+        headers = {
+            "From": self.env.SMS_TITLE,
+            "ValidityPeriod": 0
+        }
+        body = {
+            "To": [to],
+            "Message": message
+        }
+        response = requests.post(url, headers=headers, json=body, auth=basic)
+        print("===SMS====")
+        print(f"SMS Response: {response.status_code}")
+        print(f"SMS Body: {response.text}")
+        return True

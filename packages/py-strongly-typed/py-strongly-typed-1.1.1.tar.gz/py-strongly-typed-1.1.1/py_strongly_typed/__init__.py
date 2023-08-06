38ae38db6b12b7f7408544d33fddf4f62869cd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:07 +0000

    Add package py-strongly-typed/1.1.1/py-strongly-typed-1.1.1.tar.gz

---
+++ packages/py-strongly-typed/py-strongly-typed-1.1.1.tar.gz/py-strongly-typed-1.1.1/py_strongly_typed/__init__.py
@@ -0,0 +1,11 @@
+from py_strongly_typed.contracts import IValidateAnnotationsUseCase
+from py_strongly_typed.decorators import typed_function
+from py_strongly_typed.typed_class import TypedClass
+
+
+__all__ = [
+    'typed_function',
+    'TypedClass',
+    'IValidateAnnotationsUseCase'
+]
+

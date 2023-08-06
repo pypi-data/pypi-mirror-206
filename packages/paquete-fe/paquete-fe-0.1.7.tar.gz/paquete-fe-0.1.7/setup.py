d9e4d710b4501ca60815bc3ce243a3a0751ca161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:46 +0000

    Add package paquete-fe/0.1.7/paquete-fe-0.1.7.tar.gz

---
+++ packages/paquete-fe/paquete-fe-0.1.7.tar.gz/paquete-fe-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paquete-fe',
+    version='0.1.7',
+    description='Descripcion corta del paquete',
+    author='Tu nombre',
+    author_email='tu@email.com',
+    url='https://tu-url.com/',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'lxml',
+        'SQLAlchemy',
+        'pyOpenSSL',
+        'zeep',
+        'requests',
+        'pytz',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

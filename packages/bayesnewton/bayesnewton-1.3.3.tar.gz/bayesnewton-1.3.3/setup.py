5ad32371598e1d5bb06775e05430bf5c9ca0fec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:25 +0000

    Add package bayesnewton/1.3.3/bayesnewton-1.3.3.tar.gz

---
+++ packages/bayesnewton/bayesnewton-1.3.3.tar.gz/bayesnewton-1.3.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.3.3"
+
+setup(
+    name='bayesnewton',
+    version=__version__,
+    packages=find_packages(),
+    python_requires='>=3.6',
+    url='https://github.com/AaltoML/BayesNewton',
+    license='Apache-2.0',
+)

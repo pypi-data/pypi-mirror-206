57ab63d68b5dd7d873b958093b1e5e2643c097ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package ix-aims/0.1.1rc4/ix_aims-0.1.1rc4.tar.gz

---
+++ packages/ix-aims/ix_aims-0.1.1rc4.tar.gz/ix_aims-0.1.1rc4/ix_aims/imgs/__init__.py
@@ -0,0 +1,11 @@
+from pkg_resources import resource_filename
+
+processesBtn = resource_filename(
+    __name__, "processesBtn.png"
+)
+yesBtn = resource_filename(
+    __name__, "yesBtn.png"
+)
+plusTab = resource_filename(
+    __name__, "plusTab.png"
+)

de9067153d77c4100ce421b6093d5b58b7e1d085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package kaolin/0.1/kaolin-0.1.tar.gz

---
+++ packages/kaolin/kaolin-0.1.tar.gz/kaolin-0.1/kaolin/__init__.py
@@ -0,0 +1,1 @@
+raise ImportError("This is the kaolin placeholder wheel from https://pypi.org/project/kaolin/. Please follow https://kaolin.readthedocs.io/en/latest/notes/installation.html for installation.")
---
+++ packages/kaolin/kaolin-0.1.tar.gz/kaolin-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='kaolin',
+    version='0.1',
+    author='Clement Fuji Tsang',
+    description='Kaolin wheel placeholder',
+    packages=['kaolin']
+)

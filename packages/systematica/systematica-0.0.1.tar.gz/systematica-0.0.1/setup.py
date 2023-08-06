c5c158117b9adf3f0c345e3cf4cb3512228e6db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:19 +0000

    Add package systematica/0.0.1/systematica-0.0.1.tar.gz

---
+++ packages/systematica/systematica-0.0.1.tar.gz/systematica-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+VERSION = '0.0.1'
+DESCRIPTION = 'Simple Simulator for Computer Vision'
+LONG_DESCRIPTION = 'A package that allows to test how computer vision algorithms for advanced driving systems performs '
+
+# Setting up
+setup(
+    name="systematica",
+    version=VERSION,
+    author="TucuAI (Tucudean Adrian-Ionut)",
+    author_email="<Tucudean.Adrian.Ionut@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pygame'],
+    keywords=['python', 'simulator', 'computer vision', 'advanced driving', 'camera stream', 'camera handler'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

88c7741c549f8e7a46d23d8bd825e80734d8158f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:51 +0000

    Add package gravipython/0.0.2/gravipython-0.0.2.tar.gz

---
+++ packages/gravipython/gravipython-0.0.2.tar.gz/gravipython-0.0.2/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+setuptools.setup(name="gravipython",
+                 version="0.0.2",
+                 author="Tom Brandt",
+                 description="A python module for simulating planets trajectories in 2D.",
+                 packages=["D:\\python\\gravipy_package\\gravitypython"],
+                 install_requires=["pygame", "numpy"])

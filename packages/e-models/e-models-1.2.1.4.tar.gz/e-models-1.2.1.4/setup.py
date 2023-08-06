22d0767033f40ad2c5ec0b8e7dcac0107b21de05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:26 +0000

    Add package e-models/1.2.1.4/e-models-1.2.1.4.tar.gz

---
+++ packages/e-models/e-models-1.2.1.4.tar.gz/e-models-1.2.1.4/setup.py
@@ -0,0 +1,30 @@
+# Automatically created by: shub deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'e-models',
+    version      = '1.2.1.4',
+    description  = 'Tools for helping build of extraction models with scrapy spiders.',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    license      = 'BSD',
+    author       = 'Martin Olveyra',
+    author_email = 'molveyra@gmail.com',
+    url          = 'https://github.com/kalessin/emodels',
+    packages     = find_packages(),
+    install_requires=(
+        "scrapy",
+        "markdown",
+        "markdown-grid-tables",
+    ),
+    scripts = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    package_data={"emodels": ["py.typed"]}
+)

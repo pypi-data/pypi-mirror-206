f4e01556a971d60473ccafdec9d30c29c0d2dc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:35 +0000

    Add package binbinpy/1.0.2/binbinpy-1.0.2-py3-none-any.whl

---
+++ packages/binbinpy/binbinpy-1.0.2-py3-none-any.whl/binbinpy/demo.py
@@ -0,0 +1,9 @@
+class Demo:
+    def __init__(self, config: None):
+	    self.config = config
+
+    def test(self):
+        return self.config
+
+    def add_one(number):
+        return number + 1

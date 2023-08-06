66ebccec922c84e132994736773dbf89d1077f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:10 +0000

    Add package krisi/0.1.4/krisi-0.1.4-py3-none-any.whl

---
+++ packages/krisi/krisi-0.1.4-py3-none-any.whl/krisi/evaluate/__init__.py
@@ -0,0 +1,5 @@
+from .compare import compare
+from .metric import Metric
+from .score import score, score_in_outsample
+from .scorecard import ScoreCard
+from .type import Calculation, MetricCategories, SampleTypes

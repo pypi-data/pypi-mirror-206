c287f04150bbd0d82dc4608a3fe375c5fd239a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:16 +0000

    Add package PlatitudeTerminal/1.0.5/PlatitudeTerminal-1.0.5.tar.gz

---
+++ packages/PlatitudeTerminal/PlatitudeTerminal-1.0.5.tar.gz/PlatitudeTerminal-1.0.5/peregrine/errors.py
@@ -0,0 +1,11 @@
+class PeregrineVersionOutOfDateError(Exception):
+    pass
+
+class PeregrineServerError(Exception):
+    pass
+
+class PeregrineConnectionError(Exception):
+    pass
+
+class PeregrineExpiredLoginError(Exception):
+    pass

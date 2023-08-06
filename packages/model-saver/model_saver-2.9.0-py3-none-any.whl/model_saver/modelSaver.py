971860939cbd47b12ad06c947502e1973bc0d2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package model-saver/2.9.0/model_saver-2.9.0-py3-none-any.whl

---
+++ packages/model-saver/model_saver-2.9.0-py3-none-any.whl/model_saver/modelSaver.py
@@ -0,0 +1,17 @@
+import pickle
+
+class ModelSaver:
+        
+
+    def save_model(self,model, filename):
+        self.model = model
+        with open(filename + '.model', 'wb') as f:
+            pickle.dump(self.model, f)
+        print(f"Model saved as {filename}.model")
+
+    @staticmethod
+    def load_model(filename):
+        with open(filename + '.model', 'rb') as f:
+            model = pickle.load(f)
+        print(f"Model loaded from {filename}.model")
+        return model

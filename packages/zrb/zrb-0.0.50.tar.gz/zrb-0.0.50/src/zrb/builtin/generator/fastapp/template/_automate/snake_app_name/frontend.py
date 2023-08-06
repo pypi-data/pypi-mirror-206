ea626c3cbcb36d76635ec9d58fe787591e078186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:48 +0000

    Add package zrb/0.0.50/zrb-0.0.50.tar.gz

---
+++ packages/zrb/zrb-0.0.50.tar.gz/zrb-0.0.50/src/zrb/builtin/generator/fastapp/template/_automate/snake_app_name/frontend.py
@@ -0,0 +1,45 @@
+from zrb import CmdTask, Env, PathChecker, runner
+from zrb.builtin._group import project_group
+from ._common import APP_FRONTEND_DIR, APP_FRONTEND_BUILD_DIR, CURRENT_DIR
+import os
+
+###############################################################################
+# Task Definitions
+###############################################################################
+
+build_snake_app_name_frontend = CmdTask(
+    icon='🚤',
+    name='build-kebab-app-name-frontend',
+    description='Build frontend for human readable app name',
+    group=project_group,
+    cwd=APP_FRONTEND_DIR,
+    cmd_path=os.path.join(CURRENT_DIR, 'cmd', 'build-frontend.sh'),
+    checkers=[
+        PathChecker(
+            name='check-kebab-app-name-frontend-build',
+            path=APP_FRONTEND_BUILD_DIR
+        )
+    ],
+    envs=[
+        Env(name='WATCH', os_name='', default='1')
+    ]
+)
+runner.register(build_snake_app_name_frontend)
+
+build_snake_app_name_frontend_once = CmdTask(
+    icon='🚤',
+    name='build-kebab-app-name-frontend_once',
+    description='Build frontend for human readable app name',
+    group=project_group,
+    cwd=APP_FRONTEND_DIR,
+    cmd_path=os.path.join(CURRENT_DIR, 'cmd', 'build-frontend.sh'),
+    checkers=[
+        PathChecker(
+            name='check-kebab-app-name-frontend-build',
+            path=APP_FRONTEND_BUILD_DIR
+        )
+    ],
+    envs=[
+        Env(name='WATCH', os_name='', default='0')
+    ]
+)

017efcb884b1a72d4848992291beabb87c0f45e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:51 +0000

    Add package marpdown/0.7.0/marpdown-0.7.0.tar.gz

---
+++ packages/marpdown/marpdown-0.7.0.tar.gz/marpdown-0.7.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+
+]
+
+setup(
+    name="marpdown",
+    version="0.7.0",
+    author="Tao Xiang",
+    author_email="tao.xiang@tum.de",
+    description="A package of RL algorithms",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leoxiang66/marpdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+    ],
+)

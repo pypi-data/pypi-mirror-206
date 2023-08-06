017ad8fe3308cc12eef500a3a58bd12a85cbe7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:43 +0000

    Add package IslandOfMisfitToys/0.1.0/IslandOfMisfitToys-0.1.0-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/IslandOfMisfitToys/IslandOfMisfitToys-0.1.0-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.whl/misfits/__init__.py
@@ -0,0 +1,28 @@
+__all__ = [
+    "l1_l2_hybrid",
+    "sambridge_ot",
+    "sobolev",
+    "huber",
+    "cross_correlation",
+    "graph_ot",
+    "sinkorn_ot",
+    "fisher",
+    "radon_ot",
+    "w2",
+    "w1",
+    "rom",
+    "helpers"
+]
+from .l1_l2_hybrid import *
+from .sambridge_ot import *
+from .sobolev import *
+from .huber import *
+from .cross_correlation import *
+from .graph_ot import *
+from .sinkorn_ot import *
+from .fisher import *
+from .radon_ot import *
+from .w2 import *
+from .w1 import *
+from .rom import *
+from .helpers import *

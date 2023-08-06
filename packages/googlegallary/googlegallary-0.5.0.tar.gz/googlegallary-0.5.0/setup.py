0f5e915dba7a3d8e79ae1365dc391be9d0ec0efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:00 +0000

    Add package googlegallary/0.5.0/googlegallary-0.5.0.tar.gz

---
+++ packages/googlegallary/googlegallary-0.5.0.tar.gz/googlegallary-0.5.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+
+    name="googlegallary",
+
+    version="0.5.0",
+
+    description="Your can get images by using this package ",
+
+
+    author="Rahul chauhan",
+
+    packages=['googlegallary'],
+
+)

085c7500b0db39636559ceda2bb85ef2138aadb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:12 +0000

    Add package rover-arm/1.1.0/rover_arm-1.1.0-py3-none-any.whl

---
+++ packages/rover-arm/rover_arm-1.1.0-py3-none-any.whl/rover_arm/__init__.py
@@ -0,0 +1,11 @@
+from gymnasium.envs.registration import register
+
+register(
+    id='rover-arm-pick-v0',
+    entry_point='rover_arm.envs:RoverArmEnv',
+)
+
+register(
+    id='rover-arm-place-v0',
+    entry_point='rover_arm.envs:RoverArmToPlaceEnv',
+)

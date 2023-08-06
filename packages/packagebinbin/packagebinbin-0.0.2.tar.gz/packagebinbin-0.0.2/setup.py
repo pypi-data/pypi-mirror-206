8ba16f5e411214c398b8c6b08281da32b9346a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:37 +0000

    Add package packagebinbin/0.0.2/packagebinbin-0.0.2.tar.gz

---
+++ packages/packagebinbin/packagebinbin-0.0.2.tar.gz/packagebinbin-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="packagebinbin",
+    version="0.0.2",
+    author="Sophat Chhay",
+    author_email="<sophat.chhay@gmail.com>",
+    description="My demo package with my daughter name",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    long_description= "Testing",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

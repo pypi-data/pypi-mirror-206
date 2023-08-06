6731e65fb4e48d016d0350dcb12431791846051c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:54 +0000

    Add package puresnet/0.1/puresnet-0.1.tar.gz

---
+++ packages/puresnet/puresnet-0.1.tar.gz/puresnet-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+license=(this_directory / "LICENSE.txt").read_text()
+setup(
+    name='puresnet',
+    version='0.1',
+    description='PUResNetV2.0 Prediction of Protein Ligand Binding Sites',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license=license,
+    packages=find_packages(),
+    install_requires=[
+      'numpy'  
+    ],
+    package_data={'puresnet':['data/model90 70210.pt','data/training_data.npy','data/validation_data.npy']
+                  }
+)

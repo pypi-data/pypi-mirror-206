e89abc8112fd942554d01af0c2673256fa84dcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:52 +0000

    Add package tcw/0.1.4/tcw-0.1.4.tar.gz

---
+++ packages/tcw/tcw-0.1.4.tar.gz/tcw-0.1.4/tcw/apps/contest/forms.py
@@ -0,0 +1,29 @@
+import datetime
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import (StringField, IntegerField, HiddenField, TextAreaField,
+    DateTimeField, SubmitField, SelectField)
+from wtforms.validators import DataRequired
+from wtforms.fields import EmailField
+
+
+class ContestForm(FlaskForm):
+    title = StringField('contest title', validators=[DataRequired()],)
+    instructions = TextAreaField('sign-up instructions', validators=[DataRequired()])
+    email = EmailField('contest owner email', validators=[DataRequired()])
+    winners = IntegerField('number of winners (1-50)', validators=[DataRequired()])
+    maximum = IntegerField('max number of entrants (1-500)', validators=[DataRequired()])
+    hours = SelectField('contest expires after', choices=[
+        ('1', '1 hour'),
+        ('4', '4 hours'),
+        ('12', '12 hours'),
+        ('24', '1 day'),
+        ('72', '3 days'),
+        ('120', '5 days'),
+        ],  validators=[DataRequired()])
+    recaptcha = RecaptchaField()
+    submit = SubmitField('submit')
+
+
+class SignupForm(FlaskForm):
+    name = StringField('sign up!')
+    submit = SubmitField('submit')

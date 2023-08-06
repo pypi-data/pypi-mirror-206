18cd0b4be50d06495acb76ec34a6f43dde8cd363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:43 +0000

    Add package cheetiming/0.0.1/cheetiming-0.0.1.tar.gz

---
+++ packages/cheetiming/cheetiming-0.0.1.tar.gz/cheetiming-0.0.1/src/cheetiming/__init__.py
@@ -0,0 +1,14 @@
+from functools import partial
+
+from ._version import __version__
+
+from .timer_manager import create_timer
+from .timer_manager import timing
+from .timer_manager import iterate_with_timer
+from .timer_manager import range_with_timer
+from .timer_manager import timing_session
+from .timer_manager import timing_report
+from .timer_manager import timers
+
+run_with_timer = partial(range_with_timer, 1)
+

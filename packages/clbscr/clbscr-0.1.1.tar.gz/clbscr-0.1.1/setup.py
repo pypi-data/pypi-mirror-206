1a0a54d96e909302ad28b2d967a140fcad725926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:06 +0000

    Add package clbscr/0.1.1/clbscr-0.1.1.tar.gz

---
+++ packages/clbscr/clbscr-0.1.1.tar.gz/clbscr-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clbscr',
+    version='0.1.1',
+    author='Davide Gimondo',
+    author_email='davegimo@example.com',
+    description='A package for doing some useful stuff',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/davegimo/clbscr',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    keywords='my-package, example, demo',
+    install_requires=[
+        # List your project dependencies here
+    ],
+    python_requires='>=3.6, <4',
+    include_package_data=True
+)

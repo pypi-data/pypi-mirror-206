bc60e3a11fb260b54a85219fec457ca84e5f466d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:23 +0000

    Add package valaw/0.0.6/valaw-0.0.6.tar.gz

---
+++ packages/valaw/valaw-0.0.6.tar.gz/valaw-0.0.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as readme:
+    long_desc = readme.read()
+
+setup(
+    name="valaw",
+    version="0.0.6",
+    author="Jet612",
+    description="An asynchronous API wrapper for VALORANT's API",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jet612/valaw",
+    project_urls={
+        "Documentation": "https://valaw.readthedocs.io",
+        "Issue Tracker": "https://github.com/Jet612/valaw/issues",
+        "Chat/Support": "https://discord.gg/mVXpvunBbF",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers"
+    ],
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+                    'aiohttp==3.8.3',
+                    'dataclass_wizard==0.22.2',
+                    'setuptools==67.6.0'
+                    ]
+)

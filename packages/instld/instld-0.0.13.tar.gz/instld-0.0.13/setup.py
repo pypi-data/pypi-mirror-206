8c1d2f5996d8675507e6cedbe77b81c61d132f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:53 +0000

    Add package instld/0.0.13/instld-0.0.13.tar.gz

---
+++ packages/instld/instld-0.0.13.tar.gz/instld-0.0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="instld",
+    version="0.0.13",
+    author="Evgeniy Blinov",
+    author_email="zheni-b@yandex.ru",
+    description="The simplest package management in runtime",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pomponchik/instld",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

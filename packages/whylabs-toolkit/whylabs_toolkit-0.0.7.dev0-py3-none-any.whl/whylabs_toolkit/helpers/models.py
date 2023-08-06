0b3c3e655742e31490f61a8d07aff6db2fefad32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:17 +0000

    Add package whylabs-toolkit/0.0.7.dev0/whylabs_toolkit-0.0.7.dev0-py3-none-any.whl

---
+++ packages/whylabs-toolkit/whylabs_toolkit-0.0.7.dev0-py3-none-any.whl/whylabs_toolkit/helpers/models.py
@@ -0,0 +1,39 @@
+import logging
+from typing import Optional
+
+from whylabs_client.exceptions import ApiValueError
+from whylabs_client.model.model_type import ModelType
+from whylabs_client.model.time_period import TimePeriod
+
+from whylabs_toolkit.helpers.utils import get_models_api
+from whylabs_toolkit.helpers.config import Config
+
+logger = logging.getLogger(__name__)
+
+
+def update_model_metadata(
+    dataset_id: str,
+    org_id: Optional[str] = None,
+    time_period: Optional[str] = None,
+    model_type: Optional[str] = None,
+    config: Config = Config(),
+) -> None:
+    """
+    Update model attributes like model type and period.
+    """
+    api = get_models_api(config=config)
+
+    model_metadata = api.get_model(org_id=org_id, model_id=dataset_id)
+    logger.debug(f"Updating dataset with current metadata: \n {model_metadata}")
+
+    try:
+        resp = api.update_model(
+            org_id=org_id,
+            model_id=dataset_id,
+            model_name=model_metadata["name"],
+            time_period=TimePeriod(time_period) if time_period else model_metadata["time_period"],
+            model_type=ModelType(model_type) if model_type else model_metadata["model_type"],
+        )
+        logger.debug(f"Updated sucessfully! Resp: {resp}")
+    except ApiValueError as e:
+        raise e

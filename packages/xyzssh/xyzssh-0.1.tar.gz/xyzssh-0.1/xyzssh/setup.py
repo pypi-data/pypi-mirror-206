2600a043fd6550a5b7d9981f2e8af5e73b43dae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:30 +0000

    Add package xyzssh/0.1/xyzssh-0.1.tar.gz

---
+++ packages/xyzssh/xyzssh-0.1.tar.gz/xyzssh-0.1/xyzssh/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='xyzssh',
+    version='0.1',
+    description='Une brève description du projet',
+    url='https://github.com/votre-nom/nom-du-projet',
+    author='xyz.ssh',
+    author_email='d3x73rstresser@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='quelques mots clés pour votre projet',
+    packages=['xyzssh'],
+    python_requires='>=3.6',
+    install_requires=[
+        ''
+    ],
+)

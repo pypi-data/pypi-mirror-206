7d3d7ad1a53aa13d80a7fc763783709690bfd616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:52 +0000

    Add package python-graphblas/2023.5.0/python_graphblas-2023.5.0-py3-none-any.whl

---
+++ packages/python-graphblas/python_graphblas-2023.5.0-py3-none-any.whl/graphblas/monoid/__init__.py
@@ -0,0 +1,26 @@
+# All items are dynamically added by classes in operator.py
+# This module acts as a container of Monoid instances
+_delayed = {}
+
+
+def __dir__():
+    return globals().keys() | _delayed.keys()
+
+
+def __getattr__(key):
+    if key in _delayed:
+        func, kwargs = _delayed.pop(key)
+        if type(kwargs["binaryop"]) is str:
+            from ..binary import from_string
+
+            kwargs["binaryop"] = from_string(kwargs["binaryop"])
+        rv = func(**kwargs)
+        globals()[key] = rv
+        return rv
+    raise AttributeError(f"module {__name__!r} has no attribute {key!r}")
+
+
+from ..core import operator  # noqa: E402 isort:skip
+from . import numpy  # noqa: E402 isort:skip
+
+del operator

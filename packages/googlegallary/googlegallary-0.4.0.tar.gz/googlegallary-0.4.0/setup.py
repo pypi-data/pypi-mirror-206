ddf46adad69242bacdd9092909520da55cec0d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:51 +0000

    Add package googlegallary/0.4.0/googlegallary-0.4.0.tar.gz

---
+++ packages/googlegallary/googlegallary-0.4.0.tar.gz/googlegallary-0.4.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='googlegallary',
+    version='0.4.0',
+    author='Rahul chauhan',
+    packages=['googlegallary'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+    ],
+    python_requires='>=3.6'
+)
+
+# twine register --repository-url https://github.com/Rjchauhan18/google-gallary.git --username Rjchauhan18 --password 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

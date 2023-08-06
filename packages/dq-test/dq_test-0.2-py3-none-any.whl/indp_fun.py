0d5500ab60b7a36e2ad5d979d95e2dadaa6edf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:56 +0000

    Add package dq-test/0.2/dq_test-0.2-py3-none-any.whl

---
+++ packages/dq-test/dq_test-0.2-py3-none-any.whl/indp_fun.py
@@ -0,0 +1,7 @@
+import dq_test
+from dq_test import *
+def idp_test():
+    print("Its calling idp function")
+
+perform_data_quality_checks("dq_test","dq_rule_table","obj_dq_test","src_dq_test"," test")
+
---
+++ packages/dq-test/dq_test-0.2-py3-none-any.whl/test_fun.py
@@ -0,0 +1,2 @@
+def calling_test(name):
+    print(f"Its calling test function{name}")

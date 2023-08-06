2a88651258d2dbe93c41bb62bd15032e918ae9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package DL-ANN-pkg-VaibhavJoshi95/0.0.1/DL_ANN_pkg-VaibhavJoshi95-0.0.1.tar.gz

---
+++ packages/DL-ANN-pkg-VaibhavJoshi95/DL_ANN_pkg-VaibhavJoshi95-0.0.1.tar.gz/DL_ANN_pkg-VaibhavJoshi95-0.0.1/README.md
@@ -0,0 +1,6 @@
+# DL-ANN-Python-Package
+
+
+# Referencec-
+
+* [Official python Docs for PYPI packaging](https://packaging.python.org/en/latest/tutorials/packaging-projects/)
---
+++ packages/DL-ANN-pkg-VaibhavJoshi95/DL_ANN_pkg-VaibhavJoshi95-0.0.1.tar.gz/DL_ANN_pkg-VaibhavJoshi95-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PKG_NAME = "DL_ANN_pkg"
+USER_NAME = "VaibhavJoshi95"
+PROJECT_NAME = "DL_ANN-pkg"
+
+setuptools.setup(
+    name=f"{PKG_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="vaibhavgjoshi95@gmail.com",
+    description="A small package for perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy==1.24.3",
+        "pandas==2.0.1",
+        "joblib==1.2.0",
+        "matplotlib==3.7.1"
+    ]
+)

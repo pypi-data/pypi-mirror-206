3f237a563491f90ffc15dc3dd7503826ed3ea200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:36 +0000

    Add package vin-decoder-nhtsa/0.0.1/vin_decoder_nhtsa-0.0.1.tar.gz

---
+++ packages/vin-decoder-nhtsa/vin_decoder_nhtsa-0.0.1.tar.gz/vin_decoder_nhtsa-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='vin_decoder_nhtsa',
+   version='0.1',
+   description='Python module for decoding VIN metadata from NHTSA API',
+   author='Man Foo',
+   author_email='foomail@foo.example',
+   packages=['vin_decoder_nhtsa'],  #same as name
+   packages=find_packages(exclude="tests")
+#    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)

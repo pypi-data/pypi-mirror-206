9a3da22bec6d2119410f207201695953f2d4de66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:40 +0000

    Add package intecomm-form-validators/0.1.17/intecomm-form-validators-0.1.17.tar.gz

---
+++ packages/intecomm-form-validators/intecomm-form-validators-0.1.17.tar.gz/intecomm-form-validators-0.1.17/intecomm_form_validators/subject/medications_form_validator.py
@@ -0,0 +1,6 @@
+from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
+from edc_form_validators import FormValidator
+
+
+class MedicationsFormValidator(CrfFormValidatorMixin, FormValidator):
+    pass

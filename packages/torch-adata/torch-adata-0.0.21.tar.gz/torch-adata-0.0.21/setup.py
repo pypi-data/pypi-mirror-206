729c7bc97fe49c104b44f2424d0353935cf95419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:24 +0000

    Add package torch-adata/0.0.21/torch-adata-0.0.21.tar.gz

---
+++ packages/torch-adata/torch-adata-0.0.21.tar.gz/torch-adata-0.0.21/setup.py
@@ -0,0 +1,44 @@
+
+__module_name__ = "setup.py"
+__doc__ = """PYPI package distribution setup."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+
+
+# -- specify package version: --------------------------------------------------
+__version__ = "0.0.21"
+
+
+# -- import packages: ----------------------------------------------------------
+import setuptools
+import re
+import os
+import sys
+
+
+# -- run setup: ----------------------------------------------------------------
+setuptools.setup(
+    name="torch-adata",
+    version="0.0.21",
+    python_requires=">3.9.0",
+    author="Michael E. Vinyard",
+    author_email="mvinyard@g.harvard.edu",
+    url=None,
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="torch-adata",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "anndata>=0.9.1",
+        "licorice_font>=0.0.3",
+        "lightning>=2.0.1",
+        "torch>=2.0",        
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)

a9a72d2a7214296a8743d038da418e8bba6123fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:32 +0000

    Add package extr-ds/0.0.36/extr-ds-0.0.36.tar.gz

---
+++ packages/extr-ds/extr-ds-0.0.36.tar.gz/extr-ds-0.0.36/src/extr_ds/manager/cmd.py
@@ -0,0 +1,58 @@
+import sys
+
+from .process import workspace, entities, relations
+from .process import split, \
+                     save_all, \
+                     save_entities, \
+                     save_relations
+
+
+def main() -> int:
+    if len(sys.argv) == 1:
+        return -1
+
+    args = sys.argv[1:]
+    method = args[0]
+
+    if method == '--init':
+        workspace.init()
+
+    elif method == '--reset':
+        workspace.clean()
+
+    elif method == '--split':
+        split()
+        entities.annotate()
+        relations.relate()
+
+    elif method == '--annotate':
+        type_of_save = (args[1] if len(args) > 1 else '').lower()
+
+        if type_of_save == 'ents':
+            entities.annotate()
+        elif type_of_save == 'rels':
+            relations.relate()
+        else:
+            entities.annotate()
+            relations.relate()
+
+    elif method == '--relate':
+        relations.relate()
+
+    elif method == '--save':
+        type_of_save = (args[1] if len(args) > 1 else '').lower()
+
+        if type_of_save == 'ents':
+            save_entities()
+        elif type_of_save == 'rels':
+            save_relations()
+        else:
+            save_all()
+
+    elif method == '--reset':
+        workspace.clean()
+
+    else:
+        print(f'"{method}" is not a valid command.')
+
+    return 0

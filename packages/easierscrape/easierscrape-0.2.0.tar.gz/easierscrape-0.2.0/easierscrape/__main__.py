1131d12afbf6969d2d543029ad1d4de8fdc934bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:42 +0000

    Add package easierscrape/0.2.0/easierscrape-0.2.0.tar.gz

---
+++ packages/easierscrape/easierscrape-0.2.0.tar.gz/easierscrape-0.2.0/easierscrape/__main__.py
@@ -0,0 +1,22 @@
+from argparse import ArgumentParser
+from easierscrape import Scraper
+
+
+def main(arg_strings=None):
+    parser = ArgumentParser()
+    parser.add_argument('url', help='the url to scrape')
+    parser.add_argument('depth', type=int, help='the depth of the scrape tree')
+    args = parser.parse_args(arg_strings)
+
+    scraper = Scraper(args.url)
+    scraper.print_tree(int(args.depth))
+    screenshot_success = scraper.get_screenshot()
+    image_count = scraper.parse_images()
+    file_counts = scraper.parse_files(['txt', 'pdf'])
+    table_count = scraper.parse_tables()
+
+    return screenshot_success, image_count, file_counts, table_count
+
+
+if __name__ == "__main__":
+    main()

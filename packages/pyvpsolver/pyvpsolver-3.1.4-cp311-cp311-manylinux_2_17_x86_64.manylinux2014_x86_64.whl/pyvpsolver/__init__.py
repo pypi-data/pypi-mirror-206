17051ee41600e5afb9265a4bb6596897c905e3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:09 +0000

    Add package pyvpsolver/3.1.4/pyvpsolver-3.1.4-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvpsolver/pyvpsolver-3.1.4-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvpsolver/__init__.py
@@ -0,0 +1,9 @@
+"""
+This code is part of the Arc-flow Vector Packing Solver (VPSolver).
+"""
+
+__version__ = "3.1.4"
+
+from .afgraph import AFGraph
+from .vpsolver import VPSolver, VBP, MVP, AFG, MPS, LP
+from . import solvers
